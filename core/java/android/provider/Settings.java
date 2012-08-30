9950c932c41baad4e0268a4c0ed2c931738bd529
Timo Wendt <timo@tjwendt.de>
2012-08-30 12:18:41 +0300

    Runtime toggle of navbar
    This commit is signifigantly modified for DU, however,
    we maintain original authorship for API. Navbar visibility
    has no hardware contraints. Visibility is determined entirely
    from user settings and sane defaults based on factory configuration
    and emulator build prop.
    
    Visibility state and bar view management is handled
    in NavigationController class in SystemUI
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/java/android/view/WindowManagerPolicy.java
    
    Change-Id: I6ac53b8c9f7fce6f9ca6b4ad7bf31a1c1e896863
    
    Conflicts:
    	core/java/android/view/IWindowManager.aidl

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6839,6 +6839,13 @@ public final class Settings {
         public static final String STATUS_BAR_LOCKED_ON_SECURE_KEYGUARD = "status_bar_locked_on_secure_keyguard";
 
         /**
+         * Add or remove software navigation bar
+         *
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_VISIBLE = "navigation_bar_visible";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
--- core/java/android/view/WindowManagerPolicy.java
+++ core/java/android/view/WindowManagerPolicy.java
@@ -1321,6 +1321,11 @@ public interface WindowManagerPolicy {
     public boolean hasNavigationBar();
 
     /**
+     * Device requires a software navigation bar.
+     */
+    public boolean needsNavigationBar();
+
+    /**
      * Lock the device now.
      */
     public void lockNow(Bundle options);
