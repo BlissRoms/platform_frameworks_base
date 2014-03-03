47f51f0125964ad59d367da763cb97c79b0310b1
GwonHyeok <kh4975@naver.com>
2014-03-03 11:43:07 +0000

    FW_Base - XuiMod: Toast Animations [1/2]
    Change-Id: Ic4d1b573b8ed5c657e8e9b226c28944d6a0895ec
    Signed-off-by: STELIX <ssspinni@gmail.com>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4630,6 +4630,13 @@ public final class Settings {
          */
         public static final String INCALL_NOTIFICATIONS_VIBRATE = "incall_notifications_vibrate";
 
+       /**
+         * Toast animations
+         *
+         * @hide
+         */
+        public static final String TOAST_ANIMATION = "toast_animation";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
