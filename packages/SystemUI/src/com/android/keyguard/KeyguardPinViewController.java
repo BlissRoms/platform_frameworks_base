64dd2ff6fcb29d19e09abd61f460d0aac3d17137
Adnan <adnan@cyngn.com>
2014-08-03 17:00:20 -0700

    Keyguard: Add option to scramble pin layout when unlocking (2/2).
    Adapted to Lineage SDK and squashed with the following changes:
    
      From: Adnan Begovic <adnan@cyngn.com>
      Date: Wed, 05 Aug 2015 10:40:41 -0700
    
          Keyguard: Don't disable visibility when scrambling pin.
    
            Otherwise the first numbers text is flipped to invisible
            even though it shouldn't be.
    
          Change-Id: Ia2b80d594e7f11af7a60a85bdb4ea9909d0ac20b
    
      From: Adnan Begovic <adnan@cyngn.com>
      Date: Wed, 05 Aug 2015 16:21:27 -0700
    
          Keyguard: Fix scramblepin logic.
    
          Change-Id: I71e6d26e853fa7bfaeea4b3256902881ac7f74fc
    
      From: LuK1337 <priv.luk@gmail.com>
      Date: Sat Apr 24 18:14:46 2021 +0200
    
          Keyguard: Fix mapping mismatch between KeyEvent and NumPadKey
    
          Change-Id: Ida28a4f037c2f3fa843ac136779e026020d711db
    
      From: Tommy Webb <tommy@calyxinstitute.org>
      Date: 2023-03-20 14:15:05 -0400
    
          Fix issue introduced with 13 QPR2 in which the PIN layout is not
          randomized every time the PIN entry view appears.
    
          Issue: calyxos#1557
          Change-Id: I44247941a02c95cc2ec53fc42ddb1337b61f692e
    
    Original-Change-Id: Ibe8611eb0b8a5bd9275bc96682d2cbafaa37d8f5
    Change-Id: I6716b1c76b69d51bd1457ce07de2274541fca1e8
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- packages/SystemUI/src/com/android/keyguard/KeyguardPinViewController.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardPinViewController.java
@@ -22,6 +22,12 @@ import android.view.View;
 
 import com.android.internal.logging.UiEvent;
 import com.android.internal.logging.UiEventLogger;
+
+import android.os.UserHandle;
+import android.provider.Settings;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+
 import com.android.internal.util.LatencyTracker;
 import com.android.internal.widget.LockPatternUtils;
 import com.android.keyguard.KeyguardSecurityModel.SecurityMode;
@@ -31,6 +37,11 @@ import com.android.systemui.flags.Flags;
 import com.android.systemui.res.R;
 import com.android.systemui.statusbar.policy.DevicePostureController;
 import com.android.systemui.user.domain.interactor.SelectedUserInteractor;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class KeyguardPinViewController
         extends KeyguardPinBasedInputViewController<KeyguardPINView> {
@@ -49,6 +60,11 @@ public class KeyguardPinViewController
     private final UiEventLogger mUiEventLogger;
 
     private boolean mDisabledAutoConfirmation;
+
+    private boolean mScramblePin;
+
+    private List<Integer> mNumbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
+    private final List<Integer> mDefaultNumbers = List.of(mNumbers.toArray(new Integer[0]));
 
     protected KeyguardPinViewController(KeyguardPINView view,
             KeyguardUpdateMonitor keyguardUpdateMonitor,
@@ -94,6 +110,38 @@ public class KeyguardPinViewController
         }
     }
 
+    private void updatePinScrambling() {
+        boolean scramblePin = Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.LOCKSCREEN_PIN_SCRAMBLE_LAYOUT, 0,
+                UserHandle.USER_CURRENT) == 1;
+
+        if (scramblePin || scramblePin != mScramblePin) {
+            mScramblePin = scramblePin;
+            if (scramblePin) {
+                Collections.shuffle(mNumbers);
+            } else {
+                mNumbers = new ArrayList<>(mDefaultNumbers);
+            }
+
+            // get all children who are NumPadKey's
+            ConstraintLayout container = (ConstraintLayout) mView.findViewById(R.id.pin_container);
+
+            List<NumPadKey> views = new ArrayList<NumPadKey>();
+            for (int i = 0; i < container.getChildCount(); i++) {
+                View view = container.getChildAt(i);
+                if (view.getClass() == NumPadKey.class) {
+                    views.add((NumPadKey) view);
+                }
+            }
+
+            // reset the digits in the views
+            for (int i = 0; i < mNumbers.size(); i++) {
+                NumPadKey view = views.get(i);
+                view.setDigit(mNumbers.get(i));
+            }
+        }
+    }
+
     protected void onUserInput() {
         super.onUserInput();
         if (isAutoPinConfirmEnabledInSettings()) {
@@ -114,6 +162,7 @@ public class KeyguardPinViewController
 
     @Override
     public void startAppearAnimation() {
+        updatePinScrambling();
         super.startAppearAnimation();
     }
 
