06d2f3f407a4af0105080fce55140e7f8b0bbd1b
kaiyiz <kaiyiz@codeaurora.org>
2014-08-14 12:33:54 +0800

    Handle IllegalArgumentException
    In some cases startInputInnerLocked() throws
    IllegalArgumentException which needs to be handle
    
    Change-Id: I315cb46b1d16c2862989cb9c0c10ed6613884522
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- services/core/java/com/android/server/InputMethodManagerService.java
+++ services/core/java/com/android/server/InputMethodManagerService.java
@@ -1444,7 +1444,12 @@ public class InputMethodManagerService extends IInputMethodManager.Stub
             }
         }
 
-        return startInputInnerLocked();
+        try {
+            return startInputInnerLocked();
+        } catch (RuntimeException e) {
+            Slog.w(TAG, "Unexpected exception", e);
+            return null;
+        }
     }
 
     InputBindResult startInputInnerLocked() {
