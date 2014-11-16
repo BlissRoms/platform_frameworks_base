ed8c0408c389f0d378655151b05d3700781f1317
Mazda <mazdarider23@gmail.com>
2014-11-15 19:27:05 -0500

    android.view: Speed things up!!
    Change-Id: If53cba16d5385532fa53d77a829866c082808926
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>
    Signed-off-by: Simão Gomes Viana <devel@superboring.dev>
    Signed-off-by: Simão Gomes Viana <simao@halogenos.org>
    Signed-off-by: Simao Gomes Viana <simao@halogenos.org>

--- core/java/android/view/ViewConfiguration.java
+++ core/java/android/view/ViewConfiguration.java
@@ -80,7 +80,7 @@ public class ViewConfiguration {
      * a long press
      * @hide
      */
-    public static final int DEFAULT_LONG_PRESS_TIMEOUT = 400;
+    public static final int DEFAULT_LONG_PRESS_TIMEOUT = 300;
 
     /**
      * Defines the default duration in milliseconds between the first tap's up event and the second
@@ -105,7 +105,7 @@ public class ViewConfiguration {
      * appropriate button to bring up the global actions dialog (power off,
      * lock screen, etc).
      */
-    private static final int GLOBAL_ACTIONS_KEY_TIMEOUT = 500;
+    private static final int GLOBAL_ACTIONS_KEY_TIMEOUT = 250;
 
     /**
      * Defines the duration in milliseconds a user needs to hold down the
@@ -137,7 +137,7 @@ public class ViewConfiguration {
      * is a jump tap. If the user does not complete the jump tap within this interval, it is
      * considered to be a tap.
      */
-    private static final int JUMP_TAP_TIMEOUT = 500;
+    private static final int JUMP_TAP_TIMEOUT = 250;
 
     /**
      * Defines the duration in milliseconds between the first tap's up event and
@@ -171,12 +171,12 @@ public class ViewConfiguration {
      * Defines the duration in milliseconds we want to display zoom controls in response
      * to a user panning within an application.
      */
-    private static final int ZOOM_CONTROLS_TIMEOUT = 3000;
+    private static final int ZOOM_CONTROLS_TIMEOUT = 1500;
 
     /**
      * Inset in dips to look for touchable content when the user touches the edge of the screen
      */
-    private static final int EDGE_SLOP = 12;
+    private static final int EDGE_SLOP = 6;
 
     /**
      * Distance a touch can wander before we think the user is scrolling in dips.
@@ -243,7 +243,7 @@ public class ViewConfiguration {
     /**
      * Maximum velocity to initiate a fling, as measured in dips per second
      */
-    private static final int MAXIMUM_FLING_VELOCITY = 8000;
+    private static final int MAXIMUM_FLING_VELOCITY = 16000;
 
     /** Value used as a minimum fling velocity, when fling is not supported. */
     private static final int NO_FLING_MIN_VELOCITY = Integer.MAX_VALUE;
