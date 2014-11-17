c43c9612e1b5952065afa8baad991621df4b70bf
Kyrylo Mikos <kiril.mik.os@gmail.com>
2014-11-17 13:56:05 +0200

    Disable panic mechanism when PolicyControl activated.
    Change-Id: I5cf5f681237bbd17ec0fe2d041dd782cfb4b72f0

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1039,7 +1039,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         // taken over the whole screen.
         boolean panic = mImmersiveModeConfirmation.onPowerKeyDown(interactive,
                 SystemClock.elapsedRealtime(), isImmersiveMode(mLastSystemUiFlags));
-        if (panic) {
+        if (panic && !PolicyControl.isImmersiveFiltersActive()) {
             mHandler.post(mHiddenNavPanic);
         }
 
