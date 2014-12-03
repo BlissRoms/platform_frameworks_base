2c9d6646b2d2afea44ac4a1ff88de12a8dc22fcd
Lars Greiss <kufikugel@googlemail.com>
2014-12-03 19:56:41 +0100

    base: Smart Pulldown [1/2]
    @rohanpurohit: adapt to tuner
    Rewritten by @Beanstown106 for 6.0/7.0
    
    previously the detection for ongoing/persistent notifications were not working
    im not sure how long this has actually been broken but i cleaned things up a little bit
    also put to use the isOngoing boolean and properly put to use the hasActiveVisibleNotifications boolean
    
    current options now are off, no dismissable, no ongoing, and no notifications
    
    Credits: kufikugel, HardCorePawn, acardinal
    @mydongistiny: fixed for oreo
    Change-Id: I627295eaa3b1f73503c0952d72f879e4ebe6e407
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6465,6 +6465,12 @@ public final class Settings {
         public static final String QS_LABEL_USE_NEW_TINT = "qs_label_use_new_tint";
 
         /**
+         * Quick Settings Smart Pulldown
+         * @hide
+         */
+        public static final String QS_SMART_PULLDOWN = "qs_smart_pulldown";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
