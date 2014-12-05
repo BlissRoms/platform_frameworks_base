f4212b546a10c0206cfd7a28da4e162bab0ee7c5
Steve Kondik <steve@cyngn.com>
2014-12-05 08:31:29 -0800

    SystemUI: Forward-port notification counters [1/2]
    Change-Id: Ia8609c56a523906404a9155cea6987f40c10f30d
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5972,6 +5972,12 @@ public final class Settings {
         public static final String LOCKSCREEN_BATTERY_INFO = "lockscreen_battery_info";
 
         /**
+         * Show the pending notification counts as overlays on the status bar
+         * @hide
+         */
+        public static final String STATUS_BAR_NOTIF_COUNT = "status_bar_notif_count";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
