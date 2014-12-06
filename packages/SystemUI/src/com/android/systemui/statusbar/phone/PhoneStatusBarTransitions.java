31588a0e35f215a21f2a4a687fe8084f83edb01f
Jon Haus <haus.xda@gmail.com>
2014-12-06 17:19:23 +0100

    [1/2] base: Network Traffic
    Change-Id: I3535dbb7076881f797d41f1ed74b1ded77a52a74
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
    
    Change-Id: I3535dbb7076881f797d41f1ed74b1ded77a52a74

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
@@ -32,7 +32,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
     private final PhoneStatusBarView mView;
     private final float mIconAlphaWhenOpaque;
 
-    private View mLeftSide, mStatusIcons, mSignalCluster, mBattery, mClock, mBlissLogo, mWeatherTextView, mLeftWeatherTextView;
+    private View mLeftSide, mStatusIcons, mSignalCluster, mBattery, mClock, mBlissLogo, mWeatherTextView, mLeftWeatherTextView, mNetworkTraffic;
 
     private Animator mCurrentAnimation;
 
@@ -55,6 +55,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
         mWeatherTextView = mView.findViewById(R.id.weather_temp);
         mLeftWeatherTextView = mView.findViewById(R.id.left_weather_temp);
 		mBlissLogo = mView.findViewById(R.id.bliss_logo);
+        mNetworkTraffic = mView.findViewById(R.id.networkTraffic);
         applyModeBackground(-1, getMode(), false /*animate*/);
         applyMode(getMode(), false /*animate*/);
     }
@@ -102,7 +103,8 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
                     animateTransitionTo(mLeftWeatherTextView, newAlpha),
                     animateTransitionTo(mBattery, newAlphaBC),
                     animateTransitionTo(mClock, newAlphaBC),
-                    animateTransitionTo(mBlissLogo, newAlphaBC)
+                    animateTransitionTo(mBlissLogo, newAlphaBC),
+		    animateTransitionTo(mNetworkTraffic, newAlpha)
                     );
             if (isLightsOut(mode)) {
                 anims.setDuration(LIGHTS_OUT_DURATION);
@@ -118,6 +120,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
             mBattery.setAlpha(newAlphaBC);
             mClock.setAlpha(newAlphaBC);
             mBlissLogo.setAlpha(newAlphaBC);
+            mNetworkTraffic.setAlpha(newAlpha);
         }
     }
 }
