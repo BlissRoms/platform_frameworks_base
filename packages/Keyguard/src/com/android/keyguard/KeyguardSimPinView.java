9871dd7d459b0675cb08a6f8b8cdf1fad075b7aa
Matt Mower <mowerm@gmail.com>
2014-12-31 15:02:58 -0600

    Keyguard: Forward port lockscreen quick unlock (1/2)
    NOTICE: For the love of Android, please don't use this commit in any
    builds shared with the community. It almost certainly mishandles
    multiple users and bypasses important security mechanisms.
    
    * PasswordTextView QuickUnlock listener functions and method thanks to
      ParanoidAndroid project:
        Keyguard: Implement quick unlock feature [1/2]
        BigBrother1984 <carlosavignano@aospa.co>
        I0b3d92b91f72020d327a7f2f184c2fbc5519a5df
    
    Change-Id: Iaa140723ffb037062d565127257a7cb60526f25e
    
    Disable PIN entry after successful quick unlock
    
    Small polish: I still often press the enter button out of muscle memory
    when entering my PIN code, which leads to an "invalid PIN" message being
    shown for a tiny moment as the screen unlocks. Disable the entry view
    after a quick unlock to fix this.
    
    Change-Id: Icceb8311ed55564e9b1d92934cbca814a1cc4b29
    
    Keyguard: fix quick unlock for Android N.
    
      - Passes correct arguments to reportUnlockAttempt and resetPasswordText
    
      - Remove throwIfCalledOnMainThread.  We probably "shouldn't" do this but
        we probably "shouldn't" use quick unlock in the first place. :P
    
    Change-Id: I0fc3232b3001a9c874bbbce105e999cb9fbdc065
    
    Conflicts:
    	core/java/com/android/internal/widget/LockPatternUtils.java

--- packages/Keyguard/src/com/android/keyguard/KeyguardSimPinView.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardSimPinView.java
@@ -146,6 +146,7 @@ public class KeyguardSimPinView extends KeyguardPinBasedInputView {
             ((EmergencyCarrierArea) mEcaView).setCarrierTextVisible(true);
         }
         mSimImageView = (ImageView) findViewById(R.id.keyguard_sim);
+        mPasswordEntry.setQuickUnlockListener(null);
     }
 
     @Override
