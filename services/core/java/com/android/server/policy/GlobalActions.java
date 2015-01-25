4cee97f606da9577a042f47ef206180f2a0615b7
Andrew Boren <andrew.boren@gmail.com>
2015-01-25 09:19:16 +0100

    Powermenu screenshot delay [1/2]
     * Only works from power menu
     * Defaults to 1 second (1000ms)
    
    Change-Id: Ic7477d4ed962b97d5bbfd980cfdb7e3516262463
    Signed-off-by: Varun <date.varun123@gmail.com>

--- services/core/java/com/android/server/policy/GlobalActions.java
+++ services/core/java/com/android/server/policy/GlobalActions.java
@@ -140,6 +140,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
     // Power menu customizations
     String mActions;
+    private int mScreenshotDelay;	
 
     /**
      * @param context everything needs a context :(
@@ -215,6 +216,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
     private void handleShow() {
         awakenIfNecessary();
+	checkSettings();
         prepareDialog();
 
         // If we only have 1 item and it's a simple press action, just do this action.
@@ -673,7 +675,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
                         /* wait for the dialog box to close */
                         try {
-                            Thread.sleep(1000);
+                             Thread.sleep(mScreenshotDelay * 1000);
                         } catch (InterruptedException ie) {
                             // Do nothing
                         }
@@ -707,6 +709,11 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
             mContext.registerReceiver(mRingerModeReceiver, filter);
         }
     }
+   private void checkSettings() {
+        mScreenshotDelay = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.SCREENSHOT_DELAY, 1);
+    }
+
 
     private void refreshSilentMode() {
         if (!mHasVibrator) {
