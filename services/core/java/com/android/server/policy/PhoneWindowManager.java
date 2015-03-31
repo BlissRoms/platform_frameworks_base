6444f3eb247148d15687e497fcebbaa751dab6a3
Steve Ballmer <steveb@microsoft.com>
2015-03-31 14:46:45 -0700

    BootMessage: Show Bliss logo while optimizing apps
    Change-Id: I5927a153d10b5c20b5fd7c2f94ebc59358a80719

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -7042,6 +7042,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                         mBootMsgDialog.setTitle(R.string.android_start_title);
                     }
                     mBootMsgDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                    mBootMsgDialog.setIcon(com.android.internal.R.drawable.boot_logo);
                     mBootMsgDialog.setIndeterminate(true);
                     mBootMsgDialog.getWindow().setType(
                             WindowManager.LayoutParams.TYPE_BOOT_PROGRESS);
