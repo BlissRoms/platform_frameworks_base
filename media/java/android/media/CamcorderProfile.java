1e97314f8e0e42d9526b23ffbe210a351f7d1328
Sultanxda <sultanxda@gmail.com>
2015-08-15 19:28:35 -0700

    CamcorderProfile: Add 1440p camcorder profile
    Add encoding constants (regular and timelapse) for 2.5k 1440p resolution.
    
    Change-Id: I0ce1dd33458f0c837f4bb1d9e8d07c1649cf338d

--- media/java/android/media/CamcorderProfile.java
+++ media/java/android/media/CamcorderProfile.java
@@ -95,9 +95,14 @@ public class CamcorderProfile
      */
     public static final int QUALITY_2160P = 8;
 
+    /** @hide
+     * Quality level corresponding to 1440p resolution
+     */
+    public static final int QUALITY_1440P = 9;
+
     // Start and end of quality list
     private static final int QUALITY_LIST_START = QUALITY_LOW;
-    private static final int QUALITY_LIST_END = QUALITY_2160P;
+    private static final int QUALITY_LIST_END = QUALITY_1440P;
 
     /**
      * Time lapse quality level corresponding to the lowest available resolution.
@@ -144,9 +149,14 @@ public class CamcorderProfile
      */
     public static final int QUALITY_TIME_LAPSE_2160P = 1008;
 
+    /** @hide
+     * Time lapse quality level corresponding to the 1440p resolution.
+     */
+    public static final int QUALITY_TIME_LAPSE_1440P = 1009;
+
     // Start and end of timelapse quality list
     private static final int QUALITY_TIME_LAPSE_LIST_START = QUALITY_TIME_LAPSE_LOW;
-    private static final int QUALITY_TIME_LAPSE_LIST_END = QUALITY_TIME_LAPSE_2160P;
+    private static final int QUALITY_TIME_LAPSE_LIST_END = QUALITY_TIME_LAPSE_1440P;
 
     /**
      * High speed ( >= 100fps) quality level corresponding to the lowest available resolution.
