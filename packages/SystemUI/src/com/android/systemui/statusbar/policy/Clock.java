b53d3c7f4199efd9a2d626db4634193111d0507e
Janson Kang <temasek71@gmail.com>
2015-09-03 10:05:40 +0800

    FWB: Statusbar clock font size (1/2)
    Signed-off-by: Janson Kang <temasek71@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
    
    Change-Id: I4899181164f92eac16f39868cbbe39dc81f0c890

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -95,6 +95,7 @@ public class Clock extends TextView implements DemoMode {
     protected int mClockDateDisplay = CLOCK_DATE_DISPLAY_GONE;
     protected int mClockDateStyle = CLOCK_DATE_STYLE_REGULAR;
     private int mClockFontStyle = FONT_NORMAL;
+    private int mClockFontSize = 14;
 
     private SettingsObserver mSettingsObserver;
 
@@ -119,6 +120,9 @@ public class Clock extends TextView implements DemoMode {
                     this, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System
                     .getUriFor(Settings.System.STATUSBAR_CLOCK_FONT_STYLE), false,
+                    this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System
+                   .getUriFor(Settings.System.STATUSBAR_CLOCK_FONT_SIZE), false,
                     this, UserHandle.USER_ALL);
             updateSettings();
         }
@@ -344,6 +348,9 @@ public class Clock extends TextView implements DemoMode {
         mClockFontStyle = Settings.System.getIntForUser(resolver,
                 Settings.System.STATUSBAR_CLOCK_FONT_STYLE, FONT_NORMAL,
                 UserHandle.USER_CURRENT);
+        mClockFontSize = Settings.System.getIntForUser(resolver,
+                Settings.System.STATUSBAR_CLOCK_FONT_SIZE, 14,
+                UserHandle.USER_CURRENT);
 
         int defaultColor = mContext.getResources().getColor(R.color.status_bar_clock_color);
         int clockColor = Settings.System.getIntForUser(resolver,
@@ -374,6 +381,7 @@ public class Clock extends TextView implements DemoMode {
 
         getFontStyle(mClockFontStyle);
         setTextColor(clockColor);
+        setTextSize(mClockFontSize);
         updateClock();
     }
 
