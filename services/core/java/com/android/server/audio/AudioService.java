e5fdf0b7b1b9fe2dac0c3b62f9a1c1e7c15590fe
Pawit Pornkitprasan <p.pawit@gmail.com>
2015-11-02 00:02:36 -0500

    base: Option to use volume keys to control ring volume anytime (1/2)
    AICP Note: this used to be called
    Option to use volume keys to control media volume anytime
    
    With changes in P making the media volume the default stream, it makes
    more sense to add ring tone volume as the option instead.
    
    Squashed with:
    Fix forcing media stream control on tablet
    There's option volume_keys_control_ring_stream (Volume keys control ringtone
    volume / If on, volume keys control ringtone volume. If off, volume keys
    control media volume.) in Settings which is also available on tablets but it
    doesn't work on these devices (I didn't find reported issue).
    As I like this option and think it may be even more wanted on media devices
    such as tablets, here's a fix.
    
    PureNexus edits:
    *modified to force media stream when toggled on, ring/notification stream when off
    
    ABC edits: use existing content resolver variable
    use one void for the setting getter
    enabled by default
    
    AICP edits:
     * disabled by default
     * brought up to P by eyosen
    
    Change-Id: I8c0c02a9205c1accdb5404183b7fda9a0eab749b

--- services/core/java/com/android/server/audio/AudioService.java
+++ services/core/java/com/android/server/audio/AudioService.java
@@ -569,6 +569,7 @@ public class AudioService extends IAudioService.Stub
     private AudioManagerInternal.RingerModeDelegate mRingerModeDelegate;
     private VolumePolicy mVolumePolicy = VolumePolicy.DEFAULT;
     private long mLoweredFromNormalToVibrateTime;
+    private boolean mVolumeKeysControlRingTone;
 
     // Array of Uids of valid accessibility services to check if caller is one of them
     private int[] mAccessibilityServiceUids;
@@ -1624,6 +1625,8 @@ public class AudioService extends IAudioService.Stub
             sendEnabledSurroundFormats(cr, true);
             updateAssistantUId(true);
             updateRttEanbled(cr);
+
+            setVolumeKeysControlRingTone();
         }
 
         mLinkNotificationWithVolume = Settings.Secure.getInt(cr,
@@ -4311,11 +4314,17 @@ public class AudioService extends IAudioService.Stub
                         Log.v(TAG, "getActiveStreamType: Forcing STREAM_NOTIFICATION stream active");
                     return AudioSystem.STREAM_NOTIFICATION;
                 } else {
-                    if (DEBUG_VOL) {
-                        Log.v(TAG, "getActiveStreamType: Forcing DEFAULT_VOL_STREAM_NO_PLAYBACK("
-                                + DEFAULT_VOL_STREAM_NO_PLAYBACK + ") b/c default");
+                    if (mVolumeKeysControlRingTone) {
+                        if (DEBUG_VOL)
+                            Log.v(TAG, "getActiveStreamType: Forcing STREAM_RING b/c user selected");
+                        return AudioSystem.STREAM_RING;
+                    } else {
+                        if (DEBUG_VOL) {
+                            Log.v(TAG, "getActiveStreamType: Forcing DEFAULT_VOL_STREAM_NO_PLAYBACK("
+                                    + DEFAULT_VOL_STREAM_NO_PLAYBACK + ") b/c default");
+                        }
+                        return DEFAULT_VOL_STREAM_NO_PLAYBACK;
                     }
-                    return DEFAULT_VOL_STREAM_NO_PLAYBACK;
                 }
             } else if (
                     wasStreamActiveRecently(AudioSystem.STREAM_NOTIFICATION, sStreamOverrideDelayMs)) {
@@ -4355,11 +4364,16 @@ public class AudioService extends IAudioService.Stub
                     if (DEBUG_VOL) Log.v(TAG, "getActiveStreamType: Forcing STREAM_RING");
                     return AudioSystem.STREAM_RING;
                 } else {
-                    if (DEBUG_VOL) {
-                        Log.v(TAG, "getActiveStreamType: Forcing DEFAULT_VOL_STREAM_NO_PLAYBACK("
-                                + DEFAULT_VOL_STREAM_NO_PLAYBACK + ") b/c default");
+                    if (mVolumeKeysControlRingTone) {
+                        if (DEBUG_VOL) Log.v(TAG, "getActiveStreamType: Forcing STREAM_RING b/c user selected");
+                        return AudioSystem.STREAM_RING;
+                    } else {
+                        if (DEBUG_VOL) {
+                            Log.v(TAG, "getActiveStreamType: Forcing DEFAULT_VOL_STREAM_NO_PLAYBACK("
+                                    + DEFAULT_VOL_STREAM_NO_PLAYBACK + ") b/c default");
+                        }
+                        return DEFAULT_VOL_STREAM_NO_PLAYBACK;
                     }
-                    return DEFAULT_VOL_STREAM_NO_PLAYBACK;
                 }
             }
             break;
@@ -5680,6 +5694,8 @@ public class AudioService extends IAudioService.Stub
                     Settings.Secure.VOICE_INTERACTION_SERVICE), false, this);
             mContentResolver.registerContentObserver(Settings.Secure.getUriFor(
                     Settings.Secure.RTT_CALLING_MODE), false, this);
+            mContentResolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.VOLUME_KEYS_CONTROL_RING_TONE), false, this);
         }
 
         @Override
@@ -5702,6 +5718,8 @@ public class AudioService extends IAudioService.Stub
                 updateMasterBalance(mContentResolver);
                 updateEncodedSurroundOutput();
                 sendEnabledSurroundFormats(mContentResolver, mSurroundModeChanged);
+
+                setVolumeKeysControlRingTone();
                 updateAssistantUId(false);
                 updateRttEanbled(mContentResolver);
 
@@ -5730,6 +5748,12 @@ public class AudioService extends IAudioService.Stub
                 mSurroundModeChanged = false;
             }
         }
+    }
+
+    private void setVolumeKeysControlRingTone() {
+        mVolumeKeysControlRingTone = Settings.System.getIntForUser(mContentResolver,
+                Settings.System.VOLUME_KEYS_CONTROL_RING_TONE, 0,
+                UserHandle.USER_CURRENT) == 1;
     }
 
     public void avrcpSupportsAbsoluteVolume(String address, boolean support) {
