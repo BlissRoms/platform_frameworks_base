112b0dae635c349e73cd635321b6946f454659dc
Roman Birg <roman@cyngn.com>
2015-11-11 15:01:41 -0500

    Add Quick Settings Pulldown options
    PureNexus Edits:
    *add always quick pulldown option
    *fix for double finger pulldown
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3667,6 +3667,12 @@ public final class Settings {
          */
         public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
 
+        /** 
+         * Whether to allow one finger quick settings expansion on the right side of the statusbar.
+         * @hide
+         */
+        public static final String STATUS_BAR_QUICK_QS_PULLDOWN = "status_bar_quick_qs_pulldown";
+
         /**
          * Forces formal text input.  1 to replace emoticon key with enter key.
          * @hide
