78844d7b1c15c06742e8a20231d0851bc8f556a9
LorDClockaN <davor@losinj.com>
2015-11-14 21:19:17 +0100

    Toast icon switch [1/2]
    Change-Id: I50b40021235848e7036199fec4eba643c8be4140

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3610,6 +3610,13 @@ public final class Settings {
         public static final String STATUSBAR_CLOCK_COLOR = "statusbar_clock_color";
 
         /**
+         * Toast icon
+         *
+         * @hide
+         */
+        public static final String TOAST_ICON = "toast_icon";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
--- core/java/android/widget/Toast.java
+++ core/java/android/widget/Toast.java
@@ -30,6 +30,7 @@ import android.graphics.drawable.Drawable;
 import android.os.Handler;
 import android.os.RemoteException;
 import android.os.ServiceManager;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -405,18 +406,9 @@ public class Toast {
                 }
 
                 ImageView appIcon = (ImageView) mView.findViewById(android.R.id.icon);
-                if (appIcon != null) {
-                    ActivityManager am =
-                            (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
-                    if (!am.isPackageInForeground(packageName)) {
-                        PackageManager pm = context.getPackageManager();
-                        Drawable icon = null;
-                        try {
-                            icon = pm.getApplicationIcon(packageName);
-                        } catch (PackageManager.NameNotFoundException e) {
-                            // nothing to do
-                        }
-                        appIcon.setImageDrawable(icon);
+                if ((Settings.System.getInt(context.getContentResolver(),
+                        Settings.System.TOAST_ICON, 1) == 1)) {
+                    if (appIcon != null) {
                     }
                 }
                 mWM = (WindowManager)context.getSystemService(Context.WINDOW_SERVICE);
