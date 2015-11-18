8d83416175aa81025d0d015010e05b3240d2d5a8
Jmz <jmztaylor@gmail.com>
2015-11-18 16:28:03 +0100

    Allow disabling of FC dialogs [1/2]
    Change-Id: I18f81e3d3f0873c7742809d1a8c9d217c87e9531

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3505,6 +3505,12 @@ public final class Settings {
         public static final String HOLD_BACK_TO_KILL_TIMEOUT = "hold_back_to_kill_timeout";
 
         /**
+         * Disable FC Notifications
+         * @hide
+         */
+        public static final String DISABLE_FC_NOTIFICATIONS = "disable_fc_notifications";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
