765ea8cd64166d0c6f4cb21c3f451abf455e54cb
xplodwild <me@xplod.fr>
2015-11-22 06:35:53 +0100

    Time Contextual Headers [1/2]
    This commit adds image headers to the notification panel header,
    based on the date/time.
    
    There is a default provider that provides sky pictures
    as well as special events like Christmas and new
    year's eve.
    
    More will be coming, with eventually a way to use an external
    (as in, Internet) provider for those pictures.
    
    Change-Id: I946e8f4b1d4b4a40083042642b38121c93674f25
    Signed-off-by: scott <scott@ubuntu>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3469,6 +3469,18 @@ public final class Settings {
                 "volume_keys_control_ring_stream";
 
         /**
+         * Whether to use the custom status bar header or not
+         * @hide
+         */
+        public static final String STATUS_BAR_CUSTOM_HEADER = "status_bar_custom_header";
+
+        /**
+         * Current active provider
+         * @hide
+         */
+        public static final String STATUS_BAR_CUSTOM_HEADER_PROVIDER = "status_bar_custom_header_provider";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
