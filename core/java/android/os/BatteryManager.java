c482820fbab00690632e6c81587b07cda29dd630
Jorge Ruesga <jorge@ruesga.com>
2015-11-24 23:31:56 +0800

    frameworks: Reset battery stats [1/2]
    BenzoEdit: Added permission to priv-app whitelist for Settings
    
    Change-Id: I1a1c0831d55c2da01c02da29e78a2c5da441b9a6

--- core/java/android/os/BatteryManager.java
+++ core/java/android/os/BatteryManager.java
@@ -422,4 +422,12 @@ public class BatteryManager {
             throw e.rethrowFromSystemServer();
         }
     }
+
+    public void resetStatistics() {
+        try {
+            mBatteryStats.resetStatistics();
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
 }
