b845c4ab31f66fb6b3e09424e66aea09127d5288
Randall Rushing <randall.rushing@gmail.com>
2015-12-22 00:59:13 -0800

    Navbar: fix navbar transitions which was causing bizarre statusbar bleed
    Something happened along the way with navbar transitions resulting in
    vertical bar root view transition drawables not being applied correctly.
    This caused expanded statusbar views to visibly draw under the
    navigation bar when the bar was vertical.
    
    Change-Id: I05b68cdd16336e59e1011da7eba7c84468dac938

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -570,7 +570,7 @@ public class NavigationBarView extends LinearLayout {
         mDeadZone.setDisplayRotation(mCurrentRotation);
 
         // force the low profile & disabled states into compliance
-        mBarTransitions.init();
+        mBarTransitions.init(mVertical);
         setDisabledFlags(mDisabledFlags, true /* force */);
         setMenuVisibility(mShowMenu, true /* force */);
 
