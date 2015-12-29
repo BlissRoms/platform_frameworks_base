94aeea59d1567d5449e716f0f4dca2bd7fa7913b
neighbors28 <sjp0728@gmail.com>
2015-12-29 22:02:32 +0100

    Frameworks/base: Second Clock (1/2)
    Dokdo-Project/platform_frameworks_base@6ab5d62#diff-1fa71c270d063a48f921f0760d10c7f3R122
    
    Change-Id: I9f2e925ac39eb7215891cd0b173b87c53d54d620

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3675,6 +3675,12 @@ public final class Settings {
         public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
 
         /**
+         * Display second in the Clock
+         * @hide
+         */
+        public static final String CLOCK_USE_SECOND = "clock_use_second";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
