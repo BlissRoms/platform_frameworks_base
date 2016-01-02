d7d56c1026fc703fd8b05627ecb084a9ae647855
Dark Eyes <dark_eyes_@hotmail.com>
2016-01-02 17:24:45 -0500

    Blissify a few things
    Change-Id: Ifc79bf2615d1406bf7448443d0b7be02f056f50c

--- core/java/com/android/internal/util/bliss/AwesomeAnimationHelper.java
+++ core/java/com/android/internal/util/bliss/AwesomeAnimationHelper.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.internal.util.benzo;
+package com.android.internal.util.bliss;
 
 import android.content.Context;
 import android.content.res.Resources;
