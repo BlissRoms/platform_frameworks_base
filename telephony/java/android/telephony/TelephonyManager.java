e333e706cdd89f5b6f29b2cea45bd464b4f89035
Huaibin Yang <huaibiny@codeaurora.org>
2016-02-02 17:06:09 -0800

    TelephonyManager uses default voice sub instead of Default data sub.
    Fix is to use getDefaultDataSubId to get default data network type.
    Currently using getDefaultSubscription() may return voice network subId.
    
    Make mobile_data and roaming options subcription specific and independent.
    
    Fix is to provide database flags per subscription for both mobile_data
    and roaming options.
    
    CRs-Fixed: 963773
    Change-Id: I1d40bc5c84b4428d7066b28003e5b2c5d852b6f7

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -1823,7 +1823,7 @@ public class TelephonyManager {
      */
     @RequiresPermission(android.Manifest.permission.READ_PHONE_STATE)
     public int getDataNetworkType() {
-        return getDataNetworkType(getSubId(SubscriptionManager.getDefaultDataSubscriptionId()));
+        return getDataNetworkType(getDefaultDataSubscriptionId());
     }
 
     /**
@@ -4277,6 +4277,13 @@ public class TelephonyManager {
     }
 
     /**
+     * Returns Default Data subscription.
+     */
+    private static int getDefaultDataSubscriptionId() {
+        return SubscriptionManager.getDefaultDataSubscriptionId();
+    }
+
+    /**
      * Return an appropriate slot index for any situation.
      *
      * if this object has been created with {@link #createForSubscriptionId}, then the slot index
@@ -5612,7 +5619,7 @@ public class TelephonyManager {
      */
     @RequiresPermission(android.Manifest.permission.MODIFY_PHONE_STATE)
     public void setDataEnabled(boolean enable) {
-        setDataEnabled(getSubId(SubscriptionManager.getDefaultDataSubscriptionId()), enable);
+        setDataEnabled(getDefaultDataSubscriptionId(), enable);
     }
 
     /** @hide */
@@ -5661,7 +5668,7 @@ public class TelephonyManager {
      */
     @SuppressWarnings("deprecation")
     public boolean isDataEnabled() {
-        return getDataEnabled(getSubId(SubscriptionManager.getDefaultDataSubscriptionId()));
+        return getDataEnabled(getDefaultDataSubscriptionId());
     }
 
     /**
