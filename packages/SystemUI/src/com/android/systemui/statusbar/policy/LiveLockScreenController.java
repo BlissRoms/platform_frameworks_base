dd1c316470eee51bf3abdd0b492696300facaa45
d34d <clark@cyngn.com>
2016-04-07 13:14:39 -0700

    SysUI: Deprecate collapseNotificationPanel [2/2]
    Change-Id: Ie3cad5537fad278f941e233ec43e5fab8059d0e6
    TICKET: CYNGNOS-2392

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/LiveLockScreenController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/LiveLockScreenController.java
@@ -170,19 +170,6 @@ public class LiveLockScreenController {
         }
 
         @Override
-        public void collapseNotificationPanel() {
-            if (mStatusBarState == StatusBarState.KEYGUARD && isShowingLiveLockScreenView() &&
-                    mLiveLockScreenView.isInteractive()) {
-                mHandler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        mBar.focusKeyguardExternalView();
-                    }
-                });
-            }
-        }
-
-        @Override
         public void providerDied() {
             mLiveLockScreenView.unregisterKeyguardExternalViewCallback(
                     mExternalKeyguardViewCallbacks);
