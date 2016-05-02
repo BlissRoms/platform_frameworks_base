4c5f62e8134400a4e2726bb25f4b75790d061978
SpiritCroc <spiritcroc@gmail.com>
2016-05-02 11:47:57 +0200

    Battery bar enhancements [1/2]
    Add color settings for charging & low battery
    Add reversed alignment setting

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6182,6 +6182,16 @@ public final class Settings {
         public static final String BATTERY_BAR_ANIMATE = "battery_bar_animate";
 
         /**
+         * @hide
+         */
+        public static final String BATTERY_BAR_CHARGING_COLOR = "battery_bar_charging_color";
+
+        /**
+         * @hide
+         */
+        public static final String BATTERY_BAR_BATTERY_LOW_COLOR = "battery_bar_battery_low_color";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
