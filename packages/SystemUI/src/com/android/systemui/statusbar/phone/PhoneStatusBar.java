6f2858b51a456e618e73702c66c305bf451fe70e
d34d <clark@cyngn.com>
2016-05-16 10:24:39 -0700

    LLS: Ensure LLS loses focus when showing bouncer
    Let showBouncer take care of removing focus from the LLS instead of
    relying on the callers to do so.
    
    Change-Id: Iaec9ad74b3498ea385bdecbfc5173a9d482b043e
    TICKET: FEIJ-531

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -5511,6 +5511,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
     public void showBouncer() {
         if (!mRecreating && mNotificationPanel.mCanDismissKeyguard) {
+            // ensure external keyguard view does not have focus
+            unfocusKeyguardExternalView();
             mWaitingForKeyguardExit = mStatusBarKeyguardViewManager.isShowing();
             mStatusBarKeyguardViewManager.dismiss();
         }
