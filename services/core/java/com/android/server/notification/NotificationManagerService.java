8aded068b3b5e8d5b928e8811d166bca93fb2e54
Jessica Wagantall <jwagantall@cyngn.com>
2016-09-07 12:40:24 -0700

    Merge tag 'android-6.0.1_r66' into HEAD
    Android 6.0.1 release 66
    
    # gpg: Signature made Tue 06 Sep 2016 09:26:45 AM PDT using DSA key ID 9AB10E78
    # gpg: Can't check signature: public key not found
    
    Change-Id: I28f60db40eed60617467fb33fb3d26d79031bf5e
    Reviewed-on: http://review.blissroms.com/4131
    Reviewed-by: blueyes <brian.w.petroski@gmail.com>
    Tested-by: blueyes <brian.w.petroski@gmail.com>

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -2004,6 +2004,7 @@ public class NotificationManagerService extends SystemService {
         }
 
         private void enforcePolicyAccess(String pkg, String method) {
+            checkCallerIsSameApp(pkg);
             if (!checkPolicyAccess(pkg)) {
                 Slog.w(TAG, "Notification policy access denied calling " + method);
                 throw new SecurityException("Notification policy access denied");
@@ -3664,6 +3665,10 @@ public class NotificationManagerService extends SystemService {
         if (isCallerSystem()) {
             return;
         }
+        checkCallerIsSameApp(pkg);
+    }
+
+    private static void checkCallerIsSameApp(String pkg) {
         final int uid = Binder.getCallingUid();
         try {
             ApplicationInfo ai = AppGlobals.getPackageManager().getApplicationInfo(
