8b175a751cb1e799a7d6a9b8eb68e7deb3b956e8
Alex <mazdarider23@gmail.com>
2016-09-12 17:55:33 -0400

    Disable dashboard suggestions [2/2]
    Change-Id: I33879e9b8e1ef15898e781aad4766b755df36287

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4106,6 +4106,13 @@ public final class Settings {
          * @hide
          */
         public static final String STATUS_BAR_CARRIER_FONT_SIZE = "status_bar_carrier_font_size";
+      
+        /**
+         * Disable dashboard suggestions in settings
+         * @hide
+         */
+        public static final String DISABLE_SUGGESTIONS = "disable_suggestions";
+
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
