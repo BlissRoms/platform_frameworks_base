f14853b97b087ea453885c26bcb782a34d5bee75
Carlo Savignano <carlosavignano@aospa.co>
2016-09-17 04:17:40 -0400

    input: hardware buttons lights integration
    * Enable and improve partial AOSP disabled button brightness hooks.
    * Implement user-timeout/doze/dream/awake/asleep policy.
    * Addresses NOUGAT-14
    
    Change-Id: Ie1fc9d1ff98e1782bdeb3afd03b03b762f39e5fc
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>

--- core/java/android/os/PowerManager.java
+++ core/java/android/os/PowerManager.java
@@ -473,6 +473,7 @@ public final class PowerManager {
     }
 
     /**
+<<<<<<< HEAD
      * Gets the minimum supported screen brightness setting for VR Mode.
      * @hide
      */
@@ -499,6 +500,37 @@ public final class PowerManager {
     public int getDefaultScreenBrightnessForVrSetting() {
         return mContext.getResources().getInteger(
                 com.android.internal.R.integer.config_screenBrightnessForVrSettingDefault);
+    }
+
+    /**
+     * Gets the minimum supported button brightness setting.
+     * The screen may be allowed to become dimmer than this value but
+     * this is the minimum value that can be set by the user.
+     * @hide
+     */
+    public int getMinimumButtonBrightnessSetting() {
+        return mContext.getResources().getInteger(
+                com.android.internal.R.integer.config_buttonBrightnessSettingMinimum);
+    }
+
+    /**
+     * Gets the maximum supported button brightness setting.
+     * The screen may be allowed to become dimmer than this value but
+     * this is the maximum value that can be set by the user.
+     * @hide
+     */
+    public int getMaximumButtonBrightnessSetting() {
+        return mContext.getResources().getInteger(
+                com.android.internal.R.integer.config_buttonBrightnessSettingMaximum);
+    }
+
+    /**
+     * Gets the default button brightness setting.
+     * @hide
+     */
+    public int getDefaultButtonBrightnessSetting() {
+        return mContext.getResources().getInteger(
+                com.android.internal.R.integer.config_buttonBrightnessSettingDefault);
     }
 
     /**
