eef0265cd70303ce10b0b39ac94a0adefda91be9
kufikugel <kufikugel@googlemail.com>
2016-10-02 02:49:59 -0400

    [QUICK SETTINGS] Add Music QS Tile
    picked from here: http://goo.gl/ehaoMU
    
    Adapted to lollipop from commit:
    Author: kufikugel
    Quicksettings customizations Slim style
    d85304fac9bde550beeeeca0dba2cd49686f20d7
    
    Adapted double click to open music app from commit:
    Author: Jubakuba
    SystemUI: Pimp-up Media Tile (1/1)
    5a14de5db237969607e0aabb9b8c04737b122dd3
    
    AICPfy:
    PS1: Gave proper credit to kufikugel for the original code
    PS2: Fix build by overriding all needed methids
    
    Change-Id: I4d3008d3dcfdf56f1cef25a602a153264c00c80f
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -58,6 +58,7 @@ import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
+import com.android.systemui.qs.tiles.MusicTile;
 import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
@@ -453,6 +454,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("screenshot")) return new ScreenshotTile(this);
         else if (tileSpec.equals("sync")) return new SyncTile(this);
         else if (tileSpec.equals("usb_tether")) return new UsbTetherTile(this);
+        else if (tileSpec.equals("music")) return new MusicTile(this);
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else if (tileSpec.startsWith(CustomTile.PREFIX)) return CustomTile.create(this,tileSpec);
