d6cf4c076e9f8a0ffeb19a2ff68d08d6422958e6
Danny Baumann <dannybaumann@web.de>
2016-11-02 10:29:05 +0100

    Fix zen priority mode vibration logic.
    Even if zen priority mode is active and vibration is allowed, we still
    want to keep notifications with score below the threshold silent.
    
    Change-Id: I0a60bbd787e7f98f721129902054ab37fb9f58a7

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -2770,7 +2770,7 @@ public class NotificationManagerService extends SystemService {
 
         boolean canBeep = readyForBeepOrBuzz && canInterrupt;
         boolean canBuzz = readyForBeepOrBuzz &&
-            (canInterrupt || mZenModeHelper.allowVibrationForNotifications());
+            (canInterrupt || (aboveThreshold && mZenModeHelper.allowVibrationForNotifications()));
         boolean hasValidSound = false;
 
         if (canBeep || canBuzz) {
