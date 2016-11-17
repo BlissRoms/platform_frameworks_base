131d5e6aba6953e6d04fcc69b8e5298a271828da
Sashidhar Ganiga <sganig@codeaurora.org>
2016-11-17 16:59:10 +0530

    Fix for AccountSelection Dialog in Lockscreen issue
    Phone Account Selection Dialog is displayed over lockscreen for
    an outgoing call. But since the dialog gets onStop from framework,
    the dialog and hence the call gets disconnected. This is due to
    delay in sleeping state in framework as per current lock screen
    design
    
    Current Fix tries to determine the sleeping state not just based
    on the state variable but also on the wakefulness state of the
    device to determine sleeping state.
    CRs-Fixed: 1082943
    
    Change-Id: Ideaf128de9f722e58b107efda020350a0ead0304

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -11781,7 +11781,8 @@ public final class ActivityManagerService extends ActivityManagerNative
     }
 
     boolean isSleepingLocked() {
-        return mSleeping;
+        return mSleeping && (mWakefulness == PowerManagerInternal.WAKEFULNESS_ASLEEP
+                              || mWakefulness == PowerManagerInternal.WAKEFULNESS_DOZING);
     }
 
     void onWakefulnessChanged(int wakefulness) {
