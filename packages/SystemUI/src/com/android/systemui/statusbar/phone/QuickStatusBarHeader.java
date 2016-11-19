66bbdc5e37ab391b8bd09ef60626c78195fdbc73
Calin <nilac8991@gmail.com>
2016-11-19 09:27:40 +0100

    [SQUASHED] Implement XOSP Blur personalization options (1/2)
    All credits for original work goes to serjar (Author of the very famous Xposed module: Blurred System UI)
    
    Credits goes also to: Aaahh  for the initial implementation of it in MarshROM and continued by me after in XOSP
    
    Original repository of the Xposed Module: https://github.com/serajr/Blurred-System-UI
    
    Available options:
    
    - Blurred expanded StatusBar
    - Blurred StausBar header (Deprecated on N)
    - Translucent Notifications
    - Blurred QS in Notification Header expanded
    - Ability to change Blur Radius and Blur scale
    
    Squash all my previous commits from Marshmallow and adapt everything to work on N:
    
    https://github.com/XOSP-Project/android_frameworks_base/commit/2f006d0e3dec77a03bb22ce0f48ba8b8260aecc8
    https://github.com/XOSP-Project/android_frameworks_base/commit/d4309fa0f60878649b217c99e7b790f809a8bb1c
    https://github.com/XOSP-Project/android_frameworks_base/commit/543d7f5408fc3ac6878f80c41174433cf6e31dbf
    https://github.com/XOSP-Project/android_frameworks_base/commit/738338b0e8c971cfa8262d24fa776e4fd03f8295
    
    //TO-DO
    
    - Adapt and finish Recents Blur code for Nougat
    - Make Translucent Notifications work (Because right now it's completely wrong, so it will be cut off from Settings)
    
    Change-Id: I87ac86dd45835481cdee17edfe1b9e45de078188
    Signed-off-by: Calin <nilac8991@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickStatusBarHeader.java
@@ -24,6 +24,7 @@ import android.content.res.Configuration;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.RippleDrawable;
 import android.os.UserManager;
+import android.os.UserHandle;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -38,6 +39,7 @@ import com.android.internal.logging.MetricsProto;
 import com.android.keyguard.KeyguardStatusView;
 import com.android.systemui.FontSizeUtils;
 import com.android.systemui.R;
+import com.android.systemui.cm.UserContentObserver;
 import com.android.systemui.qs.QSPanel;
 import com.android.systemui.qs.QSPanel.Callback;
 import com.android.systemui.qs.QuickQSPanel;
@@ -131,7 +133,6 @@ public class QuickStatusBarHeader extends BaseStatusBarHeader implements
         // settings), so disable it for this view
         ((RippleDrawable) mSettingsButton.getBackground()).setForceSoftware(true);
         ((RippleDrawable) mExpandIndicator.getBackground()).setForceSoftware(true);
-
         updateResources();
     }
 
@@ -362,7 +363,7 @@ public class QuickStatusBarHeader extends BaseStatusBarHeader implements
             }
         }
     }
-
+    
     @Override
     public void onUserInfoChanged(String name, Drawable picture) {
         mMultiUserAvatar.setImageDrawable(picture);
