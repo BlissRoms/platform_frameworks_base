c1470093e4526462a5988e0053910a9f19d910ca
Calin <nilac8991@gmail.com>
2016-11-19 10:51:25 +0100

    Blur: Implement Recents Blur (1/2)
    - Added basic implementation of serajr Recents blur
    - Adapted the code in order to allow the user to change radius and scale
    - Added required flags in order to avoid Arithmetic exceptions
    
    Signed-off-by: Calin <nilac8991@gmail.com>
    
    RecentsBlur: Fixes
    
    Signed-off-by: Calin <nilac8991@gmail.com>
    
    RecentsBlur: More imports
    
    Signed-off-by: Calin <nilac8991@gmail.com>
    
    RecentsBlur: Fix SystemUI crash
    
    Change-Id: I04b4df7691d3a2f3255eee06de2e374bb9c4c577
    Signed-off-by: Calin <nilac8991@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -1280,6 +1280,7 @@ public abstract class BaseStatusBar extends SystemUI implements
 
     @Override
     public void toggleRecentApps() {
+        RecentsActivity.startBlurTask();
         toggleRecents();
     }
 
