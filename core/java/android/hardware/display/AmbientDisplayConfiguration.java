516d52685054662e7c9909c31469b176ed1491de
Pranav Vashi <neobuddy89@gmail.com>
2016-12-08 22:40:47 +0530

    base: Add bool to enable/disable doze by default
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Ie5d7e2560d3ef3731c5ad3087c21317ed9fb3163
    Signed-off-by: Ayan Mukherjee <mukherjeeayan725@gmail.com>

--- core/java/android/hardware/display/AmbientDisplayConfiguration.java
+++ core/java/android/hardware/display/AmbientDisplayConfiguration.java
@@ -35,6 +35,7 @@ public class AmbientDisplayConfiguration {
 
     private final Context mContext;
     private final boolean mAlwaysOnByDefault;
+    private final boolean mDozeEnabledByDefault;
     private final boolean mDeviceHasSoli;
     private final boolean mDeviceWithWeirdDtSensor;
     private final boolean mDeviceHasElmyra;
@@ -44,6 +45,7 @@ public class AmbientDisplayConfiguration {
     public AmbientDisplayConfiguration(Context context) {
         mContext = context;
         mAlwaysOnByDefault = mContext.getResources().getBoolean(R.bool.config_dozeAlwaysOnEnabled);
+        mDozeEnabledByDefault = mContext.getResources().getBoolean(R.bool.config_dozeDefaultEnabled);
         mDeviceHasSoli = mContext.getResources().getBoolean(R.bool.config_has_Soli);
         mDeviceWithWeirdDtSensor = mContext.getResources().getBoolean(R.bool.config_has_weird_dt_sensor);
         mDeviceHasElmyra = mContext.getResources().getBoolean(R.bool.config_has_elmyra);
@@ -66,7 +68,7 @@ public class AmbientDisplayConfiguration {
 
     /** {@hide} */
     public boolean pulseOnNotificationEnabled(int user) {
-        return boolSettingDefaultOn(Settings.Secure.DOZE_ENABLED, user)
+        return boolSetting(Settings.Secure.DOZE_ENABLED, user, mDozeEnabledByDefault ? 1 : 0)
                 && pulseOnNotificationAvailable();
     }
 
