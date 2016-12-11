5457e0ec95021ed1b0548636f64359fd992cf640
Jackeagle <jackeagle102@gmail.com>
2016-12-10 21:56:44 -0500

    [SQUASH]: Remove CAF AutoBrightness Implementation
    Revert "SystemUI: [RJIL new function] AutoBrightness"
    
    This reverts commit cb6068b04458f1a5a1c7aeee3e332cb501766494.
    
    Change-Id: I8df80adffdafd30762771920e7040f3ae5c4dcf2
    
    Revert "Update Auto Brightness Icon"
    
    This reverts commit 8f23af2802ad1b15fa7b0fe03b8afed6b22a05ee.

--- packages/SystemUI/src/com/android/systemui/settings/BrightnessController.java
+++ packages/SystemUI/src/com/android/systemui/settings/BrightnessController.java
@@ -30,17 +30,11 @@ import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.UserHandle;
 import android.provider.Settings;
-import android.view.View;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.service.vr.IVrManager;
 import android.service.vr.IVrStateCallbacks;
 import android.util.Log;
 import android.widget.ImageView;
-import static android.provider.Settings.System.SCREEN_BRIGHTNESS_MODE;
-import static android.provider.Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC;
-import static android.provider.Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL;
+
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 
@@ -71,7 +65,6 @@ public class BrightnessController implements ToggleSlider.Listener {
     private final Context mContext;
     private final ImageView mIcon;
     private final ToggleSlider mControl;
-    private CheckBox mAutoBrightness;
     private final boolean mAutomaticAvailable;
     private final IPowerManager mPower;
     private final CurrentUserTracker mUserTracker;
@@ -203,12 +196,6 @@ public class BrightnessController implements ToggleSlider.Listener {
                 mHandler.obtainMessage(MSG_SET_CHECKED, 0).sendToTarget();
                 mHandler.obtainMessage(MSG_UPDATE_ICON, 0 /* automatic */).sendToTarget();
             }
-            if (mAutoBrightness != null) {
-                mAutoBrightness.setChecked(Settings.System.getInt(
-                    mContext.getContentResolver(),
-                    SCREEN_BRIGHTNESS_MODE,
-                    SCREEN_BRIGHTNESS_MODE_MANUAL) != SCREEN_BRIGHTNESS_MODE_MANUAL);
-            }
         }
     };
 
@@ -289,7 +276,6 @@ public class BrightnessController implements ToggleSlider.Listener {
         mContext = context;
         mIcon = icon;
         mControl = control;
-        mAutoBrightness = null;
         mBackgroundHandler = new Handler(Looper.getMainLooper());
         mUserTracker = new CurrentUserTracker(mContext) {
             @Override
@@ -310,25 +296,6 @@ public class BrightnessController implements ToggleSlider.Listener {
                 com.android.internal.R.bool.config_automatic_brightness_available);
         mPower = IPowerManager.Stub.asInterface(ServiceManager.getService("power"));
         mVrManager = IVrManager.Stub.asInterface(ServiceManager.getService("vrmanager"));
-    }
-
-    public BrightnessController(Context context, ImageView icon,
-            ToggleSlider control, CheckBox autoBrightness) {
-        this(context, icon, control);
-        mAutoBrightness = autoBrightness;
-        mAutoBrightness.setChecked(Settings.System.getInt(
-            mContext.getContentResolver(), SCREEN_BRIGHTNESS_MODE,
-            SCREEN_BRIGHTNESS_MODE_MANUAL) != SCREEN_BRIGHTNESS_MODE_MANUAL);
-        mAutoBrightness.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView,
-                boolean isChecked) {
-                Settings.System.putInt(mContext.getContentResolver(),
-                    SCREEN_BRIGHTNESS_MODE,
-                    isChecked ? SCREEN_BRIGHTNESS_MODE_AUTOMATIC :
-                    SCREEN_BRIGHTNESS_MODE_MANUAL);
-                    }
-                });
     }
 
     public void setBackgroundLooper(Looper backgroundLooper) {
