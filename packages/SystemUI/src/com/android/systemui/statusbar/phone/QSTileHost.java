06efea31bf6ac06dc5f1c62ffd57eddc9220e1d4
Yoshinori Hirano <yoshinori.hirano@sonymobile.com>
2017-01-09 12:24:03 +0100

    Add Sound tile
    Add Sound tile to make user easier to change sound mode to manner mode.
    
    Sound Tile improvement (beanstown106 <nbenis106@gmail.com>)
    
    *uncoupled zen mode from sound tile let users use dnd for that
    *cycles through ring/vibrate/silent
    *added more descriptive labels for each state instead of always saying sound
    *added longpress action
    
    AICPfy:
    Adapt to Nougat
    
    Change-Id: I9934e72758f162bd70ca578f6077fcd231fe7a6a

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -67,6 +67,7 @@ import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenrecordTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
+import com.android.systemui.qs.tiles.SoundTile;
 import com.android.systemui.qs.tiles.SyncTile;
 import com.android.systemui.qs.tiles.UsbTetherTile;
 import com.android.systemui.qs.tiles.UserTile;
@@ -481,6 +482,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("locale")) return new LocaleTile(this);
         else if (tileSpec.equals("assist")) return new GoogleAssistTile(this);
         else if (tileSpec.equals("voiceassist")) return new GoogleVoiceAssistTile(this);
+        else if (tileSpec.equals("sound")) return new SoundTile(this);
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else if (tileSpec.startsWith(CustomTile.PREFIX)) return CustomTile.create(this,tileSpec);
