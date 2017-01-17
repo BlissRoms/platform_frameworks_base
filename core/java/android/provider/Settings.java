4a9125e1bd8f955a1e2afbe6b81ae038227230c4
beanstown106 <nbenis106@gmail.com>
2017-01-16 22:27:17 -0500

    One handed mode toggle [1/3]
    Change-Id: I4c4f85eb23a487d38636f4b2520640a45479c4c0

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7415,6 +7415,12 @@ public final class Settings {
         public static final String WEB_ACTION_ENABLED = "web_action_enabled";
 
         /**
+         * One Handed mode toggle
+         * @hide
+         */
+        public static final String ONE_HANDED_MODE_UI = "one_handed_mode_ui";
+
+        /**
          * Whether user is allowed to pull down quick settings on secure keyguard.
          * @hide
          */
