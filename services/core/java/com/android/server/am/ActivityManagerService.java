79a50568218da4c5de75df7c7345c8db0ddc317d
Mahantesh Sanakall <msanak@codeaurora.org>
2017-03-30 13:35:50 +0530

    Revert "Fix for AccountSelection Dialog in Lockscreen issue"
    This reverts commit 93a417f0827f6162d543d02d011c26f3fefa2efe.
    
    Change-Id: I229b767eb6b948fcd1eaf538af40bfc23a18ac34

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -11814,8 +11814,7 @@ public final class ActivityManagerService extends ActivityManagerNative
     }
 
     boolean isSleepingLocked() {
-        return mSleeping && (mWakefulness == PowerManagerInternal.WAKEFULNESS_ASLEEP
-                              || mWakefulness == PowerManagerInternal.WAKEFULNESS_DOZING);
+        return mSleeping;
     }
 
     void onWakefulnessChanged(int wakefulness) {
