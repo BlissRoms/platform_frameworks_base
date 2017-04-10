AICP/frameworks_base

Add Application picker and tile for it
PS3:
Sort list alphabetically
Exit picker when item is selected
Don't exclude from recents

PS4:
Ignore case when sorting

PS5:
Add CardView
Add icon to the activity

Change-Id: I8165f6ad4f232921e16cc164923fa393fc731e40

packages/SystemUI/src/com/android/systemui/aicp/apppicker/AppPickerActivity.java
@@ -1,0 +1,114 @@
+package com.android.systemui.aicp.apppicker;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.systemui.R;
+
+public class AppPickerActivity extends ListActivity {
+
+    private PackageManager packageManager = null;
+    private List<ApplicationInfo> applist = null;
+    private AppPickerAdapter listadapter = null;
+    private CardView cardView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_picker);
+
+        cardView = (CardView) findViewById(R.id.card_view);
+        cardView.setRadius(10);
+
+        packageManager = getPackageManager();
+
+        new LoadApplications().execute();
+    }
+
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        ApplicationInfo app = applist.get(position);
+
+        try{
+            Intent intent = packageManager.getLaunchIntentForPackage(app.packageName);
+
+            if(intent != null) {
+                startActivity(intent);
+            }
+        } catch(Exception e) {
+            Toast.makeText(AppPickerActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+
+        finish();
+    }
+
+    private List<ApplicationInfo> checkForLaunchIntent(List<ApplicationInfo> list) {
+
+        ArrayList<ApplicationInfo> appList = new ArrayList<>();
+
+        for(ApplicationInfo info : list) {
+            try{
+                if(packageManager.getLaunchIntentForPackage(info.packageName) != null) {
+                    appList.add(info);
+                }
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        Collections.sort(appList, new Comparator<ApplicationInfo>() {
+            @Override
+            public int compare(ApplicationInfo applicationInfo, ApplicationInfo t1) {
+                return applicationInfo.loadLabel(packageManager).toString().
+                        compareToIgnoreCase(t1.loadLabel(packageManager).toString());
+            }
+        });
+
+        return appList;
+    }
+
+    private class LoadApplications extends AsyncTask<Void, Void, Void> {
+
+        private ProgressDialog progress = null;
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            applist = checkForLaunchIntent(packageManager.getInstalledApplications(PackageManager.GET_META_DATA));
+
+            listadapter = new AppPickerAdapter(AppPickerActivity.this, R.layout.app_picker_list_item, applist);
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            setListAdapter(listadapter);
+            progress.dismiss();
+            super.onPostExecute(result);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            progress = ProgressDialog.show(AppPickerActivity.this, null, "Loading apps info...");
+            super.onPreExecute();
+        }
+    }
+}

packages/SystemUI/src/com/android/systemui/qs/tiles/AppPickerTile.java
@@ -1,0 +1,82 @@
+/*
+ * Copyright (C) 2015 The Dirty Unicorns Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.ComponentName;
+import android.widget.Toast;
+
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+import com.android.systemui.R;
+import com.android.systemui.SysUIToast;
+import com.android.systemui.qs.QSTile;
+import com.android.systemui.qs.QSTileView;
+
+public class AppPickerTile extends QSTile<QSTile.BooleanState> {
+    private boolean mListening;
+
+    private static final Intent APP_PICKER = new Intent().setComponent(new ComponentName(
+            "com.android.systemui", "com.android.systemui.aicp.apppicker.AppPickerActivity"));
+
+    public AppPickerTile(Host host) {
+        super(host);
+    }
+
+    @Override
+    public BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.QUICK_SETTINGS;
+    }
+
+    @Override
+    protected void handleClick() {
+        mHost.collapsePanels();
+        startAppPicker();
+        refreshState();
+    }
+
+    @Override
+    public Intent getLongClickIntent() {
+        return null;
+    }
+
+    @Override
+    public CharSequence getTileLabel() {
+        return mContext.getString(R.string.quick_settings_app_picker);
+    }
+
+    protected void startAppPicker() {
+        mHost.startActivityDismissingKeyguard(APP_PICKER);
+    }
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        state.icon = ResourceIcon.get(R.drawable.ic_qs_app_picker);
+        state.label = mContext.getString(R.string.quick_settings_app_picker);
+    }
+
+    @Override
+    public void setListening(boolean listening) {
+        if (mListening == listening) return;
+        mListening = listening;
+    }
+}

BlissRoms/platform_frameworks_base

packages/SystemUI/src/com/android/systemui/bliss/apppicker/AppPickerActivity.java
@@ -1,0 +1,114 @@
+package com.android.systemui.bliss.apppicker;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.systemui.R;
+
+public class AppPickerActivity extends ListActivity {
+
+    private PackageManager packageManager = null;
+    private List<ApplicationInfo> applist = null;
+    private AppPickerAdapter listadapter = null;
+    private CardView cardView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_picker);
+
+        cardView = (CardView) findViewById(R.id.card_view);
+        cardView.setRadius(10);
+
+        packageManager = getPackageManager();
+
+        new LoadApplications().execute();
+    }
+
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        ApplicationInfo app = applist.get(position);
+
+        try{
+            Intent intent = packageManager.getLaunchIntentForPackage(app.packageName);
+
+            if(intent != null) {
+                startActivity(intent);
+            }
+        } catch(Exception e) {
+            Toast.makeText(AppPickerActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+
+        finish();
+    }
+
+    private List<ApplicationInfo> checkForLaunchIntent(List<ApplicationInfo> list) {
+
+        ArrayList<ApplicationInfo> appList = new ArrayList<>();
+
+        for(ApplicationInfo info : list) {
+            try{
+                if(packageManager.getLaunchIntentForPackage(info.packageName) != null) {
+                    appList.add(info);
+                }
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        Collections.sort(appList, new Comparator<ApplicationInfo>() {
+            @Override
+            public int compare(ApplicationInfo applicationInfo, ApplicationInfo t1) {
+                return applicationInfo.loadLabel(packageManager).toString().
+                        compareToIgnoreCase(t1.loadLabel(packageManager).toString());
+            }
+        });
+
+        return appList;
+    }
+
+    private class LoadApplications extends AsyncTask<Void, Void, Void> {
+
+        private ProgressDialog progress = null;
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            applist = checkForLaunchIntent(packageManager.getInstalledApplications(PackageManager.GET_META_DATA));
+
+            listadapter = new AppPickerAdapter(AppPickerActivity.this, R.layout.app_picker_list_item, applist);
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            setListAdapter(listadapter);
+            progress.dismiss();
+            super.onPostExecute(result);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            progress = ProgressDialog.show(AppPickerActivity.this, null, "Loading apps info...");
+            super.onPreExecute();
+        }
+    }
+}

packages/SystemUI/src/com/android/systemui/qs/tiles/AppPickerTile.java
@@ -1,0 +1,82 @@
+/*
+ * Copyright (C) 2015 The Dirty Unicorns Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.ComponentName;
+import android.widget.Toast;
+
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+import com.android.systemui.R;
+import com.android.systemui.SysUIToast;
+import com.android.systemui.qs.QSTile;
+import com.android.systemui.qs.QSTileView;
+
+public class AppPickerTile extends QSTile<QSTile.BooleanState> {
+    private boolean mListening;
+
+    private static final Intent APP_PICKER = new Intent().setComponent(new ComponentName(
+            "com.android.systemui", "com.android.systemui.aosip.apppicker.AppPickerActivity"));
+
+    public AppPickerTile(Host host) {
+        super(host);
+    }
+
+    @Override
+    public BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.QUICK_SETTINGS;
+    }
+
+    @Override
+    protected void handleClick() {
+        mHost.collapsePanels();
+        startAppPicker();
+        refreshState();
+    }
+
+    @Override
+    public Intent getLongClickIntent() {
+        return null;
+    }
+
+    @Override
+    public CharSequence getTileLabel() {
+        return mContext.getString(R.string.quick_settings_app_picker);
+    }
+
+    protected void startAppPicker() {
+        mHost.startActivityDismissingKeyguard(APP_PICKER);
+    }
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        state.icon = ResourceIcon.get(R.drawable.ic_qs_app_picker);
+        state.label = mContext.getString(R.string.quick_settings_app_picker);
+    }
+
+    @Override
+    public void setListening(boolean listening) {
+        if (mListening == listening) return;
+        mListening = listening;
+    }
+}