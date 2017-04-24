b12bd2a3ed1430f5e1d6169537d4508a72fadc2c
Chris Lahaye <dev@chrislahaye.com>
2017-04-25 00:10:35 +0200

    pocket: introduce pocket bridge
    This service communicates pocket state to the pocket judge kernel driver.
    It maintains the pocket state by binding to the pocket service.
    
    Ticket: NOUGAT-49
    
    Change-Id: Iee77f767f23d3f77a0d1d871f9305f3b3c6d6630
    Signed-off-by: Chris Lahaye <dev@chrislahaye.com>
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -98,6 +98,7 @@ import com.android.server.om.OverlayManagerService;
 import com.android.server.os.DeviceIdentifiersPolicyService;
 import com.android.server.os.SchedulingPolicyService;
 import com.android.server.pocket.PocketService;
+import com.android.server.pocket.PocketBridgeService;
 import com.android.server.pm.BackgroundDexOptService;
 import com.android.server.pm.CrossProfileAppsService;
 import com.android.server.gesture.EdgeGestureService;
@@ -1611,6 +1612,13 @@ public final class SystemServer {
                 ServiceManager.addService("edgegestureservice", edgeGestureService);
             } catch (Throwable e) {
                 Slog.e(TAG, "Failure starting EdgeGesture service", e);
+            }
+
+            if (!context.getResources().getString(
+                    com.android.internal.R.string.config_pocketBridgeSysfsInpocket).isEmpty()) {
+                traceBeginAndSlog("StartPocketBridgeService");
+                mSystemServiceManager.startService(PocketBridgeService.class);
+                traceEnd();
             }
         }
 
