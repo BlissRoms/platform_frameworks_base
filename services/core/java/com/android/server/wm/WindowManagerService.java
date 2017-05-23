d570cff3f576d8b0f59869b2de0bacded1f9e974
kunalshah <kunalshah912@gmail.com>
2017-05-23 19:40:41 +0000

    Revert "[SQUASH]: Remove AOSPA Button Integration"
    This reverts commit 933e9acdf597b3e249a2944e4d93b4bfc9c49aa3.

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -10493,6 +10493,11 @@ public class WindowManagerService extends IWindowManager.Stub
     }
 
     @Override
+    public boolean needsNavigationBar() {
+        return mPolicy.needsNavigationBar();
+    }
+
+    @Override
     public void lockNow(Bundle options) {
         mPolicy.lockNow(options);
     }
