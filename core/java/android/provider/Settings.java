64f665cbf6cf3c28db56924c8995558b45aa342a
Pranav Vashi <neobuddy89@gmail.com>
2017-07-19 23:46:45 +0530

    4G icon instead of LTE [1/2]
    Change-Id: Ieb912c64a60ddd42fe7b21d18dadea293ff159c5
    
    Improve 4G icon switch code
    
    * adapted to A12 L
    * added setting key to backup array
    * use a single settings observer and use existing mReceiverHandler instead of creating new one's
    * keep the settings observer class private
    * don't update configuration when registering observer
    * only notify listeners if necessary
    
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5552,6 +5552,12 @@ public final class Settings {
         public static final String DOUBLE_TAP_SLEEP_LOCKSCREEN = "double_tap_sleep_lockscreen";
 
         /**
+         * Whether to display 4G icon instead LTE
+         * @hide
+         */
+        public static final String SHOW_FOURG_ICON = "show_fourg_icon";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
