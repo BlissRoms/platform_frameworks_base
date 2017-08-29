0fec164065389ac1256fc97d3be5852418e30710
Danny Baumann <dannybaumann@web.de>
2017-08-29 17:50:07 +0800

    Add PowerManager integration for button backlight. Allows setting button backlight brightness as well as button timeout.
    Change-Id: Icbf1bf93c2a20ac6214ac952c4ea2a5906e2b274
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- core/java/android/os/PowerManager.java
+++ core/java/android/os/PowerManager.java
@@ -1744,4 +1744,13 @@ public final class PowerManager {
             };
         }
     }
+
+    /**
+     * Gets the default button brightness value.
+     * @hide
+     */
+    public int getDefaultButtonBrightness() {
+        return mContext.getResources().getInteger(
+                com.android.internal.R.integer.config_buttonBrightnessSettingDefault);
+    }
 }
