cdb713bc001b5cd597269f460622961f825cd12b
cphelps76 <cphelps76@droidconcepts.com>
2017-10-03 20:02:52 +0200

    Battery Styles: Add BatteryBar [1/2]
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Ieebace9178144e3e76c6279893c42f10047f4cb0
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/pipeline/shared/ui/binder/HomeStatusBarViewBinder.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/pipeline/shared/ui/binder/HomeStatusBarViewBinder.kt
@@ -32,6 +32,7 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.android.app.animation.Interpolators
+import com.android.systemui.bliss.batterybar.BatteryBarController
 import com.android.systemui.display.dagger.SystemUIDisplaySubcomponent.PerDisplaySingleton
 import com.android.systemui.lifecycle.repeatWhenAttached
 import com.android.systemui.res.R
@@ -126,6 +127,8 @@ constructor(
         val rightClock: Clock = view.findViewById(R.id.clock_right)
         val notificationIconsArea = view.requireViewById<View>(R.id.notificationIcons)
 
+        val batteryBar: BatteryBarController = view.requireViewById(R.id.battery_bar)
+
         // CollapsedStatusBarFragment doesn't need this
         if (StatusBarRootModernization.isEnabled) {
             // GONE because this shouldn't take space in the layout
@@ -134,6 +137,7 @@ constructor(
             leftClock.hideInitially(state = View.GONE)
             centerClock.hideInitially(state = View.GONE)
             rightClock.hideInitially(state = View.GONE)
+            batteryBar.hideInitially()
             notificationIconsArea.hideInitially()
         }
 
@@ -472,6 +476,7 @@ constructor(
                     launch {
                         viewModel.isNotificationIconContainerVisible.collect {
                             notificationIconsArea.adjustVisibility(it)
+                            batteryBar.adjustVisibility(it)
                         }
                     }
 
