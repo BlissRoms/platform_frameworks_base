70f6a5eade2523f8609ae0013e74b46a4291fa81
Alberto97 <albertop2197@gmail.com>
2017-10-16 23:14:07 +0200

    SystemUI: Statusbar brightness control [1/2]
    This is a squash of the following changes:
    
    Author: Danesh Mondegarian <daneshm90@gmail.com>
    Date:   Sun Nov 10 23:30:56 2013 -0800
    
        SystemUI : Port forward statusbar brightness control
    
        Brings back the status bar brightness control from cm-10.2/cm-11
    
        Change-Id: Icc04e7da3286e9925f8b90528c1e022b33d33e9d
    
    Author: Roman Birg <roman@cyngn.com>
    Date:   Mon Dec 1 16:08:21 2014 -0800
    
        SystemUI: improve brightness slider behavior
    
        Mimic the brightness slider behavior in the statusbar.
        This adds logic to make the statusbar slider also work with automatic
        brightness mode enabled and it will instead adjust the temporary automatic
        brightness overrride.
    
        Change-Id: I78814a785decc83df94844fcc2f489c8cbec2b65
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: Pawit Pornkitprasan <p.pawit@gmail.com>
    Date:   Sun Dec 28 20:42:41 2014 +0700
    
        SystemUI: status bar brightness: store value as int
    
        Non-automatic brightness value is stored as int, not float.
    
        Symptom: adjust the brightness in the status bar, the brightness
                 slider in the notification bar will always be set to full
    
        Change-Id: I5f9a6e97bae2c44c3aff7c9222186a6dbda5d902
    
    Author: Zhao Wei Liew <zhaoweiliew@gmail.com>
    Date:   Fri Oct 7 10:09:04 2016 +0800
    
        SystemUI: statusbar: Fix status bar brightness control feature
    
        Bring in the missing fixes from CM 13.
    
        This commit squashes the following commits from CM 13.0:
        6397527ca0c430671503cecb69d74554dea1d399 SystemUI: fix status bar brightness slider eating events
        bbef42271cbba0eda91d214186ff2be31eb3b608 SystemUI : fix brightness slider for automatic brightness
    
        Change-Id: I85e0b99dc33468b36222128660707fe481cf8b33
    
    Author: Zhao Wei Liew <zhaoweiliew@gmail.com>
    Date:   Fri Oct 7 08:56:25 2016 +0800
    
        SystemUI: Use Tuner API for CM settings
    
        Get rid of all the excess code by implementing TunerService.Tunable
        and observing any changes made to the settings through it.
    
        Remove UserContentObserver as the Tuner API handles user switches.
    
        Also remove some unused imports that were introduced in earlier
        CM commits, but were never removed since.
    
        Change-Id: Iecafafabdaec82b3b3c72293bea865de48f0e90a
    
    Change-Id: Ic8ded146dc8605e6140f4352cafa6f961ad3decc
    
    idoybh:
    * Properly mention co-authors
    
    Co-authored-by: Danesh Mondegarian <daneshm90@gmail.com>
    Co-authored-by: Roman Birg <roman@cyngn.com>
    Co-authored-by: Pawit Pornkitprasan <p.pawit@gmail.com>
    Co-authored-by: Zhao Wei Liew <zhaoweiliew@gmail.com>
    Change-Id: Ic9b3fd1ffb0ad5b8c448d0f6f3f75dfbfe5fd83a

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -71,8 +71,10 @@ import android.content.res.Resources;
 import android.database.ContentObserver;
 import android.graphics.Point;
 import android.hardware.devicestate.DeviceStateManager;
+import android.hardware.display.DisplayManager;
 import android.metrics.LogMaker;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -95,12 +97,14 @@ import android.util.Log;
 import android.util.MathUtils;
 import android.util.Slog;
 import android.view.Display;
+import android.view.HapticFeedbackConstants;
 import android.view.IRemoteAnimationRunner;
 import android.view.IWindowManager;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.ThreadedRenderer;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.view.ViewGroup;
 import android.view.ViewRootImpl;
 import android.view.WindowInsetsController.Appearance;
@@ -289,6 +293,7 @@ public class CentralSurfacesImpl extends CoreStartable implements
 
     private static final int MSG_OPEN_SETTINGS_PANEL = 1002;
     private static final int MSG_LAUNCH_TRANSITION_TIMEOUT = 1003;
+    private static final int MSG_LONG_PRESS_BRIGHTNESS_CHANGE = 1004;
     // 1020-1040 reserved for BaseStatusBar
 
     /**
@@ -297,6 +302,10 @@ public class CentralSurfacesImpl extends CoreStartable implements
     private static final int HINT_RESET_DELAY_MS = 1200;
 
     private static final UiEventLogger sUiEventLogger = new UiEventLoggerImpl();
+
+    private static final float BRIGHTNESS_CONTROL_PADDING = 0.15f;
+    private static final int BRIGHTNESS_CONTROL_LONG_PRESS_TIMEOUT = 750; // ms
+    private static final int BRIGHTNESS_CONTROL_LINGER_THRESHOLD = 20;
 
     /**
      * If true, the system is in the half-boot-to-decryption-screen state.
@@ -547,6 +556,18 @@ public class CentralSurfacesImpl extends CoreStartable implements
     private final WallpaperManager mWallpaperManager;
 
     private CentralSurfacesComponent mCentralSurfacesComponent;
+
+    private DisplayManager mDisplayManager;
+
+    private int mMinBrightness;
+    private int mInitialTouchX;
+    private int mInitialTouchY;
+    private int mLinger;
+    private int mQuickQsOffsetHeight;
+    private boolean mAutomaticBrightness;
+    private boolean mBrightnessControl;
+    private boolean mBrightnessChanged;
+    private boolean mJustPeeked;
 
     // Flags for disabling the status bar
     // Two variables because the first one evidently ran out of room for new flags.
@@ -912,6 +933,8 @@ public class CentralSurfacesImpl extends CoreStartable implements
                 data -> mCommandQueueCallbacks.animateExpandSettingsPanel(data.mSubpanel));
         mMessageRouter.subscribeTo(MSG_LAUNCH_TRANSITION_TIMEOUT,
                 id -> onLaunchTransitionTimeout());
+        mMessageRouter.subscribeTo(MSG_LONG_PRESS_BRIGHTNESS_CHANGE,
+                id -> onLongPressBrightnessChange());
 
         deviceStateManager.registerCallback(mMainExecutor,
                 new FoldStateListener(mContext, this::onFoldedStateChanged));
@@ -974,6 +997,8 @@ public class CentralSurfacesImpl extends CoreStartable implements
         } else if (DEBUG) {
             Log.v(TAG, "start(): no wallpaper service ");
         }
+
+        mDisplayManager = mContext.getSystemService(DisplayManager.class);
 
         // Set up the initial notification state. This needs to happen before CommandQueue.disable()
         setUpPresenter();
@@ -1178,6 +1203,9 @@ public class CentralSurfacesImpl extends CoreStartable implements
         inflateStatusBarWindow();
         mNotificationShadeWindowView.setOnTouchListener(getStatusBarWindowTouchListener());
         mWallpaperController.setRootView(mNotificationShadeWindowView);
+
+        mMinBrightness = mContext.getResources().getInteger(
+                com.android.internal.R.integer.config_screenBrightnessDim);
 
         // TODO: Deal with the ugliness that comes from having some of the status bar broken out
         // into fragments, but the rest here, it leaves some awkward lifecycle and whatnot.
@@ -2002,6 +2030,7 @@ public class CentralSurfacesImpl extends CoreStartable implements
 
         void observe() {
             mSystemSettings.registerContentObserverForUser(Settings.System.LESS_BORING_HEADS_UP, this, UserHandle.USER_ALL);
+            mSystemSettings.registerContentObserverForUser(Settings.System.STATUS_BAR_BRIGHTNESS_CONTROL, this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -2010,12 +2039,16 @@ public class CentralSurfacesImpl extends CoreStartable implements
                 case Settings.System.LESS_BORING_HEADS_UP:
                     setUseLessBoringHeadsUp();
                     break;
+                case Settings.System.STATUS_BAR_BRIGHTNESS_CONTROL:
+                    setBrightnessControl();
+                    break;
             }
         }
 
         void update() {
             mBackgroundHandler.post(() -> {
                 setUseLessBoringHeadsUp();
+                setBrightnessControl();
         });
     }
 
@@ -2025,6 +2058,11 @@ public class CentralSurfacesImpl extends CoreStartable implements
             mMainHandler.post(() -> {
                 mNotificationInterruptStateProvider.setUseLessBoringHeadsUp(lessBoringHeadsUp);
             });
+        }
+
+        private void setBrightnessControl() {
+            mBrightnessControl = Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.STATUS_BAR_BRIGHTNESS_CONTROL, 0) == 1;
         }
     }
 
@@ -2172,6 +2210,96 @@ public class CentralSurfacesImpl extends CoreStartable implements
         }
     }
 
+    private void adjustBrightness(int x) {
+        mBrightnessChanged = true;
+        float raw = ((float) x) / getDisplayWidth();
+
+        // Add a padding to the brightness control on both sides to
+        // make it easier to reach min/max brightness
+        float padded = Math.min(1.0f - BRIGHTNESS_CONTROL_PADDING,
+                Math.max(BRIGHTNESS_CONTROL_PADDING, raw));
+        float value = (padded - BRIGHTNESS_CONTROL_PADDING) /
+                (1 - (2.0f * BRIGHTNESS_CONTROL_PADDING));
+        if (mAutomaticBrightness) {
+            float adj = (2 * value) - 1;
+            adj = Math.max(adj, -1);
+            adj = Math.min(adj, 1);
+            final float val = adj;
+            mDisplayManager.setTemporaryAutoBrightnessAdjustment(val);
+            AsyncTask.execute(new Runnable() {
+                public void run() {
+                    Settings.System.putFloatForUser(mContext.getContentResolver(),
+                            Settings.System.SCREEN_AUTO_BRIGHTNESS_ADJ, val,
+                            UserHandle.USER_CURRENT);
+                }
+            });
+        } else {
+            int newBrightness = mMinBrightness + (int) Math.round(value *
+                    (PowerManager.BRIGHTNESS_ON - mMinBrightness));
+            newBrightness = Math.min(newBrightness, PowerManager.BRIGHTNESS_ON);
+            newBrightness = Math.max(newBrightness, mMinBrightness);
+            final int val = newBrightness;
+            mDisplayManager.setTemporaryBrightness(mDisplayId, val);
+            AsyncTask.execute(new Runnable() {
+                @Override
+                public void run() {
+                    Settings.System.putIntForUser(mContext.getContentResolver(),
+                            Settings.System.SCREEN_BRIGHTNESS, val,
+                            UserHandle.USER_CURRENT);
+                }
+            });
+        }
+    }
+
+    private void brightnessControl(MotionEvent event) {
+        final int action = event.getAction();
+        final int x = (int) event.getRawX();
+        final int y = (int) event.getRawY();
+        mQuickQsOffsetHeight = mContext.getResources().getDimensionPixelSize(
+                com.android.internal.R.dimen.quick_qs_offset_height);
+        if (action == MotionEvent.ACTION_DOWN) {
+            if (y < mQuickQsOffsetHeight) {
+                mLinger = 0;
+                mInitialTouchX = x;
+                mInitialTouchY = y;
+                mJustPeeked = true;
+                mMessageRouter.cancelMessages(MSG_LONG_PRESS_BRIGHTNESS_CHANGE);
+                mMessageRouter.sendMessageDelayed(MSG_LONG_PRESS_BRIGHTNESS_CHANGE,
+                        BRIGHTNESS_CONTROL_LONG_PRESS_TIMEOUT);
+            }
+        } else if (action == MotionEvent.ACTION_MOVE) {
+            if (y < mQuickQsOffsetHeight && mJustPeeked) {
+                if (mLinger > BRIGHTNESS_CONTROL_LINGER_THRESHOLD) {
+                    adjustBrightness(x);
+                } else {
+                    final int xDiff = Math.abs(x - mInitialTouchX);
+                    final int yDiff = Math.abs(y - mInitialTouchY);
+                    final int touchSlop = ViewConfiguration.get(mContext).getScaledTouchSlop();
+                    if (xDiff > yDiff) {
+                        mLinger++;
+                    }
+                    if (xDiff > touchSlop || yDiff > touchSlop) {
+                        mMessageRouter.cancelMessages(MSG_LONG_PRESS_BRIGHTNESS_CHANGE);
+                    }
+                }
+            } else {
+                if (y > mQuickQsOffsetHeight) {
+                    mJustPeeked = false;
+                }
+                mMessageRouter.cancelMessages(MSG_LONG_PRESS_BRIGHTNESS_CHANGE);
+            }
+        } else if (action == MotionEvent.ACTION_UP
+                || action == MotionEvent.ACTION_CANCEL) {
+            mMessageRouter.cancelMessages(MSG_LONG_PRESS_BRIGHTNESS_CHANGE);
+        }
+    }
+
+    void onLongPressBrightnessChange() {
+        mStatusBarView.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS);
+        adjustBrightness(mInitialTouchX);
+        mLinger = BRIGHTNESS_CONTROL_LINGER_THRESHOLD + 1;
+    }
+
     /** Called when a touch event occurred on {@link PhoneStatusBarView}. */
     @Override
     public void onTouchEvent(MotionEvent event) {
@@ -2202,11 +2330,25 @@ public class CentralSurfacesImpl extends CoreStartable implements
             mGestureRec.add(event);
         }
 
+        if (mBrightnessControl) {
+            brightnessControl(event);
+            if ((mDisabled1 & StatusBarManager.DISABLE_EXPAND) != 0) {
+                return;
+            }
+        }
+
+        final boolean upOrCancel =
+                event.getAction() == MotionEvent.ACTION_UP ||
+                event.getAction() == MotionEvent.ACTION_CANCEL;
+
         if (mStatusBarWindowState == WINDOW_STATE_SHOWING) {
-            final boolean upOrCancel =
-                    event.getAction() == MotionEvent.ACTION_UP ||
-                    event.getAction() == MotionEvent.ACTION_CANCEL;
             setInteracting(StatusBarManager.WINDOW_STATUS_BAR, !upOrCancel || mExpandedVisible);
+        }
+        if (mBrightnessChanged && upOrCancel) {
+            mBrightnessChanged = false;
+            if (mJustPeeked && mExpandedVisible) {
+                mNotificationPanelViewController.fling(10, false);
+            }
         }
     }
 
