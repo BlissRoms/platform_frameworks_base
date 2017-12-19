0f6484cf755328b84c267d98c09c1f469d5293eb
ezio84 <brabus84@gmail.com>
2017-12-19 14:37:35 +0100

    SystemUI: Append app name to screenshot filename
    Author: ezio84 <brabus84@gmail.com>
    Date:   Tue Dec 19 14:37:35 2017 +0100
    
        Screenshot: append app name to filename
    
        Kang Samsung's idea.
    
        Change-Id: Ibc6a52b5e2597e6014a2da6a4211febe17ec02c7
    
    Author: Wang Han <wanghan1995315@gmail.com>
    Date:   Fri May 11 12:47:40 2018 +0200
    
        SystemUI: Don't append app name to file on lockscreen
    
        When device is locked, current implementation will append the current
        app name to file when taking screenshot. This causes information leaks.
    
        To prevent this, we check for isKeyGuardLocked() and fall back not to
        append the app name when taking screenshot on lockscreen.
    
        Change-Id: I04498faf51986e1a3723a32befd97d29e1f3fe58
    
    Author: DroidFreak32 <rushabshah32@gmail.com>
    Date:   Thu Oct 4 11:33:07 2018 +0530
    
        Screenshot: Append app name after screenshot date.
    
        Change-Id: I2ee5d65006ff22dfa381aae50e5757b8e7596e91
    
    Author: ezio84 <brabus84@gmail.com>
    Date:   Sat Jun 16 16:57:19 2018 +0200
    
        GlobalScreenshot: Fix screenshot not saved with some languages
    
        like Virgin Islands English when taking a screenshot of the Settings app
    
        Change-Id: Ic04f66f5813b9597c96835d15c74509c93530a5c
    
    Author: Han Wang <416810799@qq.com>
    Date:   Mon May 25 11:18:01 2020 +0200
    
        SystemUI: Do not convert application name to ISO-8859-15
    
         * ISO-8859-15 does not handle CJK chars, so all of them are
           'unknown' and empty eventually.
    
        Change-Id: I27c689f5d737190d381146e49c1989e998b4f623
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Mon Aug 24 17:52:39 2020 +0200
    
        SystemUI: Use TaskStackChangeListener API for contextual screenshot package name
    
        * This should hopefully fix mislabeled screenshots.
    
        Change-Id: Ifde106c0e306a6700081e4d724c1cf0c06dd126c
    
    Author: OnlyTomInSecond <q2781273965@gmail.com>
    Date:   Thu Dec 15 01:13:58 2022 +0800
    
        Screenshot: Unregister task listener on destroy
    
        This prevents submitting tasks after the threadloop has been shutdown.
    
        Change-Id: Id899ddc9476d045c78c4e85bfac7e7a9b29cee6d
    
    [ghostrider-reborn]
    - Remove spaces from app name in filename
    - Avoid NPE while getting app label
    - Adapt to qpr2
    
    Change-Id: Ic2a42536bf763a0c6f7fb9ac4bbbe84f73723f91
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
@@ -39,6 +39,7 @@ import android.annotation.NonNull;
 import android.annotation.Nullable;
 import android.app.ActivityManager;
 import android.app.ActivityOptions;
+import android.app.ActivityTaskManager;
 import android.app.ExitTransitionCoordinator;
 import android.app.ExitTransitionCoordinator.ExitTransitionCallbacks;
 import android.app.ICompatCameraControlCallback;
@@ -50,6 +51,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.Insets;
@@ -103,6 +105,9 @@ import com.android.systemui.flags.Flags;
 import com.android.systemui.screenshot.ScreenshotController.SavedImageData.ActionTransition;
 import com.android.systemui.screenshot.TakeScreenshotService.RequestCallback;
 import com.android.systemui.settings.DisplayTracker;
+import com.android.systemui.shared.system.ActivityManagerWrapper;
+import com.android.systemui.shared.system.TaskStackChangeListener;
+import com.android.systemui.shared.system.TaskStackChangeListeners;
 import com.android.systemui.util.Assert;
 
 import com.google.common.util.concurrent.ListenableFuture;
@@ -313,6 +318,38 @@ public class ScreenshotController {
                     | ActivityInfo.CONFIG_SCREEN_LAYOUT
                     | ActivityInfo.CONFIG_ASSETS_PATHS);
 
+    private ComponentName mTaskComponentName;
+    private PackageManager mPm;
+
+    private final TaskStackChangeListener mTaskListener = new TaskStackChangeListener() {
+        @Override
+        public void onTaskStackChanged() {
+            mBgExecutor.execute(() -> {
+                try {
+                    final ActivityTaskManager.RootTaskInfo focusedStack =
+                            ActivityTaskManager.getService().getFocusedRootTaskInfo();
+                    if (focusedStack != null && focusedStack.topActivity != null) {
+                        mTaskComponentName = focusedStack.topActivity;
+                    }
+                } catch (Exception e) {
+                    // do nothing
+                }
+            });
+        }
+    };
+
+    private String getForegroundAppLabel() {
+        if (mTaskComponentName != null) {
+            try {
+                final ActivityInfo ai = mPm.getActivityInfo(mTaskComponentName, 0);
+                return ai.applicationInfo.loadLabel(mPm).toString();
+            } catch (PackageManager.NameNotFoundException e) {
+                // do nothing
+            }
+        }
+        return null;
+    }
+
     @Inject
     ScreenshotController(
             Context context,
@@ -396,6 +433,15 @@ public class ScreenshotController {
         mContext.registerReceiver(mCopyBroadcastReceiver, new IntentFilter(
                         ClipboardOverlayController.COPY_OVERLAY_ACTION),
                 ClipboardOverlayController.SELF_PERMISSION, null, Context.RECEIVER_NOT_EXPORTED);
+
+        // Grab PackageManager
+        mPm = mContext.getPackageManager();
+
+        // Register task stack listener
+        TaskStackChangeListeners.getInstance().registerTaskStackListener(mTaskListener);
+
+        // Initialize current foreground package name
+        mTaskListener.onTaskStackChanged();
     }
 
     void handleScreenshot(ScreenshotData screenshot, Consumer<Uri> finisher,
@@ -594,6 +640,7 @@ public class ScreenshotController {
         removeWindow();
         releaseMediaPlayer();
         releaseContext();
+        TaskStackChangeListeners.getInstance().unregisterTaskStackListener(mTaskListener);
         mBgExecutor.shutdownNow();
     }
 
@@ -950,6 +997,7 @@ public class ScreenshotController {
                         mContext.sendBroadcast(new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS));
                     }
             );
+            mLongScreenshotHolder.setForegroundAppName(getForegroundAppLabel());
 
             final Intent intent = new Intent(mContext, LongScreenshotActivity.class);
             intent.putExtra(LongScreenshotActivity.EXTRA_SCREENSHOT_USER_HANDLE,
@@ -1168,7 +1216,7 @@ public class ScreenshotController {
         mSaveInBgTask = new SaveImageInBackgroundTask(mContext, mFlags, mImageExporter,
                 mScreenshotSmartActions, data, getActionTransitionSupplier(),
                 mScreenshotNotificationSmartActionsProvider);
-        mSaveInBgTask.execute();
+        mSaveInBgTask.execute(getForegroundAppLabel());
     }
 
 
