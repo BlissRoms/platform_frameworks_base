074836c014e4b43d5ec1fa4677cfa66954046742
xyyx <xyyx@mail.ru>
2018-01-09 19:36:56 +0100

    Unlock keystore with fingerprint after reboot (1/2)
    Signed-off-by: ZeNiXxX <zenixxx.havoc@gmail.com>
    Change-Id: I368a624ec17194980d8a2848d7b894f9fd233d4f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4833,6 +4833,12 @@ public final class Settings {
         public static final String AMBIENT_BATTERY_PERCENT = "ambient_battery_percent";
 
         /**
+         * Unlock keystore with fingerprint after reboot
+         * @hide
+         */
+        public static final String FP_UNLOCK_KEYSTORE = "fp_unlock_keystore";
+
+         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
