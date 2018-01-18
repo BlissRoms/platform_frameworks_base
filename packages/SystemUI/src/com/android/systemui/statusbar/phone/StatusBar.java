1d48decfc9b400793f329140226c8caf65c12fae
Joe Maples <joe@frap129.org>
2018-01-18 17:31:01 +0100

    Port ambient Now Playing container from Pixel [stripped down version]
    ezio84: this is a very stripped down part of code from the original commit by Joe:
    "SystemUI: Port Now Playing feature from Pixel [1/2]"
    https://review.carbonrom.org/#/c/4950/
    adapted to be called from StatusBar class for our local Ambient Display
    music ticker.
    Removed everything we don't need, services and listeners, and reparsed
    everything to make it clean.
    
    Change-Id: If1c2cf233d72ed8b31bc44f9852ba7b5c1181697

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -195,6 +195,7 @@ import com.android.systemui.SystemUI;
 import com.android.systemui.SystemUIFactory;
 import com.android.systemui.UiOffloadThread;
 import com.android.systemui.appcirclesidebar.AppCircleSidebar;
+import com.android.systemui.ambientmusic.AmbientIndicationContainer;
 import com.android.systemui.assist.AssistManager;
 import com.android.systemui.charging.WirelessChargingAnimation;
 import com.android.systemui.classifier.FalsingLog;
@@ -1238,6 +1239,9 @@ public class StatusBar extends SystemUI implements DemoMode,
 
         mAmbientIndicationContainer = mStatusBarWindow.findViewById(
                 R.id.ambient_indication_container);
+        if (mAmbientIndicationContainer != null) {
+            ((AmbientIndicationContainer) mAmbientIndicationContainer).initializeView(this);
+        }
 
         // set the initial view visibility
         setAreThereNotifications();
