6fb5d76376f1e4d55f320353f4fcd0307ceb290e
10140272 <liu.yaming@zte.com.cn>
2018-02-09 11:34:24 +0800

    core: Improve AtomicFile implementation
    Fixed issue that system.xml is lost caused by
    AtomicFille in shut-down phase with full storage.
    
    Change-Id: I5ff69e0c63aa410fcb2ff69f99b0cb08d8a66d35
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/util/AtomicFile.java
+++ core/java/android/util/AtomicFile.java
@@ -149,10 +149,18 @@ public class AtomicFile {
             FileUtils.sync(str);
             try {
                 str.close();
-                mBaseName.delete();
-                mBackupName.renameTo(mBaseName);
+                if (mBackupName.exists()) {
+                    mBaseName.delete();
+                    mBackupName.renameTo(mBaseName);
+                }
             } catch (IOException e) {
                 Log.w("AtomicFile", "failWrite: Got exception:", e);
+            }
+        } else {
+            if (!mBaseName.exists()) {
+                if (mBackupName.exists()) {
+                    mBackupName.renameTo(mBaseName);
+                }
             }
         }
     }
