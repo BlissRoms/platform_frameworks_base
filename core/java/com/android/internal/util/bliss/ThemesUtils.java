cf95b06ef91a4a0a6a9e57232b44c03699fed2ad
bigrushdog <randall.rushing@gmail.com>
2018-02-21 16:16:21 -0800

    Introduce Theme tile
    Click to show detail view.
    
    Extract & Aapted for Xtended from -
    
    https://github.com/DirtyUnicorns/android_frameworks_base/commit/205301221b06136b981f13304d9c35a07f3f2c0b#diff-a2f945e2d58b7e8c989d272123c63f22
    
    Change-Id: Ifa1e1cb6f92afbfa2fd2839b1c7586a06be5c339
    Signed-off-by: rohan <purohit.rohan@gmail.com>

--- core/java/com/android/internal/util/bliss/ThemesUtils.java
+++ core/java/com/android/internal/util/bliss/ThemesUtils.java
@@ -27,10 +27,5 @@ public class ThemesUtils {
             "com.android.theme.pitchblack.system",
             "com.android.theme.pitchblack.systemui",
     };
-
-    public static final String[] DARK_GREY = {
-            "com.android.theme.darkgrey.system",
-            "com.android.theme.darkgrey.systemui",
-    };
 }
 
