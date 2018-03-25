a1e0d52533ec81634584648dd6957e77d67ff8c3
maxwen <max.weninger@gmail.com>
2018-03-26 01:02:25 +0200

    Implement animations for back edge gestures
    Change-Id: I49765840ecd02ea5decc3ce602ff2778ee99f117
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -9444,6 +9444,18 @@ public final class Settings {
         public static final String EDGE_GESTURES_BACK_SCREEN_PERCENT = "edge_gestures_back_screen_percent";
 
         /**
+         * Show UI feedback when using back gesture.
+         * @hide
+         */
+        public static final String EDGE_GESTURES_BACK_SHOW_UI_FEEDBACK = "edge_gestures_back_show_ui_feedback";
+
+        /**
+         * Use black arrow theme instead of the white version.
+         * @hide
+         */
+        public static final String EDGE_GESTURES_BACK_USE_BLACK_ARROW = "edge_gestures_back_use_black_arrow";
+
+        /**
          * Whether to show PIE controls on the screen.
          * 0 = OFF
          * 1 = ON
