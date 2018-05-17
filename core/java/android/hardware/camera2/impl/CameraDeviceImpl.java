80a19fd192fe61e601419d0ac8b99e7dbd2c5783
Sauhard Pande <sauhardp@codeaurora.org>
2018-05-17 15:29:06 +0530

    Camera2: Notify fps as Session Based Parameter
    Provided interface to enable session based parameter
    to select sensor mode.
    
    CRs-Fixed: 2152363
    
    Change-Id: I214e51f1dff40dfa2be240e61fc99661ea0fa58a
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- core/java/android/hardware/camera2/impl/CameraDeviceImpl.java
+++ core/java/android/hardware/camera2/impl/CameraDeviceImpl.java
@@ -124,6 +124,8 @@ public class CameraDeviceImpl extends CameraDevice
     @EnabledSince(targetSdkVersion = Build.VERSION_CODES.VANILLA_ICE_CREAM)
     static final long CHECK_PARAMS_IN_IS_SESSION_CONFIGURATION_SUPPORTED = 320741775;
 
+    private int customOpMode = 0;
+
     // TODO: guard every function with if (!mRemoteDevice) check (if it was closed)
     private ICameraDeviceUserWrapper mRemoteDevice;
     private boolean mRemoteDeviceInit = false;
@@ -564,6 +566,10 @@ public class CameraDeviceImpl extends CameraDevice
         }
     }
 
+    public void setVendorStreamConfigMode(int fpsrange) {
+        customOpMode = fpsrange;
+    }
+
     @Override
     public String getId() {
         return mCameraId;
@@ -690,6 +696,7 @@ public class CameraDeviceImpl extends CameraDevice
                         mConfiguredOutputs.put(streamId, outConfig);
                     }
                 }
+                operatingMode = (operatingMode | (customOpMode << 16));
 
                 int offlineStreamIds[];
                 if (sessionParams != null) {
