9aa5828c099e097c138cc1349b2bce8c73d9dea8
shivatejapeddi <shivatejapeddi@gmail.com>
2018-06-29 17:54:25 +0800

    Add VOLTE icon
    Imported and adapted to AOSP from CAF
    
    Original Commit :
    https://source.codeaurora.org/quic/la/platform/frameworks/base/commit/?h=LA.UM.7.8.r1-03600-SDM710.0&id=970eadeaa24c684781da173f3f54f2cb8b441a65
    
    Change-Id: If591e486c196ef159e911cde14fd782ada97f1f7
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/SignalClusterView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/SignalClusterView.java
@@ -313,7 +313,7 @@ public class SignalClusterView extends LinearLayout implements NetworkController
 
     @Override
     public void setMobileDataIndicators(IconState statusIcon, IconState qsIcon, int statusType,
-            int qsType, boolean activityIn, boolean activityOut, String typeContentDescription,
+            int qsType, boolean activityIn, boolean activityOut, int volteId, String typeContentDescription,
             String description, boolean isWide, int subId, boolean roaming) {
         PhoneState state = getState(subId);
         if (state == null) {
@@ -327,6 +327,7 @@ public class SignalClusterView extends LinearLayout implements NetworkController
         state.mRoaming = roaming && !mBlockRoaming;
         state.mActivityIn = activityIn && mActivityEnabled;
         state.mActivityOut = activityOut && mActivityEnabled;
+        state.mvolteId = volteId;
 
         apply();
     }
@@ -639,7 +640,8 @@ public class SignalClusterView extends LinearLayout implements NetworkController
         public boolean mActivityIn;
         public boolean mActivityOut;
         private SignalDrawable mMobileSignalDrawable;
-
+        private int mvolteId = 0;
+        private ImageView mvolte;
         public PhoneState(int subId, Context context) {
             ViewGroup root = (ViewGroup) LayoutInflater.from(context)
                     .inflate(R.layout.mobile_signal_group, null);
@@ -657,6 +659,7 @@ public class SignalClusterView extends LinearLayout implements NetworkController
             mMobileActivityOut = root.findViewById(R.id.mobile_out);
             mMobileSignalDrawable = new SignalDrawable(mMobile.getContext());
             mMobile.setImageDrawable(mMobileSignalDrawable);
+            mvolte = (ImageView) root.findViewById(R.id.mobile_volte);
         }
 
         public boolean apply(boolean isSecondaryIcon) {
--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkControllerImpl.java
@@ -1029,6 +1029,7 @@ public class NetworkControllerImpl extends BroadcastReceiver
         boolean hspaDataDistinguishable;
         boolean inflateSignalStrengths = false;
         boolean alwaysShowDataRatIcon = false;
+        boolean showVolteIcon;
 
         static Config readConfig(Context context) {
             Config config = new Config();
@@ -1049,6 +1050,7 @@ public class NetworkControllerImpl extends BroadcastReceiver
                 config.alwaysShowDataRatIcon = b.getBoolean(
                         CarrierConfigManager.KEY_ALWAYS_SHOW_DATA_RAT_ICON_BOOL);
             }
+            config.showVolteIcon = res.getBoolean(R.bool.config_display_volte);
             return config;
         }
     }
