1a0b1994ce7950cd1cbe814469bd3c01aa7eacfe
Michael Bestas <mkbestas@lineageos.org>
2018-07-15 22:54:36 +0300

    SystemUI: Add tunables for clock AM/PM style
    Change-Id: I7f27f135ae0d1e731c936792f655c115f107d20c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12307,6 +12307,16 @@ public final class Settings {
         public static final String STATUS_BAR_CLOCK = "status_bar_clock";
 
         /**
+         * Display style of AM/PM next to clock in status bar
+         * 0: No display (Gingerbread/ICS stock)
+         * 1: Small display (Froyo stock)
+         * 2: Normal display (Eclair stock)
+         * default: 0
+         * @hide
+         */
+        public static final String STATUS_BAR_AM_PM = "status_bar_am_pm";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
