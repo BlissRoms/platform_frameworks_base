acf09ae2488238077a2e69cbd4529703054fdf43
maxwen <max.weninger@gmail.com>
2018-08-30 21:25:55 +0200

    base: SystemUI: qs quickbar columns config
    Change-Id: I11b85e6ad2a3c46fb401ec9bccd18718192e8d76
    
    base: SystemUI: add qs quick tile column auto to follow columns
    
    always use same columns as the expanded panel but
    never less then 6
    
    idoybh edits: Adapted to A11
    
    Change-Id: I13f5f871cc1362d1fb8c60ba07aeff9675f1c32a

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -175,6 +175,7 @@ import com.android.systemui.plugins.statusbar.NotificationSwipeActionHelper.Snoo
 import com.android.systemui.plugins.statusbar.StatusBarStateController;
 import com.android.systemui.qs.QSFragment;
 import com.android.systemui.qs.QSPanel;
+import com.android.systemui.qs.QuickQSPanel;
 import com.android.systemui.recents.Recents;
 import com.android.systemui.recents.ScreenPinningRequest;
 import com.android.systemui.shared.plugins.PluginManager;
@@ -408,6 +409,7 @@ public class StatusBar extends SystemUI implements DemoMode,
 
     // settings
     private QSPanel mQSPanel;
+    private QuickQSPanel mQuickQSPanel;
 
     KeyguardIndicationController mKeyguardIndicationController;
 
@@ -1233,6 +1235,7 @@ public class StatusBar extends SystemUI implements DemoMode,
                 if (qs instanceof QSFragment) {
                     mQSPanel = ((QSFragment) qs).getQsPanel();
                     mQSPanel.setBrightnessMirror(mBrightnessMirrorController);
+                    mQuickQSPanel = ((QSFragment) qs).getQuickQsPanel();
                 }
             });
         }
@@ -3072,6 +3075,9 @@ public class StatusBar extends SystemUI implements DemoMode,
         }
         if (mBrightnessMirrorController != null) {
             mBrightnessMirrorController.updateResources();
+        }
+        if (mQuickQSPanel != null) {
+            mQuickQSPanel.updateResources();
         }
     }
 
