ad80a512a2da215d958f46787685d00dbdac9491
maxwen <max.weninger@gmail.com>
2018-09-14 17:35:55 -0500

    Use Bliss fingerprint for ShortcutService
    make sure cached data gets updated
    no more derped deep shortcuts text and images
    
    Change-Id: I65d743a4991f92739e59b4655dcc086bd36a5401

--- services/core/java/com/android/server/pm/ShortcutService.java
+++ services/core/java/com/android/server/pm/ShortcutService.java
@@ -4302,7 +4302,7 @@ public class ShortcutService extends IShortcutService.Stub {
 
     // Injection point.
     String injectBuildFingerprint() {
-        return Build.FINGERPRINT;
+        return Build.BLISS_FINGERPRINT;
     }
 
     final void wtf(String message) {
