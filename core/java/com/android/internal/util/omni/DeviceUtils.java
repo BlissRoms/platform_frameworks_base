fcdfbb56fcd03704dfa21ed2b2098c4c89313a8f
maxwen <max.weninger@gmail.com>
2018-09-24 14:19:47 -0600

    DeviceUtils: Add isAvailableApp Method
    Change-Id: Ife3404da9ef4df3e537257c7e15f9c79e163aab5

--- core/java/com/android/internal/util/omni/DeviceUtils.java
+++ core/java/com/android/internal/util/omni/DeviceUtils.java
@@ -19,6 +19,7 @@ import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.hardware.camera2.CameraAccessException;
@@ -166,5 +167,18 @@ public class DeviceUtils {
     public static boolean isChineseLanguage() {
        return Resources.getSystem().getConfiguration().locale.getLanguage().startsWith(
                Locale.CHINESE.getLanguage());
+    }
+
+    public static boolean isAvailableApp(String packageName, Context context) {
+       Context mContext = context;
+       final PackageManager pm = mContext.getPackageManager();
+       try {
+           pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+           int enabled = pm.getApplicationEnabledSetting(packageName);
+           return enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED &&
+               enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED_USER;
+       } catch (NameNotFoundException e) {
+           return false;
+       }
     }
 }
