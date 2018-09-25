3ffa2eaadec50e99c6f78c5a50d749d9ed6b6251
Alex Cruz <alex@dirtyunicorns.com>
2018-09-24 19:21:59 -0500

    Fruity Pebbles
    In the commit below we added the ability to change between Dark/Light and wallpaper
    based themes.
    
    d5de41e
    
    Since then we've extended this to Settings, DU-Tweaks, Changelog and a few other
    apps in system. We've added more styles and have gone all in on this theme thing.
    
    One thing that was missing was the ability to change the system accent. While the
    pixel accent was good enough, many users had asked us to add in the ability to
    change the accents because they wanted something different. Several ROMs have gone
    about this same thing in their own way but we didn't. We wanted to use what we already
    had and just add on to it.
    
    This commit mirrors what maxwen did but with the accent, so thanks to him for that!
    
    Change-Id: I5bc383d749d34ba10c0cd96153d17e706b9dac3c
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -892,6 +892,9 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.SYSTEM_THEME),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.ACCENT_PICKER),
+                    false, this, UserHandle.USER_ALL);
             update();
         }
 
@@ -935,6 +938,10 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
             } else if (uri.equals(Settings.System.getUriFor(
                     Settings.System.SYSTEM_THEME))) {
                 updateTheme();
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.ACCENT_PICKER))) {
+                unloadAccents(); // Unload the accents when users request it
+                updateAccents(); // Update the accents when users request it
             }
 	    update();
         }
@@ -2694,12 +2701,19 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
         updateTheme();
     }
 
+    // Check for the dark system theme
     public boolean isUsingDarkTheme() {
         return ThemeAccentUtils.isUsingDarkTheme(mOverlayManager, mLockscreenUserManager.getCurrentUserId());
     }
 
+    // Unloads the stock dark theme
     public void unloadStockDarkTheme() {
         ThemeAccentUtils.unloadStockDarkTheme(mOverlayManager, mLockscreenUserManager.getCurrentUserId());
+    }
+
+    // Check for black and white accent overlays
+    public void unfuckBlackWhiteAccent() {
+        ThemeAccentUtils.unfuckBlackWhiteAccent(mOverlayManager, mLockscreenUserManager.getCurrentUserId());
     }
 
     @Nullable
@@ -4916,6 +4930,7 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
             useDarkTheme = userThemeSetting == 2;
         }
         if (isUsingDarkTheme() != useDarkTheme) {
+            unfuckBlackWhiteAccent(); // Check for black and white accent
             ThemeAccentUtils.setLightDarkTheme(mOverlayManager, mLockscreenUserManager.getCurrentUserId(), useDarkTheme);
         }
 
@@ -4945,6 +4960,18 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
             // Make sure we have the correct navbar/statusbar colors.
             mStatusBarWindowManager.setKeyguardDark(useDarkText);
         }
+    }
+
+    // Switches theme accent from to another or back to stock
+    public void updateAccents() {
+        int accentSetting = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.ACCENT_PICKER, 0, mLockscreenUserManager.getCurrentUserId());
+        ThemeAccentUtils.updateAccents(mOverlayManager, mLockscreenUserManager.getCurrentUserId(), accentSetting);
+    }
+
+    // Unload all the theme accents
+    public void unloadAccents() {
+        ThemeAccentUtils.unloadAccents(mOverlayManager, mLockscreenUserManager.getCurrentUserId());
     }
 
     private void updateDozingState() {
