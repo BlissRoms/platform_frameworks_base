d2cf1d722d9bf838f5cb34af5a422e1a7b9e54fa
Lars Greiss <kufikugel@googlemail.com>
2018-10-05 08:33:09 -0600

    Return: Smart Pulldown [1/2]
    Rewritten by @beanstown106 for 6.0/7.0
    
    previously the detection for ongoing/persistent notifications were not working
    im not sure how long this has actually been broken but i cleaned things up a little bit
    also put to use the isOngoing boolean and properly put to use the hasActiveVisibleNotifications boolean
    
    current options now are off, no dismissable, no ongoing, and no notifications
    
    Change-Id: Ia621fbc22ff784389f4384a36f36195354f37d82
    Credits: kufikugel, HardCorePawn, acardinal

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4583,6 +4583,12 @@ public final class Settings {
         public static final String STATUS_BAR_QUICK_QS_PULLDOWN = "status_bar_quick_qs_pulldown";
 
         /**
+         * Quick Settings Smart Pulldown
+         * @hide
+         */
+        public static final String QS_SMART_PULLDOWN = "qs_smart_pulldown";
+
+        /**
          *  Weather to use a custom color for the visualizer on the lock screen
          * @hide
          */
