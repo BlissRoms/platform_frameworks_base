2f31a19fd21601e2c2c1a1c2a152b8cb139b7f71
maxwen <max.weninger@gmail.com>
2018-10-06 12:37:43 +0200

    base: SystemUI: add tuner to hide qs drag handle
    Change-Id: Ibd88e1ab726abf6d397afcdf434173b9c1521a83
    Signed-off-by: rohan <purohit.rohan@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -166,6 +166,7 @@ public class QuickStatusBarHeader extends RelativeLayout implements
             "system:" + Settings.System.STATUS_BAR_BATTERY_STYLE;
     public static final String OMNI_STATUS_BAR_CUSTOM_HEADER =
             "system:" + Settings.System.OMNI_STATUS_BAR_CUSTOM_HEADER;
+    private boolean mHideDragHandle;
 
     private final BroadcastReceiver mRingerReceiver = new BroadcastReceiver() {
         @Override
@@ -441,6 +442,11 @@ public class QuickStatusBarHeader extends RelativeLayout implements
             // always add the margin below the statusbar with or without image
             qsHeight += statusBarBottomMargin;
 
+            if (mHideDragHandle) {
+                qsHeight -= resources.getDimensionPixelSize(
+                        R.dimen.quick_qs_drag_handle_height);
+            }
+
             lp.height = Math.max(getMinimumHeight(), qsHeight);
         }
 
@@ -528,6 +534,9 @@ public class QuickStatusBarHeader extends RelativeLayout implements
         super.onAttachedToWindow();
         mStatusBarIconController.addIconGroup(mIconManager);
         requestApplyInsets();
+
+        final TunerService tunerService = Dependency.get(TunerService.class);
+        tunerService.addTunable(this, QSFooterImpl.QS_SHOW_DRAG_HANDLE);
     }
 
     @Override
@@ -745,6 +754,11 @@ public class QuickStatusBarHeader extends RelativeLayout implements
                 mHeaderImageEnabled =
                         TunerService.parseIntegerSwitch(newValue, true);
                 updateStatusbarProperties();
+                break;
+            case QSFooterImpl.QS_SHOW_DRAG_HANDLE:
+                mHideDragHandle =
+                        TunerService.parseIntegerSwitch(newValue, 0);
+                updateResources();
                 break;
             default:
                 break;
