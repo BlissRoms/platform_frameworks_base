a66d366f87620f6197fe88c4edd2a26f2f0992d5
Alex Cruz <alex@dirtyunicorns.com>
2018-10-14 01:10:32 -0500

    Allow devices to configure the blanking delay on displays
    This will help resolve an issue where AOD flickers repeatedly on the
    Marlin and other devices that don't play nice with the delay.
    
    By default is set to the 1000 milliseconds but if you need to overlay it
    just add an overlay for <integer name="config_screen_off_delay"> under
    frameworks/base/core/res/res/values/config.xml and you're good to go.
    
    Change-Id: I17cb459f9969d50a5544969480c47e458881b4d6
    Signed-off-by: Mrinal Kant Shukla <kamikaze.mrinal@gmail.com>
    Signed-off-by: rohan <purohit.rohan@gmail.com>

--- services/core/java/com/android/server/display/DisplayPowerController.java
+++ services/core/java/com/android/server/display/DisplayPowerController.java
@@ -109,8 +109,6 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
     private static final int COLOR_FADE_ON_ANIMATION_DURATION_MILLIS = 250;
     private static final int COLOR_FADE_OFF_ANIMATION_DURATION_MILLIS = 400;
 
-    private static final int SCREEN_OFF_DELAY_MILLIS = 1000;
-
     private static final int MSG_UPDATE_POWER_STATE = 1;
     private static final int MSG_PROXIMITY_SENSOR_DEBOUNCED = 2;
     private static final int MSG_SCREEN_ON_UNBLOCKED = 3;
@@ -174,6 +172,9 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
 
     // The proximity sensor, or null if not available or needed.
     private Sensor mProximitySensor;
+
+    // The screen off delay
+    private final int mScreenOffDelayConfig;
 
     // The doze screen brightness.
     private final int mScreenBrightnessDozeConfig;
@@ -422,6 +423,9 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
         final Resources resources = context.getResources();
         final int screenBrightnessSettingMinimum = clampAbsoluteBrightness(resources.getInteger(
                 com.android.internal.R.integer.config_screenBrightnessSettingMinimum));
+
+        mScreenOffDelayConfig = resources.getInteger(
+                com.android.internal.R.integer.config_screen_off_delay);
 
         mScreenBrightnessDozeConfig = clampAbsoluteBrightness(resources.getInteger(
                 com.android.internal.R.integer.config_screenBrightnessDoze));
@@ -1363,7 +1367,7 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
 
     private void setScreenOffDelayed() {
         cancelDelayedScreenOff();
-        mHandler.postDelayed(mScreenOffTask, SCREEN_OFF_DELAY_MILLIS);
+        mHandler.postDelayed(mScreenOffTask, mScreenOffDelayConfig);
     }
 
     private void cancelDelayedScreenOff() {
