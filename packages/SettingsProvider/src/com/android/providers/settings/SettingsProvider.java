cba72b70199b07f198cc4437a5b00a6fc121852f
Ting Yu <ting.x.yu@sony.com>
2018-10-18 20:10:38 +0800

    base: Phone ringtone setting for Multi SIM device [1/3]
    Add below changes to handle default phone ringtone for slot2.
    1. Add Settings.System.RINGTONE2 as the key in SettingsProvider to
    save ringtone for slot2.
    2. Support "ro.config.ringtone" for Multi SIM device.
    3. Adapt ringtone for slot2 in current ringtone related APIs.
    
    Bug: 118735436
    Test: Manual
    Change-Id: I9c6ccff938122332d222853d469ad9a623c7d193
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- packages/SettingsProvider/src/com/android/providers/settings/SettingsProvider.java
+++ packages/SettingsProvider/src/com/android/providers/settings/SettingsProvider.java
@@ -882,6 +882,8 @@ public class SettingsProvider extends ContentProvider {
             // which should be in the form of "content://settings/system/ringtone_cache_xxxx".
             cacheRingtoneSetting = uri.getPathSegments().get(1)
                     .replace(Settings.System.RINGTONE_CACHE, Settings.System.RINGTONE);
+        } else if (Settings.System.RINGTONE2_CACHE_URI.equals(uri)) {
+            cacheRingtoneSetting = Settings.System.RINGTONE2;
         } else if (Settings.System.NOTIFICATION_SOUND_CACHE_URI.equals(uri)) {
             cacheRingtoneSetting = Settings.System.NOTIFICATION_SOUND;
         } else if (Settings.System.ALARM_ALERT_CACHE_URI.equals(uri)) {
@@ -904,6 +906,8 @@ public class SettingsProvider extends ContentProvider {
         } else if (setting.startsWith(Settings.System.RINGTONE
                 + RingtoneManager.RINGTONE_DELIMITER_FOR_PHONE_ACCOUNT_HANDLE)) {
             return setting.replace(Settings.System.RINGTONE, Settings.System.RINGTONE_CACHE);
+        } else if (Settings.System.RINGTONE2.equals(setting)) {
+            return Settings.System.RINGTONE2_CACHE;
         } else if (Settings.System.NOTIFICATION_SOUND.equals(setting)) {
             return Settings.System.NOTIFICATION_SOUND_CACHE;
         } else if (Settings.System.ALARM_ALERT.equals(setting)) {
