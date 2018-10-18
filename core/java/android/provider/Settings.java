30fe8e02d7c25a0272fc4a5614f5cb6c0fd726a2
maxwen <max.weninger@gmail.com>
2018-10-18 13:47:51 -0600

    base: doze settings config [1/2]
    thank you google for changing the DEVICE_IDLE_CONSTANTS from
    under the hood. So save our values in a different setting
    which is used before the system one if available
    
    Change-Id: Ie7f268c70032b03d0c06bc58c54723a57dff7ea2

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13166,6 +13166,11 @@ public final class Settings {
         public static final String OMNI_ADB_PORT = "omni_adb_port";
 
         /**
+         * @hide
+         */
+        public static final String DEVICE_IDLE_CONSTANTS_USER = "device_idle_constants_user";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
