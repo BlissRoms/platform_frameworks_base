ee3c0d9cc8c18a9d10e73ed9d98cd240b09a3e88
SuperDroidBond <superdroidbond@yahoo.com>
2018-10-24 18:10:02 +0530

    Welcome Back Lockscreen Item Colors [1/2]
    * @rohanpurohit :
    - adapt to tuner
    - remove clock changes , add it seperately
    - remove keyguard shortcut changes.
    
    **SDB - Move LockColors to KeyguardClockSwitch
          - Remove Lock Icon color option for Q
          -  Port to O -> P -> Q
    
    Change-Id: I88fff25ea9023a0fc1229a40089c902785017d8b
    Signed-off-by: rohan <purohit.rohan@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6632,6 +6632,38 @@ public final class Settings {
         public static final String PIXEL_NAV_ANIMATION = "pixel_nav_animation";
 
         /**
+         * Change the color of the lockscreen date underneath the clock
+         *
+         * @hide
+         */
+        public static final String LOCKSCREEN_DATE_COLOR = "lockscreen_date_color";
+
+        /**
+         * Change the color of the lockscreen owner's info text
+         *
+         * @hide
+         */
+        public static final String LOCKSCREEN_OWNER_INFO_COLOR = "lockscreen_owner_info_color";
+
+        /**
+         * Color of the weather temperature the lock screen
+         * @hide
+         */
+        public static final String LOCK_SCREEN_WEATHER_TEMP_COLOR = "lock_screen_weather_temp_color";
+
+        /**
+         * Color of the weather city on the lock screen
+         * @hide
+         */
+        public static final String LOCK_SCREEN_WEATHER_CITY_COLOR = "lock_screen_weather_city_color";
+
+        /**
+         * Color of the weather condition icons on the lock screen
+         * @hide
+         */
+        public static final String LOCK_SCREEN_WEATHER_ICON_COLOR = "lock_screen_weather_icon_color";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
