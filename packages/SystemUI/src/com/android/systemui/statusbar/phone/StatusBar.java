f6386d0e73a1ab888199572776c560d81b027f18
SKULSHADY <anushekprasal@gmail.com>
2018-11-04 00:14:00 -0600

    Add Oreo QS tile styles [1/3]
    Change-Id: Id0bc84a651cd4eab7f08b0c5dd15c9538985c264

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -408,7 +408,10 @@ public class StatusBar extends SystemUI implements DemoMode,
         "com.android.systemui.qstile.default", // 0
         "com.android.systemui.qstile.circletrim", // 1
         "com.android.systemui.qstile.dualtonecircletrim", // 2
-        "com.android.systemui.qstile.squircletrim", // 3
+        "com.android.systemui.qstile.oreo", // 3
+        "com.android.systemui.qstile.oreocircletrim", // 4
+        "com.android.systemui.qstile.oreosquircletrim", // 5
+        "com.android.systemui.qstile.squircletrim", // 6
     };
 
     // top bar
