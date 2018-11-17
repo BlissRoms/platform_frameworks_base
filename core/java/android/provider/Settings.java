483b9b985b474783d7c520bc900370e063fccddd
Kshitij Gupta <kshitijgm@gmail.com>
2018-11-17 12:31:58 +0530

    base: notch-city: Add 3 mode display cutout handler [1/3]
    [AgentFabulous - POSP]
    
    Modes:
     - Normal
     - Immerse
     - Hide
    
    Normal:
      You know this one ¯\_(ツ)_/¯
    
    Immerse:
      This change does not force layout parameters globally. Instead, sets
      statusbar colors and changes layout parameters for statusbar. It also
      forces corners to be drawn with LAYOUT_IN_DISPLAY_CUTOUT_MODE_NEVER so
      they are always below the statusbar. This however looks weird in
      landscape, thus the feature disables itself in landscape mode. This
      way, we can black out our cutout while being able to see our status
      icons still and have adjustable virtual corners, while not breaking
      any apps.
    
    Hide:
      We can't really register a content observer for overriding the
      resource bool in our java code (causes SystemUI to FC with a null
      pointer ref), so use overlays here. Works like most other "Hide"
      implementations but isn't categorised by the cutout category to avoid
      accidental disabling of this overlay. This is exclusively managed by
      StatusBar.java. Some people actually like the thicc statusbar, so give
      users a choice for it.
    
    @neobuddy89:
    * This patch also includes subsequent fixes made
    * Use Tuner API
    * Adapt with Hide Black fill-in and PebbleBerry
    * Some code clean up
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I2f1c262f1cf4772a616c38de386c8f6d1e8f69b7

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6492,8 +6492,18 @@ public final class Settings {
         public static final String SBC_HD_PRIORITY = "sbc_hd_priority";
 
         /**
-=======
->>>>>>> parent of 7a01465dc27d... base: notch-city: Add 3 mode display cutout handler
+         * Control how to handle the display cutout
+         * @hide
+         */
+        public static final String DISPLAY_CUTOUT_MODE = "display_cutout_mode";
+
+        /**
+         * Control how to handle the display cutout
+         * @hide
+         */
+        public static final String STOCK_STATUSBAR_IN_HIDE = "stock_statusbar_in_hide";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
