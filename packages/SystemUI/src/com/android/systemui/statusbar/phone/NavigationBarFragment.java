fc1489d12208ef438a6a7fe14ed3f19fa26b019c
SuperDroidBond <superdroidbond@yahoo.com>
2018-12-06 13:30:12 -0700

    Full nav gesture: Poor Hack to Get Everything working
    Change-Id: I2e02bd1b630a68f335057f7e8474f63f0bdea607

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarFragment.java
@@ -142,6 +142,7 @@ Navigator.OnVerticalChangedListener, KeyguardMonitor.Callback, NotificationMedia
     public static final int NAVIGATION_MODE_FLING = 2;
 
     protected Navigator mNavigationBarView = null;
+    protected NavigationBarView mOldNavBarView = null;
 
     protected AssistManager mAssistManager;
 
@@ -1152,8 +1153,8 @@ Navigator.OnVerticalChangedListener, KeyguardMonitor.Callback, NotificationMedia
         public void onChange(boolean selfChange) {
             NavigationBarFragment.this.updateAccessibilityServicesState(mAccessibilityManager);
             NavigationBarFragment.this.setFullGestureMode();
-            if (mNavigationBarView != null) {
-                mNavigationBarView.updateNavButtonIcons();
+            if (mOldNavBarView != null) {
+                mOldNavBarView.updateNavButtonIcons();
             }
         }
     }
@@ -1174,11 +1175,11 @@ Navigator.OnVerticalChangedListener, KeyguardMonitor.Callback, NotificationMedia
             }
         } catch (Settings.SettingNotFoundException e) {
         }
-		//TODO: We need to get this done in sometime or find a way for the same
-        /*mFullGestureMode = mOverviewProxyService.shouldShowSwipeUpUI() && fullModeEnabled;
-        if (mNavigationBarView != null) {
-            Navigator.setFullGestureMode(mFullGestureMode, dt2sEnabled);
-        }*/
+	//TODO: We need to get this done in sometime or find a way for the same
+        mFullGestureMode = mOverviewProxyService.shouldShowSwipeUpUI() && fullModeEnabled;
+        if (mOldNavBarView != null) {
+            mOldNavBarView.setFullGestureMode(mFullGestureMode, dt2sEnabled);
+        }
     }
 
     private final Stub mRotationWatcher = new Stub() {
