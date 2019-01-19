3496690bbde14df6edfe7b105e78cc2281605ffe
Alex Cruz <du.alexcruz@gmail.com>
2019-01-19 17:30:24 -0600

    base: [Utils] Add method to determine battery temp
    Change-Id: Id0b8addc460afae030eac0a2bf064fb21aa2c4dc
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- core/java/com/android/internal/util/bliss/BlissUtils.java
+++ core/java/com/android/internal/util/bliss/BlissUtils.java
@@ -21,6 +21,7 @@ import android.app.ActivityManager;
 import android.app.IActivityManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.res.Resources;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
@@ -37,6 +38,7 @@ import android.net.Network;
 import android.net.NetworkCapabilities;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
+import android.os.BatteryManager;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.RemoteException;
@@ -87,6 +89,19 @@ public class BlissUtils {
             }
             return null;
         }
+    }
+
+    public static String batteryTemperature(Context context, Boolean ForC) {
+        Intent intent = context.registerReceiver(null, new IntentFilter(
+                Intent.ACTION_BATTERY_CHANGED));
+        float  temp = ((float) (intent != null ? intent.getIntExtra(
+                BatteryManager.EXTRA_TEMPERATURE, 0) : 0)) / 10;
+        // Round up to nearest number
+        int c = (int) ((temp) + 0.5f);
+        float n = temp + 0.5f;
+        // Use boolean to determine celsius or fahrenheit
+        return String.valueOf((n - c) % 2 == 0 ? (int) temp :
+                ForC ? c * 9/5 + 32 + "°F" :c + "°C");
     }
 
     public static boolean isChineseLanguage() {
