96f75d80a6343e554494404ed151e095e401d732
Anushek Prasal <anushekprasal@gmail.com>
2019-02-04 19:59:50 +0530

    base: QS header improvements
    - Fix the gradient behind the qs
    - Fix the qs panel layout
    - Detach the header image from the top
    - Set the bottom margin only when custom header enabled
    
    Change-Id: I050fda51ad56b92b7f0cae498693d90af8d04430

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -424,9 +424,6 @@ public class QuickStatusBarHeader extends RelativeLayout implements
                 com.android.internal.R.dimen.quick_qs_offset_height) + (mHeaderImageEnabled ?
                 resources.getDimensionPixelSize(R.dimen.qs_header_image_offset) : 0);
 
-        int statusBarBottomMargin = resources.getDimensionPixelSize(
-                R.dimen.qs_header_image_bottom_margin);
-
         mSystemIconsView.getLayoutParams().height = topMargin;
         mSystemIconsView.setLayoutParams(mSystemIconsView.getLayoutParams());
 
@@ -442,8 +439,6 @@ public class QuickStatusBarHeader extends RelativeLayout implements
                 qsHeight += resources.getDimensionPixelSize(R.dimen.qs_header_image_offset);
             }
 
-            // always add the margin below the statusbar with or without image
-            qsHeight += statusBarBottomMargin;
 
             if (mHideDragHandle) {
                 qsHeight -= resources.getDimensionPixelSize(
