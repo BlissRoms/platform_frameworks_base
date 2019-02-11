303cd427f8ac4e2d86f00287e5a83e4261af0b0c
Dil3mm4 <dil3mm4.dev@gmail.com>
2019-02-11 16:48:44 +0000

    More infos uploaded to Memochō
    Change-Id: I106ccf31f27938067e8e8e1f83995fb771a4d9bd

--- services/core/java/com/android/server/am/AppErrors.java
+++ services/core/java/com/android/server/am/AppErrors.java
@@ -46,6 +46,7 @@ import android.os.Bundle;
 import android.os.Message;
 import android.os.Process;
 import android.os.SystemClock;
+import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.provider.Settings;
 import android.util.ArrayMap;
@@ -643,10 +644,14 @@ class AppErrors {
             }
 
             // Add paste content for Memochō option
+            String devfp = SystemProperties.get("ro.vendor.build.fingerprint", "");
+            String blissVers = SystemProperties.get("ro.bliss.version", "");
             data.paste = "time: " + timeMillis + "\n" +
-                    "msg: " + longMsg + "\n" +
-                    "stacktrace: " + stackTrace;
-
+            "device fp:" + devfp + "\n" +
+            "bliss vers:" + blissVers + "\n" +
+            "msg: " + longMsg + "\n" +
+            "stacktrace: " + stackTrace;
+          
             final Message msg = Message.obtain();
             msg.what = ActivityManagerService.SHOW_ERROR_UI_MSG;
 
