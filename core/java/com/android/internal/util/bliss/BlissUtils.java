dd4bc7ef61116b466a25b1496daeba3691c54ba5
Alex <du.alexcruz@gmail.com>
2019-02-14 22:20:59 -0600

    BlissUtils: Add screen on action
    Add an action to force the screen to turn on. This is a quick
    and effective way to turn on the screen when we need it to. Intead
    of hijacking WindowManagerPolicy in PWM, we instead call on PM to
    do this.
    
    - Make sure 'screen off' action doesn't work while screen is off.
    This action should not be called if the screen is already off.
    
    Change-Id: Icacb721ebc6493e4bd0db9d02ac10d66f22eb143

--- core/java/com/android/internal/util/bliss/BlissUtils.java
+++ core/java/com/android/internal/util/bliss/BlissUtils.java
@@ -153,6 +153,21 @@ public class BlissUtils {
                Locale.CHINESE.getLanguage());
     }
 
+    // Screen on
+    public static void switchScreenOn(Context context) {
+        PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+        if (pm == null) return;
+        PowerManager.WakeLock wakeLock = pm.newWakeLock((
+                PowerManager.SCREEN_BRIGHT_WAKE_LOCK |
+                        PowerManager.ACQUIRE_CAUSES_WAKEUP), "wakeup:device");
+        boolean isScreenOn = pm.isScreenOn();
+        /* Wake up the device only when screen is off.
+         * Otherwise don't bother to do anything. */
+        if (!wakeLock.isHeld() && !isScreenOn) {
+            wakeLock.acquire();
+        }
+    }
+
     public static boolean deviceHasFlashlight(Context ctx) {
         return ctx.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
     }
@@ -265,7 +280,7 @@ public class BlissUtils {
     // Method to turn off the screen
     public static void switchScreenOff(Context ctx) {
         PowerManager pm = (PowerManager) ctx.getSystemService(Context.POWER_SERVICE);
-        if (pm!= null) {
+        if (pm!= null && pm.isScreenOn()) {
             pm.goToSleep(SystemClock.uptimeMillis());
         }
     }
