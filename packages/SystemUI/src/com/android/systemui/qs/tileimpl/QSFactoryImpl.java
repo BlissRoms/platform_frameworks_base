9374f69e589454abbeffe2180dbf34f22dffeae6
Yoshinori Hirano <yoshinori.hirano@sonymobile.com>
2019-02-24 13:04:01 -0700

    Add Sound tile to Quick Settings
    abc ezio84: adapt to O, show volume panel with long press,
    use full dnd (no interruptions) as silent mode
    
    @beanstown106: Sound Tile improvements
    *added longpress action
    *added more descriptive labels for each state instead of always saying sound
    
    Change-Id: Idf968dcb7729ecfa7ec8c57ddd33babe7cc6c004
    ezio84: Adapt for N

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -49,6 +49,7 @@ import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenrecordTile;
 import com.android.systemui.qs.tiles.SmartPixelsTile;
 import com.android.systemui.qs.tiles.SoundSearchTile;
+import com.android.systemui.qs.tiles.SoundTile;
 import com.android.systemui.qs.tiles.SyncTile;
 import com.android.systemui.qs.tiles.UserTile;
 import com.android.systemui.qs.tiles.WeatherTile;
@@ -134,6 +135,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new PieTile(mHost);
             case "expanded_desktop":
                 return new ExpandedDesktopTile(mHost);
+            case "sound":
+                return new SoundTile(mHost);
             case "soundsearch":
                 return new SoundSearchTile(mHost);
             case "music":
