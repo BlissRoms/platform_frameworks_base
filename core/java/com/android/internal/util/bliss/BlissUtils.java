5ed91e8e0be1ef511712eb5a205f4976b2fe3635
ezio84 <brabus84@gmail.com>
2019-02-27 21:13:01 -0600

    base: Reverse actions for QS and notification panel
    This just makes sense, so lets do it!
    
    - Update method names in PWM
    
    Change-Id: Ic08bee12c90dc55265dea3c3972f4087b93d2826
    Signed-off-by: rohan <purohit.rohan@gmail.com>

--- core/java/com/android/internal/util/bliss/BlissUtils.java
+++ core/java/com/android/internal/util/bliss/BlissUtils.java
@@ -518,7 +518,7 @@ public class BlissUtils {
             IStatusBarService service = getStatusBarService();
             if (service != null) {
                 try {
-                    service.expandSettingsPanel(null);
+                    service.toggleSettingsPanel();
                 } catch (RemoteException e) {
                     // do nothing.
                 }
