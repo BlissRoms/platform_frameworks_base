adbca0f2d072e258c9ea8898ffaa40f0889251f2
Lucchetto <lucchetto.tie@live.com>
2019-03-28 18:41:03 +0100

    base: Smart Charging (1/4)
    Change-Id: Ibbc7691222a337b7a525c1dbdd4844ed0a1a9d6b

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5960,6 +5960,16 @@ public final class Settings {
         public static final String CHARGING_ANIMATION = "charging_animation";
 
         /**
+         * @hide
+         */
+        public static final String SMART_CHARGING = "smart_charging";
+
+        /**
+         * @hide
+         */
+        public static final String SMART_CHARGING_LEVEL = "smart_charging_level";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
