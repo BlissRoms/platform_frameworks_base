5e2437bd3920746238c6c5ade7c1f79458f19802
micky387 <mickaelsaibi@free.fr>
2019-04-19 21:55:49 +0200

    SystemUI: FOD: Use custom Bottom Area View for keyguard message
    Dreaming and Pulsing are not concerned
    
    Change-Id: I5ef1e6502a97589efcf40ab17c3f4dc21a1405d4
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
@@ -180,6 +180,8 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
     private String mPreviousSource;
     private int mBurnInYOffset;
 
+    private boolean mDisplayFODView;
+
     public KeyguardBottomAreaView(Context context) {
         this(context, null);
     }
@@ -248,8 +250,16 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
         mEnterpriseDisclosure = findViewById(
                 R.id.keyguard_indication_enterprise_disclosure);
         mIndicationText = findViewById(R.id.keyguard_indication_text);
-        mIndicationBottomMargin = getResources().getDimensionPixelSize(
-                R.dimen.keyguard_indication_margin_bottom);
+        mDisplayFODView = getResources()
+                .getBoolean(com.android.internal.R.bool.config_needCustomFODView);
+
+        if (mDisplayFODView) {
+            mIndicationBottomMargin = getResources().getDimensionPixelSize(
+                  R.dimen.op_keyguard_indication_margin_bottom);
+        } else {
+            mIndicationBottomMargin = getResources().getDimensionPixelSize(
+                  R.dimen.keyguard_indication_margin_bottom);
+        }
         mIndicationBottomMarginAmbient = getResources().getDimensionPixelSize(
                 R.dimen.keyguard_indication_margin_bottom_ambient);
         mBurnInYOffset = getResources().getDimensionPixelSize(
@@ -324,8 +334,13 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
     protected void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
     	Typeface tf = Typeface.create(FONT_FAMILY, Typeface.ITALIC);
-        mIndicationBottomMargin = getResources().getDimensionPixelSize(
-                R.dimen.keyguard_indication_margin_bottom);
+        if (mDisplayFODView) {
+            mIndicationBottomMargin = getResources().getDimensionPixelSize(
+                  R.dimen.op_keyguard_indication_margin_bottom);
+        } else {
+            mIndicationBottomMargin = getResources().getDimensionPixelSize(
+                  R.dimen.keyguard_indication_margin_bottom);
+        }
         mIndicationBottomMarginAmbient = getResources().getDimensionPixelSize(
                 R.dimen.keyguard_indication_margin_bottom_ambient);
         mBurnInYOffset = getResources().getDimensionPixelSize(
