5029e018e3745fab87e9a5d6a6f183bae0c000ee
ezio84 <brabus84@gmail.com>
2019-04-27 21:51:15 +0300

    Allow to suppress notifications sound/vibration if screen is On [1/2]
    Change-Id: Ic4e7f7d0af413497c97c83423ba7cdabf766f080
    Suppress notification vib/sound feature: add "if no media playing" mode [1/2]
    
    Change-Id: Ife63f95d5892bd59f3c03ff94010cb259d6745a0
    Cleanup: Remove notifications media playing check [1/2]
    
    The media playing check for "Suppress notifications when screen on" is not
    reliable. The other two available options are enough.
    
    Change-Id: Iabd91eb5f1fb10af05480fe0dfb7298fbd0d3cb2
    Signed-off-by: rohan <purohit.rohan@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5207,6 +5207,14 @@ public final class Settings {
                new SettingsValidators.InclusiveIntegerRangeValidator(0, 1);;
 
         /**
+         * Wheter to play notification sound and vibration if screen is ON
+         * 0 - never
+         * 1 - always
+         * @hide
+         */
+        public static final String NOTIFICATION_SOUND_VIB_SCREEN_ON = "notification_sound_vib_screen_on";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
