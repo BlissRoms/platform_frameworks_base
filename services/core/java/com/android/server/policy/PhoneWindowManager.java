2a5915fdf6a73c4babc81c0ed30fba5f67bcab74
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-05-08 11:52:38 -0300

    base: SwipeToScreenshot: Import MIUI implementation
    Reverse engineered from Xiaomi/beryllium/beryllium:9/PKQ1.180729.001/9.2.25:user/release-keys
    
    Change-Id: I1d5938457b7cc4c5ef1fec235d27d8a40526961a
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: Ayan Mukherjee <mukherjeeayan725@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -707,10 +707,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private static final int MSG_RINGER_TOGGLE_CHORD = 26;
     private static final int MSG_TOGGLE_TORCH = 50;
     private static final int MSG_CLEAR_PROXIMITY = 51;
-    private OPGesturesListener mOPGestures;
     private static final int MSG_DISPATCH_VOLKEY_WITH_WAKE_LOCK = 29;
 
     private boolean mHasAlertSlider = false;
+
+    private SwipeToScreenshotListener mSwipeToScreenshot;
 
     private class PolicyHandler extends Handler {
         @Override
@@ -2025,7 +2026,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
 
         mHandler = new PolicyHandler();
-        mOPGestures = new OPGesturesListener(context, new OPGesturesListener.Callbacks() {
+        mSwipeToScreenshot = new SwipeToScreenshotListener(context, new SwipeToScreenshotListener.Callbacks() {
             @Override
             public void onSwipeThreeFinger() {
                 if (!mPocketLockShowing) {
@@ -2241,11 +2242,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         if (enable) {
             if (haveEnableGesture) return;
             haveEnableGesture = true;
-            mWindowManagerFuncs.registerPointerEventListener(mOPGestures, DEFAULT_DISPLAY);
+            mWindowManagerFuncs.registerPointerEventListener(mSwipeToScreenshot, DEFAULT_DISPLAY);
         } else {
             if (!haveEnableGesture) return;
             haveEnableGesture = false;
-            mWindowManagerFuncs.unregisterPointerEventListener(mOPGestures, DEFAULT_DISPLAY);
+            mWindowManagerFuncs.unregisterPointerEventListener(mSwipeToScreenshot, DEFAULT_DISPLAY);
         }
     }
 
