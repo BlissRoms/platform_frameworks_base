10274398288a261a252b535fdc021e6fa640b82d
Anushek Prasal <anushekprasal@gmail.com>
2019-06-06 14:18:24 +0530

    base: Add toggle to disable charging animation [1/2]
    Change-Id: Ib0e8749eb7615227b399430a56aedf91a6b63002

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5825,6 +5825,12 @@ public final class Settings {
         public static final String STATUSBAR_COLORED_ICONS = "statusbar_colored_icons";
 
         /**
+         * Whether to show charging animation
+         * @hide
+         */
+        public static final String CHARGING_ANIMATION = "charging_animation";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
