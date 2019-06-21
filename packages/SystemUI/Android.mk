2ae1c81dadee1303090137e486fdc77e32f57f1f
Peter Cai <peter@typeblog.net>
2019-06-21 14:12:38 +0800

    fingerprint: switch to general hal for in-screen scanner
    * This is much cleaner and the framework does not have to depend on
    device-specific HALs.
    * TODO: The FODCircleView seems to still have some bugs on the logic of
    when it should appear.
    
    Change-Id: I565115eefcd3313d6938f6c8236ca2dddcb5c137
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- packages/SystemUI/Android.mk
+++ packages/SystemUI/Android.mk
@@ -62,8 +62,7 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     trail-drawing \
     rebound \
     guava \
-    vendor.oneplus.fingerprint.extension-V1.0-java \
-    vendor.oneplus.hardware.display-V1.0-java
+    vendor.lineage.biometrics.fingerprint.inscreen-V1.0-java
 
 LOCAL_JAVA_LIBRARIES := telephony-common \
     android.car \
