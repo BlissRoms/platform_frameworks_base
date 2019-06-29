407ed4fcbc5527591c419e58fefc1546a7e60853
Pranav Vashi <neobuddy89@gmail.com>
2019-06-29 21:05:41 +0530

    Bring back Gaming mode [1/3]
    This is a squash commit with major contributions by
    @PMS22, @ishubhamsingh, @DennySPB, @neobuddy89
    
    * Blissroms: update tile to android 10
    
    Change-Id: Ieb5cbc6356c44f629f2122c94c52ede2e0a421c6
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: rohan <purohit.rohan@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/NotificationInterruptionStateProvider.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/NotificationInterruptionStateProvider.java
@@ -74,6 +74,8 @@ public class NotificationInterruptionStateProvider {
 
     private boolean mLessBoringHeadsUp;
 
+    private boolean mSkipHeadsUp;
+
     public NotificationInterruptionStateProvider(Context context) {
         this(context,
                 (PowerManager) context.getSystemService(Context.POWER_SERVICE),
@@ -325,13 +327,17 @@ public class NotificationInterruptionStateProvider {
         mLessBoringHeadsUp = lessBoring;
     }
 
+    public void setGamingPeekMode(boolean skipHeadsUp) {
+        mSkipHeadsUp = skipHeadsUp;
+    }
+
     public boolean shouldSkipHeadsUp(StatusBarNotification sbn) {
         boolean isImportantHeadsUp = false;
         String notificationPackageName = sbn.getPackageName().toLowerCase();
         isImportantHeadsUp = notificationPackageName.contains("dialer") ||
                 notificationPackageName.contains("messaging") ||
                 notificationPackageName.contains("clock");
-        return !getShadeController().isDozing() && mLessBoringHeadsUp && !isImportantHeadsUp;
+        return !getShadeController().isDozing() && mLessBoringHeadsUp && mSkipHeadsUp && !isImportantHeadsUp;
     }
 
     /**
