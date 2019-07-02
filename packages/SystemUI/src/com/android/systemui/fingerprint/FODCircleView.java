9c5762ca0270ac33ab07a9b65fdcf9b53b2f36bf
Peter Cai <peter@typeblog.net>
2019-07-02 10:49:09 +0800

    FODCircleView: use lambdas everywhere
    Change-Id: I1fea773f619ac0586ffbbc3cee5bb0e52b5df5b0

--- packages/SystemUI/src/com/android/systemui/fingerprint/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/fingerprint/FODCircleView.java
@@ -91,14 +91,11 @@ public class FODCircleView extends ImageView implements OnTouchListener {
         public void onFingerDown() {
             mInsideCircle = true;
 
-            new Handler(Looper.getMainLooper()).post(new Runnable() {
-                @Override
-                public void run() {
-                    setDim(true);
-                    setImageResource(R.drawable.fod_icon_empty);
+            new Handler(Looper.getMainLooper()).post(() -> {
+                setDim(true);
+                setImageResource(R.drawable.fod_icon_empty);
 
-                    invalidate();
-                }
+                invalidate();
             });
         }
 
@@ -106,14 +103,11 @@ public class FODCircleView extends ImageView implements OnTouchListener {
         public void onFingerUp() {
             mInsideCircle = false;
 
-            new Handler(Looper.getMainLooper()).post(new Runnable() {
-                @Override
-                public void run() {
-                    setDim(false);
-                    setImageResource(R.drawable.fod_icon_default);
+            new Handler(Looper.getMainLooper()).post(() -> {
+                setDim(false);
+                setImageResource(R.drawable.fod_icon_default);
 
-                    invalidate();
-                }
+                invalidate();
             });
         }
     };
