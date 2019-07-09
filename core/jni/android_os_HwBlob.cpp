922ff62991c881ce9748ea3021f8986c0c81cd93
Jackeagle <jackeagle102@gmail.com>
2019-07-09 09:39:03 -0400

    Revert "HwBlob: s/malloc/calloc/"
    This reverts commit 3256e59a87466b1a1d20a6cfe22617e23fa25299.
    
    Change-Id: Ic39281e1917611fa2e06ceb796d4494bba171d8a

--- core/jni/android_os_HwBlob.cpp
+++ core/jni/android_os_HwBlob.cpp
@@ -85,7 +85,7 @@ JHwBlob::JHwBlob(JNIEnv *env, jobject thiz, size_t size)
       mOwnsBuffer(true),
       mHandle(0) {
     if (size > 0) {
-        mBuffer = calloc(size, 1);
+        mBuffer = malloc(size);
     }
 }
 
