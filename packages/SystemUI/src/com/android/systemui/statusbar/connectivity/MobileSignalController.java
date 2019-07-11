3bc21d746588a5ef4079744456fdaf27b3b50d26
varund7726 <date.varun123@gmail.com>
2019-07-11 11:04:15 +0300

    Option to Display Data Disabled Indicator Icon [1/2]
    Change-Id: Ibe9c8f5550e63ccfe7a04578ea5c9cd676b930c1
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
@@ -92,9 +92,12 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
     private int mMobileStatusHistoryIndex;
 
     private boolean mRoamingIconAllowed;
+    private boolean mDataDisabledIcon;
 
     private static final String ROAMING_INDICATOR_ICON =
             "system:" + Settings.System.ROAMING_INDICATOR_ICON;
+    private static final String DATA_DISABLED_ICON =
+            "system:" + Settings.System.DATA_DISABLED_ICON;
 
     private final MobileStatusTracker.Callback mMobileCallback =
             new MobileStatusTracker.Callback() {
@@ -172,6 +175,7 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
         mMobileStatusTracker = mobileStatusTrackerFactory.createTracker(mMobileCallback);
 
         Dependency.get(TunerService.class).addTunable(this, ROAMING_INDICATOR_ICON);
+        Dependency.get(TunerService.class).addTunable(this, DATA_DISABLED_ICON);
     }
 
     @Override
@@ -179,6 +183,11 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
         switch (key) {
             case ROAMING_INDICATOR_ICON:
                 mRoamingIconAllowed =
+                    TunerService.parseIntegerSwitch(newValue, true);
+                updateTelephony();
+                break;
+            case DATA_DISABLED_ICON:
+                mDataDisabledIcon =
                     TunerService.parseIntegerSwitch(newValue, true);
                 updateTelephony();
                 break;
@@ -549,7 +558,7 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
         mCurrentState.roaming = isRoaming() && mRoamingIconAllowed;
         if (isCarrierNetworkChangeActive()) {
             mCurrentState.iconGroup = TelephonyIcons.CARRIER_NETWORK_CHANGE;
-        } else if (isDataDisabled() && !mConfig.alwaysShowDataRatIcon) {
+        } else if (isDataDisabled() && mDataDisabledIcon) {
             if (mSubscriptionInfo.getSubscriptionId() != mDefaults.getDefaultDataSubId()) {
                 mCurrentState.iconGroup = TelephonyIcons.NOT_DEFAULT_DATA;
             } else {
