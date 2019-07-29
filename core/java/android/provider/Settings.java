363b33556ddae784201b41489555ba5f8ff723e0
ezio84 <brabus84@gmail.com>
2019-07-29 15:57:29 -0600

    Ambient brightness values: use a custom system setting [1/2]
    part 2 is in CustomDoze package
    
    Change-Id: I3a396a381b31d59d0b4ecfc44ded1e2d620033a9

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5876,6 +5876,13 @@ public final class Settings {
                 ANY_INTEGER_VALIDATOR;
 
         /**
+         * Ambient screen settings
+         * "screen_brightness_array=0:1:2:3:4"
+         * @hide
+         */
+        public static final String ALWAYS_ON_DISPLAY_CONSTANTS_CUST = "always_on_display_constants_cust";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
