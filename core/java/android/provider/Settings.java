fc387bb97cf1b1c9fd2a0a8f32618f86912c3d9c
Juan Ezquerro LLanes <arrase@gmail.com>
2019-08-19 13:18:03 -0600

    base: Wakeup when plugged or unplugged [2/2]
    Change-Id: Ide02d665613727196166048101f7ba2452e8a4cf

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5960,6 +5960,11 @@ public final class Settings {
         public static final String CHARGING_ANIMATION = "charging_animation";
 
         /**
+         * @hide
+         */
+        public static final String WAKE_WHEN_PLUGGED_OR_UNPLUGGED = "wake_when_plugged_or_unplugged";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
