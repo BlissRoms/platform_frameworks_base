3b6cfb77ed11ce4263c1ca570e59bee583a29dd8
DennySPB <dennyspb@gmail.com>
2019-08-26 16:03:02 +0300

    Fix Google Markup FC
    Grant runtime storage permissions.
    This fixes FC of google markup when using it along with Longshot
    
    Change-Id: I326a0ce9c0b7761552e7604a67bf4470045f6ab0
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
@@ -826,8 +826,12 @@ public final class DefaultPermissionGrantPolicy {
         // Google dialer
         grantSystemFixedPermissionsToSystemPackage("com.google.android.dialer", userId, PHONE_PERMISSIONS,
                 CONTACTS_PERMISSIONS, SMS_PERMISSIONS);
+
         // Lawnchair
         grantSystemFixedPermissionsToSystemPackage("ch.deletescape.lawnchair.ci", userId, STORAGE_PERMISSIONS);
+
+        // Google Markup
+        grantSystemFixedPermissionsToSystemPackage("com.google.android.markup", userId, STORAGE_PERMISSIONS);
     }
 
     private String getDefaultSystemHandlerActivityPackageForCategory(String category, int userId) {
