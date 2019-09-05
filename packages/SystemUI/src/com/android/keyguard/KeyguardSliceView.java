76f2776211f11dc868529996c5529dda779dfc89
sarthakaditya52 <sarthak.aditya52@gmail.com>
2019-09-05 14:02:04 -0600

    KeyguardSliceView: Align as per the Clock Style (Date Styles)
    Change-Id: I63a7a094a2711ae3612e49a7b1276d50ef0fed92

--- packages/SystemUI/src/com/android/keyguard/KeyguardSliceView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardSliceView.java
@@ -26,7 +26,6 @@ import android.arch.lifecycle.Observer;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.graphics.Color;
-import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
 import android.graphics.Typeface;
 import android.net.Uri;
@@ -43,7 +42,9 @@ import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.util.TypedValue;
 
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.graphics.ColorUtils;
@@ -83,6 +84,7 @@ public class KeyguardSliceView extends LinearLayout implements View.OnClickListe
     private Uri mKeyguardSliceUri;
     @VisibleForTesting
     TextView mTitle;
+    private RelativeLayout mRowContainer;
     private Row mRow;
     private int mTextColor;
     private float mDarkAmount = 0;
@@ -136,6 +138,7 @@ public class KeyguardSliceView extends LinearLayout implements View.OnClickListe
     protected void onFinishInflate() {
         super.onFinishInflate();
         mTitle = findViewById(R.id.title);
+        mRowContainer = findViewById(R.id.row_maincenter);
         mRow = findViewById(R.id.row);
         mTextColor = Utils.getColorAttr(mContext, R.attr.wallpaperTextColor);
         updateSettings();
@@ -210,13 +213,13 @@ public class KeyguardSliceView extends LinearLayout implements View.OnClickListe
         final int blendedColor = getTextColor();
         final int startIndex = mHasHeader ? 1 : 0; // First item is header; skip it
         if (mClockSelection) {
-            mRow.setPaddingRelative((int) mContext.getResources().getDimension(R.dimen.custom_clock_left_padding), 0, 0, 0);
-            mRow.setGravity(Gravity.START);
-        }
-        else {
-            mRow.setPaddingRelative(0, 0, 0, 0);
-            mRow.setGravity(Gravity.CENTER);
-        }
+            mRowContainer.setPaddingRelative((int) mContext.getResources().getDimension(R.dimen.custom_clock_left_padding), 0, 0, 0);
+            mRowContainer.setGravity(Gravity.START);
+         }
+         else {
+            mRowContainer.setPaddingRelative(0, 0, 0, 0);
+            mRowContainer.setGravity(Gravity.CENTER); 
+	}
         mRow.setVisibility(subItemsCount > 0 ? ((mShowInfo || mDarkAmount == 1) ? VISIBLE : GONE)
                 : GONE);
         mRowAvailable = subItemsCount > 0;
