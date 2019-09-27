b3ab55c647c2d4b9c8f699f7b13f286d5b2c009f
maxwen <max.weninger@gmail.com>
2019-09-27 23:13:28 +0200

    base: Add CPU info overlay
    includes all modifications and fixes from P
    
    @neobuddy89: Remove redundant DreamManager code
    
    Change-Id: I0e6dee53ebcd864a522cfd3d78a5abb253991a9c
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    Signed-off-by: Ayan Mukherjee <mukherjeeayan725@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5976,6 +5976,12 @@ public final class Settings {
         public static final String QS_FOOTER_PAGE_INDICATOR = "qs_footer_page_indicator";
 
         /**
+         * Control whether the process CPU info meter should be shown.
+         * @hide
+         */
+        public static final String SHOW_CPU_OVERLAY = "show_cpu_overlay";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
