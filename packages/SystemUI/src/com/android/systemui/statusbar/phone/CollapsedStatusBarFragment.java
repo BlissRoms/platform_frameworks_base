e6168a14df69af50ca2386dab755c2b93c851ea1
beanstown106 <nbenis106@gmail.com>
2019-10-15 09:43:58 +0200

    Custom Carrier Label & Carrier Label Placement [1/2]
    Squash Of:
    Keyguard/Statusbar Carrier label options [1/2]
    * Stock behavior(only on keyguard), only on statusbar,both disabled or both enabled
    * code originally writted by @Altaf-Mahdi i just squased into one commit
    PureNexusProject-Legacy/android_frameworks_base@688d56f
    
    Show carrier label / custom & change color [1/2]
    *code originally written by @martincz
    *includes various fixs by the du team
    PureNexusProject-Legacy/android_frameworks_base@16a247a
    
    Carrier label - fix capitilization bug
    PureNexusProject-Legacy/android_frameworks_base@4b9e22b
    
    remove carrier label color and make carrier label play nice with dark statusbar
    PureNexusProject-Legacy/android_frameworks_base@3f32102
    
    Fix Carrier Label font size not changing on system font size change
    DirtyUnicorns/android_frameworks_base@8da9dcf
    
    include carrier label in icon merger width calculation
    PureNexusProject-Legacy/android_frameworks_base@9df3a0c
    
    Credit/Thanks to @Altaf-Mahdi @beanstown106 @dwitherell and @martincz
    
    BenzoEdit: Fixed statusbar layout for pie
    @SKULSHADY: Code cleanup and improvements
    @ZeNiXxX: Fix imports for Android 10
    @rohanpurohit:
    * place carrier text in custom icon area in packages/SystemUI/res/layout/status_bar.xml
    * adapt to existing animateHide method
    
    Change-Id: I81157dd09d058b70a3e19a8242c81765bc6d6282
    Signed-off-by: rohan <purohit.rohan@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
@@ -20,8 +20,13 @@ import static android.app.StatusBarManager.DISABLE_SYSTEM_INFO;
 
 import android.annotation.Nullable;
 import android.app.Fragment;
+import android.content.ContentResolver;
+import android.database.ContentObserver;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.Parcelable;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -74,6 +79,27 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
 
     private View mBatteryBar;
 
+    private View mCustomCarrierLabel;
+    private int mShowCarrierLabel;
+
+    private class SettingsObserver extends ContentObserver {
+        SettingsObserver(Handler handler) {
+            super(handler);
+        }
+        void observe() {
+            mContentResolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_SHOW_CARRIER),
+                    false, this, UserHandle.USER_ALL);
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            updateSettings(true);
+        }
+    }
+    private SettingsObserver mSettingsObserver = new SettingsObserver(mHandler);
+    private ContentResolver mContentResolver;
+
     private SignalCallback mSignalCallback = new SignalCallback() {
         @Override
         public void setIsAirplaneMode(NetworkController.IconState icon) {
@@ -113,6 +139,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         mCenterClockLayout = (LinearLayout) mStatusBar.findViewById(R.id.center_clock_layout);
         mBatteryBar = mStatusBar.findViewById(R.id.battery_bar);
         mBlissLogoRight = mStatusBar.findViewById(R.id.bliss_logo_right);
+        mCustomCarrierLabel = mStatusBar.findViewById(R.id.statusbar_carrier_text);
         mClockController = new ClockController(mStatusBar);
         showSystemIconArea(false);
         initEmergencyCryptkeeperText();
@@ -194,8 +221,10 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         if ((diff1 & DISABLE_NOTIFICATION_ICONS) != 0) {
             if ((state1 & DISABLE_NOTIFICATION_ICONS) != 0) {
                 hideNotificationIconArea(animate);
+                hideCarrierName(animate);
             } else {
                 showNotificationIconArea(animate);
+                showCarrierName(animate);
             }
         }
     }
@@ -303,6 +332,18 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         }
     }
 
+    public void hideCarrierName(boolean animate) {
+        if (mCustomCarrierLabel != null) {
+            animateHide(mCustomCarrierLabel, animate);
+        }
+    }
+
+    public void showCarrierName(boolean animate) {
+        if (mCustomCarrierLabel != null) {
+            setCarrierLabel(animate);
+        }
+    }
+
     /**
      * Animate a view to INVISIBLE or GONE
      */
@@ -392,5 +433,19 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
     @Override
     public void onDozingChanged(boolean isDozing) {
         disable(getContext().getDisplayId(), mDisabled1, mDisabled1, false /* animate */);
+    }
+    public void updateSettings(boolean animate) {
+        mShowCarrierLabel = Settings.System.getIntForUser(mContentResolver,
+                Settings.System.STATUS_BAR_SHOW_CARRIER, 1,
+                UserHandle.USER_CURRENT);
+        setCarrierLabel(animate);
+    }
+
+    private void setCarrierLabel(boolean animate) {
+        if (mShowCarrierLabel == 2 || mShowCarrierLabel == 3) {
+            animateShow(mCustomCarrierLabel, animate);
+        } else {
+            animateHide(mCustomCarrierLabel, animate);
+        }
     }
 }
