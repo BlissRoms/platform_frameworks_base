LineageOS/android_frameworks_base

lockscreen: Add support for showing unlock screen directly
Squash of the following changes, updated for the new SDK:

  From: eray orçunus <erayorcunus@gmail.com>
  Date: Wed, 17 Jun 2015 16:12:19 +0300

      lockscreen: Add option for showing unlock screen directly

       * Option appears on PIN, pattern and password methods
       * User can press back button to see swipe-up-to-unlock screen
       * Instantly hide keyguard if Smart Lock has unlocked phone

      Change-Id: I2ceeed348d7e61632b8344f35431f30cba62ca4f
      Ticket-Id: CYNGNOS-1873
      Signed-off-by: eray orçunus <erayorcunus@gmail.com>

  From: Danesh M <daneshm90@gmail.com>
  Date: Tue, 17 Nov 2015 20:09:38 -0800

      SettingsProvider: Add missing platform sdk dependency

      Change-Id: I4844eaa0a989bf634a2251e95dc84a505037c012

Change-Id: I0cfb43bfb631fce532dfacdf56455309a04ddfa2

services/core/java/com/android/server/locksettings/LockSettingsService.java
@@ -113,6 +113,8 @@ import com.android.server.locksettings.recoverablekeystore.RecoverableKeyStoreMa
 import com.android.server.locksettings.SyntheticPasswordManager.AuthenticationResult;
 import com.android.server.locksettings.SyntheticPasswordManager.AuthenticationToken;
 
+import lineageos.providers.LineageSettings;
+
 import libcore.util.HexEncoding;
 
 import java.io.ByteArrayOutputStream;
@@ -2129,7 +2131,8 @@ public class LockSettingsService extends ILockSettings.Stub {
             Secure.LOCK_PATTERN_TACTILE_FEEDBACK_ENABLED,
             Secure.LOCK_PATTERN_SIZE,
             Secure.LOCK_DOTS_VISIBLE,
-            Secure.LOCK_SHOW_ERROR_PATH
+            Secure.LOCK_SHOW_ERROR_PATH,
+            LineageSettings.Secure.LOCK_PASS_TO_SECURITY_VIEW
     };
 
     // Reading these settings needs the contacts permission

BlissRoms/platform_frameworks_base

services/core/java/com/android/server/locksettings/LockSettingsService.java
@@ -122,6 +122,8 @@ import com.android.server.locksettings.SyntheticPasswordManager.AuthenticationTo
 import com.android.server.locksettings.recoverablekeystore.RecoverableKeyStoreManager;
 import com.android.server.wm.WindowManagerInternal;
 
+import lineageos.providers.LineageSettings;
+
 import libcore.util.HexEncoding;
 
 import java.io.ByteArrayOutputStream;
@@ -2327,7 +2329,8 @@ public class LockSettingsService extends ILockSettings.Stub {
             Secure.LOCK_PATTERN_ENABLED,
             Secure.LOCK_BIOMETRIC_WEAK_FLAGS,
             Secure.LOCK_PATTERN_VISIBLE,
-            Secure.LOCK_PATTERN_TACTILE_FEEDBACK_ENABLED
+            Secure.LOCK_PATTERN_TACTILE_FEEDBACK_ENABLED,
+            LineageSettings.Secure.LOCK_PASS_TO_SECURITY_VIEW
     };
 
     // Reading these settings needs the contacts permission