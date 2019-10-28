70032c891690b55028d50165bfdbe19c9bfced5f
Danesh Mondegarian <daneshm90@gmail.com>
2019-10-28 12:53:10 +0100

    SystemUI: Port Statusbar brightness control [1/2]
    This is a squash of the following changes:
    
    Author: Danesh Mondegarian <daneshm90@gmail.com>
    Date:   Sun Nov 10 23:30:56 2013 -0800
    
        SystemUI : Port forward statusbar brightness control
    
        Brings back the status bar brightness control from cm-10.2/cm-11
    
        Change-Id: Icc04e7da3286e9925f8b90528c1e022b33d33e9d
    
    Author: Roman Birg <roman@cyngn.com>
    Date:   Mon Dec 1 16:08:21 2014 -0800
    
        SystemUI: improve brightness slider behavior
    
        Mimic the brightness slider behavior in the statusbar.
        This adds logic to make the statusbar slider also work with automatic
        brightness mode enabled and it will instead adjust the temporary automatic
        brightness overrride.
    
        Change-Id: I78814a785decc83df94844fcc2f489c8cbec2b65
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: Pawit Pornkitprasan <p.pawit@gmail.com>
    Date:   Sun Dec 28 20:42:41 2014 +0700
    
        SystemUI: status bar brightness: store value as int
    
        Non-automatic brightness value is stored as int, not float.
    
        Symptom: adjust the brightness in the status bar, the brightness
                 slider in the notification bar will always be set to full
    
        Change-Id: I5f9a6e97bae2c44c3aff7c9222186a6dbda5d902
    
    Author: Zhao Wei Liew <zhaoweiliew@gmail.com>
    Date:   Fri Oct 7 10:09:04 2016 +0800
    
        SystemUI: statusbar: Fix status bar brightness control feature
    
        Bring in the missing fixes from CM 13.
    
        This commit squashes the following commits from CM 13.0:
        6397527 SystemUI: fix status bar brightness slider eating events
        bbef422 SystemUI : fix brightness slider for automatic brightness
    
        Change-Id: I85e0b99dc33468b36222128660707fe481cf8b33
    
    Author: Zhao Wei Liew <zhaoweiliew@gmail.com>
    Date:   Fri Oct 7 08:56:25 2016 +0800
    
        SystemUI: Use Tuner API for CM settings
    
        Get rid of all the excess code by implementing TunerService.Tunable
        and observing any changes made to the settings through it.
    
        Remove UserContentObserver as the Tuner API handles user switches.
    
        Also remove some unused imports that were introduced in earlier
        CM commits, but were never removed since.
    
    Change-Id: I13b49ee647f1e979d4513a95f4bf6c2ce5732991

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5260,6 +5260,10 @@ public final class Settings {
          */
         public static final String QS_SMART_PULLDOWN = "qs_smart_pulldown";
 
+        /**
+         * @hide
+         */
+        public static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
