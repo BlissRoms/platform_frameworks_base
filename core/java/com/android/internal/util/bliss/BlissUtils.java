ef1db994c248aa70b52f7d4c37c0ef5ac87fa957
Anushek Prasal <anushekprasal@gmail.com>
2019-11-30 16:08:56 +0530

    Utils: Update notch check logic
    Some devices with notch have set config_fillMainBuiltInDisplayCutout to false. This results in Utils returning true for hasNotch. Instead let's check if the bounding path of the cutout region is defined. Also return false if NoCutoutOverlay is active.
    
    Change-Id: Ib134312d1c49b8ad8c086a1271cc29f6df020f3c

--- core/java/com/android/internal/util/bliss/BlissUtils.java
+++ core/java/com/android/internal/util/bliss/BlissUtils.java
@@ -37,6 +37,7 @@ import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.UserHandle;
 import android.os.SystemClock;
+import android.text.TextUtils;
 import android.os.Vibrator;
 import android.util.DisplayMetrics;
 import android.os.SystemProperties;
@@ -46,6 +47,8 @@ import android.view.KeyCharacterMap;
 import android.view.KeyEvent;
 import android.view.IWindowManager;
 import android.view.WindowManagerGlobal;
+
+import com.android.internal.R;
 
 import java.util.List;
 
@@ -384,21 +387,10 @@ public class BlissUtils {
 
     // Check if device has a notch
     public static boolean hasNotch(Context context) {
-        int result = 0;
-        int resid;
-        int resourceId = context.getResources().getIdentifier(
-                "status_bar_height", "dimen", "android");
-        resid = context.getResources().getIdentifier("config_fillMainBuiltInDisplayCutout",
-                "bool", "android");
-        if (resid > 0) {
-            return context.getResources().getBoolean(resid);
-        }
-        if (resourceId > 0) {
-            result = context.getResources().getDimensionPixelSize(resourceId);
-        }
-        DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
-        float px = 24 * (metrics.densityDpi / 160f);
-        return result > Math.round(px);
+        String displayCutout = context.getResources().getString(R.string.config_mainBuiltInDisplayCutout);
+        boolean maskDisplayCutout = context.getResources().getBoolean(R.bool.config_maskMainBuiltInDisplayCutout);
+        boolean displayCutoutExists = (!TextUtils.isEmpty(displayCutout) && !maskDisplayCutout);
+        return displayCutoutExists;
     }
 
    /* e.g.
