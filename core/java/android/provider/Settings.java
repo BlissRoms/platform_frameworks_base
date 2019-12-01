6a00308e08e89fcbd9f54be1746411a5f0c0ff44
Pranav Vashi <neobuddy89@gmail.com>
2019-12-01 21:28:27 +0530

    Add toggle for emergency affordance in power menu [1/2]
    Change-Id: I7eb6872eb1cfc0cf5dc213d4140adf4209f8b428
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -15129,6 +15129,11 @@ public final class Settings {
         public static final String POWER_MENU_EMERGENCY_AFFORDANCE = "power_menu_emergency_affordance";
 
         /**
+         * @hide
+         */
+        public static final String POWER_MENU_EMERGENCY_AFFORDANCE = "power_menu_emergency_affordance";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
