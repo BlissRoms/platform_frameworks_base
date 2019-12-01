45f30967f480ab72a9ee14db8551cbfaf219ff71
ezio84 <brabus84@gmail.com>
2019-12-01 18:19:48 +0100

    SystemUI: block gestural navigation when using partial screenshot
    Change-Id: I68d25a6eab9ea16707e8736b9e56055532bcdf87
    Signed-off-by: rohan <purohit.rohan@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -2206,6 +2206,13 @@ public class StatusBar extends SystemUI implements DemoMode,
         }
     }
 
+    @Override
+    public void setBlockedGesturalNavigation(boolean blocked) {
+        if (getNavigationBarView() != null) {
+            getNavigationBarView().setBlockedGesturalNavigation(blocked);
+        }
+    }
+
     /**
      * All changes to the status bar and notifications funnel through here and are batched.
      */
