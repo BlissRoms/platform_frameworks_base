244a63a762c15a1bdf7c1d5931c40dc39b97a3ce
Anushek Prasal <anushekprasal@gmail.com>
2019-12-05 21:16:13 +0530

    BatteryService: Add Warp charging support
    Eyosen: Port to S
    Not really needed but feels good to have it. Based on https://github.com/Havoc-OS/android_frameworks_base/commit/9fcb3663e315d2c2884a66d951b1dd30a9c034bb
    
    Change-Id: I6bdc72a0654bed75bfcf595049378e61736d28d8
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -2241,7 +2241,7 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
         }
 
         // Take a guess at initial SIM state, battery status and PLMN until we get an update
-        mBatteryStatus = new BatteryStatus(BATTERY_STATUS_UNKNOWN, 100, 0, 0, 0, 0, 0, 0, true, false, false);
+        mBatteryStatus = new BatteryStatus(BATTERY_STATUS_UNKNOWN, 100, 0, 0, 0, 0, 0, 0, true, false, false, false);
 
         // Watch for interesting updates
         final IntentFilter filter = new IntentFilter();
@@ -3510,6 +3510,11 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
 
         // change in dash charging while plugged in
         if (nowPluggedIn && current.dashChargeStatus != old.dashChargeStatus) {
+            return true;
+        }
+
+        // change in warp charging while plugged in
+        if (nowPluggedIn && current.warpChargeStatus != old.warpChargeStatus) {
             return true;
         }
 
