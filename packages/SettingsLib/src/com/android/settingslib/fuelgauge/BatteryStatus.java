05e05362b4c0b8285a0f85c929e848b52290f828
Anushek Prasal <anushekprasal@gmail.com>
2019-12-05 21:16:13 +0530

    BatteryService: Add Warp charging support
    Eyosen: Port to S
    Not really needed but feels good to have it. Based on https://github.com/Havoc-OS/android_frameworks_base/commit/9fcb3663e315d2c2884a66d951b1dd30a9c034bb
    
    Change-Id: I6bdc72a0654bed75bfcf595049378e61736d28d8
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- packages/SettingsLib/src/com/android/settingslib/fuelgauge/BatteryStatus.java
+++ packages/SettingsLib/src/com/android/settingslib/fuelgauge/BatteryStatus.java
@@ -30,6 +30,7 @@ import static android.os.BatteryManager.EXTRA_STATUS;
 import static android.os.BatteryManager.EXTRA_OEM_FAST_CHARGER;
 import static android.os.BatteryManager.EXTRA_TEMPERATURE;
 import static android.os.BatteryManager.EXTRA_DASH_CHARGER;
+import static android.os.BatteryManager.EXTRA_WARP_CHARGER;
 
 import android.content.Context;
 import android.content.Intent;
@@ -49,6 +50,7 @@ public class BatteryStatus {
     public static final int CHARGING_REGULAR = 1;
     public static final int CHARGING_FAST = 2;
     public static final int CHARGING_DASH = 3;
+    public static final int CHARGING_WARP = 4;
 
     public final int status;
     public final int level;
@@ -62,10 +64,11 @@ public class BatteryStatus {
     public final boolean oemFastChargeStatus;
 
     public final boolean dashChargeStatus;
+    public final boolean warpChargeStatus;
 
     public BatteryStatus(int status, int level, int plugged, int health,
             int maxChargingWattage, int maxChargingVoltage, int maxChargingWattage,
-	    float temperature, boolean present, boolean oemFastChargeStatus, boolean dashChargeStatus) {
+	    float temperature, boolean present, boolean oemFastChargeStatus, boolean dashChargeStatus, boolean warpChargeStatus) {
         this.status = status;
         this.level = level;
         this.plugged = plugged;
@@ -75,6 +78,7 @@ public class BatteryStatus {
         this.maxChargingWattage = maxChargingWattage;
         this.temperature = temperature;
         this.dashChargeStatus = dashChargeStatus;
+        this.warpChargeStatus = warpChargeStatus;
         this.present = present;
         this.oemFastChargeStatus = oemFastChargeStatus;
     }
@@ -88,6 +92,7 @@ public class BatteryStatus {
         maxChargingVoltage = batteryChangedIntent.getIntExtra(EXTRA_MAX_CHARGING_VOLTAGE, -1);
         temperature = batteryChangedIntent.getIntExtra(EXTRA_TEMPERATURE, -1);
         dashChargeStatus = batteryChangedIntent.getBooleanExtra(EXTRA_DASH_CHARGER, false);
+        warpChargeStatus = batteryChangedIntent.getBooleanExtra(EXTRA_WARP_CHARGER, false);
         present = batteryChangedIntent.getBooleanExtra(EXTRA_PRESENT, true);
 
         final int maxChargingMicroAmp = maxChargingCurrent;
@@ -190,6 +195,7 @@ public class BatteryStatus {
         final int fastThreshold = context.getResources().getInteger(
                 R.integer.config_chargingFastThreshold);
         return dashChargeStatus ? CHARGING_DASH :
+                warpChargeStatus ? CHARGING_WARP :
                 maxChargingWattage <= 0 ? CHARGING_UNKNOWN :
                 maxChargingWattage < slowThreshold ? CHARGING_SLOWLY :
                         maxChargingWattage > fastThreshold ? CHARGING_FAST :
--- services/core/java/com/android/server/BatteryService.java
+++ services/core/java/com/android/server/BatteryService.java
@@ -178,6 +178,10 @@ public final class BatteryService extends SystemService {
     private boolean mHasDashCharger;
     private boolean mLastDashCharger;
 
+    private boolean mWarpCharger;
+    private boolean mHasWarpCharger;
+    private boolean mLastWarpCharger;
+
     private long mDischargeStartTime;
     private int mDischargeStartLevel;
 
@@ -213,6 +217,8 @@ public final class BatteryService extends SystemService {
 
         mHasDashCharger = mContext.getResources().getBoolean(
                 com.android.internal.R.bool.config_hasDashCharger);
+        mHasWarpCharger = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_hasWarpCharger);
 
         mCriticalBatteryLevel = mContext.getResources().getInteger(
                 com.android.internal.R.integer.config_criticalBatteryWarningLevel);
@@ -521,6 +527,8 @@ public final class BatteryService extends SystemService {
 
         mOemFastCharger = isOemFastCharger();
         mDashCharger = mHasDashCharger && isDashCharger();
+        mWarpCharger = mHasWarpCharger && isWarpCharger();
+
         if (force
                 || (mHealthInfo.batteryStatus != mLastBatteryStatus
                         || mHealthInfo.batteryHealth != mLastBatteryHealth
@@ -534,7 +542,8 @@ public final class BatteryService extends SystemService {
                         || mHealthInfo.batteryChargeCounterUah != mLastChargeCounter
                         || mInvalidCharger != mLastInvalidCharger
                         || mOemFastCharger != mLastOemFastCharger
-                        || mDashCharger != mLastDashCharger)) {
+                        || mDashCharger != mLastDashCharger
+                        || mWarpCharger != mLastWarpCharger)) {
 
             if (mPlugType != mLastPlugType) {
                 if (mLastPlugType == BATTERY_PLUGGED_NONE) {
@@ -710,6 +719,7 @@ public final class BatteryService extends SystemService {
             mLastInvalidCharger = mInvalidCharger;
             mLastOemFastCharger = mOemFastCharger;
             mLastDashCharger = mDashCharger;
+            mLastWarpCharger = mWarpCharger;
         }
     }
 
@@ -743,6 +753,7 @@ public final class BatteryService extends SystemService {
         intent.putExtra(BatteryManager.EXTRA_CHARGE_COUNTER, mHealthInfo.batteryChargeCounterUah);
         intent.putExtra(BatteryManager.EXTRA_OEM_FAST_CHARGER, mOemFastCharger);
         intent.putExtra(BatteryManager.EXTRA_DASH_CHARGER, mDashCharger);
+        intent.putExtra(BatteryManager.EXTRA_WARP_CHARGER, mWarpCharger);
         if (DEBUG) {
             Slog.d(TAG, "Sending ACTION_BATTERY_CHANGED. scale:" + BATTERY_SCALE
                     + ", info:" + mHealthInfo.toString()
@@ -817,6 +828,20 @@ public final class BatteryService extends SystemService {
     }
 
     private boolean isDashCharger() {
+        try {
+            FileReader file = new FileReader("/sys/class/power_supply/battery/fastchg_status");
+            BufferedReader br = new BufferedReader(file);
+            String state = br.readLine();
+            br.close();
+            file.close();
+            return "1".equals(state);
+        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
+        }
+        return false;
+    }
+
+    private boolean isWarpCharger() {
         try {
             FileReader file = new FileReader("/sys/class/power_supply/battery/fastchg_status");
             BufferedReader br = new BufferedReader(file);
