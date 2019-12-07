aaa3aecd7f24fa480c85d9d451c385742d43a3b6
AnkitGourav <Ankitgourav035@gmail.com>
2019-12-07 15:43:55 +0530

    Add Network Traffic Layout [1/2]
    - Added option to select horizontal/verticle layout
    - horizontal layout was same as before
    - verticle layout is based on https://github.com/ArrowOS/android_frameworks_base/commit/e211cafe844abac4fb417400fe2e3caa0ff41202
    - Update units to KiB & MiB from KB & MB as of IEC standards
    
    Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>
    
    Change-Id: I7ba19aaa7b3aeeed2ff833fb2fe6c86f9d83a82e
    
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Change-Id: Ida25284099ea038991ef505f17ff6f995a6db8bb

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5917,6 +5917,12 @@ public final class Settings {
          */
          public static final String NETWORK_TRAFFIC_FONT_SIZE = "network_traffic_font_size";
 
+         /**
+         * Network traffic layout
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_LAYOUT = "network_traffic_layout";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
@@ -6044,6 +6050,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_TYPE);
             PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD);
 	    PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_FONT_SIZE);
+ 	    PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_LAYOUT);
         }
 
         /**
