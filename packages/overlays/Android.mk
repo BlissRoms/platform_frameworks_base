2533e22b042bd19c92478d4d303b32d68162a113
Dyneteve <dyneteve@pixelexperience.org>
2019-12-19 19:50:18 +0000

    Revert "Move font overlay packages to Pixel."
    * Also some minor edits to the revert
    
    Change-Id: I2e9a933451e12fcf7016a0992676db376c7f357d

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -35,6 +35,9 @@ LOCAL_REQUIRED_MODULES := \
     DisplayCutoutEmulationHoleOverlay \
 	DisplayCutoutEmulationTallOverlay \
 	DisplayCutoutEmulationWaterfallOverlay \
+	FontArbutusSourceOverlay \
+	FontArvoLatoOverlay \
+	FontRubikRubikOverlay \
 	IconPackCircularAndroidOverlay \
 	IconPackCircularLauncherOverlay \
 	IconPackCircularSettingsOverlay \
