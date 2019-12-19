55d00c59bd5caee434d8cdc601f84ec1f0151486
00day0 <therandomuser11@gmail.com>
2019-12-19 23:29:50 +0100

    [SQUASH] base: Tint QS Tile with accent color [1/2]
    * Tinted QS Tile stuffs (icon, background, label)
    * Using bolder font
    
    Adjust QS tile background and icon color
    
    + make qs tile icon smaller by 1dp
    + reduce qs tile vertical margin
    
    SystemUI: Add a toggle for the new QS tint [1/2]
    
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Change-Id: I599c9aa1353f76ff0685cea091cc7f4a28843deb
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Ayan Mukherjee <mukherjeeayan725@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5682,6 +5682,12 @@ public final class Settings {
         public static final String LOCKSCREEN_BLUR = "lockscreen_blur";
 
         /**
+         * Whether to use new QS panel bg tint or not
+         * @hide
+         */
+        public static final String QS_PANEL_BG_USE_NEW_TINT = "qs_panel_bg_use_new_tint";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -5825,6 +5831,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(USE_OLD_MOBILETYPE);
             PRIVATE_SETTINGS.add(FOOTER_TEXT_SHOW);
             PRIVATE_SETTINGS.add(SETTINGS_DASHBOARD_ICONS);
+            PRIVATE_SETTINGS.add(QS_PANEL_BG_USE_NEW_TINT);
         }
 
         /**
