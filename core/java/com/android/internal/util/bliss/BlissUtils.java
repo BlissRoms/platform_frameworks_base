a623ea11d48ad05a3477acceb5167d2050e3b4bb
Alex Cruz <du.alexcruz@gmail.com>
2019-12-22 14:39:55 -0500

    BlissUtils: Add method to detect whether overlay is enabled
    Change-Id: Ibd4ecb00ceed29604fdcd5d2822edcdb3ac399e2

--- core/java/com/android/internal/util/bliss/BlissUtils.java
+++ core/java/com/android/internal/util/bliss/BlissUtils.java
@@ -21,6 +21,8 @@ import android.app.NotificationManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.om.IOverlayManager;
+import android.content.om.OverlayInfo;
 import android.content.pm.ActivityInfo;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
@@ -62,6 +64,8 @@ import java.util.List;
 import java.util.Locale;
 
 public class BlissUtils {
+
+    private static OverlayManager mOverlayService;
 
     private static final String TAG = "Utils";
 
@@ -560,6 +564,42 @@ public class BlissUtils {
             case AudioManager.RINGER_MODE_SILENT:
                 am.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
                 break;
+        }
+    }
+
+    // Method to detect whether an overlay is enabled or not
+    public static boolean isThemeEnabled(String packageName) {
+        mOverlayService = new OverlayManager();
+        try {
+            List<OverlayInfo> infos = mOverlayService.getOverlayInfosForTarget("android",
+                    UserHandle.myUserId());
+            for (int i = 0, size = infos.size(); i < size; i++) {
+                if (infos.get(i).packageName.equals(packageName)) {
+                    return infos.get(i).isEnabled();
+                }
+            }
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public static class OverlayManager {
+        private final IOverlayManager mService;
+
+        public OverlayManager() {
+            mService = IOverlayManager.Stub.asInterface(
+                    ServiceManager.getService(Context.OVERLAY_SERVICE));
+        }
+
+        public void setEnabled(String pkg, boolean enabled, int userId)
+                throws RemoteException {
+            mService.setEnabled(pkg, enabled, userId);
+        }
+
+        public List<OverlayInfo> getOverlayInfosForTarget(String target, int userId)
+                throws RemoteException {
+            return mService.getOverlayInfosForTarget(target, userId);
         }
     }
 }
