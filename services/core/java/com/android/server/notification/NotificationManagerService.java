9f8d122e3c1a30e04b9d5199919305fcfac89a45
AnkitGourav <Ankitgourav035@gmail.com>
2019-12-25 11:06:26 +0530

    GamingMode: Option to disable notification feedback [1/2]
    - Disables notifications sound & vibration
    
        I want it since I want calls but I don't want notifications neither their feedback.
    
    Change-Id: I5602378a79bd52784694bea035fa2cc7de3ba2f9
    Signed-off-by: rohan <purohit.rohan@gmail.com>

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -410,6 +410,10 @@ public class NotificationManagerService extends SystemService {
 
     private boolean mSoundVibScreenOn;
 
+    // Gaming Mode
+    private boolean mGamingMode;
+    private boolean mSoundVibGamingMode;
+
     private Uri mInCallNotificationUri;
     private AudioAttributes mInCallNotificationAudioAttributes;
     private float mInCallNotificationVolume;
@@ -1409,6 +1413,10 @@ public class NotificationManagerService extends SystemService {
                 = Settings.Global.getUriFor(Settings.Global.MAX_NOTIFICATION_ENQUEUE_RATE);
         private final Uri NOTIFICATION_SOUND_VIB_SCREEN_ON
                 = Settings.System.getUriFor(Settings.System.NOTIFICATION_SOUND_VIB_SCREEN_ON);
+        private final Uri GAMING_MODE_ACTIVE
+                = Settings.System.getUriFor(Settings.System.GAMING_MODE_ACTIVE);
+        private final Uri GAMING_MODE_NOTIFICATIONS_FEEDBACK
+                = Settings.System.getUriFor(Settings.System.GAMING_MODE_NOTIFICATIONS_FEEDBACK);
 
         SettingsObserver(Handler handler) {
             super(handler);
@@ -1427,6 +1435,10 @@ public class NotificationManagerService extends SystemService {
             resolver.registerContentObserver(NOTIFICATION_BUBBLES_URI_SECURE,
                     false, this, UserHandle.USER_ALL);
             resolver.registerContentObserver(NOTIFICATION_SOUND_VIB_SCREEN_ON,
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(GAMING_MODE_ACTIVE,
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(GAMING_MODE_NOTIFICATIONS_FEEDBACK,
                     false, this, UserHandle.USER_ALL);
             update(null);
         }
@@ -1469,6 +1481,17 @@ public class NotificationManagerService extends SystemService {
 
             if (NOTIFICATION_BUBBLES_URI_SECURE.equals(uri)) {
                 syncBubbleSettings(resolver, NOTIFICATION_BUBBLES_URI_SECURE);
+            }
+
+            if (uri == null || GAMING_MODE_ACTIVE.equals(uri)) {
+                mGamingMode = Settings.System.getIntForUser(resolver,
+                        Settings.System.GAMING_MODE_ACTIVE, 0,
+                        UserHandle.USER_CURRENT) == 1;
+            }
+            if (uri == null || GAMING_MODE_NOTIFICATIONS_FEEDBACK.equals(uri)) {
+                mSoundVibGamingMode = Settings.System.getIntForUser(resolver,
+                        Settings.System.GAMING_MODE_NOTIFICATIONS_FEEDBACK, 1,
+                        UserHandle.USER_CURRENT) == 1;
             }
         }
 
@@ -5913,7 +5936,8 @@ public class NotificationManagerService extends SystemService {
         if (aboveThreshold && isNotificationForCurrentUser(record)) {
 
             boolean beNoisy = !mScreenOn || (mScreenOn && mSoundVibScreenOn);
-            if (mSystemReady && mAudioManager != null && beNoisy) {
+            boolean noNoiseOnGamingMode = (mScreenOn && mGamingMode && mSoundVibGamingMode);
+            if (mSystemReady && mAudioManager != null && beNoisy && !noNoiseOnGamingMode) {
                 Uri soundUri = record.getSound();
                 hasValidSound = soundUri != null && !Uri.EMPTY.equals(soundUri);
                 long[] vibration = record.getVibration();
