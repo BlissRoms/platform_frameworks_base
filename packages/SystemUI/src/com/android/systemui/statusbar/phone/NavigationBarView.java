omnirom/android_frameworks_base_old

SystsmUI: gesture mode navbar nitpick
nor sure if that will ever made a diff but it looks just wrong

Change-Id: I6be3433f3bbdb6897bf1f5fd45fb88eb0a994005

packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -1002,13 +1002,16 @@ public class NavigationBarView extends FrameLayout implements
 
         if (isGesturalMode(mNavBarMode)) {
             // Update the nav bar background to match the height of the visible nav bar
-            int height = mIsVertical
+            int height = newVertical
                     ? getResources().getDimensionPixelSize(
                             com.android.internal.R.dimen.navigation_bar_height_landscape)
                     : getResources().getDimensionPixelSize(
                             com.android.internal.R.dimen.navigation_bar_height);
-            int frameHeight = getResources().getDimensionPixelSize(
-                    com.android.internal.R.dimen.navigation_bar_frame_height);
+            int frameHeight = newVertical
+                    ? getResources().getDimensionPixelSize(
+                            com.android.internal.R.dimen.navigation_bar_frame_height)
+                    : getResources().getDimensionPixelSize(
+                            com.android.internal.R.dimen.navigation_bar_frame_height_landscape);
             mBarTransitions.setBackgroundFrame(new Rect(0, frameHeight - height, w, h));
         }
 

BlissRoms/platform_frameworks_base

packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -1054,13 +1054,16 @@ public class NavigationBarView extends FrameLayout implements
 
         if (isGesturalMode(mNavBarMode)) {
             // Update the nav bar background to match the height of the visible nav bar
-            int height = mIsVertical
+            int height = newVertical
                     ? getResources().getDimensionPixelSize(
                             com.android.internal.R.dimen.navigation_bar_height_landscape)
                     : getResources().getDimensionPixelSize(
                             com.android.internal.R.dimen.navigation_bar_height);
-            int frameHeight = getResources().getDimensionPixelSize(
-                    com.android.internal.R.dimen.navigation_bar_frame_height);
+            int frameHeight = newVertical
+                    ? getResources().getDimensionPixelSize(
+                            com.android.internal.R.dimen.navigation_bar_frame_height_landscape)
+                    : getResources().getDimensionPixelSize(
+                            com.android.internal.R.dimen.navigation_bar_frame_height);
             mBarTransitions.setBackgroundFrame(new Rect(0, frameHeight - height, w, h));
         }
 