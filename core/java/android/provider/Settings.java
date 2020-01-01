2d7c4d867560bc3cccd56ba9cc7e6f865d3e54e1
00day0 <therandomuser11@gmail.com>
2020-01-01 21:19:16 +0100

    OneUI-like settings actionbar [1/3]
    todo: add settings observer so you won't need to restart settings app to toggle it
    
    Signed-off-by: 00day0 <therandomuser11@gmail.com>
    Change-Id: I882ce24dd9b4b8fc9ca60997aefb682029ad7269

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6444,6 +6444,13 @@ public final class Settings {
         public static final String SCREEN_STATE_ON_DELAY = "screen_state_on_delay";
 
         /**
+         * Some creation simillar to samsung OneUI
+         * @hide
+         */
+        public static final String SETTINGS_SPACER = "settings_spacer";
+
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
