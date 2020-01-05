d420e55785fd2a46d82174093d10964a4736cbdc
SuperDroidBond <superdroidbond@yahoo.com>
2020-01-05 20:47:06 +0530

    Introduce RGB Gradient Picker [1/2]
    inspired and based on POSP RGB Accent Picker by AgentFabulous
    Commit - https://github.com/PotatoProject/frameworks_base/commit/1282c5441424a9e2bc923ca5c2404a5611725201
    
    Change-Id: Iad818a88f1b36f3e44f8b6216f8bc39bee749d5c

--- core/java/android/content/res/AccentUtils.java
+++ core/java/android/content/res/AccentUtils.java
@@ -16,7 +16,11 @@ public class AccentUtils {
                     "accent_device_default_dark",
                     "gradient_start"));
 
+    private static ArrayList<String> gradientResources = new ArrayList<>(
+            Arrays.asList("gradient_end"));
+
     private static final String ACCENT_COLOR_PROP = "persist.sys.theme.accentcolor";
+    private static final String GRADIENT_COLOR_PROP = "persist.sys.theme.gradientcolor";
 
     static boolean isResourceAccent(String resName) {
         for (String ar : accentResources)
@@ -25,8 +29,19 @@ public class AccentUtils {
         return false;
     }
 
+    static boolean isResourceGradient(String resName) {
+        for (String gr : gradientResources)
+            if (resName.contains(gr))
+                return true;
+        return false;
+    }
+
     public static int getNewAccentColor(int defaultColor) {
         return getAccentColor(defaultColor, ACCENT_COLOR_PROP);
+    }
+
+    public static int getNewGradientColor(int defaultColor) {
+        return getAccentColor(defaultColor, GRADIENT_COLOR_PROP);
     }
 
     private static int getAccentColor(int defaultColor, String property) {
