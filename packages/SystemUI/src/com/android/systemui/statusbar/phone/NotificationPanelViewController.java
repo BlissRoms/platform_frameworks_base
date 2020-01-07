4caa6bdccc75b6a540601d56f28c30743ab592ac
maxwen <max.weninger@gmail.com>
2020-01-07 21:50:30 +0300

    SystemUI: custom keyguard clock kick off
    Blissroms edit: Stripped down version of the original,
    remove one line and two line clock.
    
    AicpEdit: keep dateline
    
    allow custom clock to be in the same position as the default
    clock. Add bold hour clock back from the days where we
    had custom keyguard clock already
    
    [poad42] Port to eleven
    Change-Id: I11bb2b0b70327bfc824b126f07043db49262c13b
    
    Change-Id: I7953a645896b31ee6833db4f0e0736759dac5b98
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -709,6 +709,10 @@ public class NotificationPanelViewController extends PanelViewController {
         return mKeyguardStatusView;
     }
 
+    public boolean hasCustomClockInBigContainer() {
+        return mKeyguardStatusView.hasCustomClockInBigContainer();
+    }
+
     private void setStatusBar(StatusBar bar) {
         // TODO: this can be injected.
         mStatusBar = bar;
@@ -864,7 +868,7 @@ public class NotificationPanelViewController extends PanelViewController {
             mClockPositionAlgorithm.setup(mStatusBarMinHeight, totalHeight - bottomPadding,
                     mNotificationStackScroller.getIntrinsicContentHeight(), getExpandedFraction(),
                     totalHeight, (int) (mKeyguardStatusView.getHeight() - mShelfHeight / 2.0f
-                            - mDarkIconSize / 2.0f), clockPreferredY, hasCustomClock(),
+                            - mDarkIconSize / 2.0f), clockPreferredY, hasCustomClockInBigContainer(),
                     hasVisibleNotifications, mInterpolatedDarkAmount, mEmptyDragAmount,
                     bypassEnabled, getUnlockedStackScrollerPadding());
             mClockPositionAlgorithm.run(mClockPositionResult);
