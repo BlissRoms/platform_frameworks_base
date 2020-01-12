791a2f14048a13d54162414901c178838f5733de
Josh Fox <joshfox87@gmail.com>
2020-01-12 16:23:09 -0700

    Overlay: Add Nature primary
    Change-Id: If96555465a10fd252de87ae306bae003cd451e5a

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -74,6 +74,7 @@ LOCAL_REQUIRED_MODULES := \
 	PrimaryColorBlissClearOverlay \
 	PrimaryColorEyeSootherOverlay \
 	PrimaryColorOceanOverlay \
+	PrimaryColorNatureOverlay \
 	NavigationBarMode3ButtonOverlay \
 	NavigationBarMode2ButtonOverlay \
 	NavigationBarModeGesturalOverlay \
