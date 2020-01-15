9032349c11ad338c24ffe18b4a1e828fadbfa131
Anushek Prasal <anushekprasal@gmail.com>
2020-01-15 20:34:54 +0530

    Decrease line spacing in sammy clock styles
    Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Change-Id: I564c8cc08b252c5dcc3b48b691eb790009d73614
    Signed-off-by: rohan <purohit.rohan@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -38,6 +38,7 @@ import android.util.TypedValue;
 import android.view.View;
 import android.widget.GridLayout;
 import android.widget.LinearLayout;
+import android.widget.TextClock;
 import android.widget.TextView;
 
 import androidx.core.graphics.ColorUtils;
@@ -121,6 +122,7 @@ public class KeyguardStatusView extends GridLayout implements
     private KeyguardClockSwitch mClockView;
     private View mSmallClockView;
     private TextView mOwnerInfo;
+    private TextClock mDefaultClockView;
     private KeyguardSliceView mKeyguardSlice;
     private View mNotificationIcons;
     private Runnable mPendingMarqueeStart;
@@ -292,6 +294,7 @@ public class KeyguardStatusView extends GridLayout implements
         }
 
         mClockView = findViewById(R.id.keyguard_clock_container);
+        mDefaultClockView = findViewById(R.id.default_clock_view);
         mClockView.setShowCurrentUserTime(true);
         mSmallClockView  = findViewById(R.id.clock_view);
         mOwnerInfo = findViewById(R.id.owner_info);
@@ -354,6 +357,10 @@ public class KeyguardStatusView extends GridLayout implements
             } else {
                 setFontSize(mClockView, mLockClockFontSize);
             }
+
+            if (mClockSelection == 8 || mClockSelection == 9
+                    || mClockSelection == 10 || mClockSelection == 11)
+                mDefaultClockView.setLineSpacing(0, 0.8f);
 
             switch (mClockSelection) {
                 case 1: // hidden
