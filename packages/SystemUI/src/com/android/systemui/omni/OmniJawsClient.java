55342b5be128e07171c93422a54c06866ab48457
Ali B <abittin@gmail.com>
2020-01-24 14:46:41 +0300

    base: omnijaws: fix settings intent
    * also we r not aicp, fix fragment
    Change-Id: I9354b1b2fad8cdba07fe3c0782c0b4dd0155c8c3

--- packages/SystemUI/src/com/android/systemui/omni/OmniJawsClient.java
+++ packages/SystemUI/src/com/android/systemui/omni/OmniJawsClient.java
@@ -86,10 +86,10 @@ public class OmniJawsClient {
     private static final String WEATHER_UPDATE = "org.omnirom.omnijaws.WEATHER_UPDATE";
     private static final String WEATHER_ERROR = "org.omnirom.omnijaws.WEATHER_ERROR";
 
-    private static final String AE_EXTRA_FRAGMENT_CLASS = "com.aicp.extras.extra.preference_fragment";
-    private static final String AE_SETTINGSACTIVITY = "com.aicp.extras.SettingsActivity";
-    private static final String SETTINGS_PACKAGE_NAME = "com.aicp.extras";
-    private static final String WEATHER_SETTINGS = "com.aicp.extras.fragments.Weather";
+    private static final String AE_SETTINGSACTIVITY = "com.blissroms.blissify.SettingsActivity";
+    private static final String SETTINGS_PACKAGE_NAME = "com.blissroms.blissify";
+    private static final String WEATHER_SETTINGS = "com.blissroms.blissify.fragments.lockscreen.Weather";
+    private static final String EXTRA_SHOW_FRAGMENT = ":android:show_fragment";
 
     private static final DecimalFormat sNoDigitsFormat = new DecimalFormat("0");
 
@@ -249,7 +249,7 @@ public class OmniJawsClient {
         if (isOmniJawsServiceInstalled()) {
             Intent settings = new Intent(Intent.ACTION_MAIN);
             settings.setClassName(SETTINGS_PACKAGE_NAME, AE_SETTINGSACTIVITY);
-            settings.putExtra(AE_EXTRA_FRAGMENT_CLASS, WEATHER_SETTINGS);
+            settings.putExtra(EXTRA_SHOW_FRAGMENT, WEATHER_SETTINGS);
             return settings;
         }
         return null;
