e97a9e5e0b8433ceb85432e6f8ffbbbfc5fcbedc
ZVNexus <zvnexus@outlook.com>
2020-01-24 17:00:41 -0500

    SystemUI: Make tri-state SystemUI dialog dimensions conditional
    Adjust this based on the left/right config.
    
    SKULSHADY: Fix dimens for right dialog
    
    Change-Id: I282f1a318bc9bf1cfd20c6b8c41be5f79fab4534
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- packages/SystemUI/src/com/android/systemui/tristate/TriStateUiControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/tristate/TriStateUiControllerImpl.java
@@ -356,7 +356,7 @@ public class TriStateUiControllerImpl implements ConfigurationListener, TriState
                             if (mTriStateMode != MODE_VIBRATE) {
                                 if (mTriStateMode == MODE_NORMAL) {
                                     positionY2 = res.getDimensionPixelSize(R.dimen.tri_state_down_dialog_position) + res.getDimensionPixelSize(com.android.internal.R.dimen.status_bar_height);
-                                    bg = R.drawable.dialog_tri_state_down_bg;
+                                    bg = isTsKeyRight ? R.drawable.right_dialog_tri_state_down_bg : R.drawable.left_dialog_tri_state_down_bg;
                                     break;
                                 }
                             }
@@ -365,7 +365,7 @@ public class TriStateUiControllerImpl implements ConfigurationListener, TriState
                             break;
                         }
                         positionY2 = res.getDimensionPixelSize(R.dimen.tri_state_up_dialog_position) + res.getDimensionPixelSize(com.android.internal.R.dimen.status_bar_height);
-                        bg = R.drawable.dialog_tri_state_up_bg;
+                        bg = isTsKeyRight ? R.drawable.right_dialog_tri_state_up_bg : R.drawable.left_dialog_tri_state_up_bg;
                         break;
                 }
                 if (mTriStateMode != -1) {
