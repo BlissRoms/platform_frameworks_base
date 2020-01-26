f2d592bcc4f6df0ed27f7c1223f4cacea37b05d7
blinoff82 <blinov.in@gmail.com>
2020-01-26 13:24:36 +0300

    base: Add San Francisco Display Pro font [1/2]
    Signed-off-by: blinoff82 <blinov.in@gmail.com>
    Change-Id: I771fd8ced670e064a9cdb8d60b79491d6e73cabd

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -58,6 +58,7 @@ LOCAL_REQUIRED_MODULES := \
 	FontQuandoSourceOverlay \
 	FontGrandHotelSourceOverlay \
 	FontRedressedSourceOverlay \
+	FontSanFranciscoDisplayProSourceOverlay \
 	IconPackCircularAndroidOverlay \
 	IconPackCircularPixelLauncherOverlay \
 	IconPackCircularPixelThemePickerOverlay \
