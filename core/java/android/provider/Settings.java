1c649fcf208f870506b2b7da949860debc1f0927
00day0 <therandomuser11@gmail.com>
2020-01-28 00:07:58 +0100

    FODCircleView: Recognizing fingerprint animation [1/2]
    Change-Id: I485e83e12557f5e2d443012c1d6475d2d690f85d
    Signed-off-by: 00day0 <therandomuser11@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5996,6 +5996,12 @@ public final class Settings {
         public static final String RINGTONE_VIBRATION_PATTERN = "ringtone_vibration_pattern";
 
         /**
+         * FOD recognizing animation
+         * @hide
+         */
+        public static final String FOD_RECOGNIZING_ANIMATION = "fod_recognizing_animation";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
