bd47d4aac1806361e4f9355d7681ac8ba6f6faab
Marko Man <darkobas@gmail.com>
2020-01-29 00:24:33 +0100

    SystemUI: Pulse light accent color option
    Change-Id: If402e96d7549aff7f758236e6f54a6034d08f9c2

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5796,6 +5796,16 @@ public final class Settings {
         private static final Validator AMBIENT_NOTIFICATION_LIGHT_HIDE_AOD_VALIDATOR =
                 BOOLEAN_VALIDATOR;
 
+         /**
+         * Whether to use accent color for pulse
+         * @hide
+         */
+
+        public static final String AMBIENT_NOTIFICATION_LIGHT_ACCENT = "ambient_notification_light_accent";
+
+        private static final Validator AMBIENT_NOTIFICATION_LIGHT_ACCENT_VALIDATOR =
+                BOOLEAN_VALIDATOR;
+
         /**
          * helper setting to contain the current pulse reason so we can check
          * if it has been triggered from a notification or a gesture
@@ -6037,6 +6047,7 @@ public final class Settings {
             AMBIENT_NOTIFICATION_LIGHT_ACTIVATED,
             AMBIENT_NOTIFICATION_LIGHT_ENABLED,
             AMBIENT_NOTIFICATION_LIGHT_HIDE_AOD,
+            AMBIENT_NOTIFICATION_LIGHT_ACCENT,
         };
 
         /**
@@ -6369,6 +6380,7 @@ public final class Settings {
             VALIDATORS.put(AMBIENT_NOTIFICATION_LIGHT_ACTIVATED, AMBIENT_NOTIFICATION_LIGHT_ACTIVATED_VALIDATOR);
             VALIDATORS.put(AMBIENT_NOTIFICATION_LIGHT_ENABLED, AMBIENT_NOTIFICATION_LIGHT_ENABLED_VALIDATOR);
             VALIDATORS.put(AMBIENT_NOTIFICATION_LIGHT_HIDE_AOD, AMBIENT_NOTIFICATION_LIGHT_HIDE_AOD_VALIDATOR);
+            VALIDATORS.put(AMBIENT_NOTIFICATION_LIGHT_ACCENT, AMBIENT_NOTIFICATION_LIGHT_ACCENT_VALIDATOR);
         }
 
         /**
