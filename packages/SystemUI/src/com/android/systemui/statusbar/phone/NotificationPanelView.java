2a285d41098169c940ebab9e357bb92bcb9ab184
maxwen <max.weninger@gmail.com>
2020-02-01 23:28:03 +0100

    SystemUI: only call  showAodContent(true) if we are still on the keyguard
    Change-Id: I51439c3596d32532fba650a91d1213b041299db4

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -3431,9 +3431,13 @@ public class NotificationPanelView extends PanelView implements
                 // if the current notifications "would" turn the screen on
                 // just checking hasActiveClearableNotifications is obviusly not
                 // enough here - so for now dont even try to do it
+                showAodContent(true);
             } else {
                 // screen on!
-                showAodContent(true);
+                if (mBarState == StatusBarState.KEYGUARD
+                        || mBarState == StatusBarState.SHADE_LOCKED) {
+                    showAodContent(true);
+                }
                 mPulseLightsView.setVisibility(View.GONE);
                 Settings.System.putIntForUser(mContext.getContentResolver(),
                          Settings.System.AMBIENT_NOTIFICATION_LIGHT, 0,
@@ -3520,7 +3524,10 @@ public class NotificationPanelView extends PanelView implements
                     Settings.System.putIntForUser(mContext.getContentResolver(),
                             Settings.System.AMBIENT_NOTIFICATION_LIGHT, 0,
                             UserHandle.USER_CURRENT);
-                    showAodContent(true);
+                    if (mBarState == StatusBarState.KEYGUARD
+                            || mBarState == StatusBarState.SHADE_LOCKED) {
+                        showAodContent(true);
+                    }
                 }
             }
         }
@@ -3530,6 +3537,9 @@ public class NotificationPanelView extends PanelView implements
     }
 
     private void showAodContent(boolean show) {
+        if (DEBUG_PULSE_LIGHT) {
+            Log.d(TAG, "showAodContent show = " + show);
+        }
         mKeyguardStatusView.setVisibility(show ? View.VISIBLE : View.INVISIBLE);
         mKeyguardStatusBar.setVisibility(show ? View.VISIBLE : View.INVISIBLE);
         mKeyguardBottomArea.setVisibility(show ? View.VISIBLE : View.INVISIBLE);
