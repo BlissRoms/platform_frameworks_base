c217e9bd8be076336af6daffe8ec3f063f47ea31
AnkitGourav <Ankitgourav035@gmail.com>
2020-02-04 15:02:53 +0530

    SystemUI: Custom Text Clock Customizations [1/2]
    - Add customizable clock alignment
    - Add customizable edge padding
    
    Improve Lockscreen CustomTextClock Style
    - Cleanup
    - Make It's color based on Accent Color
    - Add 24hr support
    
    BlissRoms{@rohanpurohit] edit:
    - remove clock and font changes ( will be added later )
    - move keyguardslice handling to KeyguardClockSwitch to avoid layout changes
      which breaks padding/gravity for custom plugin clocks
    
    Change-Id: I799eaab2b6df5c28e331e5c72e4ba936aeaff6eb
    Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>
    Signed-off-by: rohan <purohit.rohan@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/clock/CustomTextClock.java
+++ packages/SystemUI/src/com/android/keyguard/clock/CustomTextClock.java
@@ -15,7 +15,6 @@
  */
 package com.android.keyguard.clock;
 
-import android.app.WallpaperManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.ContentResolver;
@@ -24,9 +23,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.database.ContentObserver;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Typeface;
-import android.os.Bundle;
 import android.os.Handler;
 import android.os.UserHandle;
 import android.provider.Settings;
@@ -38,40 +35,27 @@ import android.text.TextUtils;
 import android.text.format.DateFormat;
 import android.text.style.ForegroundColorSpan;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.widget.TextView;
 
-import com.android.internal.colorextraction.ColorExtractor;
-import com.android.internal.colorextraction.ColorExtractor.GradientColors;
-import com.android.internal.graphics.ColorUtils;
 import com.android.systemui.R;
-import com.android.systemui.Dependency;
-import com.android.systemui.colorextraction.SysuiColorExtractor;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.TimeZone;
 
-public class CustomTextClock extends TextView implements ColorExtractor.OnColorsChangedListener {
+public class CustomTextClock extends TextView {
 
     private String mDescFormat;
-    private final String[] mHours;
-    private final String[] mMinutes;
+    private String[] mHours;
+    private String[] mMinutes;
     private final Resources mResources;
-    private final Calendar mTime;
+    private final Calendar mTime = Calendar.getInstance(TimeZone.getDefault());
     private TimeZone mTimeZone;
-    private SysuiColorExtractor mColorExtractor;
 
-    private int mPrimaryColor;
-    private int mAmbientColor;
-    private int mSystemAccent;
-    private int mFallbackColor;
-    private int mCurrentAccent;
-    private float mDarkAmount;
-    private float[] mHslOut = new float[3];
-
-    private int mClockSize = 40;
-    private SettingsObserver mSettingsObserver;
+    private boolean h24;
+    private int mAccentColor;
+    private int hours;
+    private int mClockSize = 54;
 
     private final BroadcastReceiver mTimeZoneChangedReceiver = new BroadcastReceiver() {
         @Override
@@ -94,30 +78,36 @@ public class CustomTextClock extends TextView implements ColorExtractor.OnColors
 
     public CustomTextClock(Context context, AttributeSet attributeSet, int defStyleAttr) {
         super(context, attributeSet, defStyleAttr);
-
-        mColorExtractor = Dependency.get(SysuiColorExtractor.class);
-        mColorExtractor.addOnColorsChangedListener(this);
-        mTime = Calendar.getInstance(TimeZone.getDefault());
         mDescFormat = ((SimpleDateFormat) DateFormat.getTimeFormat(context)).toLocalizedPattern();
         mResources = context.getResources();
-        mHours = mResources.getStringArray(R.array.type_clock_hours);
-        mMinutes = mResources.getStringArray(R.array.type_clock_minutes);
-        mSystemAccent = mResources.getColor(R.color.accent_device_default_light, null);
-        mFallbackColor = mResources.getColor(R.color.custom_text_clock_top_fallback_color, null);
-        onColorsChanged(mColorExtractor, 0);
+        h24 = DateFormat.is24HourFormat(getContext());
+        if (!h24) mHours = mResources.getStringArray(R.array.type_clock_hours_12);
+            else mHours = mResources.getStringArray(R.array.type_clock_hours_24);
+        if (qpie()) mMinutes = mResources.getStringArray(R.array.type_clock_minutes);
+            else mMinutes = mResources.getStringArray(R.array.type_clock_minutes_alt);
+        mAccentColor = mResources.getColor(R.color.accent_device_default_light);
     }
 
     public void onTimeChanged() {
+        h24 = DateFormat.is24HourFormat(getContext());
         mTime.setTimeInMillis(System.currentTimeMillis());
         setContentDescription(DateFormat.format(mDescFormat, mTime));
-        int hours = mTime.get(Calendar.HOUR) % 12;
-        int minutes = mTime.get(Calendar.MINUTE) % 60;
+        if (!h24) {
+             mHours = mResources.getStringArray(R.array.type_clock_hours_12);
+             hours = mTime.get(Calendar.HOUR) % 12;
+        } else {
+             mHours = mResources.getStringArray(R.array.type_clock_hours_24);
+             hours = mTime.get(Calendar.HOUR_OF_DAY);
+        }
+        if (qpie()) mMinutes = mResources.getStringArray(R.array.type_clock_minutes);
+            else mMinutes = mResources.getStringArray(R.array.type_clock_minutes_alt);
+        final int minutes = mTime.get(Calendar.MINUTE) % 60;
         SpannedString rawFormat = (SpannedString) mResources.getQuantityText(R.plurals.type_clock_header, hours);
         Annotation[] annotationArr = (Annotation[]) rawFormat.getSpans(0, rawFormat.length(), Annotation.class);
         SpannableString colored = new SpannableString(rawFormat);
         for (Annotation annotation : annotationArr) {
             if ("color".equals(annotation.getValue())) {
-                colored.setSpan(new ForegroundColorSpan(mCurrentAccent),
+                colored.setSpan(new ForegroundColorSpan(mAccentColor),
                         colored.getSpanStart(annotation),
                         colored.getSpanEnd(annotation),
                         Spanned.SPAN_POINT_POINT);
@@ -129,52 +119,6 @@ public class CustomTextClock extends TextView implements ColorExtractor.OnColors
     public void onTimeZoneChanged(TimeZone timeZone) {
         mTimeZone = timeZone;
         mTime.setTimeZone(timeZone);
-    }
-
-    @Override
-    public void onColorsChanged(ColorExtractor extractor, int which) {
-        GradientColors colors = extractor.getColors(WallpaperManager.FLAG_LOCK);
-        setWallpaperColors(colors.getMainColor(), colors.supportsDarkText(), colors.getColorPalette());
-    }
-
-    private void setWallpaperColors(int mainColor, boolean supportsDarkText, int[] colorPalette) {
-        int scrimColor = supportsDarkText ? Color.WHITE : Color.BLACK;
-        int scrimTinted = ColorUtils.setAlphaComponent(ColorUtils.blendARGB(scrimColor, mainColor, 0.5f), 64);
-        int bgColor = ColorUtils.compositeColors(scrimTinted, mainColor);
-
-        int paletteColor = getColorFromPalette(colorPalette);
-        bgColor = ColorUtils.compositeColors(bgColor, Color.BLACK);
-        mPrimaryColor = findColor(paletteColor, bgColor, !supportsDarkText, mSystemAccent, mFallbackColor);
-        mAmbientColor = findColor(paletteColor, Color.BLACK, true, mSystemAccent, mFallbackColor);
-
-        setDarkAmount(mDarkAmount);
-    }
-
-    private int getColorFromPalette(int[] palette) {
-        if (palette != null && palette.length != 0) {
-            return palette[Math.max(0, palette.length - 5)];
-        } else {
-            return mSystemAccent;
-        }
-    }
-
-    private int findColor(int color, int background, boolean againstDark, int accent, int fallback) {
-        if (!isGreyscale(color)) {
-            return color;
-        } else {
-            return fallback;
-        }
-    }
-
-    private boolean isGreyscale(int color) {
-        ColorUtils.RGBToHSL(Color.red(color), Color.green(color), Color.blue(color), mHslOut);
-        return mHslOut[1] < 0.1f || mHslOut[2] < 0.1f;
-    }
-
-    public void setDarkAmount(float dark) {
-        mDarkAmount = dark;
-        mCurrentAccent = ColorUtils.blendARGB(mPrimaryColor, mAmbientColor, mDarkAmount);
-        onTimeChanged();
     }
 
     @Override
@@ -196,207 +140,16 @@ public class CustomTextClock extends TextView implements ColorExtractor.OnColors
         IntentFilter filter = new IntentFilter();
         filter.addAction(Intent.ACTION_TIMEZONE_CHANGED);
         getContext().registerReceiver(mTimeZoneChangedReceiver, filter);
-
-        if (mSettingsObserver == null) {
-            mSettingsObserver = new SettingsObserver(new Handler());
-        }
-        mSettingsObserver.observe();
-        updateClockSize();
     }
 
     @Override
     protected void onDetachedFromWindow() {
         super.onDetachedFromWindow();
         getContext().unregisterReceiver(mTimeZoneChangedReceiver);
-        mColorExtractor.removeOnColorsChangedListener(this);
     }
 
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        refreshLockFont();
-    }
-
-    private int getLockClockFont() {
-        return Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.LOCK_CLOCK_FONTS, 28);
-    }
-
-    private void refreshLockFont() {
-        final Resources res = getContext().getResources();
-        boolean isPrimary = UserHandle.getCallingUserId() == UserHandle.USER_OWNER;
-        int lockClockFont = isPrimary ? getLockClockFont() :28;
-        switch (lockClockFont) {
-            case 0:
-                setTypeface(Typeface.create("sans-serif", Typeface.NORMAL));
-                break;
-            case 1:
-                setTypeface(Typeface.create("sans-serif", Typeface.BOLD));
-                break;
-            case 2:
-                setTypeface(Typeface.create("sans-serif", Typeface.ITALIC));
-                break;
-            case 3:
-                setTypeface(Typeface.create("sans-serif", Typeface.BOLD_ITALIC));
-                break;
-            case 4:
-                setTypeface(Typeface.create("sans-serif-light", Typeface.ITALIC));
-                break;
-            case 5:
-                setTypeface(Typeface.create("sans-serif-light", Typeface.NORMAL));
-                break;
-            case 6:
-                setTypeface(Typeface.create("sans-serif-thin", Typeface.ITALIC));
-                break;
-            case 7:
-                setTypeface(Typeface.create("sans-serif-thin", Typeface.NORMAL));
-                break;
-            case 8:
-                setTypeface(Typeface.create("sans-serif-condensed", Typeface.NORMAL));
-                break;
-            case 9:
-                setTypeface(Typeface.create("sans-serif-condensed", Typeface.ITALIC));
-                break;
-            case 10:
-                setTypeface(Typeface.create("sans-serif-condensed", Typeface.BOLD));
-                break;
-            case 11:
-                setTypeface(Typeface.create("sans-serif-condensed", Typeface.BOLD_ITALIC));
-                break;
-            case 12:
-                setTypeface(Typeface.create("sans-serif-medium", Typeface.NORMAL));
-                break;
-            case 13:
-                setTypeface(Typeface.create("sans-serif-medium", Typeface.ITALIC));
-                break;
-            case 14:
-                setTypeface(Typeface.create("sans-serif-condensed-light", Typeface.NORMAL));
-                break;
-            case 15:
-                setTypeface(Typeface.create("sans-serif-condensed-light", Typeface.ITALIC));
-                break;
-            case 16:
-                setTypeface(Typeface.create("sans-serif-black", Typeface.NORMAL));
-                break;
-            case 17:
-                setTypeface(Typeface.create("sans-serif-black", Typeface.ITALIC));
-                break;
-            case 18:
-                setTypeface(Typeface.create("cursive", Typeface.NORMAL));
-                break;
-            case 19:
-                setTypeface(Typeface.create("cursive", Typeface.BOLD));
-                break;
-            case 20:
-                setTypeface(Typeface.create("casual", Typeface.NORMAL));
-                break;
-            case 21:
-                setTypeface(Typeface.create("serif", Typeface.NORMAL));
-                break;
-            case 22:
-                setTypeface(Typeface.create("serif", Typeface.ITALIC));
-                break;
-            case 23:
-                setTypeface(Typeface.create("serif", Typeface.BOLD));
-                break;
-            case 24:
-                setTypeface(Typeface.create("serif", Typeface.BOLD_ITALIC));
-                break;
-            case 25:
-                setTypeface(Typeface.create("gobold-light-sys", Typeface.NORMAL));
-                break;
-            case 26:
-                setTypeface(Typeface.create("roadrage-sys", Typeface.NORMAL));
-                break;
-            case 27:
-                setTypeface(Typeface.create("snowstorm-sys", Typeface.NORMAL));
-                break;
-            case 28:
-                setTypeface(Typeface.create("googlesans-sys", Typeface.NORMAL));
-                break;
-            case 29:
-                setTypeface(Typeface.create("neoneon-sys", Typeface.NORMAL));
-                break;
-            case 30:
-                setTypeface(Typeface.create("themeable-sys", Typeface.NORMAL));
-                break;
-            case 31:
-                setTypeface(Typeface.create("samsung-sys", Typeface.NORMAL));
-                break;
-            case 32:
-                setTypeface(Typeface.create("mexcellent-sys", Typeface.NORMAL));
-                break;
-            case 33:
-                setTypeface(Typeface.create("burnstown-sys", Typeface.NORMAL));
-                break;
-            case 34:
-                setTypeface(Typeface.create("dumbledor-sys", Typeface.NORMAL));
-                break;
-            case 35:
-                setTypeface(Typeface.create("phantombold-sys", Typeface.NORMAL));
-                break;
-            case 36:
-                setTypeface(Typeface.create("sourcesanspro-sys", Typeface.NORMAL));
-                break;
-            case 37:
-                setTypeface(Typeface.create("circularstd-sys", Typeface.NORMAL));
-                break;
-            case 38:
-                setTypeface(Typeface.create("oneplusslate-sys", Typeface.NORMAL));
-                break;
-            case 39:
-                setTypeface(Typeface.create("aclonica-sys", Typeface.NORMAL));
-                break;
-            case 40:
-                setTypeface(Typeface.create("amarante-sys", Typeface.NORMAL));
-                break;
-            case 41:
-                setTypeface(Typeface.create("bariol-sys", Typeface.NORMAL));
-                break;
-            case 42:
-                setTypeface(Typeface.create("cagliostro-sys", Typeface.NORMAL));
-                break;
-            case 43:
-                setTypeface(Typeface.create("coolstory-sys", Typeface.NORMAL));
-                break;
-            case 44:
-                setTypeface(Typeface.create("lgsmartgothic-sys", Typeface.NORMAL));
-                break;
-            case 45:
-                setTypeface(Typeface.create("rosemary-sys", Typeface.NORMAL));
-                break;
-            case 46:
-                setTypeface(Typeface.create("sonysketch-sys", Typeface.NORMAL));
-                break;
-            case 47:
-                setTypeface(Typeface.create("surfer-sys", Typeface.NORMAL));
-                break;
-        }
-    }
-
-    public void updateClockSize() {
-        mClockSize = Settings.System.getIntForUser(mContext.getContentResolver(),
-                Settings.System.LOCKCLOCK_FONT_SIZE, 40,
-                UserHandle.USER_CURRENT);
-            setTextSize(mClockSize);
-            onTimeChanged();
-    }
-
-    protected class SettingsObserver extends ContentObserver {
-        SettingsObserver(Handler handler) {
-            super(handler);
-        }
-        void observe() {
-            ContentResolver resolver = mContext.getContentResolver();
-
-            resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.LOCKCLOCK_FONT_SIZE),
-                    false, this, UserHandle.USER_ALL);
-        }
-
-        @Override
-        public void onChange(boolean selfChange) {
-            updateClockSize();
-        }
+    private boolean qpie() {
+        return Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.LOCKSCREEN_CLOCK_SELECTION, 0, UserHandle.USER_CURRENT) == 12;
     }
 }
