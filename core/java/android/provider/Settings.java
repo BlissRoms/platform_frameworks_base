6132508289371cc5b658ef9832964e8eb2a8622e
rohan <purohit.rohan@gmail.com>
2020-02-11 12:43:22 +0530

    KeyguardStatusView: add option to hide date
    Change-Id: If133c4fdec8de211a787cb7d4347e3eb59b8847f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5466,6 +5466,12 @@ public final class Settings {
         public static final String LOCK_DATE_FONT_STYLE = "lock_date_font_style";
 
         /**
+         * Whether to show the items underneath lockscreen clock
+         * @hide
+         */
+        public static final String LOCKSCREEN_INFO = "lockscreen_info";
+
+        /**
          * Select which lockscreen clock style to display
          * @hide
          */
