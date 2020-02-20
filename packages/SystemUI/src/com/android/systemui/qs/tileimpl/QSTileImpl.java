7f0e60a1152f0a5281ae110e0b847bcbc22bcba4
Joey Huab <joey@evolution-x.org>
2020-02-20 13:05:55 +0000

    SystemUI: Add a toggle for the new QS tint [1/2]
    Change-Id: I97026e218323b3a22741bd4bea36d0e1b15ddc6e
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
@@ -439,6 +439,8 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
                     Settings.System.QS_PANEL_BG_USE_FW, 1, UserHandle.USER_CURRENT) == 1;
         boolean setQsFromAccent = Settings.System.getIntForUser(context.getContentResolver(),
                     Settings.System.QS_PANEL_BG_USE_ACCENT, 1, UserHandle.USER_CURRENT) == 1;
+        boolean setQsUseNewTint = Settings.System.getIntForUser(context.getContentResolver(),
+                    Settings.System.QS_PANEL_BG_USE_NEW_TINT, 0, UserHandle.USER_CURRENT) == 1;
 
         int qsBackGroundColor = Settings.System.getIntForUser(context.getContentResolver(),
                 Settings.System.QS_PANEL_BG_COLOR, activeDefault, UserHandle.USER_CURRENT);
@@ -453,7 +455,10 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
                 return Utils.getColorAttrDefaultColor(context, android.R.attr.textColorSecondary);
             case Tile.STATE_ACTIVE:
                 if (setQsFromResources) {
-                    return Utils.getColorAttrDefaultColor(context, android.R.attr.colorAccent);
+                    if (setQsUseNewTint)
+                        return Utils.getColorAttrDefaultColor(context, android.R.attr.colorAccent);
+                    else
+                        return Utils.getColorAttrDefaultColor(context, android.R.attr.colorPrimary);
                 } else {
                      if (setQsFromAccent) {
                         return context.getResources().getColor(R.color.accent_device_default_light);
