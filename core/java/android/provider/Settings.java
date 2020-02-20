4b6aa49d09c2150f4671e498bdd3cb865cf43dae
DennySPB <dennyspb@gmail.com>
2020-02-20 18:52:57 +0200

    SystemUI: add optional back gesture vibration tick [1/2]
    Change-Id: Ibeb3850a427605ecfe203acf8a00b8b895cb2646

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6004,6 +6004,9 @@ public final class Settings {
 
         public static final String STABILIZATION_VELOCITY_AMPLITUDE = "stabilization_velocity_amplitude";
 
+        /** @hide */
+        public static final String BACK_GESTURE_HAPTIC = "back_gesture_haptic";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
