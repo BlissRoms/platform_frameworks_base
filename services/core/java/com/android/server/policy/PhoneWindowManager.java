i-o-n/frameworks_base

fwb: option to disable extended screenshot [1/2]
extended screenshot behaviour is not like normal one so it's better to hava toggle for it

- extended screenshot option will only appear when extened will be selected from settings else normal screenshot will be taken

reason why we've done this:
    - screenshot sound cannot be disabled on extended screenshot
    - capture time is sometimes slow

Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -678,6 +678,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private int mPowerButtonSuppressionDelayMillis = POWER_BUTTON_SUPPRESSION_DELAY_DEFAULT_MILLIS;
 
     private int mTorchActionMode;
+    private int sshotType;
 
     private static final float PROXIMITY_DISTANCE_THRESHOLD = 5.0f;
     private int mProximityTimeOut;
@@ -1040,6 +1041,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.DOZE_TRIGGER_DOUBLETAP), false, this,
+                    UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.SCREENSHOT_TYPE), false, this,
                     UserHandle.USER_ALL);
             updateSettings();
         }
@@ -1684,8 +1688,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                             + SCREENSHOT_CHORD_DEBOUNCE_DELAY_MILLIS) {
                 mScreenshotChordVolumeDownKeyConsumed = true;
                 cancelPendingPowerKeyAction();
-                if (Settings.System.getIntForUser(mContext.getContentResolver(),
-                    Settings.System.SCREENSHOT_TYPE, 0, UserHandle.USER_CURRENT) == 1) {
+                if (sshotType == 1) {
                     mScreenshotRunnable.setScreenshotType(TAKE_SCREENSHOT_SELECTED_REGION);
                 } else {
                     mScreenshotRunnable.setScreenshotType(TAKE_SCREENSHOT_FULLSCREEN);
@@ -1780,8 +1783,12 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         @Override
         public void run() {
             if (!mPocketLockShowing) {
-                boolean dockMinimized = mWindowManagerInternal.isMinimizedDock();
-                mDefaultDisplayPolicy.takeScreenshot(mScreenshotType, dockMinimized);
+                if (sshotType == 2) {
+                    boolean dockMinimized = mWindowManagerInternal.isMinimizedDock();
+                    mDefaultDisplayPolicy.takeScreenshot(mScreenshotType, dockMinimized);
+                } else {
+                    mDefaultDisplayPolicy.takeScreenshot(mScreenshotType);
+                }
             }
         }
     }
@@ -2567,6 +2574,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         if (updateRotation) {
             updateRotation(true);
         }
+
+        sshotType = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.SCREENSHOT_TYPE, 0, UserHandle.USER_CURRENT);
     }
 
     private void updateWakeGestureListenerLp() {

BlissRoms/platform_frameworks_base

Change-Id: I7730380825f3f0e8e192e8edb68ba6b27efe079c

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -709,6 +709,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private int mPowerButtonSuppressionDelayMillis = POWER_BUTTON_SUPPRESSION_DELAY_DEFAULT_MILLIS;
 
     private final List<DeviceKeyHandler> mDeviceKeyHandlers = new ArrayList<>();
+    private int mScreenshotType;
 
     private LineageButtons mLineageButtons;
 
@@ -987,6 +988,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.DOZE_TRIGGER_DOUBLETAP), false, this,
+                    UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.SCREENSHOT_TYPE), false, this,
                     UserHandle.USER_ALL);
             updateSettings();
         }
@@ -1607,8 +1611,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                             + SCREENSHOT_CHORD_DEBOUNCE_DELAY_MILLIS) {
                 mScreenshotChordVolumeDownKeyConsumed = true;
                 cancelPendingPowerKeyAction();
-                if (Settings.System.getIntForUser(mContext.getContentResolver(),
-                    Settings.System.SCREENSHOT_TYPE, 0, UserHandle.USER_CURRENT) == 1) {
+                if (mScreenshotType == 1) {
                     mScreenshotRunnable.setScreenshotType(TAKE_SCREENSHOT_SELECTED_REGION);
                 } else {
                     mScreenshotRunnable.setScreenshotType(TAKE_SCREENSHOT_FULLSCREEN);
@@ -1703,8 +1706,12 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         @Override
         public void run() {
-                boolean dockMinimized = mWindowManagerInternal.isMinimizedDock();
-                mDefaultDisplayPolicy.takeScreenshot(mScreenshotType, dockMinimized);
+                if (mScreenshotType == 2) {
+                    boolean dockMinimized = mWindowManagerInternal.isMinimizedDock();
+                    mDefaultDisplayPolicy.takeScreenshot(mScreenshotType, dockMinimized);
+                } else {
+                    mDefaultDisplayPolicy.takeScreenshot(mScreenshotType);
+                }
         }
     }
 
@@ -2625,6 +2632,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         if (updateRotation) {
             updateRotation(true);
         }
+
+        mScreenshotType = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.SCREENSHOT_TYPE, 0, UserHandle.USER_CURRENT);
     }
 
     private void updateWakeGestureListenerLp() {