d82d141fa9345740c224810111abd8111d06ba6e
Pranav Vashi <neobuddy89@gmail.com>
2020-02-28 00:19:59 +0530

    Alert Slider: Add toggle to disable notifications [1/2]
    Change-Id: If7aca1f3f52b338e74f504652ebf16e26d77a8fc
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5540,6 +5540,12 @@ public final class Settings {
         public static final String ALERT_SLIDER_ORDER = "alert_slider_order";
 
         /**
+         * Whether to show or hide alert slider notifications on supported devices
+         * @hide
+         */
+        public static final String ALERT_SLIDER_NOTIFICATIONS = "alert_slider_notifications";
+
+        /**
          * Preferred silent mode for Alert Slider..
          * 0: Alarms only.
          * 1: Total silence
