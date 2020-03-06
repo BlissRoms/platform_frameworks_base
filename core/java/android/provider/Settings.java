932bc08bb6d5cec68d4d6d3d555c3257df6280b2
Alexander Koskovich <zvnexus@outlook.com>
2020-03-05 21:06:06 -0500

    Alert Slider: Switch to AudioManager, kill setZenMode
    Change-Id: I129807f32ce743d32843a818ede0d68264985cde

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5080,14 +5080,6 @@ public final class Settings {
          */
         public static final String ALERT_SLIDER_ORDER = "alert_slider_order";
 
-        /**
-         * Preferred silent mode for Alert Slider..
-         * 0: Alarms only.
-         * 1: Total silence
-         * @hide
-         */
-        public static final String ALERT_SLIDER_SILENT_MODE = "alert_slider_silent_mode";
-
        /**
         * Defines the global heads up notification snooze
         * @hide
