crdroidandroid/android_frameworks_base

Add device key action to toggle flashlight [2/3]
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -2394,6 +2394,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case CLOSE_APP:
                 closeApp();
                 break;
+            case TORCH:
+                Utils.toggleCameraFlash();
+                break;
             default:
                 break;
         }

BlissRoms/platform_frameworks_base

Change-Id: Ie1241e92f5cf9d58f8379a286c70f1ce647c391b

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1932,6 +1932,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case CLOSE_APP:
                 closeApp();
                 break;
+            case TORCH:
+                BlissUtils.toggleCameraFlash();
+                break;
             default:
                 break;
         }