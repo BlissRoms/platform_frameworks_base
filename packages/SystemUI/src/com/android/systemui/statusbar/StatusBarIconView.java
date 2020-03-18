0c4e2ade9190cf2198fbab1d059484d502e7c4ee
pengus77 <ivan@cerrato.biz>
2020-03-18 13:17:51 +0100

    statusbar: fix paddings for our poor notched device
    Change-Id: I9488b6eddcd85fdb292dd51b9e5da9b1c6ccae8e

--- packages/SystemUI/src/com/android/systemui/statusbar/StatusBarIconView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/StatusBarIconView.java
@@ -74,9 +74,7 @@ public class StatusBarIconView extends AnimatedImageView implements StatusIconDi
      * 17dp * (15 / 17) = 15dp, the new height. After the first call to {@link #reloadDimens} all
      * values will be in px.
      */
-    private float mSystemIconDesiredHeight = 15f;
-    private float mSystemIconIntrinsicHeight = 17f;
-    private float mSystemIconDefaultScale = mSystemIconDesiredHeight / mSystemIconIntrinsicHeight;
+    private static final float SYSTEM_ICON_SCALE = 15.f / 17.f;
     private final int ANIMATION_DURATION_FAST = 100;
 
     public static final int STATE_ICON = 0;
@@ -207,21 +205,7 @@ public class StatusBarIconView extends AnimatedImageView implements StatusIconDi
     // Makes sure that all icons are scaled to the same height (15dp). If we cannot get a height
     // for the icon, it uses the default SCALE (15f / 17f) which is the old behavior
     private void updateIconScaleForSystemIcons() {
-        float iconHeight = getIconHeight();
-        if (iconHeight != 0) {
-            mIconScale = mSystemIconDesiredHeight / iconHeight;
-        } else {
-            mIconScale = mSystemIconDefaultScale;
-        }
-    }
-
-    private float getIconHeight() {
-        Drawable d = getDrawable();
-        if (d != null) {
-            return (float) getDrawable().getIntrinsicHeight();
-        } else {
-            return mSystemIconIntrinsicHeight;
-        }
+	mIconScale = SYSTEM_ICON_SCALE;
     }
 
     public float getIconScaleIncreased() {
@@ -239,8 +223,8 @@ public class StatusBarIconView extends AnimatedImageView implements StatusIconDi
         if (density != mDensity) {
             mDensity = density;
             reloadDimens();
-            updateDrawable();
             maybeUpdateIconScaleDimens();
+            updateDrawable();
         }
         boolean nightMode = (newConfig.uiMode & Configuration.UI_MODE_NIGHT_MASK)
                 == Configuration.UI_MODE_NIGHT_YES;
@@ -262,11 +246,6 @@ public class StatusBarIconView extends AnimatedImageView implements StatusIconDi
         if (applyRadius) {
             mDotRadius = mStaticDotRadius;
         }
-        mSystemIconDesiredHeight = res.getDimension(
-                com.android.internal.R.dimen.status_bar_system_icon_size);
-        mSystemIconIntrinsicHeight = res.getDimension(
-                com.android.internal.R.dimen.status_bar_system_icon_intrinsic_size);
-        mSystemIconDefaultScale = mSystemIconDesiredHeight / mSystemIconIntrinsicHeight;
     }
 
     public void setNotification(StatusBarNotification notification) {
@@ -274,7 +253,6 @@ public class StatusBarIconView extends AnimatedImageView implements StatusIconDi
         if (notification != null) {
             setContentDescription(notification.getNotification());
         }
-        maybeUpdateIconScaleDimens();
     }
 
     public StatusBarIconView(Context context, AttributeSet attrs) {
@@ -283,7 +261,7 @@ public class StatusBarIconView extends AnimatedImageView implements StatusIconDi
         mBlocked = false;
         mAlwaysScaleIcon = true;
         reloadDimens();
-        maybeUpdateIconScaleDimens();
+        updateIconScaleForNotifications();
         mDensity = context.getResources().getDisplayMetrics().densityDpi;
     }
 
@@ -329,8 +307,6 @@ public class StatusBarIconView extends AnimatedImageView implements StatusIconDi
             if (!updateDrawable(false /* no clear */)) return false;
             // we have to clear the grayscale tag since it may have changed
             setTag(R.id.icon_is_grayscale, null);
-            // Maybe set scale based on icon height
-            maybeUpdateIconScaleDimens();
         }
         if (!levelEquals) {
             setImageLevel(icon.iconLevel);
