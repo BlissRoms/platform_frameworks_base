a24b6987cef1fb2e04076bbf928a4e10472ad3d4
rohan <purohit.rohan@gmail.com>
2020-03-18 02:02:23 -0400

    base: omnijaws: fix settings intent
    * also we r not aicp
    
    Change-Id: I9354b1b2fad8cdba07fe3c0782c0b4dd0155c8c3

--- packages/SystemUI/src/com/android/systemui/omni/OmniJawsClient.java
+++ packages/SystemUI/src/com/android/systemui/omni/OmniJawsClient.java
@@ -86,11 +86,6 @@ public class OmniJawsClient {
     private static final String WEATHER_UPDATE = "org.omnirom.omnijaws.WEATHER_UPDATE";
     private static final String WEATHER_ERROR = "org.omnirom.omnijaws.WEATHER_ERROR";
 
-    private static final String AE_EXTRA_FRAGMENT_CLASS = "com.aicp.extras.extra.preference_fragment";
-    private static final String AE_SETTINGSACTIVITY = "com.aicp.extras.SettingsActivity";
-    private static final String SETTINGS_PACKAGE_NAME = "com.aicp.extras";
-    private static final String WEATHER_SETTINGS = "com.aicp.extras.fragments.Weather";
-
     private static final DecimalFormat sNoDigitsFormat = new DecimalFormat("0");
 
     public static class WeatherInfo {
@@ -247,9 +242,8 @@ public class OmniJawsClient {
 
     public Intent getSettingsIntent() {
         if (isOmniJawsServiceInstalled()) {
-            Intent settings = new Intent(Intent.ACTION_MAIN);
-            settings.setClassName(SETTINGS_PACKAGE_NAME, AE_SETTINGSACTIVITY);
-            settings.putExtra(AE_EXTRA_FRAGMENT_CLASS, WEATHER_SETTINGS);
+            Intent settings = new Intent(Intent.ACTION_MAIN)
+                    .setClassName("org.omnirom.omnijaws", "org.omnirom.omnijaws.SettingsActivity");
             return settings;
         }
         return null;
