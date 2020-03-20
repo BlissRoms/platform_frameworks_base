omnirom/android_frameworks_base_old

[1/2] SystemUI: dont block IME space for back gesture
by default if ime is visible the back gesture region starts
above the ime height. add an option to disable that
behaviour so that the back gesture stays the same

Change-Id: I576bb480059b8a531dd7f99f6b4f357fe310969f

packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
@@ -81,7 +81,11 @@ public class EdgeBackGestureHandler implements DisplayListener {
         @Override
         public void onImeVisibilityChanged(boolean imeVisible, int imeHeight) {
             // No need to thread jump, assignments are atomic
-            mImeHeight = imeVisible ? imeHeight : 0;
+            if (mBlockImeSpace) {
+                mImeHeight = imeVisible ? imeHeight : 0;
+            } else {
+                mImeHeight = 0;
+            }
             // TODO: Probably cancel any existing gesture
         }
 
@@ -171,6 +175,8 @@ public class EdgeBackGestureHandler implements DisplayListener {
 
     // omni additions start
     private int mEdgeHeight;
+    // should back gesture be movewd above ime if its visible
+    private boolean mBlockImeSpace = true;
 
     public EdgeBackGestureHandler(Context context, OverviewProxyService overviewProxyService) {
         final Resources res = context.getResources();
@@ -244,7 +250,9 @@ public class EdgeBackGestureHandler implements DisplayListener {
 
     public void onSettingsChanged() {
         updateEdgeHeightValue();
-    }
+        mBlockImeSpace = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.OMNI_BACK_GESTURE_BLOCK_IME, 1, UserHandle.USER_CURRENT) == 1;
+}
 
     private void disposeInputChannel() {
         if (mInputEventReceiver != null) {

BlissRoms/platform_frameworks_base

Signed-off-by: Joey Huab <joey@evolution-x.org>

packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
@@ -94,7 +94,11 @@ public class EdgeBackGestureHandler implements DisplayListener, TunerService.Tun
         @Override
         public void onImeVisibilityChanged(boolean imeVisible, int imeHeight) {
             // No need to thread jump, assignments are atomic
-            mImeHeight = imeVisible ? imeHeight : 0;
+            if (mBlockImeSpace) {
+                mImeHeight = imeVisible ? imeHeight : 0;
+            } else {
+                mImeHeight = 0;
+            }
             // TODO: Probably cancel any existing gesture
         }
 
@@ -190,6 +194,8 @@ public class EdgeBackGestureHandler implements DisplayListener, TunerService.Tun
     private static final int HAPTIC_DURATION = 20;
 
     private final Vibrator mVibrator;
+    // should back gesture be movewd above ime if its visible
+    private boolean mBlockImeSpace = true;
 
     public EdgeBackGestureHandler(Context context, OverviewProxyService overviewProxyService) {
         final Resources res = context.getResources();
@@ -215,6 +221,7 @@ public class EdgeBackGestureHandler implements DisplayListener, TunerService.Tun
         mFingerOffset = res.getDimensionPixelSize(R.dimen.navigation_edge_finger_offset);
         updateEdgeHaptic();
         updateCurrentUserResources(res);
+        onSettingsChanged();
     }
 
     public void updateCurrentUserResources(Resources res) {
@@ -279,6 +286,8 @@ public class EdgeBackGestureHandler implements DisplayListener, TunerService.Tun
     public void onSettingsChanged() {
         updateEdgeHeightValue();
         updateEdgeHaptic();
+        mBlockImeSpace = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.BACK_GESTURE_BLOCK_IME, 1, UserHandle.USER_CURRENT) == 1;
     }
 
     public void setStateForBackArrowGesture() {