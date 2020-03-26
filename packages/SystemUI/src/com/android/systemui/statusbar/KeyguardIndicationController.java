674684b786ec694af18fb6051f3c52b41927066a
Pranav Vashi <neobuddy89@gmail.com>
2020-03-26 21:11:56 +0530

    Add lockscreen battery info as TunerService tunable
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I23ef82d70f0e48d1249d4d6c4b30944a71399482

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -97,7 +97,8 @@ public class KeyguardIndicationController implements StateListener,
 
     private static final String LOCKSCREEN_CHARGING_ANIMATION_STYLE =
             "system:" + Settings.System.LOCKSCREEN_CHARGING_ANIMATION_STYLE;
-
+    private static final String LOCKSCREEN_BATTERY_INFO =
+            "system:" + Settings.System.LOCKSCREEN_BATTERY_INFO;
     private final Context mContext;
     private final ShadeController mShadeController;
     private final AccessibilityController mAccessibilityController;
@@ -139,6 +140,8 @@ public class KeyguardIndicationController implements StateListener,
     private double mChargingVoltage;
     private float mTemperature;
     private String mMessageToShowOnScreenOn;
+
+    private boolean mShowBatteryInfo;
 
     private KeyguardUpdateMonitorCallback mUpdateMonitorCallback;
 
@@ -216,6 +219,7 @@ public class KeyguardIndicationController implements StateListener,
 
         final TunerService tunerService = Dependency.get(TunerService.class);
         tunerService.addTunable(this, LOCKSCREEN_CHARGING_ANIMATION_STYLE);
+        tunerService.addTunable(this, LOCKSCREEN_BATTERY_INFO);
 
         mBiometricHelpShowOnlyWhenFailed = res.getBoolean(R.bool.config_biometricHelpShowOnlyWhenFailed);
     }
@@ -227,6 +231,10 @@ public class KeyguardIndicationController implements StateListener,
                 mChargingIndication =
                         TunerService.parseInteger(newValue, 1);
                 if (mChargingIndicationView != null) updateChargingIndicationStyle();
+                break;
+            case LOCKSCREEN_BATTERY_INFO:
+                mShowBatteryInfo =
+                        TunerService.parseIntegerSwitch(newValue, true);
                 break;
             default:
                 break;
@@ -691,9 +699,7 @@ public class KeyguardIndicationController implements StateListener,
         }
 
         String batteryInfo = "";
-        boolean showbatteryInfo = Settings.System.getIntForUser(mContext.getContentResolver(),
-            Settings.System.LOCKSCREEN_BATTERY_INFO, 1, UserHandle.USER_CURRENT) == 1;
-         if (showbatteryInfo) {
+         if (mShowBatteryInfo) {
             if (mChargingWattage > 0) {
                 batteryInfo = (batteryInfo == "" ? "" : batteryInfo + " · ") +
                         String.format("%.1f" , (mChargingWattage / 1000 / 1000)) + "W";
