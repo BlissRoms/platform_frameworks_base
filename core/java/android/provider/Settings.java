2ae44d27833d2ab5289cdd078ced0347d0425f5d
DarkJoker360 <simoespo159@gmail.com>
2020-04-15 15:09:40 +0000

    Add an option to ask pin for shutdown/reboot on secure lockscreen [1/2]
    Kang Samsung's idea
    
    Based on: https://github.com/ezio84/abc_frameworks_base/commit/62537d0ea76331fe838f25732fbe38c69eb0904e
    https://github.com/crdroidandroid/android_frameworks_base/commit/9aea78f60ebac0d678438f3d86a0ab45d40996a9
    
    @neobuddy89: Code clean up, optimization and disabled by default.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I2f2c560df9d41e4263ed6624b9f4b8de9251f81c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -16319,6 +16319,12 @@ public final class Settings {
         private static final Validator SHOW_FPS_OVERLAY_VALIDATOR = BOOLEAN_VALIDATOR;
 
         /**
+         * Whether to ask pin for shutdown/reboot on secure LockScreen
+         * @hide
+         */
+        public static final String LOCKSCREEN_POWERMENU_SECURE = "lockscreen_powermenu_secure";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
