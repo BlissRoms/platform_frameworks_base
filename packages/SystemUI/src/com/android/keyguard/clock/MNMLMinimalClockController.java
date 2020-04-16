c4c8705ce983d972794fd2a47fdd67b6680d4264
rohan <purohit.rohan@gmail.com>
2020-04-16 13:01:07 -0400

    SystemUI: fix preview for few clocks
    Change-Id: I7dfc9a415c3648561d589a345c9cbeb7daf2b0a0

--- packages/SystemUI/src/com/android/keyguard/clock/MNMLMinimalClockController.java
+++ packages/SystemUI/src/com/android/keyguard/clock/MNMLMinimalClockController.java
@@ -119,21 +119,15 @@ public class MNMLMinimalClockController implements ClockPlugin {
     @Override
     public Bitmap getPreview(int width, int height) {
 
-        View previewView = mLayoutInflater.inflate(R.layout.digital_mnml_minimal, null);
-        TextClock previewTime = previewView.findViewById(R.id.clock);
-        TextClock previewDate = previewView.findViewById(R.id.date);
+        // Use the big clock view for the preview
+        View view = getBigClockView();
 
         // Initialize state of plugin before generating preview.
         ColorExtractor.GradientColors colors = mColorExtractor.getColors(
                 WallpaperManager.FLAG_LOCK);
-        int[] colorPalette = colors.getColorPalette();
-        int accentColor = mResources.getColor(R.color.typeClockAccentColor, null);
-        if (colorPalette != null) {
-            accentColor = colorPalette[Math.max(0, colorPalette.length - 5)];
-        }
-        GradientDrawable dateBg = (GradientDrawable) previewDate.getBackground();
-        dateBg.setColor(accentColor);
-        dateBg.setStroke(0,Color.TRANSPARENT);
+        ColorExtractor.GradientColors colors = mColorExtractor.getColors(
+                WallpaperManager.FLAG_LOCK);
+        setColorPalette(colors.supportsDarkText(), colors.getColorPalette());
         onTimeTick();
 
         return mRenderer.createPreview(previewView, width, height);
