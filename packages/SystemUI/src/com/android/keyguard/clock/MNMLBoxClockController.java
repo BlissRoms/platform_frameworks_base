61373cc2ab61f99a1c2469f3b5186bb8e0197a22
rohan <purohit.rohan@gmail.com>
2020-04-17 04:09:31 -0400

    SystemUI: add ability to change font style for new clocks
    * use our setTypeface() method
    
    Change-Id: I60c1e02e7d3950fcd06117fef56495af8c5cf1e5

--- packages/SystemUI/src/com/android/keyguard/clock/MNMLBoxClockController.java
+++ packages/SystemUI/src/com/android/keyguard/clock/MNMLBoxClockController.java
@@ -23,6 +23,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.Paint.Style;
+import android.graphics.Typeface;
 import android.icu.text.DateFormat;
 import android.icu.text.DisplayContext;
 import android.text.Html;
@@ -180,6 +181,11 @@ public class MNMLBoxClockController implements ClockPlugin {
     @Override
     public void setTextSize(int unit, float size) {
         mClock.setTextSize(unit, size);
+    }
+
+    @Override
+    public void setTypeface(Typeface tf) {
+        mClock.setTypeface(tf);
     }
 
     @Override
