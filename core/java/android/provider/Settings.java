f6a0537978f616db311bc6627c49f95c980a7da7
Pranav Vashi <neobuddy89@gmail.com>
2020-04-21 00:44:58 +0530

    Add tunable to hide display cutout [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Iff3a905fd1fadcdffb07208e2f10b60a5ee312e0

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11441,6 +11441,12 @@ public final class Settings {
         public static final String FACE_UNLOCK_ALWAYS_REQUIRE_SWIPE = "face_unlock_always_require_swipe";
 
         /**
+         * Setting to disable black in cut-out
+         * @hide
+         */
+        public static final String SYSUI_DISPLAY_CUTOUT = "sysui_display_cutout";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
