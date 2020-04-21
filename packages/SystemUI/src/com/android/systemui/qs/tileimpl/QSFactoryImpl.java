a52beb5e1b249e56ad657f12a47c3dd22d1ba46c
rohan <purohit.rohan@gmail.com>
2020-04-21 14:37:35 -0400

    base: Drop LTE tile
    Revert "LteTile: Fix network modes for 10"
    
    This reverts commit 2ee2ac5051da9b6c9ee68f56e29e3dbe9f5b3b06.
    
    Revert "Support more Network modes for LTE-tile."
    
    This reverts commit a68997bc98fa8fc5d143aedcde593e92b580fef6.
    
    Revert "LteTile: multisim compatibility"
    
    This reverts commit 11b5b632d9d4c094c13ec5c76ae5ccbc4cefe0eb.
    
    Revert "SystemUI: Add LTE Tile"
    
    This reverts commit 9cf9087a1d7f7b86a2af8e76ea597f87c69bf3d8.
    
    Change-Id: I9bb728bf74151c6b1975fd5319239033f7c41013

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -53,7 +53,6 @@ import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LiveDisplayTile;
 import com.android.systemui.qs.tiles.LocaleTile;
 import com.android.systemui.qs.tiles.LocationTile;
-import com.android.systemui.qs.tiles.LteTile;
 import com.android.systemui.qs.tiles.MusicTile;
 import com.android.systemui.qs.tiles.MonoToggleTile;
 import com.android.systemui.qs.tiles.NfcTile;
@@ -119,7 +118,6 @@ public class QSFactoryImpl implements QSFactory {
     private final Provider<UsbTetherTile> mUsbTetherTileProvider;
     private final Provider<VolumeTile> mVolumeTileProvider;
     private final Provider<CPUInfoTile> mCPUInfoTileProvider;
-    private final Provider<LteTile> mLteTileProvider;
     private final Provider<ScreenshotTile> mScreenshotTileProvider;
     private final Provider<SoundSearchTile> mSoundSearchTileProvider;
     private final Provider<AODTile> mAODTileProvider;
@@ -172,7 +170,6 @@ public class QSFactoryImpl implements QSFactory {
             Provider<UsbTetherTile> usbTetherTileProvider,
             Provider<VolumeTile> volumeTileProvider,
             Provider<CPUInfoTile> cpuInfoTileProvider,
-            Provider<LteTile> lteTileProvider,
             Provider<ScreenshotTile> screenshotTileProvider,
             Provider<SoundSearchTile> soundSearchTileProvider,
             Provider<AODTile> aodTileProvider,
@@ -221,7 +218,6 @@ public class QSFactoryImpl implements QSFactory {
         mUsbTetherTileProvider = usbTetherTileProvider;
         mVolumeTileProvider = volumeTileProvider;
         mCPUInfoTileProvider = cpuInfoTileProvider;
-        mLteTileProvider = lteTileProvider;
         mScreenshotTileProvider = screenshotTileProvider;
         mSoundSearchTileProvider = soundSearchTileProvider;
         mAODTileProvider = aodTileProvider;
@@ -317,8 +313,6 @@ public class QSFactoryImpl implements QSFactory {
                 return mVolumeTileProvider.get();
             case "cpuinfo":
                 return mCPUInfoTileProvider.get();
-            case "lte":
-                return mLteTileProvider.get();
             case "screenshot":
                 return mScreenshotTileProvider.get();
             case "soundsearch":
