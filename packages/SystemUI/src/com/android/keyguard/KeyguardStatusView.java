5c9037c62699666666fb970f604ff7c1ec782f12
rohan <purohit.rohan@gmail.com>
2020-04-28 05:27:07 -0400

    KeyguardStatusView: Extend padding to slice view as well
    Change-Id: I6bb922775828f3517350c3b5db983cf96421928e

--- packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -439,6 +439,19 @@ public class KeyguardStatusView extends GridLayout implements
 
             // Dont hide slice view in doze
             mKeyguardSlice.setVisibility(mDarkAmount != 1 ? (mLockDateHide ? View.GONE : View.VISIBLE) : View.VISIBLE);
+            final ContentResolver resolver = mContext.getContentResolver();
+            String currentClock = Settings.Secure.getString(
+                resolver, Settings.Secure.LOCK_SCREEN_CUSTOM_CLOCK_FACE);
+            boolean mCustomClockSelection = currentClock == null ? false : currentClock.contains("Type");
+
+            // If text style clock, align the textView to start else keep it center.
+            if (mCustomClockSelection) {
+                mKeyguardSlice.setRowPadding(8, 0, 0, 0);
+                mKeyguardSlice.setRowGravity(Gravity.START);
+            } else {
+                mKeyguardSlice.setRowPadding(0, 0, 0, 0);
+                mKeyguardSlice.setRowGravity(Gravity.CENTER);
+            }
         }
 
         switch (mDateSelection) {
