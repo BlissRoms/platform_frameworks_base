691b0455d26c3b53bcaaf464a0355663e2ab0869
rohan <purohit.rohan@gmail.com>
2020-05-07 04:37:13 -0400

    TypeClock: Fix padding and gravity
    Revert "TypeClock: Add padding to date widget"
    
    This reverts commit 3c9b382e0f6f4559104edbf21415a1fb0445c814.
    
    Revert "TypeClock: Align Date widget as per the clock style"
    
    Reason for revert: we already have this 7b27b8a3470f03fec26f02d242476868572167ed
    
    This reverts commit ad42b5aff43497292f1f148453258875b22d963b.
    
    Change-Id: I5421d70d1f2f6d62b0a1d177b7a7dba07fbc6edb

--- packages/SystemUI/src/com/android/keyguard/KeyguardSliceView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardSliceView.java
@@ -249,19 +249,11 @@ public class KeyguardSliceView extends LinearLayout implements View.OnClickListe
     }
 
     public void setRowGravity(int gravity) {
-        mRow.setGravity(gravity);
-    }
-
-    public void setRowPadding(int left, int top, int right, int bottom) {
-        mRow.setPadding(left, top, right, bottom);
-    }
-
-    public void setRowContainerGravity(int gravity) {
         mRowContainer.setGravity(gravity);
     }
 
-    public void setRowContainerPadding(int left, int top, int right, int bottom) {
-        mRowContainer.setPaddingRelative(left, top, right, bottom);
+    public void setRowPadding(int left, int top, int right, int bottom) {
+        mRowContainer.setPadding(left, top, right, bottom);
     }
 
     /**
