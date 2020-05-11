290173b6aef4fb65a37b08632bdfceea31b2df48
rohan <purohit.rohan@gmail.com>
2020-05-11 11:19:00 -0400

    Revert longshot for asus stitch image
    Revert "fwb: Increase Longshot timeout to 2 seconds"
    
    This reverts commit 7599d11ea3cc3d9445a1c3fe9de8ead882d370b6.
    
    Change-Id: I713aa2016189fc5dc2ab42f2dc48753691537706
    
    Revert "fwb: Port extended screenshot function from OOS"
    
    This reverts commit 9b4b6182453d9c64231ac7af8774ca86af746686.
    
    Change-Id: Ic9c19bacbbe45ea7c0717b721c1ff8801e850251

--- services/core/java/com/android/server/policy/WindowManagerPolicy.java
+++ services/core/java/com/android/server/policy/WindowManagerPolicy.java
@@ -1512,11 +1512,4 @@ public interface WindowManagerPolicy extends WindowManagerPolicyConstants {
      * @return whether the value was changed.
      */
     boolean setAodShowing(boolean aodShowing);
-
-    /**
-     * Long screenshot
-     * @hide
-     */
-    public void takeOPScreenshot(int type);
-    public void stopLongshotConnection();
 }
