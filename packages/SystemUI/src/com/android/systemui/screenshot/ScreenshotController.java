e232f1b0b38693298caaa145ee01a4ad27ea2c16
Ashwin R C <ashwin2001achu@gmail.com>
2020-05-27 10:26:16 +0000

    SystemUI: Adapt screenshot sound to ringer modes
    Squashed:
    
    SystemUI: Screenshots: Refactor shutter sound logic
    
    * Commit 2f09bacae02fe7a99d62355331c3a26b83752a6a introduced some
      duplicated code
    * Move it to an own method to reduce the footprint in the AOSP code parts
      and to reduce duplication
    
    Change-Id: I57eaaee4db401d16cc6ef65c68604cdb4053ca01
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    
    SystemUI: Fix shutter sound
    
    * When shutter sound for camera is forced on as required in some states,
      (config_camera_sound_forced, set via mcc/mnc), we also want to
      (or should) play it when a screenshot is taken from the preview
      instead of an actual picture
    * This change is loosely based on
      https://android-review.googlesource.com/c/platform/frameworks/base/+/1517742/
      but uses publicly available APIs
    
    Testing:
    - Set config_camera_sound_forced to true and push a build to device
    - Turn down all stream volumes to muted
    - Take screenshot of any normal screen
      -> No sound played
    - Open camera, take screenshot
      -> Sound played
    - Turn up volume and repeat the screenshots
      -> Sound played in all cases
    
    Change-Id: Iacbb577c64f73b79cdfae7fb0487fc6a34ffe41c
    
    [ghostrider-reborn] updated for android 12, 13
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: I5fca6c6207672a6d2fcdf20544c76dd7c5b6ab23
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
@@ -53,14 +53,21 @@ import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.Insets;
 import android.graphics.Rect;
+import android.hardware.camera2.CameraManager;
 import android.hardware.display.DisplayManager;
 import android.media.AudioAttributes;
+import android.media.AudioManager;
 import android.media.AudioSystem;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Looper;
 import android.os.RemoteException;
+import android.os.SystemProperties;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.provider.Settings;
 import android.util.DisplayMetrics;
 import android.util.Log;
@@ -261,6 +268,9 @@ public class ScreenshotController {
     private final WindowManager.LayoutParams mWindowLayoutParams;
     private final AccessibilityManager mAccessibilityManager;
     private final ListenableFuture<MediaPlayer> mCameraSound;
+    private final AudioManager mAudioManager;
+    private final CameraManager mCameraManager;
+    private final Vibrator mVibrator;
     private final ScrollCaptureClient mScrollCaptureClient;
     private final PhoneWindow mWindow;
     private final DisplayManager mDisplayManager;
@@ -274,6 +284,7 @@ public class ScreenshotController {
     private SaveImageInBackgroundTask mSaveInBgTask;
     private boolean mScreenshotTakenInPortrait;
     private boolean mBlockAttach;
+    private int mCamsInUse = 0;
 
     private Animator mScreenshotAnimation;
     private RequestCallback mCurrentRequestCallback;
@@ -382,6 +393,13 @@ public class ScreenshotController {
 
         // Initialize current foreground package name
         mTaskListener.onTaskStackChanged();
+
+        // Grab system services needed for screenshot sound
+        mAudioManager = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
+        mVibrator = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
+        mCameraManager = (CameraManager) mContext.getSystemService(Context.CAMERA_SERVICE);
+        mCameraManager.registerAvailabilityCallback(mCamCallback,
+                new Handler(Looper.getMainLooper()));
 
         mCopyBroadcastReceiver = new BroadcastReceiver() {
             @Override
@@ -894,6 +912,27 @@ public class ScreenshotController {
     }
 
     private void playCameraSound() {
+       boolean playSound = readCameraSoundForced() && mCamsInUse > 0;
+
+        switch (mAudioManager.getRingerMode()) {
+            case AudioManager.RINGER_MODE_SILENT:
+                // do nothing
+                break;
+            case AudioManager.RINGER_MODE_VIBRATE:
+                if (mVibrator != null && mVibrator.hasVibrator()) {
+                    mVibrator.vibrate(VibrationEffect.createOneShot(50,
+                            VibrationEffect.DEFAULT_AMPLITUDE));
+                }
+                break;
+            case AudioManager.RINGER_MODE_NORMAL:
+                // in this case we want to play sound even if not forced on
+                playSound = true;
+                break;
+        }
+
+        if (!playSound)
+            return;
+
         mCameraSound.addListener(() -> {
             try {
                 MediaPlayer player = mCameraSound.get();
@@ -950,6 +989,7 @@ public class ScreenshotController {
         if (DEBUG_ANIM) {
             Log.d(TAG, "starting post-screenshot animation");
         }
+
         mScreenshotAnimation.start();
     }
 
@@ -1182,5 +1222,24 @@ public class ScreenshotController {
                 }
             };
         }
+    }
+
+    private CameraManager.AvailabilityCallback mCamCallback =
+            new CameraManager.AvailabilityCallback() {
+        @Override
+        public void onCameraOpened(String cameraId, String packageId) {
+            mCamsInUse++;
+        }
+
+        @Override
+        public void onCameraClosed(String cameraId) {
+            mCamsInUse--;
+        }
+    };
+
+    private boolean readCameraSoundForced() {
+        return SystemProperties.getBoolean("audio.camerasound.force", false) ||
+                mContext.getResources().getBoolean(
+                        com.android.internal.R.bool.config_camera_sound_forced);
     }
 }
