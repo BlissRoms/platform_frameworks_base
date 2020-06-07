f857ea0fed7111798758a80f86a5f7382541e302
rohan <purohit.rohan@gmail.com>
2020-06-07 05:08:43 -0400

    [DNM] improve disabling longshot
    Change-Id: I59b566243452702e5ebd964725f1ffa7a792418f

--- services/core/java/com/android/server/wm/DisplayPolicy.java
+++ services/core/java/com/android/server/wm/DisplayPolicy.java
@@ -3732,15 +3732,27 @@ public class DisplayPolicy {
      */
     public void takeScreenshot(int screenshotType) {
         if (mScreenshotHelper != null) {
-            final boolean fullShot = Settings.System.getIntForUser(mContext.getContentResolver(),
-                    Settings.System.SCREENSHOT_TYPE, 0, UserHandle.USER_CURRENT) == 0;
-            String packageName = fullShot ? "com.android.systemui" :
+            String packageName = isFullShot() ? "com.android.systemui" :
                     (mFocusedWindow == null ? "" : mFocusedWindow.getAttrs().packageName);
             mScreenshotHelper.takeScreenshot(screenshotType,
                     mStatusBar != null && mStatusBar.isVisibleLw(),
                     mNavigationBar != null && mNavigationBar.isVisibleLw(),
                     mHandler, null /* completionConsumer */,
                     packageName);
+        }
+    }
+
+    private boolean isFullShot() {
+        final boolean fullShot = Settings.System.getIntForUser(mContext.getContentResolver(),
+              Settings.System.SCREENSHOT_TYPE, 0, UserHandle.USER_CURRENT) == 0;
+        final int defaultShot = Settings.System.getIntForUser(mContext.getContentResolver(),
+              Settings.System.SCREENSHOT_DEFAULT_MODE, 0, UserHandle.USER_CURRENT);
+        if (defaultShot == 0) {
+            return true;
+        } else if (fullShot) {
+            return true;
+        } else {
+            return false;
         }
     }
 
