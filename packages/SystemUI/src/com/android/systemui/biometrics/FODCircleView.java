7a01152cb210ecaa5dc73227c2bd2862ce766537
Dil3mm4 <dil3mm4.dev@gmail.com>
2020-06-08 21:03:15 +0000

    FODCircleView: handle more visibility cases
    This commit accounts the cases where the device has already been unlocked by another biometric method or Trust Agent.
    In these cases, showing the FOD view it's useless.
    
    Tests executed:
        - Added a Bluetooth device as Trusted Device via Smart Lock, ensured FOD wasn't visible
        - Used other biometric methods to unlock the device, ensured FOD wasn't visible
        - Enabled On-body detection, ensured FOD wasn't visible
        - Enabled Trusted places, ensured FOD wasn't visible
        - Triggered biometric failure with too many unrecognized attempts, ensured FOD wasn't visible while transitioning back and forth from AOD.
        - Triggered floating biometric dialog and ensured its functionality
    
    Change-Id: I94496d4a42f60dfb1ce13a5c306ff08be3eba23e

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -110,6 +110,7 @@ public class FODCircleView extends ImageView implements ConfigurationListener, T
     private int mColorBackground;
     private int mCurrentBrightness;
 
+    private boolean mIsBiometricRunning;
     private boolean mIsBouncer;
     private boolean mIsDreaming;
     private boolean mIsKeyguard;
@@ -188,9 +189,29 @@ public class FODCircleView extends ImageView implements ConfigurationListener, T
 
     private KeyguardUpdateMonitorCallback mMonitorCallback = new KeyguardUpdateMonitorCallback() {
         @Override
+        public void onBiometricAuthenticated(int userId, BiometricSourceType biometricSourceType) {
+            // We assume that if biometricSourceType matches Fingerprint it will be
+            // handled here, so we hide only when other biometric types authenticate
+            if (biometricSourceType != BiometricSourceType.FINGERPRINT) hide();
+        }
+
+        @Override
+        public void onBiometricRunningStateChanged(boolean running,
+                BiometricSourceType biometricSourceType) {
+            if (biometricSourceType == BiometricSourceType.FINGERPRINT) {
+                mIsBiometricRunning = running;
+            }
+        }
+
+        @Override
         public void onDreamingStateChanged(boolean dreaming) {
             mIsDreaming = dreaming;
-            updateAlpha();
+
+            if (mIsKeyguard && mUpdateMonitor.isFingerprintDetectionRunning()) {
+                show();
+            } else {
+                hide();
+            }
 
             if (dreaming) {
                 mBurnInProtectionTimer = new Timer();
@@ -209,6 +230,11 @@ public class FODCircleView extends ImageView implements ConfigurationListener, T
             updatePosition();
             if (mFODAnimation != null) {
                 mFODAnimation.setAnimationKeyguard(mIsKeyguard);
+            }
+            if (!showing) {
+                hide();
+            } else {
+                updateAlpha();
             }
         }
 
@@ -530,6 +556,16 @@ public class FODCircleView extends ImageView implements ConfigurationListener, T
 
         if (!mCanUnlockWithFp){
             // Ignore when unlocking with fp is not possible
+            return;
+        }
+
+        if (mUpdateMonitor.getUserCanSkipBouncer(mUpdateMonitor.getCurrentUser())) {
+            // Ignore show calls if user can skip bouncer
+            return;
+        }
+
+        if (mIsKeyguard && !mIsBiometricRunning) {
+            // Ignore show calls if biometric state is false
             return;
         }
 
