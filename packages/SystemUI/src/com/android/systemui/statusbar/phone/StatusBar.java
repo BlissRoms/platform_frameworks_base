d882092915110dd71763645934011cb4eacb94f2
rohan <purohit.rohan@gmail.com>
2020-06-10 08:56:24 +0530

    Revert "base: Improve navigation bar hiding toggle"
    * broke navbar visibility for some
    
    This reverts commit 2462a777037da78f9eadfe028bf645618b4d8b4b.
    
    Change-Id: I9a07432002bbc2a9ccb703452ed8ab69d57ac199

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -5690,16 +5690,12 @@ public class StatusBar extends SystemUI implements DemoMode,
                 boolean hasNavbar = getNavigationBarView() != null;
                 if (mNavbarVisible) {
                     if (!hasNavbar) {
-                        try {
-                           mNavigationBarController.onDisplayReady(mDisplayId,
-                                   mNavigationBarSystemUiVisibility);
-                        } catch (Exception e) { }
+                        mNavigationBarController.onDisplayReady(mDisplayId,
+                                mNavigationBarSystemUiVisibility);
                     }
                 } else {
                     if (hasNavbar) {
-                        try {
-                            mNavigationBarController.onDisplayRemoved(mDisplayId);
-                        } catch (Exception e) { }
+                        mNavigationBarController.onDisplayRemoved(mDisplayId);
                     }
                 }
                 break;
