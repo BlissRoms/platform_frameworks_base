047d0d11ceea0f21730d929e1e32b4bed16a8a45
El Dainosor <eldainosor@gmail.com>
2020-06-21 17:26:57 +0530

    Add InkDrop QS Tile Style [1/3]
    Change-Id: Ic1e08b55ae4826736fef24bcd060586c43d9ed36
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- core/java/com/android/internal/util/bliss/ThemesUtils.java
+++ core/java/com/android/internal/util/bliss/ThemesUtils.java
@@ -29,6 +29,7 @@ public class ThemesUtils {
 		    "com.android.systemui.qstile.dottedcircle", // 7
 		    "com.android.systemui.qstile.attemptmountain", // 8
 		    "com.android.systemui.qstile.squaremedo", // 9
+		    "com.android.systemui.qstile.inkdrop", // 10
     };
 
 }
