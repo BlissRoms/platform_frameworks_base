37e4fd3be09926a13e932f270876c938553439da
Pranav Vashi <neobuddy89@gmail.com>
2020-06-22 20:18:00 +0530

    SystemUI: Improve VoLTE detection logic
    check for voice or video capabilities
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: I57e9370f3e7bc285c7f7b1567657761994413675
    Signed-off-by: Ayan Mukherjee <mukherjeeayan725@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
@@ -502,7 +502,8 @@ public class MobileSignalController extends SignalController<
             return resId;
         }
 
-        if (mCurrentState.imsRegistered && mVoLTEicon) {
+        if (mCurrentState.imsRegistered && (mCurrentState.voiceCapable ||
+                  mCurrentState.videoCapable) && mVoLTEicon) {
             switch(mVoLTEstyle) {
                 // VoLTE
                 case 1:
