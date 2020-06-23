fcf8a7c7b4b1a0d0cfc96a155ae8d5aa52618d76
rohan <purohit.rohan@gmail.com>
2020-06-23 00:20:33 -0400

    vol dialog
    Change-Id: I016b959cec1c60fb139f2a1cd167f39d3f0bb53c

---
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogInfo.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.systemui.volume;
+
+import java.util.function.Supplier;
+
+/**
+ * Metadata about an available clock face.
+ */
+final class VolumeDialogInfo {
+
+    private final String mName;
+    private final Supplier<String> mTitle;
+    private final String mId;
+
+    private VolumeDialogInfo(String name, Supplier<String> title, String id) {
+        mName = name;
+        mTitle = title;
+        mId = id;
+    }
+
+    /**
+     * Gets the non-internationalized name for the clock face.
+     */
+    String getName() {
+        return mName;
+    }
+
+    /**
+     * Gets the name (title) of the clock face to be shown in the picker app.
+     */
+    String getTitle() {
+        return mTitle.get();
+    }
+
+    /**
+     * Gets the ID of the clock face, used by the picker to set the current selection.
+     */
+    String getId() {
+        return mId;
+    }
+
+    static Builder builder() {
+        return new Builder();
+    }
+
+    static class Builder {
+        private String mName;
+        private Supplier<String> mTitle;
+        private String mId;
+
+        public VolumeDialogInfo build() {
+            return new VolumeDialogInfo(mName, mTitle, mId);
+        }
+
+        public Builder setName(String name) {
+            mName = name;
+            return this;
+        }
+
+        public Builder setTitle(Supplier<String> title) {
+            mTitle = title;
+            return this;
+        }
+
+        public Builder setId(String id) {
+            mId = id;
+            return this;
+        }
+    }
+}
