45944ecaf6cbf15b962956da6172289a3533e848
rohan <purohit.rohan@gmail.com>
2020-06-23 00:20:33 -0400

    vol dialog
    Change-Id: I016b959cec1c60fb139f2a1cd167f39d3f0bb53c

--- packages/SystemUI/src/com/android/systemui/Dependency.java
+++ packages/SystemUI/src/com/android/systemui/Dependency.java
@@ -111,6 +111,7 @@ import com.android.systemui.util.AsyncSensorManager;
 import com.android.systemui.util.leak.GarbageMonitor;
 import com.android.systemui.util.leak.LeakDetector;
 import com.android.systemui.util.leak.LeakReporter;
+import com.android.systemui.volume.VolumeDialogManager;
 
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
@@ -291,6 +292,7 @@ public class Dependency {
     @Nullable
     @Inject @Named(LEAK_REPORT_EMAIL_NAME) Lazy<String> mLeakReportEmail;
     @Inject Lazy<ClockManager> mClockManager;
+    @Inject Lazy<VolumeDialogManager> mVolumeManager;
     @Inject Lazy<ActivityManagerWrapper> mActivityManagerWrapper;
     @Inject Lazy<DevicePolicyManagerWrapper> mDevicePolicyManagerWrapper;
     @Inject Lazy<PackageManagerWrapper> mPackageManagerWrapper;
@@ -480,6 +482,7 @@ public class Dependency {
         mProviders.put(ForegroundServiceNotificationListener.class,
                 mForegroundServiceNotificationListener::get);
         mProviders.put(ClockManager.class, mClockManager::get);
+        mProviders.put(VolumeDialogManager.class, mVolumeManager::get);
         mProviders.put(PrivacyItemController.class, mPrivacyItemController::get);
         mProviders.put(ActivityManagerWrapper.class, mActivityManagerWrapper::get);
         mProviders.put(DevicePolicyManagerWrapper.class, mDevicePolicyManagerWrapper::get);
