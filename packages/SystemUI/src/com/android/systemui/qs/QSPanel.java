90755135cca17e1b9cd9baf0beb2b96e1f8fb4b3
rohan <purohit.rohan@gmail.com>
2020-06-26 03:48:31 -0400

    QSPanel: fix brightness slider logic
    * lets not add brightness slider in qs panel when slider position is quick qs panel
      as it adds another slider on qs detail view collpase.
    
    Change-Id: I0b402fd871488f7b0eb6a8ed779aafb33a2927d0

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -133,7 +133,7 @@ public class QSPanel extends LinearLayout implements Tunable, Callback,
 
     private ImageView mMinBrightness;
     private ImageView mMaxBrightness;
-    private int mBrightnessSlider = 1;
+    private int mBrightnessSlider;
     private int animStyle, animDuration, interpolatorType;
 
     private final Vibrator mVibrator;
@@ -159,10 +159,12 @@ public class QSPanel extends LinearLayout implements Tunable, Callback,
 
         mBrightnessView = LayoutInflater.from(mContext).inflate(
             R.layout.quick_settings_brightness_dialog, this, false);
+        addView(mBrightnessView);
 
         mTileLayout = (QSTileLayout) LayoutInflater.from(mContext).inflate(
                 R.layout.qs_paged_tile_layout, this, false);
         mTileLayout.setListening(mListening);
+        addView((View) mTileLayout);
 
         mQsTileRevealController = new QSTileRevealController(mContext, this,
                 (PagedTileLayout) mTileLayout);
@@ -229,12 +231,22 @@ public class QSPanel extends LinearLayout implements Tunable, Callback,
     }
 
     private void addQSPanel() {
-        if (mBrightnessSlider == 1 || mBrightnessSlider == 3) {
-            addView(mBrightnessView);
-            addView((View) mTileLayout);
-        } else {
-            addView((View) mTileLayout);
-            addView(mBrightnessView);
+        switch (mBrightnessSlider) {
+            case 1:
+            default:
+                addView(mBrightnessView);
+                addView((View) mTileLayout);
+                break;
+            case 2:
+                addView((View) mTileLayout);
+                addView(mBrightnessView);
+                break;
+            case 3:
+                addView((View) mTileLayout);
+                break;
+            case 4:
+                addView((View) mTileLayout);
+                break;
         }
 
         addDivider();
