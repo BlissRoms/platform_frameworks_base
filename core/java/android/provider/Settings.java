5b7544f7fb8a58da5e5d635a864510bb66092256
Dil3mm4 <dil3mm4.dev@gmail.com>
2020-07-06 01:35:10 +0000

    SystemUI: Use app icons for notifications in statusbar
    Squashed:
    
        From: spkal01 <spkal01@users.noreply.github.com>
        Date: Thu, 9 Dec 2021 18:32:21 +0000
        Subject: SystemUI: Make app icons in statusbar optional [1/2]
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: Pranav Vashi <neobuddy89@gmail.com>
        Date: Sat, 15 Jan 2022 17:09:09 +0530
        Subject: SystemUI: Refactor colored statusbar icons
    
        * Remove painful systemui checks everywhere in StatusBariconView
        * Apply NO_COLOR when colored icons are enabled.
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    [neobuddy89: Updated for A14 QPR1]
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Iccc1ca929729a6b6b26194fadc437ef667d7bd2c
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6813,6 +6813,12 @@ public final class Settings {
         public static final String FLASHLIGHT_ON_CALL_RATE = "flashlight_on_call_rate";
 
         /**
+         * Whether StatusBar icons should use app icon
+         * @hide
+         */
+        public static final String STATUSBAR_COLORED_ICONS = "statusbar_colored_icons";
+
+        /**
          * Whether keyguard will rotate
          * 0 = false, 1 = true
          */
