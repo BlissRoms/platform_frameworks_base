86390734e00e8d3773417c091524b2ea496cbf5e
TheScarastic <warabhishek@gmail.com>
2020-07-09 08:34:19 +0000

    fwb: Add screen off fod [1/2]
    Change-Id: Ic45cf7619951480714153a571f1f94fa5207ce65
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4826,6 +4826,12 @@ public final class Settings {
         public static final String VOLUME_ROCKER_WAKE = "volume_rocker_wake";
 
         /**
+         * Screen off fod
+         * @hide
+         */
+        public static final String FOD_GESTURE = "fod_gesture";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
