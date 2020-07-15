9ac9b283d7e9e17afe29ce8bf83ec4b4fab57955
rohan <purohit.rohan@gmail.com>
2020-07-15 04:11:31 -0400

    SystemUI: Add OnePlus Analog Clock Style
    * Inspired by oneplus clock styles
      thanks to @ROGERdotT for the design.
    
    Change-Id: Id3133b2bdc8c28fae36c5bc28e275f8cb42347ef
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -167,6 +167,7 @@ public final class ClockManager {
         addBuiltinClock(() -> new MNMLBoxClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new MNMLMinimalClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new OPNumbersClockController(res, layoutInflater, colorExtractor));
+        addBuiltinClock(() -> new OPAnalogClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new OPMinimalClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new OPRomanClockController(res, layoutInflater, colorExtractor));
 
