53954774f3956f40162e80980dff6d97919ec66c
Pranav Vashi <neobuddy89@gmail.com>
2020-07-19 04:24:41 +0530

    base: notch-city: Dejank immersive mode with QS panel
    * This patch removes black cutouts when QS panel is pull down.
    * Inspired from an abandoned potato tomato salad patch in March 2020:
      https://github.com/PotatoProject/frameworks_base/commit/be479bc6645631ef6c19abdd43fbaec1eeccd5be
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Idc178f91cdd6fb60d3dfc15600075422244c7e9b

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -75,6 +75,8 @@ import com.android.systemui.Dependency;
 import com.android.systemui.Interpolators;
 import com.android.systemui.R;
 import com.android.systemui.doze.DozeLog;
+import com.android.systemui.ScreenDecorations;
+import com.android.systemui.SysUiServiceProvider;
 import com.android.systemui.fragments.FragmentHostManager;
 import com.android.systemui.fragments.FragmentHostManager.FragmentListener;
 import com.android.systemui.plugins.FalsingManager;
@@ -454,6 +456,7 @@ public class NotificationPanelView extends PanelView implements
 
     private boolean mIsLockscreenDoubleTapEnabled;
     private int mStatusBarHeaderHeight;
+    private int mStatusBarHeight;
 
     /**
      * Cache the resource id of the theme to avoid unnecessary work in onThemeChanged.
@@ -484,6 +487,7 @@ public class NotificationPanelView extends PanelView implements
      * the keyguard is dismissed to show the status bar.
      */
     private boolean mDelayShowingKeyguardStatusBar;
+    private ScreenDecorations mScreenDecorations;
 
     private int mOneFingerQuickSettingsIntercept;
     private NotificationLightsView mPulseLightsView;
@@ -544,6 +548,7 @@ public class NotificationPanelView extends PanelView implements
         mVibratorHelper = Dependency.get(VibratorHelper.class);
         mVibrateOnOpening = mContext.getResources().getBoolean(
                 R.bool.config_vibrateOnIconAnimation);
+        mScreenDecorations = SysUiServiceProvider.getComponent(mContext, ScreenDecorations.class);
     }
 
     /**
@@ -681,6 +686,8 @@ public class NotificationPanelView extends PanelView implements
                 R.dimen.keyguard_indication_bottom_padding);
         mQsNotificationTopPadding = getResources().getDimensionPixelSize(
                 R.dimen.qs_notification_padding);
+        mStatusBarHeight = getResources().getDimensionPixelSize(
+                com.android.internal.R.dimen.status_bar_height_portrait);
         mShelfHeight = getResources().getDimensionPixelSize(R.dimen.notification_shelf_height);
         mDarkIconSize = getResources().getDimensionPixelSize(
                 R.dimen.status_bar_icon_drawing_size_dark);
@@ -1966,6 +1973,13 @@ public class NotificationPanelView extends PanelView implements
         } else if (height <= mQsMinExpansionHeight && mQsExpanded) {
             setQsExpanded(false);
         }
+        if (mKeyguardShowing) {
+            if (height > mStatusBarHeight) {
+                setTopCorners(false);
+            } else {
+                setTopCorners(true);
+            }
+        }
         mQsExpansionHeight = height;
         updateQsExpansion();
         requestScrollerTopPaddingUpdate(false /* animate */);
@@ -2287,6 +2301,13 @@ public class NotificationPanelView extends PanelView implements
             setQsExpansion(mQsMinExpansionHeight
                     + t * (mQsMaxExpansionHeight - mQsMinExpansionHeight));
         }
+        if (!mKeyguardShowing) {
+            if (expandedHeight > mStatusBarHeight) {
+                setTopCorners(false);
+            } else {
+                setTopCorners(true);
+            }
+        }
         updateExpandedHeight(expandedHeight);
         updateHeader();
         updateNotificationTranslucency();
@@ -2497,6 +2518,12 @@ public class NotificationPanelView extends PanelView implements
                 ? 0 : KeyguardBouncer.ALPHA_EXPANSION_THRESHOLD, 1f, 0f, 1f, getExpandedFraction());
         float alpha = Math.min(expansionAlpha, 1 - getQsExpansionFraction());
         mBigClockContainer.setAlpha(alpha);
+    }
+
+    private void setTopCorners(boolean enable) {
+        if (mScreenDecorations == null)
+            mScreenDecorations = SysUiServiceProvider.getComponent(mContext, ScreenDecorations.class);
+    	mScreenDecorations.setTopCorners(enable);
     }
 
     @Override
