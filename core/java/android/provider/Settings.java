4fdbb3bfb45d9e99d1126b7a655796942590a598
Anay Wadhera <awadhera@berkeley.edu>
2020-07-21 00:28:29 -0700

    base: clipboard toast toggle [1/2]
    Change-Id: I1c8c47b1865eae864e44f4e0b682dee4fe07c827
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6614,6 +6614,13 @@ public final class Settings {
         public static final String QS_HEADER_BACKGROUND = "qs_header_background";
 
         /**
+         * Toggle for clipboard access toast.
+         * @hide
+         */
+
+        public static final String SHOW_CLIPBOARD_TOAST = "show_clipboard_toast";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
