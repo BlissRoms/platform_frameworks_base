a3b4e7c1dcdb85ba9ae706cdb4fd76930538d291
Arian <arian.kulmer@web.de>
2020-07-29 16:01:55 +0200

    Introduce anti flicker mode in LiveDisplay
    Change-Id: Iba7387ff16d463d087c9a38bced90b6c1bd503b1
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Signed-off-by: Ayan Mukherjee <mukherjeeayan725@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5862,6 +5862,12 @@ public final class Settings {
          */
         public static final String LIVE_DISPLAY_HINTED = "live_display_hinted";
 
+        /**
+         * Anti flicker
+         * 0 = 0ff, 1 = on
+         */
+        public static final String DISPLAY_ANTI_FLICKER = "display_anti_flicker";
+
 	/**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
@@ -6009,6 +6015,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(QS_PANEL_BG_USE_NEW_TINT);
             PRIVATE_SETTINGS.add(SCREENSHOT_TYPE);
             PRIVATE_SETTINGS.add(PROXIMITY_ON_WAKE);
+            PRIVATE_SETTINGS.add(DISPLAY_ANTI_FLICKER);
         }
 
         /**
