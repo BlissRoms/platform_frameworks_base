ec87ba491ba5823ad018ed30caabeff6c53c86b9
Marko Man <darkobas@gmail.com>
2020-10-17 21:42:41 +0200

    base: Add timeout for ambient notification pulse
    idoybh (YAAP): Adapt to A12
    
    Change-Id: Ifb2a7f4e14711b522fbfe8b9c546ea78c48af268
    Signed-off-by: Ayan Mukherjee <mukherjeeayan725@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1480,6 +1480,7 @@ public class StatusBar extends SystemUI implements
         filter.addAction(Intent.ACTION_SCREEN_OFF);
         filter.addAction(DevicePolicyManager.ACTION_SHOW_DEVICE_MONITORING_DIALOG);
         filter.addAction(Intent.ACTION_SCREEN_CAMERA_GESTURE);
+        filter.addAction(NotificationPanelViewController.CANCEL_NOTIFICATION_PULSE_ACTION);
         mBroadcastDispatcher.registerReceiver(mBroadcastReceiver, filter, null, UserHandle.ALL);
     }
 
@@ -2766,6 +2767,10 @@ public class StatusBar extends SystemUI implements
                     break;
                 case DevicePolicyManager.ACTION_SHOW_DEVICE_MONITORING_DIALOG:
                     mQSPanelController.showDeviceMonitoringDialog();
+                    break;
+                case NotificationPanelViewController.CANCEL_NOTIFICATION_PULSE_ACTION:
+                    mNotificationPanelViewController.stopNotificationPulse();
+                    break;
             }
             Trace.endSection();
         }
