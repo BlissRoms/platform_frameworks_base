f4651d7c5a60d395107be95f61bacbc5275abf1d
Jyotiraditya <jyotiraditya@aospa.co>
2020-10-30 11:30:59 -0300

    SystemUI: Introduce Adaptive Playback [1/2]
    Automatically pause media when muted. Resume if unmuted within a minute.
    
    Co-Authored-By: Stylogey <stylogey@gmail.com>
    Change-Id: I5d37478b3739309dcaae4eb6a0e4aac6c87f120f
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5973,6 +5973,22 @@ public final class Settings {
          */
         public static final String INCREASING_RING_RAMP_UP_TIME = "increasing_ring_ramp_up_time";
 
+	/**
+         * Adaptive playback
+         * Automatically pause media when the volume is muted and
+         * will resume automatically when volume is restored.
+         *   0 = disabled
+         *   1 = enabled
+         * @hide
+         */
+        public static final String ADAPTIVE_PLAYBACK_ENABLED = "adaptive_playback_enabled";
+
+        /**
+         * Adaptive playback's timeout in ms
+         * @hide
+         */
+        public static final String ADAPTIVE_PLAYBACK_TIMEOUT = "adaptive_playback_timeout";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
