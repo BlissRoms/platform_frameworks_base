de8c98372dc8b4cca09ed7b85622395c8907414a
AshutoshSundresh <ashutoshsundresh@gmail.com>
2020-11-02 19:26:42 +0530

    DataSwitchTile: Make it behave like OxygenOS
    Signed-off-by: Ayan Mukherjee <mukherjeeayan725@gmail.com>
    Change-Id: If2a3aef739a07b3a97ea641b5a4b0c50d87c5631

--- packages/SystemUI/src/com/android/systemui/qs/tiles/DataSwitchTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/DataSwitchTile.java
@@ -119,10 +119,15 @@ public class DataSwitchTile extends QSTileImpl<BooleanState> {
             Log.d(TAG, "Call state=" + mTelephonyManager.getCallState());
         } else if (mSimCount == 0) {
             Log.d(TAG, "handleClick:no sim card");
+            SysUIToast.makeText(mContext,
+                    mContext.getString(R.string.qs_data_switch_toast_0),
+                    Toast.LENGTH_LONG).show();
         } else if (mSimCount == 1) {
             Log.d(TAG, "handleClick:only one sim card");
+            SysUIToast.makeText(mContext,
+                    mContext.getString(R.string.qs_data_switch_toast_1),
+                    Toast.LENGTH_LONG).show();
         } else {
-            mHost.collapsePanels();
             AsyncTask.execute(new Runnable() {
                 public final void run() {
                     toggleMobileDataEnabled();
