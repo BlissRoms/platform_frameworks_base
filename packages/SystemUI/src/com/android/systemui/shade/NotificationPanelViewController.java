a3744bec9a842509fa42da76f9791ffb128a32a5
Ido Ben-Hur <idoybh2@gmail.com>
2020-11-09 20:54:55 +0200

    base: Ensure edge lightning is off when we are done with repeats
    And fix some bad logic
    Change-Id: I8ca3ebd64c334aecf2b3b02156b892dda3961e36
    
    Change-Id: Id31daade1bd9f7f47b61981cbb4c44310ceaf5d9
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -631,6 +631,7 @@ public final class NotificationPanelViewController extends PanelViewController {
     private NotificationLightsView mPulseLightsView;
     private boolean mPulseLightHandled;
     private boolean mAmbientPulseLightRunning;
+    private boolean mAmbientPulseRanOnce = false; // used only for repeats
     public static final String CANCEL_NOTIFICATION_PULSE_ACTION = "cancel_notification_pulse";
 
     private boolean mAnimatingQS;
@@ -3979,6 +3980,8 @@ public final class NotificationPanelViewController extends PanelViewController {
                 Settings.System.AOD_NOTIFICATION_PULSE_TIMEOUT, 0, UserHandle.USER_CURRENT);
         boolean pulseColorAutomatic = Settings.System.getIntForUser(resolver,
                 Settings.System.NOTIFICATION_PULSE_COLOR_MODE, 0, UserHandle.USER_CURRENT) == 2;
+        int repeats = Settings.System.getIntForUser(resolver,
+                Settings.System.NOTIFICATION_PULSE_REPEATS, 0, UserHandle.USER_CURRENT);
         if (animatePulse) {
             mAnimateNextPositionUpdate = true;
         }
@@ -3988,7 +3991,7 @@ public final class NotificationPanelViewController extends PanelViewController {
             mAnimateNextPositionUpdate = false;
         }
 
-        if (mPulseLightsView != null && (pulseLights || ambientLights)) {
+        if (mPulseLightsView != null && pulseLights) {
             if (DEBUG_PULSE_LIGHT) {
                 Log.d(TAG, "setPulsing pulsing = " + pulsing + " pulseLights = " + pulseLights
                         + " ambientLights = " + ambientLights + " activeNotif = " + activeNotif
@@ -4043,9 +4046,14 @@ public final class NotificationPanelViewController extends PanelViewController {
                     mPulseLightsView.animateNotificationWithColor(pulseColor);
                     mPulseLightsView.setVisibility(View.VISIBLE);
                     mAmbientPulseLightRunning = true;
-                    if (ambientLightsTimeout != 0) {
+                    if (ambientLightsTimeout != 0 && repeats == 0) {
                         // start the end timer
                         startNotificationPulseTimer(ambientLightsTimeout);
+                    } else if (repeats != 0 && !mAmbientPulseRanOnce) {
+                        mAmbientPulseRanOnce = true;
+                    } else if (repeats != 0 && mAmbientPulseRanOnce) {
+                        // stop if using repeats and already ran once
+                        stopNotificationPulse();
                     }
                 } else {
                     // no active notifications or just pulse without aod - so no reason to continue
@@ -5347,6 +5355,7 @@ public final class NotificationPanelViewController extends PanelViewController {
         boolean doShowAodContent = true;
         if (mAmbientPulseLightRunning) {
             mAmbientPulseLightRunning = false;
+            mAmbientPulseRanOnce = false;
             mPulseLightHandled = true;
             stopNotificationPulseTimer();
             // only do it if we continue to doze but never when we already woke up
