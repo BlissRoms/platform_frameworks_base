ec7d8b0a9d1499f9510398bb3e52d1648638d7e2
Ido Ben-Hur <idoybh2@gmail.com>
2020-11-09 03:19:55 +0200

    base: Allow using wallpaper color for notification pulse
    And default to accent color
    
    Change-Id: Ifee73af77dbb0cb80820607abcb9b3610f368c19

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6036,8 +6036,9 @@ public final class Settings {
 
         /**
          * Pulse ambient lights color mode
-         * 0 - Default
-         * 1 - Accent
+         * Pulse ambient lights color mode
+         * 0 - Default (Accent)
+         * 1 - Wallpaper
          * 2 - Notification
          * 3 - Custom color
          * @hide
