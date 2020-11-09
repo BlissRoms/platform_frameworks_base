49d19954494d41dc8ae84b6971f204e43e5c18e5
Ido Ben-Hur <idoybh2@gmail.com>
2020-11-09 03:19:55 +0200

    base: Allow using wallpaper color for notification pulse
    And default to accent color
    
    Change-Id: Id367b7a332cccf9802e10ada50c5f05004924fe8
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6031,8 +6031,8 @@ public final class Settings {
 
         /**
          * Pulse ambient lights color mode
-         * 0 - Default
-         * 1 - Accent
+         * 0 - Default (Accent)
+         * 1 - Wallpaper
          * 2 - Notification
          * 3 - Custom color
          * @hide
@@ -6066,6 +6066,7 @@ public final class Settings {
          * Notification pulse repeats count - 0 means go by timeout
          * @hide
          */
+        @Readable
         public static final String NOTIFICATION_PULSE_REPEATS = "ambient_notification_light_repeats";
 
         /**
