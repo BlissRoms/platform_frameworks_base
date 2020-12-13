128eb2d3a488af00e7910262b1022cbf595b289e
dataoutputstream <mb150996@gmail.com>
2020-12-13 14:54:41 +0100

    FODCircleView: Restore DIM_BEHIND and dim amount to 0.0f on base view
    * This will fix problem onTouch of apparently boost brightness for Xiaomi Displayfeature FOD implementation
    
    Change-Id: I1ae2d3cf8a89d8cc56d92acc01066473da0064ea

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -323,7 +323,8 @@ public class FODCircleView extends ImageView implements TunerService.Tunable {
         mParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE |
                 WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN |
                 WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH |
-                WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED;
+                WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED |
+                WindowManager.LayoutParams.FLAG_DIM_BEHIND;
         mParams.gravity = Gravity.TOP | Gravity.LEFT;
 
         mPressedParams.copyFrom(mParams);
@@ -333,7 +334,10 @@ public class FODCircleView extends ImageView implements TunerService.Tunable {
         mParams.setTitle("Fingerprint on display");
         mPressedParams.setTitle("Fingerprint on display.touched");
 
+        mParams.dimAmount = 0.0f;
+
         mPressedView = new ImageView(mContext)  {
+
             @Override
             protected void onDraw(Canvas canvas) {
                 if (mIsCircleShowing) {
