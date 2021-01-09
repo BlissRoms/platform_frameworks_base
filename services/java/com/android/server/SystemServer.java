1f93579732beeafc0e7b9188f3d4efdf8daae004
NurKeinNeid <mralexman3000@gmail.com>
2021-01-09 19:12:33 +0100

    SystemServer: This is DerpFest
    * and we don't build Q anymore
    
    Change-Id: I774ac62c8867151f21712cfeee25f96a591415af
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -824,9 +824,9 @@ public final class SystemServer implements Dumpable {
                         uptimeMillis);
             }
 
-            Slog.i("halogenOS",
-                "Welcome to halogenOS! We believe that you'll love it. " +
-                "Ready to... uh... Q!");
+            Slog.i("DerpFest",
+                "Welcome to DerpFest! We believe that you'll love it. " +
+                "Ready to... uh... Upside Down Cake!");
 
             // In case the runtime switched since last boot (such as when
             // the old runtime was removed in an OTA), set the system
