dcac9dc3284887e7210ed48c22483166bc8eddc7
kondors1995 <normandija1945@gmail.com>
2021-01-11 12:09:56 +0000

    SystemUI: FOD icon picker [1/2]
    Co-authored-by: Stallix <joey@evolution-x.org>
    Change-Id: I5742051025182204555bb5edb6c89269d128d50b
    Signed-off-by: Ayan Mukherjee <mukherjeeayan725@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4990,6 +4990,12 @@ public final class Settings {
         public static final String FOD_GESTURE = "fod_gesture";
 
         /**
+         * FOD icon picker
+         * @hide
+         */
+        public static final String FOD_ICON = "fod_icon";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
