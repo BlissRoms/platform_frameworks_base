8c847f73bbe097bda69485c153053557b8cc43dc
TikkiTikki <radstylemusic@gmail.com>
2021-02-02 01:49:56 -0300

    Solid render: Round lines [1/2]
    Signed-off-by: Ayan Mukherjee <mukherjeeayan725@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -10525,6 +10525,12 @@ public final class Settings {
         public static final String SYSUI_ROUNDED_FWVALS = "sysui_rounded_fwvals";
 
         /**
+         * Pulse Solid units rounded
+         * @hide
+         */
+        public static final String PULSE_SOLID_UNITS_ROUNDED = "pulse_solid_units_rounded";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
