29211c7f47f08be0dc8bd71f55083f2fca669f9f
TikkiTikki <radstylemusic@gmail.com>
2021-02-03 21:43:34 -0300

    [SQUASH] SystemUI: media notification background customizations [1/2]
    * Squash of the following commits:
    
    commit d443f7d1648a55e28845375d8e271a1d0ac37499
    Author: TikkiTikki <radstylemusic@gmail.com>
    Date:   Wed Feb 3 21:43:34 2021 -0300
    
        base: Replace media notification background with artwork [1/2]
    
        Why not?
    
    commit 91805d5afaaacc6c3327cb1bcc2530fca63336b8
    Author: jhonboy121 <alfredmathew05@gmail.com>
    Date:   Sun Aug 1 16:55:32 2021 +0530
    
        SystemUI: ditch unnecessary scaling in media notification background
    
        * original commit scaled the image enormously which caused oob exception sometimes,
          it also made the qs transition animation to stutter during the artwork rendering period
        * centerCrop scaletype will scale the media art properly
        * use start and bottom edge of parents instead of left and right for rtl support
    
        Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    commit 1ccabb359bb18d74525f1683054d2bd52fb304ab
    Author: jhonboy121 <alfredmathew05@gmail.com>
    Date:   Mon Aug 2 01:31:37 2021 +0530
    
        SystemUI: allow adjusting media notification background blur
    
        Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    commit a39f56b3f31a12c4ec3e5b08b5542b64f2fc1e90
    Author: jhonboy121 <alfredmathew05@gmail.com>
    Date:   Mon Aug 2 08:17:16 2021 +0530
    
        MediaCarouselController: update background when settings are changed
    
        Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    commit 7e8751a8b90388ef0ec7922715515c91c978df78
    Author: jhonboy121 <alfredmathew05@gmail.com>
    Date:   Thu Aug 12 12:57:30 2021 +0530
    
        SystemUI: allow adjusting media notification background alpha
    
        * to acheive this blending artwork based colors on canvas in MediaArtworkProcessor was made optional
        * simplified some things:
          * set album_art and bg_album_art visibility to gone by default and adjust the visibility only when needed
          * also made sure that bg_album_art / album_art drawables were not unnecessarily set when they are not used
    
        Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    * Adapted to A12 changes and some other improvements to layout
    
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Change-Id: I8c5c23c271e359ca3087bb17b007b30ed2ed7628
    Signed-off-by: Ayan Mukherjee <mukherjeeayan725@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5553,6 +5553,30 @@ public final class Settings {
         public static final String QS_FOOTER_TEXT_STRING = "qs_footer_text_string";
 
         /**
+         * Enable and disable Artwork on background media notification
+         * @hide
+         */
+        public static final String ARTWORK_MEDIA_BACKGROUND = "artwork_media_background";
+
+        /**
+         * Whether to enable artwork blur
+         * @hide
+         */
+        public static final String ARTWORK_MEDIA_BACKGROUND_ENABLE_BLUR = "artwork_media_background_enable_blur";
+
+        /**
+         * Adjust blur radius for background media
+         * @hide
+         */
+        public static final String ARTWORK_MEDIA_BACKGROUND_BLUR_RADIUS = "artwork_media_background_blur_radius";
+
+        /**
+         * Adjust alpha for background media
+         * @hide
+         */
+        public static final String ARTWORK_MEDIA_BACKGROUND_ALPHA = "artwork_media_background_alpha";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
