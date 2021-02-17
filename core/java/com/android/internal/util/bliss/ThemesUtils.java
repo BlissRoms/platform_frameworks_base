00a84914cb18917ee4921961f13f5a555b798a2e
Maitreya29 <maitreyapatni30@gmail.com>
2021-02-17 02:51:11 +0100

    base: Implement Gvisual mod navbar colors [1/3]
    Change-Id: Id5af4f9679a21d95e7880607a3626aefeff7c7e0

--- core/java/com/android/internal/util/bliss/ThemesUtils.java
+++ core/java/com/android/internal/util/bliss/ThemesUtils.java
@@ -71,4 +71,10 @@ public class ThemesUtils {
             "com.gnonymous.gvisualmod.urm_l", // 3
     };
 
+    public static final String NAVBAR_COLOR_PURP = "com.gnonymous.gvisualmod.pgm_purp";
+
+    public static final String NAVBAR_COLOR_ORCD = "com.gnonymous.gvisualmod.pgm_orcd";
+
+    public static final String NAVBAR_COLOR_OPRD = "com.gnonymous.gvisualmod.pgm_oprd";
+
 }
