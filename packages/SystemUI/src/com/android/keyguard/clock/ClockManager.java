d2ec492b8d3c2e1711d249cd3e11ab51dccf8522
Maitreya29 <maitreyapatni30@gmail.com>
2021-02-23 16:03:39 +0530

    Introduce Fluid lock clock V2
    Co-authored-by: AndroBuddy <anirudhchimpidi8@gmail.com>
    Change-Id: I18d0395cf971cdd05dc28f6ba0faeab0b668b53a

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -181,6 +181,8 @@ public final class ClockManager {
         addBuiltinClock(() -> new StickerTagClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new OOSClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new OctaviDigitalClockController(res, layoutInflater, colorExtractor));
+        addBuiltinClock(() -> new Fluidv2ClockController(res, layoutInflater, colorExtractor));
+
         // Store the size of the display for generation of clock preview.
         DisplayMetrics dm = res.getDisplayMetrics();
         mWidth = dm.widthPixels;
