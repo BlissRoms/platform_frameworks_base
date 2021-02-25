20409b277a12779189c5b42d2dc330f88647d662
Oliver Scott <olivercscott@gmail.com>
2021-02-25 11:29:35 -0500

    Wi-Fi timeout feature
    Add Wi-Fi timeout setting
    
    Issue: calyxos#228
    Change-Id: I1ac6b19eb35af656afd38eac24bb37fe618859f1
    (cherry picked from commit 12a44815c0d1c64c2684782a87707675181e19e6)
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- packages/SettingsProvider/src/android/provider/settings/validators/GlobalSettingsValidators.java
+++ packages/SettingsProvider/src/android/provider/settings/validators/GlobalSettingsValidators.java
@@ -212,6 +212,21 @@ public class GlobalSettingsValidators {
         VALIDATORS.put(Global.MUTE_ALARM_STREAM_WITH_RINGER_MODE, BOOLEAN_VALIDATOR);
         VALIDATORS.put(
                 Global.MUTE_ALARM_STREAM_WITH_RINGER_MODE_USER_PREFERENCE, BOOLEAN_VALIDATOR);
+        VALIDATORS.put(Global.WIFI_OFF_TIMEOUT,
+                new DiscreteValueValidator(new String[] {
+                        "0",
+                        "15000",
+                        "30000",
+                        "60000",
+                        "120000",
+                        "300000",
+                        "600000",
+                        "1800000",
+                        "3600000",
+                        "7200000",
+                        "14400000",
+                        "28800000"
+                }));
 
         VALIDATORS.put(Global.Wearable.HAS_PAY_TOKENS, BOOLEAN_VALIDATOR);
         VALIDATORS.put(Global.Wearable.GMS_CHECKIN_TIMEOUT_MIN, ANY_INTEGER_VALIDATOR);
