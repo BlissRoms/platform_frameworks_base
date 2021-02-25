bf9594947819a50903971cb07913b26ed7345dcf
Maitreya29 <maitreyapatni30@gmail.com>
2021-02-25 18:03:20 +0000

    base: QS Header size customizations. [1/3]
    Change-Id: I21c64bc83d244bea5f96d37044b563cf7bae60ec
    Signed-off-by: Ayan Mukherjee <mukherjeeayan725@gmail.com>

--- core/java/com/android/internal/util/bliss/ThemesUtils.java
+++ core/java/com/android/internal/util/bliss/ThemesUtils.java
@@ -81,6 +81,10 @@ public static final String TAG = "ThemesUtils";
 
     public static final String NAVBAR_COLOR_ACCENT = "com.gnonymous.gvisualmod.pgm_accent";
 
+    public static final String HEADER_LARGE = "com.android.theme.header.large";
+
+    public static final String HEADER_XLARGE = "com.android.theme.header.xlarge";
+
     public static final String[] STOCK = {
             "com.android.theme.stock.system",
     };
