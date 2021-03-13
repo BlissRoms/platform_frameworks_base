345902caf0085cf84407e87d3d37f5d2b9e82579
AshutoshSundresh <ashutoshsundresh@gmail.com>
2021-03-13 14:26:37 +0530

    QS Tint: Improve code and colours for OOS option
    Signed-off-by: Ayan Mukherjee <mukherjeeayan725@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSIconViewImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSIconViewImpl.java
@@ -197,10 +197,8 @@ public class QSIconViewImpl extends QSIconView {
                 int alpha = (int) (fromAlpha + (toAlpha - fromAlpha) * fraction);
                 int channel = (int) (fromChannel + (toChannel - fromChannel) * fraction);
 
-                if (setQsUseNewTint == 1) {
+                if (setQsUseNewTint == 1 || setQsUseNewTint == 2) {
                     setTint(iv, toColor);
-		} else if (setQsUseNewTint == 2) {
-		    setTint(iv, toColor);
                 } else {
                     setTint(iv, Color.argb(alpha, channel, channel, channel));
                 }
