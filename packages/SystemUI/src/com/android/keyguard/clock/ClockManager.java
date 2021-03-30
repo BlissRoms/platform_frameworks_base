31b821c61394310a6bd438e214b4701c76e422f3
mezz <mezzlasha@gmail.com>
2021-03-30 16:08:23 +0000

    base: Add ShapeShift lockscreen clock
    Very very very heavily inspired by OOS. pretty much OOS. it's OOS.
    
    DerpFest edit: So let's rename it to OOS then
    
    Change-Id: I14a13f777bb8c9c8b1eb9ae2c8ecbef6d4244514

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -179,7 +179,7 @@ public final class ClockManager {
         addBuiltinClock(() -> new ClockertinoClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new TypeAltClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new StickerTagClockController(res, layoutInflater, colorExtractor));
-
+        addBuiltinClock(() -> new OOSClockController(res, layoutInflater, colorExtractor));
         // Store the size of the display for generation of clock preview.
         DisplayMetrics dm = res.getDisplayMetrics();
         mWidth = dm.widthPixels;
