b32d3385c5ce07de4c4ef24c08e1e3e7338a2434
MezzLasha <64702427+MezzLasha@users.noreply.github.com>
2021-04-07 13:11:50 +0400

    add Android Twelve QS Tint Style (1/2)

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
@@ -533,7 +533,7 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
                 return Utils.getDisabled(context,
                         Utils.getColorAttrDefaultColor(context, android.R.attr.textColorSecondary));
             case Tile.STATE_INACTIVE:
-                if (setQsUseNewTint == 1) {
+                if (setQsUseNewTint == 1 || setQsUseNewTint == 3) {
                     return Utils.getColorAttrDefaultColor(context, android.R.attr.textColorPrimary);
                 } else if (setQsUseNewTint == 2) {
                     return context.getResources().getColor(R.color.qs_tile_icon_oos);
