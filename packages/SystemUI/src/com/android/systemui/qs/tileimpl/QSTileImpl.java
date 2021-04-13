c99e0686726bd59ca41ac9890d88922251be2ab9
AshutoshSundresh <ashutoshsundresh@gmail.com>
2021-04-13 08:56:08 +0530

    SystemUI: Improve Android 12 style QS Tile tint

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
@@ -533,7 +533,7 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
                 return Utils.getDisabled(context,
                         Utils.getColorAttrDefaultColor(context, android.R.attr.textColorSecondary));
             case Tile.STATE_INACTIVE:
-                if (setQsUseNewTint == 1 || setQsUseNewTint == 3) {
+                if (setQsUseNewTint == 1) {
                     return Utils.getColorAttrDefaultColor(context, android.R.attr.textColorPrimary);
                 } else if (setQsUseNewTint == 2) {
                     return context.getResources().getColor(R.color.qs_tile_icon_oos);
@@ -545,6 +545,8 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
                     return Utils.getColorAttrDefaultColor(context, android.R.attr.colorAccent);
 		} else if (setQsUseNewTint == 2){
                     return context.getResources().getColor(R.color.qs_tile_oos);
+                } else if (setQsUseNewTint == 3){
+                    return Utils.getColorAttrDefaultColor(context, android.R.attr.textColorPrimaryInverse);
 		} else {
                     return Utils.getColorAttrDefaultColor(context, android.R.attr.colorPrimary);
                 }
