f900b1d498da3012694a5fbca075866b78e0c896
spkal01 <kalligeross@gmail.com>
2021-05-20 12:44:24 +0530

    base: Introduce Fluid Style Switches [1/3]

--- core/java/com/android/internal/util/bliss/ThemesUtils.java
+++ core/java/com/android/internal/util/bliss/ThemesUtils.java
@@ -118,6 +118,7 @@ public class ThemesUtils {
         "com.android.system.switch.md2", // 5
         "com.android.system.switch.retro", // 6
         "com.android.system.switch.oos", // 7
+        "com.android.system.switch.fluid", // 8
     };
 
     public static void updateSwitchStyle(IOverlayManager om, int userId, int switchStyle) {
