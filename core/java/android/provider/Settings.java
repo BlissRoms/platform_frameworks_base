afad1c0536c4d8643b4ca93072e4dbae48689e50
Kshitij Gupta <kshitijgm@gmail.com>
2021-06-15 00:12:58 +0000

    base: Introduce RGB accenter 3.0 [1/2] [AgentFabulous - POSP] - Compared to v1 and v2 of accenter on older versions of android, other   than just build fixes, this implementation completely deprecates use   of props. Instead, we use settings. - When it comes to reading settings without proper contexts, early on in   the boot process, we may have no ContentProviders or ContentResolvers.   To address this, we simply try catch and bail as necessary, returning   the real resource. When things have been setup nicely by the zygote   setup, there will be no more exceptions and all relevant color   resources will be overriden as expected
    [spezi77] Just added settings strings for my convenience.
    
    Change-Id: I0a6917b417eae87d80decd4e16a85295f32a0cd6
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>
     11
    Signed-off-by: Ayan Mukherjee <mukherjeeayan725@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -10277,6 +10277,18 @@ public final class Settings {
         public static final String DOZE_ON_CHARGE_NOW = "doze_on_charge_now";
 
         /**
+         * Dark Accent Color
+         * @hide
+         */
+        public static final String ACCENT_DARK = "accent_dark";
+
+        /**
+         * Light Accent Color
+         * @hide
+         */
+        public static final String ACCENT_LIGHT = "accent_light";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
