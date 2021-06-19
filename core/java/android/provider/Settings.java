86a620e5b18866c8e3b92c5b32b912e13740d145
Jackeagle <jackeagle102@gmail.com>
2021-06-19 06:18:44 -0400

    Revert "Base: HeadsUp snooze function [1/2]"
    This reverts commit 7337fd930bc089ec1d8a340b63ceaa360200ae30.
    
    Change-Id: Icf16969bdc104b9a12dbec4c5353ee8544da3f7b

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4977,12 +4977,6 @@ public final class Settings {
         public static final String HEADS_UP_TIMEOUT = "heads_up_timeout";
 
         /**
-         * Defines the global heads up notification snooze
-         * @hide
-         */
-        public static final String HEADS_UP_NOTIFICATION_SNOOZE = "heads_up_notification_snooze";
-
-        /**
          * Show or hide clock
          * 0 - hide
          * 1 - show (default)
