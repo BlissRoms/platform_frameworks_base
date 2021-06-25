ShapeShiftOS/android_frameworks_base

SystemUI: Left align owner info on ShapeShift and IDE clock
Signed-off-by: Joey Huab <joey@evolution-x.org>

packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -321,9 +321,10 @@ public class KeyguardStatusView extends GridLayout implements
             final ContentResolver resolver = mContext.getContentResolver();
             String currentClock = Settings.Secure.getString(
                 resolver, Settings.Secure.LOCK_SCREEN_CUSTOM_CLOCK_FACE);
-            boolean mCustomClockSelection = currentClock == null ? false : currentClock.contains("Type");
+            boolean mCustomClockSelection = currentClock == null ? false : currentClock.contains("Type")
+                    || currentClock.contains("ide") || currentClock.contains("ShapeShift");
 
-            // If text style clock, align the textView to start else keep it center.
+            // If left aligned style clock, align the textView to start else keep it center.
             if (mCustomClockSelection) {
                 mOwnerInfo.setPaddingRelative((int) mContext.getResources()
                     .getDimension(R.dimen.custom_clock_left_padding) + 8, 0, 0, 0);

BlissRoms/platform_frameworks_base

Change-Id: Ifa61a9e9dbbaeaf814623bfcb3cac05bd4d16e07

packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -311,9 +311,10 @@ public class KeyguardStatusView extends GridLayout implements
             final ContentResolver resolver = mContext.getContentResolver();
             String currentClock = Settings.Secure.getString(
                 resolver, Settings.Secure.LOCK_SCREEN_CUSTOM_CLOCK_FACE);
-            boolean mClockSelection = currentClock == null ? false : currentClock.contains("Type");
+            boolean mClockSelection = currentClock == null ? false : currentClock.contains("Type")
+                    || currentClock.contains("ide") || currentClock.contains("OOS 11");
 
-            // If text style clock, align the textView to start else keep it center.
+            // If left aligned style clock, align the textView to start else keep it center.
             if (mClockSelection) {
                 mOwnerInfo.setPaddingRelative((int) mContext.getResources()
                     .getDimension(R.dimen.custom_clock_left_padding) + 8, 0, 0, 0);