09028b191908747be1c92e1ae7618ab973e118fe
spezi77 <spezi7713@gmx.net>
2021-07-31 11:51:31 +0200

    Revert "Custom Rounded Corner and Padding preferences [1/2] (WIP)"
    This reverts commit 5bd050508f93e62b7a5ee6cacccc2e1895f16200.
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
    Change-Id: I2b2e84c92c78462551ea5c35784feadcb4e1b6cb
    Signed-off-by: Ayan Mukherjee <mukherjeeayan725@gmail.com>

--- packages/SystemUI/src/com/android/systemui/ScreenDecorations.java
+++ packages/SystemUI/src/com/android/systemui/ScreenDecorations.java
@@ -244,9 +244,6 @@ public class ScreenDecorations extends SystemUI implements Tunable,
         mIsRoundedCornerMultipleRadius = mContext.getResources().getBoolean(
                 R.bool.config_roundedCornerMultipleRadius);
         updateRoundedCornerRadii();
-
-        mMainHandler.post(() -> mTunerService.addTunable(this, SIZE));
-
         setupDecorations();
         setupCameraListener();
 
@@ -318,6 +315,8 @@ public class ScreenDecorations extends SystemUI implements Tunable,
             DisplayMetrics metrics = new DisplayMetrics();
             mDisplayManager.getDisplay(DEFAULT_DISPLAY).getMetrics(metrics);
             mDensity = metrics.density;
+
+            mMainHandler.post(() -> mTunerService.addTunable(this, SIZE));
 
             // Watch color inversion and invert the overlay as needed.
             if (mColorInversionSetting == null) {
@@ -737,12 +736,10 @@ public class ScreenDecorations extends SystemUI implements Tunable,
         }
     }
     private boolean hasRoundedCorners() {
-        final int size = Secure.getIntForUser(mContext.getContentResolver(), SIZE, 0, UserHandle.USER_CURRENT);
         return mRoundedDefault.x > 0
                 || mRoundedDefaultBottom.x > 0
                 || mRoundedDefaultTop.x > 0
-                || mIsRoundedCornerMultipleRadius
-                || size != 0;
+                || mIsRoundedCornerMultipleRadius;
     }
 
     private boolean shouldShowRoundedCorner(@BoundsPosition int pos) {
@@ -792,13 +789,8 @@ public class ScreenDecorations extends SystemUI implements Tunable,
     @Override
     public void onTuningChanged(String key, String newValue) {
         mHandler.post(() -> {
+            if (mOverlays == null) return;
             if (SIZE.equals(key)) {
-                if (mOverlays == null) {
-                    if (TunerService.parseIntegerSwitch(newValue, false))
-                        setupDecorations();
-                    else
-                        return;
-                }
                 Point size = mRoundedDefault;
                 Point sizeTop = mRoundedDefaultTop;
                 Point sizeBottom = mRoundedDefaultBottom;
@@ -864,7 +856,6 @@ public class ScreenDecorations extends SystemUI implements Tunable,
             sizeBottom = sizeDefault;
         }
 
-        updateOrientation();
         for (int i = 0; i < BOUNDS_POSITION_LENGTH; i++) {
             if (mOverlays[i] == null) {
                 continue;
