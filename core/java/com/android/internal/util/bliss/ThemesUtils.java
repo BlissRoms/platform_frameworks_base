Spark-Rom/frameworks_base

ThemesUtils: Add function to return clockfaces
Change-Id: I025662c04dc42d93f2b132a7997209474ac8b0d0
Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

core/java/com/android/internal/util/spark/ThemesUtils.java
@@ -16,8 +16,14 @@
 
 package com.android.internal.util.spark;
 
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import static android.os.UserHandle.USER_SYSTEM;
-
+import android.content.ContentResolver;
+import android.content.pm.ProviderInfo;
+import android.database.Cursor;
+import com.android.internal.util.spark.clock.ClockFace;
+import android.net.Uri;
 import android.app.UiModeManager;
 import android.content.Context;
 import android.content.om.IOverlayManager;
@@ -27,9 +33,18 @@ import android.os.ServiceManager;
 import android.provider.Settings;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class ThemesUtils {
 
+    private Context mContext;
     public static final String TAG = "ThemesUtils";
+
+    public ThemesUtils(Context context) {
+        mContext = context;
+    }
 
     // QS Tile Styles
     public static final String[] QS_TILE_THEMES = {
@@ -153,5 +168,38 @@ public class ThemesUtils {
                 e.printStackTrace();
             }
         }
+    }
+
+    public List<ClockFace> getClocks() {
+        ProviderInfo providerInfo = mContext.getPackageManager().resolveContentProvider("com.android.keyguard.clock",
+                        PackageManager.MATCH_SYSTEM_ONLY);
+
+        Uri optionsUri = new Uri.Builder()
+                .scheme(ContentResolver.SCHEME_CONTENT)
+                .authority(providerInfo.authority)
+                .appendPath("list_options")
+                .build();
+
+        ContentResolver resolver = mContext.getContentResolver();
+        List<ClockFace> clocks = new ArrayList<>();
+        try (Cursor c = resolver.query(optionsUri, null, null, null, null)) {
+            while(c.moveToNext()) {
+                String id = c.getString(c.getColumnIndex("id"));
+                String title = c.getString(c.getColumnIndex("title"));
+                String previewUri = c.getString(c.getColumnIndex("preview"));
+                Uri preview = Uri.parse(previewUri);
+                String thumbnailUri = c.getString(c.getColumnIndex("thumbnail"));
+                Uri thumbnail = Uri.parse(thumbnailUri);
+
+                ClockFace.Builder builder = new ClockFace.Builder();
+                builder.setId(id).setTitle(title).setPreview(preview).setThumbnail(thumbnail);
+                clocks.add(builder.build());
+            }
+        } catch (Exception e) {
+            clocks = null;
+        } finally {
+            // Do Nothing
+        }
+        return clocks;
     }
 }

core/java/com/android/internal/util/spark/clock/ClockFace.java
@@ -1,0 +1,81 @@
+/*
+ * Copyright (C) 2021 The AospExtended Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.spark.clock;
+
+import android.net.Uri;
+
+public class ClockFace {
+
+    private final String mTitle;
+    private final String mId;
+    private final Uri mPreview;
+	private final Uri mThumbnail;
+
+    private ClockFace(String title, String id, Uri preview, Uri thumbnail) {
+        mTitle = title;
+        mId = id;
+        mPreview = preview;
+        mThumbnail = thumbnail;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public Uri getPreviewUri() {
+        return mPreview;
+    }
+
+    public Uri getThumbnailUri() {
+        return mThumbnail;
+    }
+
+    public String getId() {
+        return mId;
+    }
+
+    public static class Builder {
+        private String mTitle;
+        private String mId;
+        private Uri mPreview;
+        private Uri mThumbnail;
+
+        public ClockFace build() {
+            return new ClockFace(mTitle, mId, mPreview, mThumbnail);
+        }
+
+        public Builder setTitle(String title) {
+            mTitle = title;
+            return this;
+        }
+
+        public Builder setId(String id) {
+            mId = id;
+            return this;
+        }
+
+        public Builder setPreview(Uri preview) {
+            mPreview = preview;
+            return this;
+        }
+
+        public Builder setThumbnail(Uri thumbnail) {
+            mThumbnail = thumbnail;
+            return this;
+        }
+    }
+}

BlissRoms/platform_frameworks_base

Signed-off-by: Ayan Mukherjee <mukherjeeayan725@gmail.com>

core/java/com/android/internal/util/bliss/ThemesUtils.java
@@ -18,6 +18,13 @@ package com.android.internal.util.bliss;
 
 import static android.os.UserHandle.USER_SYSTEM;
 
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.ContentResolver;
+import android.content.pm.ProviderInfo;
+import android.database.Cursor;
+import com.android.internal.util.bliss.clock.ClockFace;
+import android.net.Uri;
 import android.app.UiModeManager;
 import android.content.Context;
 import android.content.om.IOverlayManager;
@@ -27,9 +34,18 @@ import android.os.ServiceManager;
 import android.provider.Settings;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class ThemesUtils {
 
+private Context mContext;
 public static final String TAG = "ThemesUtils";
+
+    public ThemesUtils(Context context) {
+        mContext = context;
+    }
 
     public static final String[] QS_TILE_THEMES = {
             "com.android.systemui.qstile.default",
@@ -163,4 +179,37 @@ public static final String TAG = "ThemesUtils";
             "com.android.theme.navbar.sammy",
             "com.android.theme.navbar.tecno",
     };
+
+    public List<ClockFace> getClocks() {
+        ProviderInfo providerInfo = mContext.getPackageManager().resolveContentProvider("com.android.keyguard.clock",
+                        PackageManager.MATCH_SYSTEM_ONLY);
+
+        Uri optionsUri = new Uri.Builder()
+                .scheme(ContentResolver.SCHEME_CONTENT)
+                .authority(providerInfo.authority)
+                .appendPath("list_options")
+                .build();
+
+        ContentResolver resolver = mContext.getContentResolver();
+        List<ClockFace> clocks = new ArrayList<>();
+        try (Cursor c = resolver.query(optionsUri, null, null, null, null)) {
+            while(c.moveToNext()) {
+                String id = c.getString(c.getColumnIndex("id"));
+                String title = c.getString(c.getColumnIndex("title"));
+                String previewUri = c.getString(c.getColumnIndex("preview"));
+                Uri preview = Uri.parse(previewUri);
+                String thumbnailUri = c.getString(c.getColumnIndex("thumbnail"));
+                Uri thumbnail = Uri.parse(thumbnailUri);
+
+                ClockFace.Builder builder = new ClockFace.Builder();
+                builder.setId(id).setTitle(title).setPreview(preview).setThumbnail(thumbnail);
+                clocks.add(builder.build());
+            }
+        } catch (Exception e) {
+            clocks = null;
+        } finally {
+            // Do Nothing
+        }
+        return clocks;
+    }
 }

core/java/com/android/internal/util/bliss/clock/ClockFace.java
@@ -1,0 +1,81 @@
+/*
+ * Copyright (C) 2021 The AospExtended Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.bliss.clock;
+
+import android.net.Uri;
+
+public class ClockFace {
+
+    private final String mTitle;
+    private final String mId;
+    private final Uri mPreview;
+	private final Uri mThumbnail;
+
+    private ClockFace(String title, String id, Uri preview, Uri thumbnail) {
+        mTitle = title;
+        mId = id;
+        mPreview = preview;
+        mThumbnail = thumbnail;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public Uri getPreviewUri() {
+        return mPreview;
+    }
+
+    public Uri getThumbnailUri() {
+        return mThumbnail;
+    }
+
+    public String getId() {
+        return mId;
+    }
+
+    public static class Builder {
+        private String mTitle;
+        private String mId;
+        private Uri mPreview;
+        private Uri mThumbnail;
+
+        public ClockFace build() {
+            return new ClockFace(mTitle, mId, mPreview, mThumbnail);
+        }
+
+        public Builder setTitle(String title) {
+            mTitle = title;
+            return this;
+        }
+
+        public Builder setId(String id) {
+            mId = id;
+            return this;
+        }
+
+        public Builder setPreview(Uri preview) {
+            mPreview = preview;
+            return this;
+        }
+
+        public Builder setThumbnail(Uri thumbnail) {
+            mThumbnail = thumbnail;
+            return this;
+        }
+    }
+}