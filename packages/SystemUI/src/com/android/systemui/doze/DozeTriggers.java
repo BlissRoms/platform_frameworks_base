omnirom/android_frameworks_base_old

SystemUI: trigger pulse light only for notifications
store the doze trigger reason in a setting to check later
if it has been triggered for a notification or any other reason

Change-Id: Iecee97b04e5e943c766fb4794d89e036ded65b3a

packages/SystemUI/src/com/android/systemui/doze/DozeTriggers.java
@@ -33,6 +33,7 @@ import android.metrics.LogMaker;
 import android.os.Handler;
 import android.os.SystemClock;
 import android.os.UserHandle;
+import android.provider.Settings;
 import android.text.format.Formatter;
 import android.util.Log;
 
@@ -403,6 +404,9 @@ public class DozeTriggers implements DozeMachine.Part {
                     mDozeHost.isPulsingBlocked());
             return;
         }
+        Settings.System.putIntForUser(mContext.getContentResolver(),
+                Settings.System.OMNI_PULSE_TRIGGER_REASON, reason,
+                UserHandle.USER_CURRENT);
         mMachine.requestPulse(reason);
     }
 

BlissRoms/platform_frameworks_base

Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>
Change-Id: I70f5377eb2e96052705bbebaa92860f14119e64f

packages/SystemUI/src/com/android/systemui/doze/DozeTriggers.java
@@ -28,6 +28,7 @@ import android.hardware.display.AmbientDisplayConfiguration;
 import android.metrics.LogMaker;
 import android.os.SystemClock;
 import android.os.UserHandle;
+import android.provider.Settings;
 import android.text.format.Formatter;
 import android.util.Log;
 import android.view.Display;
@@ -534,6 +535,9 @@ public class DozeTriggers implements DozeMachine.Part {
                     mDozeHost.isPulsingBlocked());
             return;
         }
+        Settings.System.putIntForUser(mContext.getContentResolver(),
+                Settings.System.PULSE_TRIGGER_REASON, reason,
+                UserHandle.USER_CURRENT);
         mMachine.requestPulse(reason);
     }
 