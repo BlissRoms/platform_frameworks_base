6cfc788b7477517e1caa9a32067d7e58ea42ebf5
Ramakant Singh <ramakant@codeaurora.org>
2021-09-06 10:15:55 +0530

    Display: Fix dump dead lock
    mLock not needed to collect sensor dump
    
    Change-Id: I501b2d93fa6fce681629ed1c42afc0799243fe06
    CRs-Fixed: 3035453
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: Ayan Mukherjee <mukherjeeayan725@gmail.com>

--- services/core/java/com/android/server/display/DisplayModeDirector.java
+++ services/core/java/com/android/server/display/DisplayModeDirector.java
@@ -618,10 +618,11 @@ public class DisplayModeDirector {
             mAppRequestObserver.dumpLocked(pw);
             mBrightnessObserver.dumpLocked(pw);
             mUdfpsObserver.dumpLocked(pw);
-            mSensorObserver.dumpLocked(pw);
             mHbmObserver.dumpLocked(pw);
             mSkinThermalStatusObserver.dumpLocked(pw);
         }
+           //mLock not needed to collect sensor dump
+            mSensorObserver.dumpLocked(pw);
     }
 
     private void updateVoteLocked(int priority, Vote vote) {
@@ -2243,14 +2244,12 @@ public class DisplayModeDirector {
 
         void dumpLocked(PrintWriter pw) {
             pw.println("  SensorObserver");
-            synchronized (mSensorObserverLock) {
                 pw.println("    mIsProxActive=" + mIsProxActive);
                 pw.println("    mDozeStateByDisplay:");
                 for (int i = 0; i < mDozeStateByDisplay.size(); i++) {
                     final int id = mDozeStateByDisplay.keyAt(i);
                     final boolean dozed = mDozeStateByDisplay.valueAt(i);
                     pw.println("      " + id + " -> " + dozed);
-                }
             }
         }
 
