df261a5b82a2458429a2111ac6d182c8f37d4936
Yoshinori Hirano <yoshinori.hirano@sonymobile.com>
2021-10-16 10:02:58 +0000

    Add Sound tile to Quick Settings
        abc ezio84: adapt to O, show volume panel with long press,
        use full dnd (no interruptions) as silent mode
    
        @beanstown106: Sound Tile improvements
        *added longpress action
        *added more descriptive labels for each state instead of always saying sound
    
        ezio84: Adapt for N
        Signed-off-by: Shubham Singh <coolsks94@gmail.com>
        Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
        Change-Id: I46f4f8cc62683144a9bd714ca67e2a0f46940d96
        Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
        Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>
    
    Squashed commit of the following:
    
    commit 69f86d9a38b5a942e3f9c914895ee45c0079e8e9
    Author: maxwen <max.weninger@gmail.com>
    Date:   Fri Apr 13 03:32:51 2018 +0200
    
        base: SystemUI: stop crashing qs tiles during boot
    
        this triggers my ocd
        continues a579c2430f1a0bf088023b4283dcec37776c070d
    
        Change-Id: I31719b595cb50a330f26e25074243e04c06f63c1
        Signed-off-by: xyyx <xyyx@mail.ru>
        Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
        Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
        Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
        Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>
    
    commit f419619c033fa99a809f53aa82dbdd7d6732177c
    Author: xyyx <xyyx@mail.ru>
    Date:   Thu Sep 28 11:55:36 2017 +0800
    
        SoundTile: Change ZEN_MODE_NO_INTERRUPTIONS to ZEN_MODE_ALARMS
    
        Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
        Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
        Change-Id: I5edaaa4d551630049b29e09d3cecfb5ffd503e99
        Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
        Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
        Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>
    
    commit 916645271f57c47d60bb0a04c4e51776b51a18f1
    Author: Yoshinori Hirano <yoshinori.hirano@sonymobile.com>
    Date:   Sun Sep 18 22:33:08 2016 +0200
    
        Add Sound tile to Quick Settings
    
        abc ezio84: adapt to O, show volume panel with long press,
        use full dnd (no interruptions) as silent mode
    
        @beanstown106: Sound Tile improvements
        *added longpress action
        *added more descriptive labels for each state instead of always saying sound
    
        ezio84: Adapt for N
        Signed-off-by: Shubham Singh <coolsks94@gmail.com>
        Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
        Change-Id: I46f4f8cc62683144a9bd714ca67e2a0f46940d96
        Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>
    Change-Id: I062bdf2364fdda4e6e95a25b80520f473a7f9764
    
    commit 5bcc4ac2669ff730369726c302e1b3b1e860eeaa (HEAD)
    Author: AnkitGourav <Ankitgourav035@gmail.com>
    Date:   Tue Oct 8 09:43:58 2019 +0530
    
    SystemUI: Update SoundTile
    
    Make the tile active only in a state different than its
    "normal" state (ring). When it's in normal state, no need
    to draw any attention to it.
    
    - SOS edits:
    Make the default sound state also active as the footer has on/off string so even when it's sound mode it should show on instead of off
    
    Change-Id: I20c20c7c2a05ebe92e887c023e792ad47ad5bbfa
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>
    
    commit 32177369d44c7ebf7a5054754547b9ff558f4a44
    Author: Ali B <abittin@gmail.com>
    Date:   Tue Feb 26 21:49:06 2019 +0300
    
    SystemUI: QS: SoundTile: Don't toggle DND
    
    * Allow Sound tile to toggle "Mute" instead of "DND" since we've DND Tile for DND work.
    
    Change-Id: I3a1b03c9d1763602ed436233ba77045d35fd700c
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -54,6 +54,7 @@ import com.android.systemui.qs.tiles.ReduceBrightColorsTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenRecordTile;
 import com.android.systemui.qs.tiles.UiModeNightTile;
+import com.android.systemui.qs.tiles.SoundTile;
 import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.qs.tiles.WorkModeTile;
 import com.android.systemui.util.leak.GarbageMonitor;
@@ -100,6 +101,7 @@ public class QSFactoryImpl implements QSFactory {
     private final Provider<OneHandedModeTile> mOneHandedModeTileProvider;
     private final Provider<CaffeineTile> mCaffeineTileProvider;
     private final Provider<DataSwitchTile> mDataSwitchTileProvider;
+    private final Provider<SoundTile> mSoundTileProvider;
 
     private final Lazy<QSHost> mQsHostLazy;
     private final Provider<CustomTile.Builder> mCustomTileBuilderProvider;
@@ -138,7 +140,8 @@ public class QSFactoryImpl implements QSFactory {
             Provider<OneHandedModeTile> oneHandedModeTileProvider,
             Provider<ColorCorrectionTile> colorCorrectionTileProvider,
             Provider<CaffeineTile> caffeineTileProvider,
-            Provider<DataSwitchTile> dataSwitchTileProvider) {
+            Provider<DataSwitchTile> dataSwitchTileProvider,
+            Provider<SoundTile> soundTileProvider) {
         mQsHostLazy = qsHostLazy;
         mCustomTileBuilderProvider = customTileBuilderProvider;
 
@@ -173,6 +176,7 @@ public class QSFactoryImpl implements QSFactory {
         mColorCorrectionTileProvider = colorCorrectionTileProvider;
         mCaffeineTileProvider = caffeineTileProvider;
         mDataSwitchTileProvider = dataSwitchTileProvider;
+        mSoundTileProvider = soundTileProvider;
     }
 
     /** Creates a tile with a type based on {@code tileSpec} */
@@ -250,6 +254,8 @@ public class QSFactoryImpl implements QSFactory {
                 return mCaffeineTileProvider.get();
             case "dataswitch":
                 return mDataSwitchTileProvider.get();
+            case "sound":
+                return mSoundTileProvider.get();
         }
 
         // Custom tiles
