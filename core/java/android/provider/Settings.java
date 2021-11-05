2fdeb74ef5cb0264a82c2da52f4be51a5af36230
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2021-11-05 13:14:46 +0100

    frameworks: Add support for clock auto-hiding
    Hide statusbar clock when launcher is visible. Useful when you already have a clock as widget.
    
    Change-Id: I03dfc81bc055f2f865ac704e3bf9d9d104fac272
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6387,6 +6387,13 @@ public final class Settings {
         public static final String STATUS_BAR_AM_PM = "status_bar_am_pm";
 
         /**
+         * Whether to hide clock when launcher is visible
+         * default: false
+         * @hide
+         */
+        public static final String STATUS_BAR_CLOCK_AUTO_HIDE = "status_bar_clock_auto_hide";
+
+        /**
          * These are all public system settings
          *
          * @hide
