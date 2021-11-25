dcc85a236dc35faec54fb424164f506e4b72d7b4
jhonboy121 <alfredmathew05@gmail.com>
2021-11-25 17:32:41 +0530

    base: allow hiding navbar [1/3]
    Change-Id: Ia48cd86287cc9f57035b900099e4c2b8ba381ff5

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -85,5 +85,6 @@ public class SystemSettings {
         Settings.System.ALARM_ALERT,
         Settings.System.NOTIFICATION_LIGHT_PULSE,
         Settings.System.LOCKSCREEN_BATTERY_INFO,
+        Settings.System.FULLSCREEN_GESTURES,
     };
 }
