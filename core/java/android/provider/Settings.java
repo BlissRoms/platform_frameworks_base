dbc732e039d52a0e8e0f4ec78e6b57e735a6902f
SuperDroidBond <superdroidbond@yahoo.com>
2021-12-03 20:57:47 +0530

    UDFPS: Allow user to disable night light on FOD [1/2]
    FOD: allow user to disable night light when active [1/2]
    Make this optional for some devices and config for default true on devices which weird light sensors on FOD
    eg: OP7TPRO
    
    Remove FOD night light if not enabled by default [1/2]
    
    SDB: adapt it for A12 UDFPS by Google
    
    Co-authored-by: Varun Date <date.varun123@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: SuperDroidBond <superdroidbond@yahoo.com>
    Change-Id: I8470e6a942e21580a874650c91ce8c08539768b9
    Signed-off-by: Ayan Mukherjee <mukherjeeayan725@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6341,6 +6341,12 @@ public final class Settings {
         public static final String FOD_COLOR = "fod_color";
 
         /**
+         * FOD night light
+         * @hide
+         */
+        public static final String FOD_NIGHT_LIGHT = "fod_night_light";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
