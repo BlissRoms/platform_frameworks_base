ea9a2c7ab15250007cbb117084a0dc05ef3a8164
spkal01 <kalligeross@gmail.com>
2021-12-09 22:18:05 +0100

    overlays: Introduce PUI iconpack
    FIles taken from here @PavlovaUI
    
    Change-Id: Icd8d2afd4e63a5a542e9517a30b69c373479cccf

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -84,6 +84,11 @@ LOCAL_REQUIRED_MODULES := \
         IconPackOutlineAndroidOverlay \
         IconPackOutlineSettingsOverlay \
         IconPackOutlineSystemUIOverlay \
+        IconPackPUIAndroidOverlay \
+        IconPackPUILauncherOverlay \
+        IconPackPUISettingsOverlay \
+        IconPackPUISystemUIOverlay \
+        IconPackPUIThemePickerOverlay \
 	NavigationBarMode3ButtonOverlay \
 	NavigationBarMode2ButtonOverlay \
 	NavigationBarModeGesturalOverlay \
