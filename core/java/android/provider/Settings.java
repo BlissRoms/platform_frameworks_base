0c0a027c2d49d6b589ab6644b90828b0d4e39135
spkal01 <spkal01@users.noreply.github.com>
2021-12-09 18:32:21 +0000

    Make colored Statusbar Icons optional [1/2]
    Change-Id: If6ee9aa1f4f95596b3354646fdcb1a9e17d84ed5

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5600,6 +5600,12 @@ public final class Settings {
         public static final String NAVIGATION_BAR_INVERSE = "navigation_bar_inverse";
 
         /**
+         * Whether StatusBar icons should use app icon
+         * @hide
+         */
+        public static final String STATUSBAR_COLORED_ICONS = "statusbar_colored_icons";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
