eed10e3896d9caff8e8741c5ccb07c2f18759ec5
naveenjohnsonv <naveenjohnsonv@users.noreply.github.com>
2021-12-10 20:22:52 +0000

    fixup! base: HeadsUp blacklists/stoplists
    Change-Id: Iefbf53bb3437a49f864246e86dbcb5e90310e176
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Ayan Mukherjee <mukherjeeayan725@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5500,7 +5500,6 @@ public final class Settings {
         public static final String HEADS_UP_BLACKLIST_VALUES = "heads_up_blacklist_values";
 
         /**
-         * Whether to enable status and navigation bar color in battery saver mode.
          * Heads up timeout configuration
          * @hide
          */
