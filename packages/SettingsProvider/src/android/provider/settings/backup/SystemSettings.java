1eee8521a679f07034fb3702a8ee953f3c37dc61
Ido Ben-Hur <idoybh2@gmail.com>
2022-01-06 20:45:48 +0200

    BatteryMeterView: Allow disabling QS battery estimates [1/2]
    Adapted to Sv2
    
    Change-Id: Ib5da6671505f598168326124618ec236df854dd7
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -91,5 +91,6 @@ public class SystemSettings {
         Settings.System.ENABLE_RIPPLE_EFFECT,
         Settings.System.NETWORK_TRAFFIC_STATE,
         Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD,
+        Settings.System.QS_SHOW_BATTERY_ESTIMATE,
     };
 }
