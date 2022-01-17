3aad8928b0c54be5cfc09f14d8b679c6bf76d5b3
Pranav Vashi <neobuddy89@gmail.com>
2022-01-17 21:09:53 +0530

    Privacy indicators [1/2]
    Kill old privacy indicator icons completely
    
    * It's messy to keep both versions
    
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11078,6 +11078,24 @@ public final class Settings {
         public static final String SHOW_BACK_ARROW_GESTURE = "show_back_arrow_gesture";
 
         /**
+         * Whether to show privacy indicator for location
+         * @hide
+         */
+        public static final String ENABLE_LOCATION_PRIVACY_INDICATOR = "enable_location_privacy_indicator";
+
+        /**
+         * Whether to show privacy indicator for camera
+         * @hide
+         */
+        public static final String ENABLE_CAMERA_PRIVACY_INDICATOR = "enable_camera_privacy_indicator";
+
+        /**
+         * Whether to show privacy indicator for media projection
+         * @hide
+         */
+        public static final String ENABLE_PROJECTION_PRIVACY_INDICATOR = "enable_projection_privacy_indicator";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
