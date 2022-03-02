9aeb66a518d48d99ec8bbb18fc5a3a1898ef0e78
jhonboy121 <alfredmathew05@gmail.com>
2022-03-03 00:30:45 +0530

    SystemUI: improve mobile data style switch logic
    * adapted to A12 L
    * ditch TunerService and use ContentObserver
    * added setting key to backup array
    * move settings observer to StatusBarIconControllerImpl class
    * set style when an icon manager is added and update it whenever settings changes, this way we don't update all icons whenever addMobileIcon is called
    
    Change-Id: Ieb423829584f906ba68944b4bdb75540188de608
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
@@ -407,10 +407,7 @@ public interface StatusBarIconController {
 
         private static final String SHOW_WIFI_STANDARD_ICON = Settings.Secure.SHOW_WIFI_STANDARD_ICON;
 
-        private boolean mOldStyleType;
-
-        private static final String USE_OLD_MOBILETYPE =
-            "system:" + Settings.System.USE_OLD_MOBILETYPE;
+        private boolean mIsOldSignalStyle = false;
 
         public IconManager(
                 ViewGroup group,
@@ -567,7 +564,7 @@ public interface StatusBarIconController {
             StatusBarMobileView mobileView = onCreateStatusBarMobileView(state.subId, slot);
             mobileView.applyMobileState(state);
             mGroup.addView(mobileView, index, onCreateLayoutParams());
-            Dependency.get(TunerService.class).addTunable(this, USE_OLD_MOBILETYPE);
+            mobileView.updateDisplayType(mIsOldSignalStyle);
 
             if (mIsInDemoMode) {
                 Context mobileContext = mMobileContextProvider
@@ -659,7 +656,6 @@ public interface StatusBarIconController {
 
         protected void destroy() {
             mGroup.removeAllViews();
-            Dependency.get(TunerService.class).removeTunable(this);
         }
 
         protected void onIconExternal(int viewIndex, int height) {
@@ -817,11 +813,6 @@ public interface StatusBarIconController {
                         TunerService.parseIntegerSwitch(newValue, false);
                     updateShowWifiStandard();
                     break;
-                case USE_OLD_MOBILETYPE:
-                    mOldStyleType =
-                        TunerService.parseIntegerSwitch(newValue, true);
-                    updateOldStyleMobileDataIcons();
-                    break;
                 default:
                     break;
             }
@@ -837,11 +828,15 @@ public interface StatusBarIconController {
             }
         }
 
-        private void updateOldStyleMobileDataIcons() {
+        protected void setMobileSignalStyle(boolean isOldSignalStyle) {
+            mIsOldSignalStyle = isOldSignalStyle;
+        }
+
+        protected void updateMobileIconStyle() {
             for (int i = 0; i < mGroup.getChildCount(); i++) {
-                View child = mGroup.getChildAt(i);
+                final View child = mGroup.getChildAt(i);
                 if (child instanceof StatusBarMobileView) {
-                    ((StatusBarMobileView) child).updateDisplayType(mOldStyleType);
+                    ((StatusBarMobileView) child).updateDisplayType(mIsOldSignalStyle);
                 }
             }
         }
