b244f45b8078baa9dca05287fd83246d6b3e0dce
jhonboy121 <alfredmathew05@gmail.com>
2022-03-03 02:08:12 +0530

    SystemUI: StatusBarIconController: remove unnecessary global variable for notification count visibility
    * this is overriden by NotificationAreaController
    
    Change-Id: I72cb6a0942d52b6873cf2f2770b15ee06b8e70da

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
@@ -24,6 +24,8 @@ import android.content.Context;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.provider.Settings;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.text.TextUtils;
 import android.util.ArraySet;
 import android.view.Gravity;
@@ -255,7 +257,6 @@ public interface StatusBarIconController {
         protected ArrayList<String> mBlockList = new ArrayList<>();
 
         private final boolean mNewIconStyle;
-        private final boolean mShowNotificationCount;
         private boolean mIsOldSignalStyle = false;
 
         public IconManager(ViewGroup group, FeatureFlags featureFlags) {
@@ -263,13 +264,9 @@ public interface StatusBarIconController {
             mGroup = group;
             mContext = group.getContext();
             mIconSize = mContext.getResources().getDimensionPixelSize(
-                    com.android.internal.R.dimen.status_bar_icon_size);
+                com.android.internal.R.dimen.status_bar_icon_size);
             mNewIconStyle = Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.STATUSBAR_COLORED_ICONS, 0, UserHandle.USER_CURRENT) == 1;
-            mShowNotificationCount = Settings.System.getIntForUser(mContext.getContentResolver(),
-                Settings.System.STATUSBAR_NOTIF_COUNT,
-                mContext.getResources().getBoolean(R.bool.config_statusBarShowNumber) ? 1 : 0,
-                UserHandle.USER_CURRENT) == 1;
         }
 
         public boolean isDemoable() {
@@ -331,7 +328,6 @@ public interface StatusBarIconController {
                 StatusBarIcon icon) {
             StatusBarIconView view = onCreateStatusBarIconView(slot, blocked);
             view.setIconStyle(mNewIconStyle);
-            view.setShowCount(mShowNotificationCount);
             view.set(icon);
             mGroup.addView(view, index, onCreateLayoutParams());
             return view;
