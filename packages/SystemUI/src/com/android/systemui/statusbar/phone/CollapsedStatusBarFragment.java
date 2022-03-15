Evolution-X-AOSP/frameworks_base

base: Fix carrier label being white on keyguard when using a light wallpaper.
* Reusing portions from AmbientIndicationContainer and from the AOSP statusbar
  to distinguish whether a light or dark wallpaper is in use, and whether the
  carrier label is shown on keyguard.

Signed-off-by: spezi77 <spezi7713@gmx.net>
Signed-off-by: Joey Huab <joey@evolution-x.org>

packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
@@ -44,6 +44,7 @@ import android.widget.LinearLayout;
 
 import com.android.systemui.R;
 import com.android.systemui.animation.Interpolators;
+import com.android.systemui.evolution.carrierlabel.CarrierLabel;
 import com.android.systemui.plugins.statusbar.StatusBarStateController;
 import com.android.systemui.statusbar.CommandQueue;
 import com.android.systemui.statusbar.FeatureFlags;
@@ -503,6 +504,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
 
     public void showCarrierName(boolean animate) {
         if (mCustomCarrierLabel != null) {
+            ((CarrierLabel)mCustomCarrierLabel).updateKeyguardState(false);
             setCarrierLabel(animate);
         }
     }

packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
@@ -54,6 +54,7 @@ import com.android.systemui.BatteryMeterView;
 import com.android.systemui.Dependency;
 import com.android.systemui.R;
 import com.android.systemui.animation.Interpolators;
+import com.android.systemui.evolution.carrierlabel.CarrierLabel;
 import com.android.systemui.plugins.DarkIconDispatcher.DarkReceiver;
 import com.android.systemui.statusbar.FeatureFlags;
 import com.android.systemui.statusbar.events.SystemStatusAnimationCallback;
@@ -254,9 +255,11 @@ public class KeyguardStatusBarView extends RelativeLayout implements
         mBatteryView.setForceShowPercent(mBatteryCharging && mShowPercentAvailable);
         if (mCarrierLabel != null) {
             if (mShowCarrierLabel == 1 || mShowCarrierLabel == 3) {
+                ((CarrierLabel)mCarrierLabel).updateKeyguardState(true);
                 mCarrierLabel.setVisibility(View.VISIBLE);
                 mCarrierLabel.setSelected(true);
             } else {
+                ((CarrierLabel)mCarrierLabel).updateKeyguardState(false);
                 mCarrierLabel.setVisibility(View.GONE);
                 mCarrierLabel.setSelected(false);
             }
@@ -536,7 +539,7 @@ public class KeyguardStatusBarView extends RelativeLayout implements
                 Color.luminance(textColor) < 0.5 ? R.color.dark_mode_icon_color_single_tone :
                 R.color.light_mode_icon_color_single_tone);
         float intensity = textColor == Color.WHITE ? 0 : 1;
-        mCarrierLabel.setTextColor(iconColor);
+        //mCarrierLabel.setTextColor(iconColor);
         if (mIconManager != null) {
             mIconManager.setTint(iconColor);
         }

BlissRoms/platform_frameworks_base

Change-Id: Icc290acc44143d71172460b4b45a0b80f664923e

packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
@@ -44,6 +44,7 @@ import android.widget.LinearLayout;
 
 import com.android.systemui.R;
 import com.android.systemui.animation.Interpolators;
+import com.android.systemui.bliss.carrierlabel.CarrierLabel;
 import com.android.systemui.plugins.statusbar.StatusBarStateController;
 import com.android.systemui.statusbar.CommandQueue;
 import com.android.systemui.statusbar.FeatureFlags;
@@ -503,6 +504,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
 
     public void showCarrierName(boolean animate) {
         if (mCustomCarrierLabel != null) {
+            ((CarrierLabel)mCustomCarrierLabel).updateKeyguardState(false);
             setCarrierLabel(animate);
         }
     }

packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
@@ -54,6 +54,7 @@ import com.android.systemui.BatteryMeterView;
 import com.android.systemui.Dependency;
 import com.android.systemui.R;
 import com.android.systemui.animation.Interpolators;
+import com.android.systemui.bliss.carrierlabel.CarrierLabel;
 import com.android.systemui.plugins.DarkIconDispatcher.DarkReceiver;
 import com.android.systemui.statusbar.FeatureFlags;
 import com.android.systemui.statusbar.events.SystemStatusAnimationCallback;
@@ -254,9 +255,11 @@ public class KeyguardStatusBarView extends RelativeLayout implements
         mBatteryView.setForceShowPercent(mBatteryCharging && mShowPercentAvailable);
         if (mCarrierLabel != null) {
             if (mShowCarrierLabel == 1 || mShowCarrierLabel == 3) {
+                ((CarrierLabel)mCarrierLabel).updateKeyguardState(true);
                 mCarrierLabel.setVisibility(View.VISIBLE);
                 mCarrierLabel.setSelected(true);
             } else {
+                ((CarrierLabel)mCarrierLabel).updateKeyguardState(false);
                 mCarrierLabel.setVisibility(View.GONE);
                 mCarrierLabel.setSelected(false);
             }
@@ -536,7 +539,7 @@ public class KeyguardStatusBarView extends RelativeLayout implements
                 Color.luminance(textColor) < 0.5 ? R.color.dark_mode_icon_color_single_tone :
                 R.color.light_mode_icon_color_single_tone);
         float intensity = textColor == Color.WHITE ? 0 : 1;
-        mCarrierLabel.setTextColor(iconColor);
+        //mCarrierLabel.setTextColor(iconColor);
         if (mIconManager != null) {
             mIconManager.setTint(iconColor);
         }