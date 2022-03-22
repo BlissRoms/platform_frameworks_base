1df5905ce2eff99e241a46ae7c93941adb029923
Ido Ben-Hur <idoybh2@gmail.com>
2022-03-22 22:02:04 +0530

    SystemUI: NavigationBar: disable secondary handle
    AKA fake navbar
    AGAIN!!! For the 3rd time. This ain't working dear google.
    Not even getting better with their attempts.
    I gave up hope on this, I don't see it ever coming back.
    
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

--- packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBar.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBar.java
@@ -708,7 +708,7 @@ public class NavigationBar extends ViewController<NavigationBarView> implements 
         }
         setDisabled2Flags(mDisabledFlags2);
 
-        initSecondaryHomeHandleForRotation();
+        //initSecondaryHomeHandleForRotation();
 
         // Unfortunately, we still need it because status bar needs LightBarController
         // before notifications creation. We cannot directly use getLightBarController()
@@ -1601,7 +1601,7 @@ public class NavigationBar extends ViewController<NavigationBarView> implements 
     }
 
     private boolean canShowSecondaryHandle() {
-        return mNavBarMode == NAV_BAR_MODE_GESTURAL && mOrientationHandle != null;
+        return false;
     }
 
     private final Consumer<Integer> mRotationWatcher = rotation -> {
