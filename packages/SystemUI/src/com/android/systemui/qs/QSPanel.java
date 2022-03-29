ab96d273955802f0f7aae60b61a741cebf255761
Pranav Vashi <neobuddy89@gmail.com>
2022-03-29 15:05:00 +0530

    QSPanel: Fix NPE in updateViewPositions()
    Log:
    03-29 12:29:10.729 D/AndroidRuntime(11908): Shutting down VM
    03-29 12:29:10.730 E/AndroidRuntime(11908): FATAL EXCEPTION: main
    03-29 12:29:10.730 E/AndroidRuntime(11908): Process: com.android.systemui, PID: 11908
    03-29 12:29:10.730 E/AndroidRuntime(11908): java.lang.NullPointerException: Attempt to invoke virtual method 'int java.lang.Integer.intValue()' on a null object reference
    03-29 12:29:10.730 E/AndroidRuntime(11908): 	at com.android.systemui.qs.QSPanel.updateViewPositions(QSPanel.java:294)
    03-29 12:29:10.730 E/AndroidRuntime(11908): 	at com.android.systemui.qs.QSPanel.setSquishinessFraction(QSPanel.java:225)
    03-29 12:29:10.730 E/AndroidRuntime(11908): 	at com.android.systemui.qs.QSPanelControllerBase.setSquishinessFraction(QSPanelControllerBase.java:152)
    03-29 12:29:10.730 E/AndroidRuntime(11908): 	at com.android.systemui.qs.QSSquishinessController.updateSquishiness(QSSquishinessController.kt:32)
    03-29 12:29:10.730 E/AndroidRuntime(11908): 	at com.android.systemui.qs.QSSquishinessController.setSquishiness(QSSquishinessController.kt:25)
    03-29 12:29:10.730 E/AndroidRuntime(11908): 	at com.android.systemui.qs.QSFragment.setQsExpansion(QSFragment.java:575)
    03-29 12:29:10.730 E/AndroidRuntime(11908): 	at com.android.systemui.statusbar.phone.NotificationPanelViewController.updateQsExpansion(NotificationPanelViewController.java:2330)
    03-29 12:29:10.730 E/AndroidRuntime(11908): 	at com.android.systemui.statusbar.phone.NotificationPanelViewController.setQsExpansion(NotificationPanelViewController.java:2300)
    03-29 12:29:10.730 E/AndroidRuntime(11908): 	at com.android.systemui.statusbar.phone.NotificationPanelViewController.closeQs(NotificationPanelViewController.java:1641)
    03-29 12:29:10.730 E/AndroidRuntime(11908): 	at com.android.systemui.statusbar.phone.NotificationPanelViewController.resetViews(NotificationPanelViewController.java:1601)
    03-29 12:29:10.730 E/AndroidRuntime(11908): 	at com.android.systemui.statusbar.phone.StatusBarKeyguardViewManager.reset(StatusBarKeyguardViewManager.java:563)
    03-29 12:29:10.730 E/AndroidRuntime(11908): 	at com.android.systemui.statusbar.phone.StatusBarKeyguardViewManager.show(StatusBarKeyguardViewManager.java:413)
    03-29 12:29:10.730 E/AndroidRuntime(11908): 	at com.android.systemui.keyguard.KeyguardViewMediator.handleShow(KeyguardViewMediator.java:2089)
    03-29 12:29:10.730 E/AndroidRuntime(11908): 	at com.android.systemui.keyguard.KeyguardViewMediator.access$3300(KeyguardViewMediator.java:179)
    03-29 12:29:10.730 E/AndroidRuntime(11908): 	at com.android.systemui.keyguard.KeyguardViewMediator$6.handleMessage(KeyguardViewMediator.java:1825)
    03-29 12:29:10.730 E/AndroidRuntime(11908): 	at android.os.Handler.dispatchMessage(Handler.java:106)
    03-29 12:29:10.730 E/AndroidRuntime(11908): 	at android.os.Looper.loopOnce(Looper.java:201)
    03-29 12:29:10.730 E/AndroidRuntime(11908): 	at android.os.Looper.loop(Looper.java:288)
    03-29 12:29:10.730 E/AndroidRuntime(11908): 	at android.app.ActivityThread.main(ActivityThread.java:7870)
    03-29 12:29:10.730 E/AndroidRuntime(11908): 	at java.lang.reflect.Method.invoke(Native Method)
    03-29 12:29:10.730 E/AndroidRuntime(11908): 	at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:548)
    03-29 12:29:10.730 E/AndroidRuntime(11908): 	at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:1003)
    
    Change-Id: I7fe4448e0562c40762d37f65b89ab7475a5061c4
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -324,7 +324,7 @@ public class QSPanel extends LinearLayout implements Tunable {
     }
 
     private void updateViewPositions() {
-        if (mTileLayout == null) {
+        if (mTileLayout == null || mChildrenLayoutTop == null || mChildrenLayoutTop.isEmpty()) {
             return;
         }
         // Adjust view positions based on tile squishing
