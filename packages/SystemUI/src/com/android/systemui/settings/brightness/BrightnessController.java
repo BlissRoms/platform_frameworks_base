d344c1a30495b56225348c33a04fe8caa25ceff4
yukiscape <garikapatiamartya@gmail.com>
2022-04-05 22:40:09 +0800

    BrightnessController: Update icon state for auto bg icon
    - Change button color depending on state
    - Update off state icon from Amartya (@ yukiscape)
    
    Change-Id: Ibf588b58f9c410177863a78a532c0375e86251bf
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- packages/SystemUI/src/com/android/systemui/settings/brightness/BrightnessController.java
+++ packages/SystemUI/src/com/android/systemui/settings/brightness/BrightnessController.java
@@ -470,8 +470,11 @@ public class BrightnessController implements ToggleSlider.Listener, MirroredBrig
     private void updateIcon(boolean automatic) {
         if (mIcon != null) {
             mIcon.setImageResource(mAutomatic ?
-                    com.android.systemui.R.drawable.ic_qs_brightness_auto_on :
-                    com.android.systemui.R.drawable.ic_qs_brightness_auto_off);
+                    R.drawable.ic_qs_brightness_auto_on_new :
+                    R.drawable.ic_qs_brightness_auto_off_new);
+            mIcon.setBackgroundResource(mAutomatic ?
+                    R.drawable.bg_qs_brightness_auto_on :
+                    R.drawable.bg_qs_brightness_auto_off);
         }
     }
 
