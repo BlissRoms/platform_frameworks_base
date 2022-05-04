3773a55a0ae9d004c5117eb292aecb44ae4b6c06
jhonboy121 <alfredmathew05@gmail.com>
2022-05-04 21:04:54 +0200

    Fullscreen gestures [1/3]
    Change-Id: Ic6695dfe929db443f169d59f153e14a6de249590

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5552,6 +5552,13 @@ public final class Settings {
         public static final String HIDE_LOCKSCREEN_STATUS_BAR = "hide_lockscreen_status_bar";
 
         /**
+         * Whether to hide navbar pill and keyboard space.
+         * Default 0.
+         * @hide
+         */
+        public static final String FULLSCREEN_GESTURES = "fullscreen_gestures";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
