31dc9e375931d7f2ef09f6ed63694d7317bb68f4
LibXZR <i@xzr.moe>
2022-05-08 00:54:05 +0800

    base: Add support for hiding navigation bar under screen keyboard [1/3]
    Some users prefer to hide it while others prefer to show it
    because the soft keyboard can be too bottom to touch comfortably.
    
    Change-Id: I7437839c0b936455e918772fd9248577b7a51932
    Signed-off-by: LibXZR <i@xzr.moe>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11280,6 +11280,13 @@ public final class Settings {
          * @hide
          */
         public static final String NAVIGATION_BAR_HINT = "navigation_bar_hint";
+
+        /**
+         * boolean value. toggles navigation bar under screen keyboard
+         * @hide
+         */
+        public static final String KEYBOARD_NO_NAVIGATION_BAR = "keyboard_no_navigation_bar";
+
     }
 
     /**
