CherishOS/android_frameworks_base

Gestures nav: Add Partial Screenshot action.
* Switch to the method in our utils, as this also supports the partial screenshot

Signed-off-by: spezi77 <spezi7713@gmx.net>
Change-Id: Ie8045e35ecb19e4c07113865c6a092859aab9746
Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -1024,7 +1024,7 @@ public class EdgeBackGestureHandler extends CurrentUserTracker
                 ActionUtils.switchScreenOff(mContext);
                 break;
             case 7: // Screenshot
-                ActionUtils.takeScreenshot(true);
+                CherishUtils.takeScreenshot(true);
                 break;
             case 8: // Notification panel
                 ActionUtils.toggleNotifications();
@@ -1052,6 +1052,9 @@ public class EdgeBackGestureHandler extends CurrentUserTracker
                 break;
             case 16: // Powermenu
                 CherishUtils.showPowerMenu();
+                break;
+            case 17: // Partial Screenshot
+                CherishUtils.takeScreenshot(false);
                 break;
         }
     }

BlissRoms/platform_frameworks_base

packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -1033,7 +1033,7 @@ public class EdgeBackGestureHandler extends CurrentUserTracker
                 ActionUtils.switchScreenOff(mContext);
                 break;
             case 7: // Screenshot
-                ActionUtils.takeScreenshot(true);
+                BlissUtils.takeScreenshot(true);
                 break;
             case 8: // Notification panel
                 ActionUtils.toggleNotifications();
@@ -1061,6 +1061,9 @@ public class EdgeBackGestureHandler extends CurrentUserTracker
                 break;
             case 16: // Powermenu
                 BlissUtils.showPowerMenu();
+                break;
+            case 17: // Partial Screenshot
+                BlissUtils.takeScreenshot(false);
                 break;
         }
     }