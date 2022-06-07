3a7b6d0de7abaedf011596a09fd46d7517447951
timjosten <timjosten@gmail.com>
2022-06-07 12:44:25 +0900

    base: Remove IME space under keyboard feature [1/2]
    Change-Id: I7e322d85d8dbd937cf657d37d0a86bdc7f371c3f
    
    base: Improve IME space feature [1/2]
    
     ** AICP Edits: add backup validators
    
    Change-Id: Ic0c514850e3318077ad0964d0c0234ceccc86c83
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6918,6 +6918,15 @@ public final class Settings {
         public static final String LOCKSCREEN_BATTERY_INFO = "lockscreen_battery_info";
 
         /**
+         * Gesture navbar IME space.
+         * Supported modes: 0 for normal length, 1 for narrow and 2 for hidden.
+         * Default 0.
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_IME_SPACE = "navigation_bar_ime_space";
+
+
+        /**
          * These are all public system settings
          *
          * @hide
@@ -7043,6 +7052,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(CLICK_PARTIAL_SCREENSHOT);
             PRIVATE_SETTINGS.add(KEY_EDGE_LONG_SWIPE_ACTION);
             PRIVATE_SETTINGS.add(SWAP_VOLUME_KEYS_ON_ROTATION);
+            PRIVATE_SETTINGS.add(NAVIGATION_BAR_IME_SPACE);
         }
 
         /**
