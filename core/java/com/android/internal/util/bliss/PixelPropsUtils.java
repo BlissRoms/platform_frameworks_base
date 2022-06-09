712f29771c8f5f26a56ce232ac378626c0a8562a
jhenrique09 <jhsv09@gmail.com>
2022-06-09 22:10:02 +0000

    PixelPropsUtils: Press F for bonito and sargo
    Change-Id: I7880e01c57f931d5dbca658e4a548f4ade69e697
    (cherry picked from commit d1d4c662a0e56653eb27483acea097f6eecb7a2f)

--- core/java/com/android/internal/util/bliss/PixelPropsUtils.java
+++ core/java/com/android/internal/util/bliss/PixelPropsUtils.java
@@ -145,9 +145,7 @@ public class PixelPropsUtils {
             "bramble",
             "sunfish",
             "coral",
-            "flame",
-            "bonito",
-            "sargo"
+            "flame"
     };
 
     private static volatile boolean sIsGms = false;
