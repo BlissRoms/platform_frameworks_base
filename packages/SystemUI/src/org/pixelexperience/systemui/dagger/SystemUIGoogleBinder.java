Evolution-X-AOSP/frameworks_base

[SQUASH] SystemUI: Adjust to SystemUIGoogle changes
* Squash of the following commits:

commit https://github.com/Evolution-X/frameworks_base/commit/58320d2c249d43ec207e355a4cf7f8c2b880a1a7

    SystemUIGoogle: Update QSFactoryImplGoogle for newly added QS tiles

commit https://github.com/Evolution-X/frameworks_base/commit/86da4f7073c2e5220ef33fd35c2e4988feb78b68

    SystemUI: Use Evolution ThemeOverlayController instead of Google ThemeOverlayController

commit https://github.com/Evolution-X/frameworks_base/commit/b5ed667c49deda65b47be0da3bfcf1d4d8fa9f16

    SystemUI: Update sequence to match pixel framework

commit https://github.com/Evolution-X/frameworks_base/commit/5902977ee39c04868ad61451a64d9de986f2206e

    SystemUI: Mover Tunable out of constructer to avoid conflict with pixel framework

Revert "SystemUI: Add HBM provider for UDFPS on Pixel devices"

This reverts commit 36f479e9dbc4ea8c29c0dcaf341592b5353e8ff8.

Revert "SystemUI: Import Pixel display interfaces"

This reverts commit 30a53138bd0dacc1e2242b57a9eec43db73b9076.

packages/SystemUI/src/org/pixelexperience/systemui/dagger/SystemUIGoogleBinder.java
@@ -44,7 +44,7 @@ import com.android.systemui.toast.ToastUI;
 import com.android.systemui.util.leak.GarbageMonitor;
 import com.android.systemui.volume.VolumeUI;
 import com.android.systemui.wmshell.WMShell;
-import com.google.android.systemui.theme.ThemeOverlayControllerGoogle;
+import com.evolution.android.systemui.theme.EvolutionThemeOverlayController;
 
 import org.pixelexperience.systemui.GoogleServices;
 import org.pixelexperience.systemui.columbus.ColumbusTargetRequestServiceWrapper;
@@ -168,7 +168,7 @@ public abstract class SystemUIGoogleBinder {
     @Binds
     @IntoMap
     @ClassKey(ThemeOverlayController.class)
-    public abstract SystemUI bindThemeOverlayController(ThemeOverlayControllerGoogle sysui);
+    public abstract SystemUI bindThemeOverlayController(EvolutionThemeOverlayController sysui);
 
     /**
      * Inject into ToastUI.

packages/SystemUI/src/org/pixelexperience/systemui/dagger/SystemUIGoogleDependencyProvider.java
@@ -67,7 +67,6 @@ import com.android.systemui.dagger.qualifiers.Background;
 import com.android.systemui.dagger.qualifiers.Main;
 import com.android.systemui.dock.DockManager;
 import com.android.systemui.dump.DumpManager;
-import com.android.systemui.flags.SystemPropertiesHelper;
 import com.android.systemui.keyguard.KeyguardViewMediator;
 import com.android.systemui.keyguard.WakefulnessLifecycle;
 import com.android.systemui.log.LogBuffer;
@@ -240,7 +239,7 @@ import com.google.android.systemui.statusbar.notification.voicereplies.Notificat
 import com.google.android.systemui.statusbar.notification.voicereplies.NotificationVoiceReplyLogger;
 import com.google.android.systemui.statusbar.notification.voicereplies.NotificationVoiceReplyManager;
 import com.google.android.systemui.statusbar.phone.WallpaperNotifier;
-import com.google.android.systemui.theme.ThemeOverlayControllerGoogle;
+import com.evolution.android.systemui.theme.EvolutionThemeOverlayController;
 
 import org.pixelexperience.systemui.GoogleServices;
 import org.pixelexperience.systemui.assist.AssistManagerGoogle;
@@ -418,8 +417,8 @@ public class SystemUIGoogleDependencyProvider {
 
     @Provides
     @SysUISingleton
-    static ThemeOverlayControllerGoogle provideThemeOverlayControllerGoogle(Context context, BroadcastDispatcher broadcastDispatcher, @Background Handler handler, @Main Executor executor, @Background Executor executorB, ThemeOverlayApplier themeOverlayApplier, SecureSettings secureSettings, SystemPropertiesHelper systemPropertiesHelper, @Main Resources resources, WallpaperManager wallpaperManager, UserManager userManager, DumpManager dumpManager, DeviceProvisionedController deviceProvisionedController, UserTracker userTracker, FeatureFlags featureFlags, WakefulnessLifecycle wakefulnessLifecycle, ConfigurationController configurationController) {
-        return new ThemeOverlayControllerGoogle(context, broadcastDispatcher, handler, executor, executorB, themeOverlayApplier, secureSettings, systemPropertiesHelper, resources, wallpaperManager, userManager, dumpManager, deviceProvisionedController, userTracker, featureFlags, wakefulnessLifecycle, configurationController);
+    static EvolutionThemeOverlayController provideThemeOverlayControllerGoogle(Context context, BroadcastDispatcher broadcastDispatcher, @Background Handler handler, @Main Executor executor, @Background Executor executorB, ThemeOverlayApplier themeOverlayApplier, SecureSettings secureSettings, WallpaperManager wallpaperManager, UserManager userManager, DumpManager dumpManager, DeviceProvisionedController deviceProvisionedController, UserTracker userTracker, FeatureFlags featureFlags, WakefulnessLifecycle wakefulnessLifecycle) {
+        return new EvolutionThemeOverlayController(context, broadcastDispatcher, handler, executor, executorB, themeOverlayApplier, secureSettings, wallpaperManager, userManager, dumpManager, deviceProvisionedController, userTracker, featureFlags, wakefulnessLifecycle);
     }
 
     @Provides

BlissRoms/platform_frameworks_base

Change-Id: Id1da7bb5c368966317d06e5f9c93a6a34e5917df
Signed-off-by: Ayan Mukherjee <mukherjeeayan725@gmail.com>

packages/SystemUI/src/org/pixelexperience/systemui/dagger/SystemUIGoogleBinder.java
@@ -18,7 +18,6 @@ package org.pixelexperience.systemui.dagger;
 
 import android.app.Activity;
 import android.app.Service;
-
 import com.android.systemui.LatencyTester;
 import com.android.systemui.ScreenDecorations;
 import com.android.systemui.SliceBroadcastRelayHandler;
@@ -44,7 +43,7 @@ import com.android.systemui.toast.ToastUI;
 import com.android.systemui.util.leak.GarbageMonitor;
 import com.android.systemui.volume.VolumeUI;
 import com.android.systemui.wmshell.WMShell;
-import com.google.android.systemui.theme.ThemeOverlayControllerGoogle;
+import com.bliss.android.systemui.theme.BlissThemeOverlayController;
 
 import org.pixelexperience.systemui.GoogleServices;
 import org.pixelexperience.systemui.columbus.ColumbusTargetRequestServiceWrapper;
@@ -58,6 +57,7 @@ import dagger.multibindings.IntoMap;
 
 @Module(includes = {RecentsModule.class, StatusBarGoogleModule.class, KeyguardModule.class})
 public abstract class SystemUIGoogleBinder {
+
     /**
      * Inject into AuthController.
      */
@@ -168,7 +168,7 @@ public abstract class SystemUIGoogleBinder {
     @Binds
     @IntoMap
     @ClassKey(ThemeOverlayController.class)
-    public abstract SystemUI bindThemeOverlayController(ThemeOverlayControllerGoogle sysui);
+    public abstract SystemUI bindThemeOverlayController(BlissThemeOverlayController sysui);
 
     /**
      * Inject into ToastUI.

packages/SystemUI/src/org/pixelexperience/systemui/dagger/SystemUIGoogleDependencyProvider.java
@@ -67,7 +67,6 @@ import com.android.systemui.dagger.qualifiers.Background;
 import com.android.systemui.dagger.qualifiers.Main;
 import com.android.systemui.dock.DockManager;
 import com.android.systemui.dump.DumpManager;
-import com.android.systemui.flags.SystemPropertiesHelper;
 import com.android.systemui.keyguard.KeyguardViewMediator;
 import com.android.systemui.keyguard.WakefulnessLifecycle;
 import com.android.systemui.log.LogBuffer;
@@ -240,7 +239,7 @@ import com.google.android.systemui.statusbar.notification.voicereplies.Notificat
 import com.google.android.systemui.statusbar.notification.voicereplies.NotificationVoiceReplyLogger;
 import com.google.android.systemui.statusbar.notification.voicereplies.NotificationVoiceReplyManager;
 import com.google.android.systemui.statusbar.phone.WallpaperNotifier;
-import com.google.android.systemui.theme.ThemeOverlayControllerGoogle;
+import com.bliss.android.systemui.theme.BlissThemeOverlayController;
 
 import org.pixelexperience.systemui.GoogleServices;
 import org.pixelexperience.systemui.assist.AssistManagerGoogle;
@@ -418,8 +417,8 @@ public class SystemUIGoogleDependencyProvider {
 
     @Provides
     @SysUISingleton
-    static ThemeOverlayControllerGoogle provideThemeOverlayControllerGoogle(Context context, BroadcastDispatcher broadcastDispatcher, @Background Handler handler, @Main Executor executor, @Background Executor executorB, ThemeOverlayApplier themeOverlayApplier, SecureSettings secureSettings, SystemPropertiesHelper systemPropertiesHelper, @Main Resources resources, WallpaperManager wallpaperManager, UserManager userManager, DumpManager dumpManager, DeviceProvisionedController deviceProvisionedController, UserTracker userTracker, FeatureFlags featureFlags, WakefulnessLifecycle wakefulnessLifecycle, ConfigurationController configurationController) {
-        return new ThemeOverlayControllerGoogle(context, broadcastDispatcher, handler, executor, executorB, themeOverlayApplier, secureSettings, systemPropertiesHelper, resources, wallpaperManager, userManager, dumpManager, deviceProvisionedController, userTracker, featureFlags, wakefulnessLifecycle, configurationController);
+    static BlissThemeOverlayController provideThemeOverlayControllerGoogle(Context context, BroadcastDispatcher broadcastDispatcher, @Background Handler handler, @Main Executor executor, @Background Executor executorB, ThemeOverlayApplier themeOverlayApplier, SecureSettings secureSettings, WallpaperManager wallpaperManager, UserManager userManager, DumpManager dumpManager, DeviceProvisionedController deviceProvisionedController, UserTracker userTracker, FeatureFlags featureFlags, WakefulnessLifecycle wakefulnessLifecycle) {
+        return new BlissThemeOverlayController(context, broadcastDispatcher, handler, executor, executorB, themeOverlayApplier, secureSettings, wallpaperManager, userManager, dumpManager, deviceProvisionedController, userTracker, featureFlags, wakefulnessLifecycle);
     }
 
     @Provides