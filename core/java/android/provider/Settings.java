4bf4a4d5fb51051a84c17e1c7e9ae293223dd081
althafvly <althafvly@gmail.com>
2022-06-29 15:02:37 +0530

    Keyguard: Allow user configurable fingerprint wake-and-unlock
    Co-authored-by: Steve Kondik <shade@chemlab.org>
    Co-authored-by: mikeNG <mkbestas@lineageos.org>
    Change-Id: I8a095e83930eadc3e03ff79c00dc9c53208b933a

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6211,6 +6211,12 @@ public final class Settings {
         public static final String QS_TRANSPARENCY = "qs_transparency";
 
         /**
+         * Whether to enable fingerprint wake-and-unlock.
+         * @hide
+         */
+        public static final String FINGERPRINT_WAKE_UNLOCK = "fingerprint_wake_unlock";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
