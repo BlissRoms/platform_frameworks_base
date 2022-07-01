faa228820d965f56eb350e2f16f2c03df39686b5
Pranav Vashi <neobuddy89@gmail.com>
2022-07-02 02:48:55 +0530

    SystemUI: Add current divider config for lockscreen charging
    * Some modern devices measure directly in mA instead uA.
    * We will let devices specify config_currentInfoDivider to check
      if uA to mA conversion is required.
    * Use same divider for wattage calculation.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -184,6 +184,8 @@ public class KeyguardIndicationController {
     private boolean mFaceDetectionRunning;
     private boolean mFaceDetectionIndicationShowing;
 
+    private int mCurrentDivider;
+
     private KeyguardUpdateMonitorCallback mUpdateMonitorCallback;
 
     private boolean mDozing;
@@ -308,6 +310,8 @@ public class KeyguardIndicationController {
         mKeyguardStateController.addCallback(mKeyguardStateCallback);
 
         mStatusBarStateListener.onDozingChanged(mStatusBarStateController.isDozing());
+
+        mCurrentDivider = mContext.getResources().getInteger(R.integer.config_currentInfoDivider);
     }
 
     public void setIndicationArea(ViewGroup indicationArea) {
@@ -507,7 +511,7 @@ public class KeyguardIndicationController {
         if (mPowerPluggedIn || mEnableBatteryDefender) {
             String powerIndication = computePowerIndication();
             if (DEBUG_CHARGING_SPEED) {
-                powerIndication += ",  " + (mChargingWattage / 1000) + " mW";
+                powerIndication += ",  " + (mChargingWattage / mCurrentDivider) + " mW";
             }
 
             mRotateTextViewController.updateIndication(
@@ -950,15 +954,15 @@ public class KeyguardIndicationController {
         boolean showbatteryInfo = Settings.System.getIntForUser(mContext.getContentResolver(),
             Settings.System.LOCKSCREEN_BATTERY_INFO, 1, UserHandle.USER_CURRENT) == 1;
         voltage = mChargingVoltage / 1000 / 1000;
+        current = (mChargingCurrent < 5 ? (mChargingCurrent * 1000)
+                   : (mChargingCurrent < 4000 ? mChargingCurrent : (mChargingCurrent / mCurrentDivider)));
         if (showbatteryInfo) {
             if (mChargingCurrent > 0) {
-                current = (mChargingCurrent < 5 ? (mChargingCurrent * 1000)
-                        : (mChargingCurrent < 4000 ? mChargingCurrent : (mChargingCurrent / 1000)));
                 batteryInfo = batteryInfo + current + "mA";
             }
             if (mChargingVoltage > 0 && mChargingCurrent > 0) {
                 batteryInfo = (batteryInfo == "" ? "" : batteryInfo + " · ") +
-                        String.format("%.1f" , ((double) current / 1000) * voltage) + "W";
+                        String.format("%.1f" , ((double) current / mCurrentDivider) * voltage) + "W";
             }
             if (mChargingVoltage > 0) {
                 batteryInfo = (batteryInfo == "" ? "" : batteryInfo + " · ") +
