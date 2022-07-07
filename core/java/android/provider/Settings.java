095e66371b3315fc86bd919a557fdefce8945036
Ayan Mukherjee <mukherjeeayan725@gmail.com>
2022-07-06 22:28:15 -0400

    Revert "[SQUASH] Custom logo customizations [1/2]"
    This reverts commit 1ff57764fb3bc7fd93e3efdd9bd4b9a06a55e7bc.
    
    Change-Id: I447ac54bd8d3543f0212b284ccb08f7ddb17d3e1

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6015,38 +6015,6 @@ public final class Settings {
         public static final String FLASHLIGHT_ON_CALL_IGNORE_DND = "flashlight_on_call_ignore_dnd";
 
         /**
-         * Statusbar logo
-         * @hide
-         */
-        public static final String STATUS_BAR_LOGO = "status_bar_logo";
-
-        /**
-         * Statusbar logo color setting
-         * @hide
-         */
-        public static final String STATUS_BAR_LOGO_COLOR = "status_bar_logo_color";
-
-        /**
-         * Position of Status bar logo
-         * 0 - Left (default)
-         * 1 - Right
-         * @hide
-         */
-        public static final String STATUS_BAR_LOGO_POSITION = "status_bar_logo_position";
-
-        /**
-         * Statusbar logo custom style
-         * @hide
-         */
-        public static final String STATUS_BAR_LOGO_STYLE = "status_bar_logo_style";
-
-        /**
-         * Statusbar logo accent color setting
-         * @hide
-         */
-        public static final String STATUS_BAR_LOGO_COLOR_ACCENT = "status_bar_logo_color_accent";
-
-        /**
          * Rate in Hz in which to blink flashlight_on_call
          * @hide
          */
@@ -6898,11 +6866,6 @@ public final class Settings {
             PRIVATE_SETTINGS.add(FLASHLIGHT_ON_CALL_IGNORE_DND);
             PRIVATE_SETTINGS.add(FLASHLIGHT_ON_CALL_RATE);
             PRIVATE_SETTINGS.add(BACK_GESTURE_HAPTIC);
-            PRIVATE_SETTINGS.add(STATUS_BAR_LOGO);
-            PRIVATE_SETTINGS.add(STATUS_BAR_LOGO_COLOR);
-            PRIVATE_SETTINGS.add(STATUS_BAR_LOGO_POSITION);
-            PRIVATE_SETTINGS.add(STATUS_BAR_LOGO_STYLE);
-            PRIVATE_SETTINGS.add(STATUS_BAR_LOGO_COLOR_ACCENT);
             PRIVATE_SETTINGS.add(VOLTE_ICON_STYLE);
             PRIVATE_SETTINGS.add(VOWIFI_ICON_STYLE);
             PRIVATE_SETTINGS.add(VOLTE_VOWIFI_OVERRIDE);
