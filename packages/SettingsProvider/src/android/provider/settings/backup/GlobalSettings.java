7117e9d708e7ece1a5f96e94a9aa4d7470c99628
t-m-w <tmw-likes-calyx@pm.me>
2022-08-26 15:45:17 +0000

    Move Bluetooth timeout back to AOSP settings
    This is so it can be accessed by the Bluetooth APEX module.
    
    Change-Id: I81941dc4e390b17e4ec8f57bf45327f3b8b525db
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- packages/SettingsProvider/src/android/provider/settings/backup/GlobalSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/GlobalSettings.java
@@ -112,6 +112,7 @@ public class GlobalSettings {
         Settings.Global.Wearable.SCREENSHOT_ENABLED,
         Settings.Global.Wearable.SCREEN_UNLOCK_SOUND_ENABLED,
         Settings.Global.Wearable.CHARGING_SOUNDS_ENABLED,
-        Settings.Global.WIFI_OFF_TIMEOUT
+        Settings.Global.WIFI_OFF_TIMEOUT,
+        Settings.Global.BLUETOOTH_OFF_TIMEOUT
     };
 }
