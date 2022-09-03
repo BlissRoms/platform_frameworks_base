c374c69998fd9573b0abe07fc591dc1dc8195de6
Alberto97 <alberto97@gmail.com>
2022-09-03 09:41:31 +0200

    Port brightness slider changes [1/2]
     * Expose AutoBrightness button
     * Add tunables
     * Use vector drawables for brightness icons
    
    [BadDaemon]
     * Adapt to SystemUI changes
     * Change code style to use lambdas and locals where possible
    
    [marshmello61]
     * Forward port to Android 13
    
    Author: Luca Stefani <luca020400@lineageos.org>
    Date:   Thu Apr 20 18:00:52 2017 +0200
    
        SystemUI: Use AOSP AutoBrightness implementation
    
        Change-Id: I3e3660065407d460f30c6e936d951d4d5eae30c6
    
    Author: Michael Bestas <mikeioannina@gmail.com>
    Date:   Thu Apr 27 14:57:49 2017 +0300
    
        SystemUI: Use already existing overlay to show QS auto brightness
    
        * No need for a new overlay when one exists for years
    
        Change-Id: I9e09a1770f8e645093e880cf1a48597405e0bcf2
    
    Author: Alberto97 <albertop2197@gmail.com>
    Date:   Mon May 29 21:07:05 2017 +0200
    
        SystemUI: Specify user while changing brightness mode
    
        Allow any user changing brightness mode,
        without this only the main one is able to do that.
    
        BUGBASH-598
    
        Change-Id: Ib51e9a45c42837cabb1cfb0d48334165b023a827
    
    Author: Michael Bestas <mkbestas@lineageos.org>
    Date:   Sun Apr 30 13:45:39 2017 +0300
    
        SystemUI: Add tunable for auto brightness icon
    
        Change-Id: I02fe3f5500f95ba9369b952bbec2a073b7ae8316
    
    Author: Alexander Westphal <westphal.alex94@gmail.com>
    Date:   Wed Apr 26 09:20:53 2017 +0100
    
        SystemUI: use vector drawables for brightness icons
    
        Change-Id: I8d22f8de22040d68ed1bfab0da3a78aec717fa62
    
    Author: qjohn <townex22@gmail.com>
    Date:   Sun Jun 18 16:28:10 2017 +0700
    
        SystemUI: Remove unnecessary brightness icons
    
        Change-Id: I7e60ff73125bd0304f9ca14ad5c5a9ce11ffceda
    
    Author: Michael W <baddaemon87@gmail.com>
    Date:   Mon May 25 15:38:33 2020 +0200
    
        SystemUI: Brightness slider: Always show the brightness icon
    
        * Right now, we reserve the space for the icon but it's not displayed
          (reserved space is there so the slider doesn't jump)
        * Always display the icon (if automatic brightness is available)
          so it's more consistent
    
        Change-Id: I5238dc6aad51407349a8d42fae840ea76841dc3d
    
    Author: Michael W <baddaemon87@gmail.com>
    Date:   Fri May 29 20:09:18 2020 +0200
    
        SystemUI: Fix for auto brightness icon being shown when disabled
    
        * With commit 9d42c48c16bba2fcc836cbb68e2979ed9c0d1b23 the case of a
          manually disabled auto brightness icon got lost
        * Re-implement it by also respecting the Settings value instead of just
          the build time flag
    
        Change-Id: I991977be7e1f7c33947073dbd3ce7a800fb158bf
    
    Change-Id: Ic72b0084a4108fb4fa26e893bd6d1eaca96cbb6c
    
    Allow tuning position for brightness slider
    
    @neobuddy89: Cleaned up, fixed and optimized
    @marshmello61: Forward port to Android 13
    
    Revert "Remove QQS brightness controller"
    
    This reverts commit 32b7c35f24d29a4e87e386267c725549cc1b6259.
    
    Bring back QQS brightness slider
    
    @marshmello61: forward port to Android 13
    
    Fix brightness slider issues with media mode
    
    * Properly add brightness slider to required position
    
    Refactor brightness slider paddings
    
    Use brightness icon matching with slider redesign
    
    Clean up and fix QQS brightness slider padding
    
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Change-Id: Ib9eabd73920a432d6fcc73a21353b7b508cbbd5f

--- packages/SystemUI/src/com/android/systemui/qs/QuickQSPanelController.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickQSPanelController.java
@@ -33,6 +33,11 @@ import com.android.systemui.qs.customize.QSCustomizerController;
 import com.android.systemui.qs.dagger.QSScope;
 import com.android.systemui.qs.logging.QSLogger;
 import com.android.systemui.util.leak.RotationUtils;
+import com.android.systemui.settings.brightness.BrightnessController;
+import com.android.systemui.settings.brightness.BrightnessMirrorHandler;
+import com.android.systemui.settings.brightness.BrightnessSliderController;
+import com.android.systemui.statusbar.policy.BrightnessMirrorController;
+import com.android.systemui.tuner.TunerService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -53,6 +58,11 @@ public class QuickQSPanelController extends QSPanelControllerBase<QuickQSPanel> 
             };
 
     private final boolean mUsingCollapsedLandscapeMedia;
+    private final BrightnessController mBrightnessController;
+    private final TunerService mTunerService;
+    private final BrightnessSliderController mBrightnessSliderController;
+    private final BrightnessMirrorHandler mBrightnessMirrorHandler;
+    private BrightnessMirrorController mBrightnessMirrorController;
 
     @Inject
     QuickQSPanelController(QuickQSPanel view, QSTileHost qsTileHost,
@@ -61,11 +71,21 @@ public class QuickQSPanelController extends QSPanelControllerBase<QuickQSPanel> 
             @Named(QUICK_QS_PANEL) MediaHost mediaHost,
             @Named(QS_USING_COLLAPSED_LANDSCAPE_MEDIA) boolean usingCollapsedLandscapeMedia,
             MetricsLogger metricsLogger, UiEventLogger uiEventLogger, QSLogger qsLogger,
-            DumpManager dumpManager
+            DumpManager dumpManager,
+            TunerService tunerService,
+            BrightnessController.Factory brightnessControllerFactory,
+            BrightnessSliderController.Factory brightnessSliderFactory
     ) {
         super(view, qsTileHost, qsCustomizerController, usingMediaPlayer, mediaHost, metricsLogger,
                 uiEventLogger, qsLogger, dumpManager);
         mUsingCollapsedLandscapeMedia = usingCollapsedLandscapeMedia;
+        mTunerService = tunerService;
+
+        mBrightnessSliderController = brightnessSliderFactory.create(getContext(), mView);
+        mView.setBrightnessView(mBrightnessSliderController.getRootView());
+
+        mBrightnessController = brightnessControllerFactory.create(mBrightnessSliderController);
+        mBrightnessMirrorHandler = new BrightnessMirrorHandler(mBrightnessController);
     }
 
     @Override
@@ -74,6 +94,7 @@ public class QuickQSPanelController extends QSPanelControllerBase<QuickQSPanel> 
         updateMediaExpansion();
         mMediaHost.setShowsOnlyActiveMedia(true);
         mMediaHost.init(MediaHierarchyManager.LOCATION_QQS);
+        mBrightnessSliderController.init();
     }
 
     private void updateMediaExpansion() {
@@ -95,18 +116,61 @@ public class QuickQSPanelController extends QSPanelControllerBase<QuickQSPanel> 
     @Override
     protected void onViewAttached() {
         super.onViewAttached();
+
+        mTunerService.addTunable(mView, QSPanel.QS_BRIGHTNESS_SLIDER_POSITION);
+        mTunerService.addTunable(mView, QSPanel.QS_SHOW_AUTO_BRIGHTNESS);
+        mTunerService.addTunable(mView, QSPanel.QS_SHOW_BRIGHTNESS_SLIDER);
+
+        mView.setBrightnessRunnable(() -> {
+            mView.updateResources();
+            updateBrightnessMirror();
+        });
+
         mView.addOnConfigurationChangedListener(mOnConfigurationChangedListener);
+        mBrightnessMirrorHandler.onQsPanelAttached();
     }
 
     @Override
     protected void onViewDetached() {
         super.onViewDetached();
+        mTunerService.removeTunable(mView);
+        mView.setBrightnessRunnable(null);
         mView.removeOnConfigurationChangedListener(mOnConfigurationChangedListener);
+        mBrightnessMirrorHandler.onQsPanelDettached();
+    }
+
+    private void updateBrightnessMirror() {
+        if (mBrightnessMirrorController != null) {
+            mBrightnessSliderController.setMirrorControllerAndMirror(mBrightnessMirrorController);
+        }
+    }
+
+    @Override
+    void setListening(boolean listening) {
+        super.setListening(listening);
+
+        // Set the listening as soon as the QS fragment starts listening regardless of the
+        //expansion, so it will update the current brightness before the slider is visible.
+        if (listening) {
+            mBrightnessController.registerCallbacks();
+        } else {
+            mBrightnessController.unregisterCallbacks();
+        }
+    }
+
+    public boolean isListening() {
+        return mView.isListening();
     }
 
     private void setMaxTiles(int parseNumTiles) {
         mView.setMaxTiles(parseNumTiles);
         setTiles();
+    }
+
+    @Override
+    public void refreshAllTiles() {
+        mBrightnessController.checkRestrictionAndSetEnabled();
+        super.refreshAllTiles();
     }
 
     @Override
@@ -133,5 +197,10 @@ public class QuickQSPanelController extends QSPanelControllerBase<QuickQSPanel> 
 
     public int getNumQuickTiles() {
         return mView.getNumQuickTiles();
+    }
+
+    public void setBrightnessMirror(BrightnessMirrorController brightnessMirrorController) {
+        mBrightnessMirrorController = brightnessMirrorController;
+        mBrightnessMirrorHandler.setController(brightnessMirrorController);
     }
 }
