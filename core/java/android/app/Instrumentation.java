44aa94b6dcc9a2b9db6351ba8af4fd87a20eea4e
Danny Lin <danny@kdrag0n.dev>
2022-09-12 12:54:31 +0000

    [SQUASH] Switch to AttestationHooks
    Also includes:
    
    Spoof product name for Google Play Services (https://github.com/hentaiOS/platform_frameworks_base/commit/05b3987d7b892ea50ba0eee865fe988c27ae90c1)
    
    NB: This code is under the gmscompat package, but it does not depend on
    any code from gmscompat.
    
    Change-Id: Ic018c0d7abe4573143c3b92301a2625b91e6673a
    
    keystore: Block key attestation for SafetyNet (https://github.com/hentaiOS/platform_frameworks_base/commit/cac828325ab375775d7e0b0cc39f428ad17e0ea5)
    
    SafetyNet (part of Google Play Services) opportunistically uses
    hardware-backed key attestation via KeyStore as a strong integrity
    check. This causes SafetyNet to fail on custom ROMs because the verified
    boot key and bootloader unlock state can be detected from attestation
    certificates.
    
    As a workaround, we can take advantage of the fact that SafetyNet's
    usage of key attestation is opportunistic (i.e. falls back to basic
    integrity checks if it fails) and prevent it from getting the
    attestation certificate chain from KeyStore. This is done by checking
    the stack for DroidGuard, which is the codename for SafetyNet, and
    pretending that the device doesn't support key attestation.
    
    Key attestation has only been blocked for SafetyNet specifically, as
    Google Play Services and other apps have many valid reasons to use it.
    For example, it appears to be involved in Google's mobile security key
    ferature.
    
    Change-Id: I5146439d47f42dc6231cb45c4dab9f61540056f6
    
    Limit SafetyNet workarounds to unstable GMS process (https://github.com/hentaiOS/platform_frameworks_base/commit/fce9b65304829f16ad9bc87ad0cdd1472cb5a594)
    
    The unstable process is where SafetyNet attestation actually runs, so
    we only need to spoof the model in that process. Leaving other processes
    fixes various issues caused by model detection and flag provisioning,
    including screen-off Voice Match in Google Assistant, broken At a Glance
    weather and settings on Android 12, and more.
    
    Change-Id: Idcf663907a6c3d0408dbd45b1ac53c9eb4200df8
    
    gmscompat: Apply the SafetyNet workaround to Play Store aswell (https://github.com/hentaiOS/platform_frameworks_base/commit/c9a086f9b635bf4e3e61fbd501ade464f8f5b713)
    
    Play Store is used for the new Play Integrity API, extend the hack
    to it aswell
    
    Test: Device Integrity and Basic Integrity passes.
    
    Signed-off-by: Dyneteve <dyneteve@hentaios.com>
    Change-Id: Id607cdff0b902f285a6c1b769c0a4ee4202842b1
    
    gmscompat: Use Nexus 6P fingerprint for CTS/Integrity (https://github.com/hentaiOS/platform_frameworks_base/commit/813f11628014a93d45f55dedd434fdddd9510eb0)
    
    Google seems to have patched the KM block to Play Store in record time,
    but is still not enforced for anything under android N.
    
    Since we moved to angler FP we don't need to spoof model to Play Store
    anymore, however the KM block is still needed.
    
    Test: Run Play Intregrity Attestation
    
    Signed-off-by: Dyneteve <dyneteve@hentaios.com>
    Change-Id: Ic2401a6e40ddfc4318a1d0faa87e42eb118ac3d1
    
    Co-authored-by: Dyneteve <dyneteve@hentaios.com>
    Change-Id: I47c13dce33ac210071002f7b1ca2158502afc2f1
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -65,6 +65,7 @@ import java.util.List;
 import java.util.concurrent.TimeoutException;
 
 import com.android.internal.util.bliss.PixelPropsUtils;
+import com.android.internal.util.bliss.AttestationHooks;
 
 /**
  * Base class for implementing application instrumentation code.  When running
@@ -1244,6 +1245,7 @@ public class Instrumentation {
         Application app = getFactory(context.getPackageName())
                 .instantiateApplication(cl, className);
         app.attach(context);
+        AttestationHooks.initApplicationBeforeOnCreate(app);
         String packageName = app.getPackageName();
         PixelPropsUtils.setProps(packageName);
         return app;
@@ -1263,6 +1265,7 @@ public class Instrumentation {
             ClassNotFoundException {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
+        AttestationHooks.initApplicationBeforeOnCreate(app);
         String packageName = app.getPackageName();
         PixelPropsUtils.setProps(packageName);
         return app;
--- keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
+++ keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
@@ -42,6 +42,7 @@ import android.system.keystore2.ResponseCode;
 import android.util.Log;
 
 import com.android.internal.annotations.VisibleForTesting;
+import com.android.internal.util.bliss.AttestationHooks;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -166,7 +167,7 @@ public class AndroidKeyStoreSpi extends KeyStoreSpi {
 
     @Override
     public Certificate[] engineGetCertificateChain(String alias) {
-        PixelPropsUtils.onEngineGetCertificateChain();
+        AttestationHooks.onEngineGetCertificateChain();
 
         KeyEntryResponse response = getKeyMetadata(alias);
 
