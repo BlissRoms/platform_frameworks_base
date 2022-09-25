67f586a4c1a823cde763931b9bfbdac7d9164e5a
LibXZR <i@xzr.moe>
2022-09-25 16:22:02 +0800

    ActivityStarter: Resolve launcher intent to parallel owner user
    Some apps e.g. com.hicorenational.antifraud, start launcher intent
    on exit. This can be a problem when doing it in parallel spaces.
    
    Start the launcher of parallel owner user instead to workaround
    this issue.
    
    Change-Id: I98a1f11f1925d8fa79bcc2f1e0787165d5506cb5
    Signed-off-by: LibXZR <i@xzr.moe>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- services/core/java/com/android/server/wm/ActivityStarter.java
+++ services/core/java/com/android/server/wm/ActivityStarter.java
@@ -149,6 +149,8 @@ import com.android.server.wm.BackgroundActivityStartController.BalCode;
 import com.android.server.wm.LaunchParamsController.LaunchParams;
 import com.android.server.wm.TaskFragment.EmbeddingCheckResult;
 
+import ink.kaleidoscope.server.ParallelSpaceManagerService;
+
 import java.io.PrintWriter;
 import java.text.DateFormat;
 import java.util.Date;
@@ -683,6 +685,12 @@ class ActivityStarter {
      */
     int execute() {
         try {
+            if (ParallelSpaceManagerService.isCurrentParallelUser(mRequest.userId) &&
+                    Intent.ACTION_MAIN.equals(mRequest.intent.getAction()) &&
+                    mRequest.intent.hasCategory(Intent.CATEGORY_HOME)) {
+                mRequest.userId = ParallelSpaceManagerService.getCurrentParallelOwnerId();
+            }
+
             onExecutionStarted();
 
             // Refuse possible leaked file descriptors
