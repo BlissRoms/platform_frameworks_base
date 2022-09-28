9cc94ae9180a46905a68ff30f81b6bdbab7397f1
Anay Wadhera <anay1018@gmail.com>
2022-09-28 11:24:43 +0900

    SystemUI: mark reevaluateSystemTheme as protected so we can access it
    Change-Id: I1658bff306da604b0d419afd60c8c5c4aa35d9eb
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
@@ -568,7 +568,7 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
         }
     }
 
-    private void reevaluateSystemTheme(boolean forceReload) {
+    protected void reevaluateSystemTheme(boolean forceReload) {
         final WallpaperColors currentColors = mCurrentColors.get(mUserTracker.getUserId());
         final int mainColor;
         if (currentColors == null) {
