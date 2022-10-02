Project-Xtended/frameworks_base

PowerManagerService: Send broadcast when going to sleep
Signed-off-by: Mukesh Singh <mks22584@gmail.com>

core/java/com/android/internal/util/xtended/content/Intent.java
@@ -1,0 +1,15 @@
+package com.android.internal.util.xtended.content;
+import android.Manifest;
+
+/**
+ * crDroid specific intent definition class.
+ */
+public class Intent {
+
+    /**
+     * Broadcast action: go to sleep
+     * @hide
+     */
+    public static final String ACTION_GO_TO_SLEEP =
+            "xtended.intent.action.GO_TO_SLEEP";
+}

services/core/java/com/android/server/power/PowerManagerService.java
@@ -2030,7 +2030,15 @@ public final class PowerManagerService extends SystemService
         } finally {
             Trace.traceEnd(Trace.TRACE_TAG_POWER);
         }
+        mHandler.post(() -> sendSleepBroadcast());
         return true;
+    }
+
+    private void sendSleepBroadcast() {
+        Intent intent = new Intent(
+                com.android.internal.util.xtended.content.Intent.ACTION_GO_TO_SLEEP);
+        intent.setFlags(Intent.FLAG_RECEIVER_INCLUDE_BACKGROUND);
+        mContext.sendBroadcastAsUser(intent, UserHandle.SYSTEM);
     }
 
     private void napInternal(long eventTime, int uid) {

BlissRoms/platform_frameworks_base

Signed-off-by: mukesh22584 <mks22584@gmail.com>
Signed-off-by: Jackeagle <jackeagle102@gmail.com>
Change-Id: Ie0f7643671d9ae1aae01cd8a6f6bcf10bd6af48f

core/java/com/android/internal/util/bliss/content/Intent.java
@@ -1,0 +1,15 @@
+package com.android.internal.util.bliss.content;
+import android.Manifest;
+
+/**
+ * crDroid specific intent definition class.
+ */
+public class Intent {
+
+    /**
+     * Broadcast action: go to sleep
+     * @hide
+     */
+    public static final String ACTION_GO_TO_SLEEP =
+            "bliss.intent.action.GO_TO_SLEEP";
+}

services/core/java/com/android/server/power/PowerManagerService.java
@@ -5748,7 +5748,15 @@ public final class PowerManagerService extends SystemService
             } finally {
                 Binder.restoreCallingIdentity(ident);
             }
+            mHandler.post(() -> sendSleepBroadcast());
         }
+        
+        private void sendSleepBroadcast() {
+        Intent intent = new Intent(
+                com.android.internal.util.bliss.content.Intent.ACTION_GO_TO_SLEEP);
+        intent.setFlags(Intent.FLAG_RECEIVER_INCLUDE_BACKGROUND);
+        mContext.sendBroadcastAsUser(intent, UserHandle.SYSTEM);
+    }
 
         @Override // Binder call
         public void nap(long eventTime) {
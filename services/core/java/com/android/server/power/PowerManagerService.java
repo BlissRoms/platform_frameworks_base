ccda8deedabf0c5ea6029476b439c304c6e59116
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2021-08-05 14:57:33 +0530

    PowerManagerService: Send broadcast when going to sleep
    Signed-off-by: mukesh22584 <mks22584@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Change-Id: Ie0f7643671d9ae1aae01cd8a6f6bcf10bd6af48f

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -5748,7 +5748,15 @@ public final class PowerManagerService extends SystemService
             } finally {
                 Binder.restoreCallingIdentity(ident);
             }
+            mHandler.post(() -> sendSleepBroadcast());
         }
+        
+        private void sendSleepBroadcast() {
+        Intent intent = new Intent(
+                com.android.internal.util.bliss.content.Intent.ACTION_GO_TO_SLEEP);
+        intent.setFlags(Intent.FLAG_RECEIVER_INCLUDE_BACKGROUND);
+        mContext.sendBroadcastAsUser(intent, UserHandle.SYSTEM);
+    }
 
         @Override // Binder call
         public void nap(long eventTime) {
