80b22f427994b508d33d9658e6e72ed98d1c608e
jhenrique09 <jhsv09@gmail.com>
2022-11-08 09:30:15 -0300

    Hide face recognizing message on bouncer when authenticated
    Change-Id: I28869635ba9d7361b4db042a1049c6afe501dbf9

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarKeyguardViewManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarKeyguardViewManager.java
@@ -270,6 +270,14 @@ public class StatusBarKeyguardViewManager implements RemoteInputController.Callb
                  }
              }
          }
+
+        @Override
+        public void onBiometricAuthenticated(int userId, BiometricSourceType biometricSourceType,
+                boolean isStrongBiometric) {
+            if (biometricSourceType == BiometricSourceType.FACE) {
+                hideFaceRecognizingMessage();
+            }
+        }
     };
 
     @Inject
@@ -1234,6 +1242,10 @@ public class StatusBarKeyguardViewManager implements RemoteInputController.Callb
                 mKeyguardUpdateManager.isUnlockWithFacePossible(mKeyguardUpdateManager.getCurrentUser())) {
             showBouncerMessage(mContext.getString(R.string.face_unlock_recognizing), null);
         }
+    }
+
+    private void hideFaceRecognizingMessage(){
+        showBouncerMessage("", null);
     }
 
     public void showBouncerMessage(String message, ColorStateList colorState) {
