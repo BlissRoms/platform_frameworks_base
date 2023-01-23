23722a2aa589f4de31254e6ee71f7bcbb18bd606
rezaadi0105 <rezaadipangestu5@gmail.com>
2023-01-23 22:19:38 +0700

    base: Data icon style [1/3]
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayApplier.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayApplier.java
@@ -125,6 +125,9 @@ public class ThemeOverlayApplier implements Dumpable {
     @VisibleForTesting
     static final String OVERLAY_CATEGORY_ICON_WIFI =
             "android.theme.customization.wifi_icon";
+    @VisibleForTesting
+    static final String OVERLAY_CATEGORY_DATA =
+            "android.customization.sb_data";
 
     /*
      * All theme customization categories used by the system, in order that they should be applied,
@@ -142,7 +145,8 @@ public class ThemeOverlayApplier implements Dumpable {
             OVERLAY_CATEGORY_ICON_SETTINGS,
             OVERLAY_CATEGORY_ICON_THEME_PICKER,
             OVERLAY_CATEGORY_ICON_SIGNAL,
-            OVERLAY_CATEGORY_ICON_WIFI);
+            OVERLAY_CATEGORY_ICON_WIFI,
+            OVERLAY_CATEGORY_DATA);
 
     /* Categories that need to be applied to the current user as well as the system user. */
     @VisibleForTesting
@@ -153,7 +157,8 @@ public class ThemeOverlayApplier implements Dumpable {
             OVERLAY_CATEGORY_FONT,
             OVERLAY_CATEGORY_SHAPE,
             OVERLAY_CATEGORY_ICON_ANDROID,
-            OVERLAY_CATEGORY_ICON_SYSUI);
+            OVERLAY_CATEGORY_ICON_SYSUI,
+            OVERLAY_CATEGORY_DATA);
 
     /* Allowed overlay categories for each target package. */
     private final Map<String, Set<String>> mTargetPackageToCategories = new ArrayMap<>();
@@ -201,6 +206,7 @@ public class ThemeOverlayApplier implements Dumpable {
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_ICON_THEME_PICKER, mThemePickerPackage);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_ICON_SIGNAL, SYSUI_PACKAGE);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_ICON_WIFI, SYSUI_PACKAGE);
+        mCategoryToTargetPackage.put(OVERLAY_CATEGORY_DATA, SYSUI_PACKAGE);
 
         dumpManager.registerDumpable(TAG, this);
     }
