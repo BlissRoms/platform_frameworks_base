bc1199c13b4476af502be83305f822c432e8d77c
Dmitrii <bankersenator@gmail.com>
2023-02-16 06:48:12 +0000

    fwb: remove spammy wallet lockscreen icon error
    There no wallet by default in AOSP
    Change-Id: Icb11015d1b993fc42d8a221b31f045001079fd99
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: mukesh22584 <mks22584@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- packages/SystemUI/src/com/android/systemui/keyguard/data/quickaffordance/QuickAccessWalletKeyguardQuickAffordanceConfig.kt
+++ packages/SystemUI/src/com/android/systemui/keyguard/data/quickaffordance/QuickAccessWalletKeyguardQuickAffordanceConfig.kt
@@ -78,10 +78,6 @@ constructor(
                         }
 
                         override fun onWalletCardRetrievalError(error: GetWalletCardsError) {
-                            Log.e(
-                                TAG,
-                                "Wallet card retrieval error, message: \"${error?.message}\""
-                            )
                             trySendWithFailureLogging(
                                 null,
                                 TAG,
