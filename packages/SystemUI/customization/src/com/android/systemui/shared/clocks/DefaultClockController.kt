314d690b8b155fad9edcbfc0fc20039f91451d43
minaripenguin <minaripenguin@users.noreply.github.com>
2023-04-01 16:45:53 +0800

    SystemUI: Introduce dynamic lockscreen clock spacing
    Change-Id: I22f0f27111af36b2599293c9ac648321757fd67d
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/customization/src/com/android/systemui/shared/clocks/DefaultClockController.kt
+++ packages/SystemUI/customization/src/com/android/systemui/shared/clocks/DefaultClockController.kt
@@ -58,6 +58,7 @@ class DefaultClockController(
     private val burmeseLineSpacing =
         resources.getFloat(R.dimen.keyguard_clock_line_spacing_scale_burmese)
     private val defaultLineSpacing = resources.getFloat(R.dimen.keyguard_clock_line_spacing_scale)
+    private val defaultFont = resources.getString(com.android.internal.R.string.config_clockFontFamily);
 
     override val events: DefaultClockEvents
     override lateinit var animations: DefaultClockAnimations
@@ -206,8 +207,25 @@ class DefaultClockController(
             val nf = NumberFormat.getInstance(locale)
             if (nf.format(FORMAT_NUMBER.toLong()) == burmeseNumerals) {
                 clocks.forEach { it.setLineSpacingScale(burmeseLineSpacing) }
-            } else {
+            } else if (defaultFont.toString().toLowerCase().contains("sans") && !defaultFont.toString().toLowerCase().contains("google")) {
+                clocks.forEach { it.setLineSpacingScale(0.88f) }
+            } else if (defaultFont.toString().toLowerCase().contains("google")) {
                 clocks.forEach { it.setLineSpacingScale(defaultLineSpacing) }
+            } else if (defaultFont.toString().toLowerCase().contains("apice") 
+            	  || defaultFont.toString().toLowerCase().contains("coolstory")
+            	  || defaultFont.toString().toLowerCase().contains("evolve")) {
+                clocks.forEach { it.setLineSpacingScale(0.92f) }
+            } else if (defaultFont.toString().toLowerCase().contains("BetsyFlanagan") 
+            	  || defaultFont.toString().toLowerCase().contains("AlphaClouds")
+            	  || defaultFont.toString().toLowerCase().contains("AlphaFlowers")
+            	  || defaultFont.toString().toLowerCase().contains("EditPoints")
+            	  || defaultFont.toString().toLowerCase().contains("EditPointsFilled")
+            	  || defaultFont.toString().toLowerCase().contains("HotSweat")
+            	  || defaultFont.toString().toLowerCase().contains("LMSClifford")
+            	  || defaultFont.toString().toLowerCase().contains("frankfrt")) {
+                clocks.forEach { it.setLineSpacingScale(0.97f) }
+            } else {
+                clocks.forEach { it.setLineSpacingScale(0.9f) }
             }
 
             clocks.forEach { it.refreshFormat() }
