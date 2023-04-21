64fcce23559cb1f235c0187500ca5389bfbc7b11
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-04-21 23:39:51 +0530

    PixelPropsUtils: Spoof Snapchat to Pixel XL
    Inspired by
    https://github.com/PixysOS/frameworks_base/commit/3afdf68b5bb48a89eb8b3782cca0fa354ab9889f
    https://github.com/PixysOS/frameworks_base/commit/b0475ea8f522d3a156ed262189bc4c70bf91b24f
    
    Co-authored-by: aswin7469 <aswinas@pixysos.com>
    Co-authored-by: John Galt <johngaltfirstrun@gmail.com>
    Change-Id: I8b5d7a6a59b0e049bad07b69d9df847890ba5881

--- core/java/com/android/internal/util/bliss/PixelPropsUtils.java
+++ core/java/com/android/internal/util/bliss/PixelPropsUtils.java
@@ -52,6 +52,7 @@ public class PixelPropsUtils {
 
     private static final String[] packagesToChangePixelXL = {
             "com.google.android.apps.photos",
+            "com.snapchat.android",
             "com.google.android.inputmethod.latin"
     };
 
