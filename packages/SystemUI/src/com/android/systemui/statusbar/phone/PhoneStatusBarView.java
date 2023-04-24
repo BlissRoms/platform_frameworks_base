5bebe707cce9fe01c2732761b1b4b790b8ebd6a0
SuperDroidBond <superdroidbond@yahoo.com>
2023-04-24 13:06:15 +0000

    Statusbar Padding: correct the padding for big Ass Notch devices [1/2]
    SDB: All Notch shit depends on Statusbar padding. Why not keep it as default value
    travarilo: Also add for top padding
    
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>
    Signed-off-by: rizkybenggolo <travarilo@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
@@ -19,6 +19,9 @@ package com.android.systemui.statusbar.phone;
 
 import android.annotation.Nullable;
 import android.content.Context;
+import android.content.pm.IPackageManager;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.graphics.Rect;
@@ -66,6 +69,9 @@ public class PhoneStatusBarView extends FrameLayout implements TunerService.Tuna
     private int mTopPad;
     private int mLeftPad;
     private int mRightPad;
+    private int sbPaddingTopRes;
+    private int sbPaddingStartRes;
+    private int sbPaddingEndRes;
 
     private DarkReceiver mBattery;
     private ClockController mClockController;
@@ -241,12 +247,23 @@ public class PhoneStatusBarView extends FrameLayout implements TunerService.Tuna
         mStatusBarHeight = SystemBarUtils.getStatusBarHeight(mContext);
         layoutParams.height = mStatusBarHeight - waterfallTopInset;
 
+        float density = Resources.getSystem().getDisplayMetrics().density;
+        Resources res = null;
+        try {
+            res = mContext.getPackageManager().getResourcesForApplication("com.android.systemui");
+        } catch (NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
         int statusBarPaddingTop = getResources().getDimensionPixelSize(
                 R.dimen.status_bar_padding_top);
         int statusBarPaddingStart = getResources().getDimensionPixelSize(
                 R.dimen.status_bar_padding_start);
         int statusBarPaddingEnd = getResources().getDimensionPixelSize(
                 R.dimen.status_bar_padding_end);
+        sbPaddingTopRes = (int) (statusBarPaddingTop / density);
+        sbPaddingStartRes = (int) (statusBarPaddingStart / density);
+        sbPaddingEndRes = (int) (statusBarPaddingEnd / density);
 
         mStatusBarContents.setPaddingRelative(
                 (int) mLeftPad,
@@ -340,19 +357,19 @@ public class PhoneStatusBarView extends FrameLayout implements TunerService.Tuna
     @Override
     public void onTuningChanged(String key, String newValue) {
         if (TOP_PADDING.equals(key)) {
-            int mTPadding = TunerService.parseInteger(newValue, 0);
+            int mTPadding = TunerService.parseInteger(newValue, sbPaddingTopRes);
             mTopPad = Math.round(TypedValue.applyDimension(
                 TypedValue.COMPLEX_UNIT_DIP, mTPadding,
                 getResources().getDisplayMetrics()));
             updateStatusBarHeight();
         } else if (LEFT_PADDING.equals(key)) {
-            int mLPadding = TunerService.parseInteger(newValue, 0);
+            int mLPadding = TunerService.parseInteger(newValue, sbPaddingStartRes);
             mLeftPad = Math.round(TypedValue.applyDimension(
                 TypedValue.COMPLEX_UNIT_DIP, mLPadding,
                 getResources().getDisplayMetrics()));
             updateStatusBarHeight();
         } else if (RIGHT_PADDING.equals(key)) {
-            int mRPadding = TunerService.parseInteger(newValue, 0);
+            int mRPadding = TunerService.parseInteger(newValue, sbPaddingEndRes);
             mRightPad = Math.round(TypedValue.applyDimension(
                 TypedValue.COMPLEX_UNIT_DIP, mRPadding,
                 getResources().getDisplayMetrics()));
