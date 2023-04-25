6a0ed2fb81ffbf227a186cc984caa3d0d638688c
Riddle Hsu <riddlehsu@google.com>
2023-04-25 12:28:05 +0000

    Update config according to insets of status bar
    Previously, only cutout and navbar are included, so only the change
    of decor frame may trigger config change.
    But screenWidthDp and screenHeightDp are calculated according to
    config (stable) frame, which includes the insets from status bar.
    
    So this change makes the display configuration consistent with the
    latest state of the display decor insets.
    
    Also includes Ieedcc6388237b7feccd8a869105a5f8e70ff4afd to ensure
    insets in non-current rotation are updated.
    
    Fix: 259693044
    Test: DisplayPolicyTests#testUpdateDisplayConfigurationByDecor
    
    (cherry picked from commit e3d243d3e7740bd856e2ba5ce53802c39b3cc341)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:494547431fdff4bde9f2f45367c6e4741380ea7c)
    Merged-In: Idaa7280ed23076b3282c987a249259e0c38b6630
    Change-Id: Idaa7280ed23076b3282c987a249259e0c38b6630

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -1891,7 +1891,7 @@ public class WindowManagerService extends IWindowManager.Stub
 
             boolean needToSendNewConfiguration =
                     win.isVisibleRequestedOrAdding() && displayContent.updateOrientation();
-            if (win.providesNonDecorInsets()) {
+            if (win.providesDisplayDecorInsets()) {
                 needToSendNewConfiguration |= displayPolicy.updateDecorInsetsInfo();
             }
             if (needToSendNewConfiguration) {
@@ -2364,7 +2364,7 @@ public class WindowManagerService extends IWindowManager.Stub
                         & WindowManager.LayoutParams.SYSTEM_UI_VISIBILITY_CHANGED) != 0) {
                     win.mLayoutNeeded = true;
                 }
-                if (layoutChanged && win.providesNonDecorInsets()) {
+                if (layoutChanged && win.providesDisplayDecorInsets()) {
                     configChanged = displayPolicy.updateDecorInsetsInfo();
                 }
                 if (win.mActivityRecord != null && ((flagChanges & FLAG_SHOW_WHEN_LOCKED) != 0
