816077ab0f573d5cbfc18796a9e892e93cb166a7
Jis G Jacob <studiokeys@blissroms.org>
2023-04-26 11:17:20 -0400

    [SQUASH]: Revert PixelPropUtils stack
    Change-Id: I7d094e7dc8d6dfb7cc9749cef0fc813c914c532e

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -64,8 +64,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeoutException;
 
-import com.android.internal.util.voltage.PixelPropsUtils;
-
 /**
  * Base class for implementing application instrumentation code.  When running
  * with instrumentation turned on, this class will be instantiated for you
@@ -1244,8 +1242,6 @@ public class Instrumentation {
         Application app = getFactory(context.getPackageName())
                 .instantiateApplication(cl, className);
         app.attach(context);
-        String packageName = context.getPackageName();
-        PixelPropsUtils.setProps(packageName);
         return app;
     }
     
@@ -1263,8 +1259,6 @@ public class Instrumentation {
             ClassNotFoundException {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
-        String packageName = context.getPackageName();
-        PixelPropsUtils.setProps(packageName);
         return app;
     }
 
