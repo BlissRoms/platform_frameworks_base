a9e9435eb1aa251b5e55f7804e90efd7e632ed77
REV3NT3CH <rev3nt3ch@gmail.com>
2023-04-26 11:42:35 -0700

    Revert "SystemUI: Bring back good ol' circle battery style"
    This reverts commit c17e5e78caa9a17fc9963fff23496d56cc5fb45c.
    
    Change-Id: If429fe8efeb8c8aa883b89a7449d00f88651bf86

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -76,7 +76,6 @@ public class SystemSettings {
         Settings.System.NOTIFICATION_SOUND,
         Settings.System.ACCELEROMETER_ROTATION,
         Settings.System.SHOW_BATTERY_PERCENT,
-        Settings.System.SHOW_BATTERY_PERCENT_CHARGING,
         Settings.System.ALARM_VIBRATION_INTENSITY,
         Settings.System.MEDIA_VIBRATION_INTENSITY,
         Settings.System.NOTIFICATION_VIBRATION_INTENSITY,
@@ -93,8 +92,6 @@ public class SystemSettings {
         Settings.System.NETWORK_TRAFFIC_STATE,
         Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD,
         Settings.System.QS_SHOW_BATTERY_ESTIMATE,
-        Settings.System.STATUS_BAR_BATTERY_STYLE,
-        Settings.System.SHOW_BATTERY_PERCENT_INSIDE,
         Settings.System.LOCKSCREEN_BATTERY_INFO,
         Settings.System.LOCKSCREEN_BATTERY_INFO_TEMP_UNIT,
     };
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
@@ -66,6 +66,7 @@ public class KeyguardStatusBarView extends RelativeLayout {
 
     private final ArrayList<Rect> mEmptyTintRect = new ArrayList<>();
 
+    private boolean mShowPercentAvailable;
     private boolean mBatteryCharging;
 
     private TextView mCarrierLabel;
@@ -188,6 +189,8 @@ public class KeyguardStatusBarView extends RelativeLayout {
                 R.dimen.ongoing_appops_dot_min_padding);
         mCutoutSideNudge = getResources().getDimensionPixelSize(
                 R.dimen.display_cutout_margin_consumption);
+        mShowPercentAvailable = getContext().getResources().getBoolean(
+                com.android.internal.R.bool.config_battery_percentage_setting_available);
         mRoundedCornerPadding = res.getDimensionPixelSize(
                 R.dimen.rounded_corner_content_padding);
     }
@@ -224,6 +227,7 @@ public class KeyguardStatusBarView extends RelativeLayout {
                 mMultiUserAvatar.setVisibility(View.GONE);
             }
         }
+        mBatteryView.setForceShowPercent(mBatteryCharging && mShowPercentAvailable);
     }
 
     private void updateSystemIconsLayoutParams() {
