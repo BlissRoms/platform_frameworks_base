e2425ab81043032e54e563f0cbf5a41c8ad6bebc
REV3NT3CH <rev3nt3ch@gmail.com>
2023-04-26 11:42:35 -0700

    Revert "SystemUI: Bring back good ol' circle battery style"
    This reverts commit c17e5e78caa9a17fc9963fff23496d56cc5fb45c.
    
    Change-Id: I884a40d9c6edc4ffea826f2d4202eeccb607fac1

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5522,15 +5522,6 @@ public final class Settings {
         public static final String SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
 
         /**
-         * Setting to determine whether or not to show the battery percentage in the status bar when charging
-         *    0 - Don't show percentage
-         *    1 - Show percentage
-         * @hide
-         */
-        @Readable
-        public static final String SHOW_BATTERY_PERCENT_CHARGING = "status_bar_show_battery_percent_charging";
-
-        /**
          * Whether or not to enable multiple audio focus.
          * When enabled, requires more management by user over application playback activity,
          * for instance pausing media apps when another starts.
@@ -5959,22 +5950,6 @@ public final class Settings {
          */
         @Readable
         public static final String QS_SHOW_BATTERY_ESTIMATE = "qs_show_battery_estimate";
-
-        /**
-         * Style of battery to show.
-         * 0 = protrait (default)
-         * 1 = circle
-         * 2 = text
-         * @hide
-         */
-        @Readable
-        public static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
-
-        /**
-         * Whether to draw battery percent inside the icon
-         * @hide
-         */
-        public static final String SHOW_BATTERY_PERCENT_INSIDE = "status_bar_show_battery_percent_inside";
 
         /**
          * Whether to show the battery info on the lockscreen while charging
