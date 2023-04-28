c187a20eabb523b7efb460844bf744322b56e9a4
Pranav Vashi <neobuddy89@gmail.com>
2023-04-28 11:01:16 +0530

    SystemUI: Add pixel style lockscreen weather based OmniJaws
    * Similar to SmartSpace but more consistent with keyguard status area.
    * Complete rewrite of omni implementation on Android 11.
    
    Ref:
    https://github.com/crdroidandroid/android_frameworks_base/commit/97734230d0c1f92f5bc87ff8a515b198d92cdd15
    https://github.com/crdroidandroid/android_frameworks_base/commit/05fae49a27afdc68ff9747cbe03de7da0d26bb52
    https://github.com/crdroidandroid/android_frameworks_base/commit/8a923c24fb633b7d9f28e4d78145abc44dbb1350
    
    Change-Id: I138c0dc94f08142f6614659037a501d6ae8909b1
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7286,6 +7286,16 @@ public final class Settings {
         public static final String VIBRATE_ON_CONNECT = "vibrate_on_connect";
 
         /**
+         * @hide
+         */
+        public static final String LOCKSCREEN_WEATHER_ENABLED = "lockscreen_weather_enabled";
+
+        /**
+         * @hide
+         */
+        public static final String LOCKSCREEN_WEATHER_LOCATION = "lockscreen_weather_location";
+
+        /**
          * Whether the phone vibrates on call waiting
          * @hide
          */
