b64c6ae23b3a3ff263087db8dd675e6fb9d02efb
minaripenguin <minaripenguin@users.noreply.github.com>
2023-05-10 07:03:09 +0800

    SystemUI: Lock screen clock top margin settings [1/2]
    Change-Id: I2c53b4e2c5188bb753695d5a101abeaec41c8860
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11988,6 +11988,11 @@ public final class Settings {
         public static final String KG_FONT_TYPE = "kg_font_type";
 
         /**
+         * @hide
+         */
+        public static final String KG_CUSTOM_CLOCK_TOP_MARGIN = "kg_custom_clock_top_margin";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
