f88adac4afbb66a9820db868dd9be0c288efef22
jro1979oliver <jroliveira.oliveira301@gmail.com>
2023-05-16 18:55:20 +0200

    Revert "AudioService: do not log sound effects in audio dumpsys"
    This reverts commit e2a281aa250654cfc3e1d57629c10721ac2e0867.
    
    Reason for revert: It seems causing media volume decreasing and not returning to the last value after a notification sound
    
    Same thing happens on android 14.
    
    Change-Id: Ic5c3becd44917ec0b8a04c9d0810e5e3055f584b
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- services/core/java/com/android/server/audio/PlaybackActivityMonitor.java
+++ services/core/java/com/android/server/audio/PlaybackActivityMonitor.java
@@ -219,18 +219,6 @@ public final class PlaybackActivityMonitor
     }
 
     //=================================================================
-    // Player to ignore (only handling single player, designed for ignoring
-    // in the logs one specific player such as the touch sounds player)
-    @GuardedBy("mPlayerLock")
-    private ArrayList<Integer> mDoNotLogPiidList = new ArrayList<>();
-
-    /*package*/ void ignorePlayerIId(int doNotLogPiid) {
-        synchronized (mPlayerLock) {
-            mDoNotLogPiidList.add(doNotLogPiid);
-        }
-    }
-
-    //=================================================================
     // Track players and their states
     // methods playerAttributes, playerEvent, releasePlayer are all oneway calls
     //  into AudioService. They trigger synchronous dispatchPlaybackChange() which updates
@@ -358,16 +346,10 @@ public final class PlaybackActivityMonitor
             Log.v(TAG, TextUtils.formatSimple("playerEvent(piid=%d, event=%s, eventValue=%d)",
                     piid, AudioPlaybackConfiguration.playerStateToString(event), eventValue));
         }
-        boolean change;
+        final boolean change;
         synchronized(mPlayerLock) {
             final AudioPlaybackConfiguration apc = mPlayers.get(new Integer(piid));
             if (apc == null) {
-                return;
-            }
-
-            final boolean doNotLog = mDoNotLogPiidList.contains(piid);
-            if (doNotLog && event != AudioPlaybackConfiguration.PLAYER_STATE_RELEASED) {
-                // do not log nor dispatch events for "ignored" players other than the release
                 return;
             }
             sEventLogger.enqueue(new PlayerEvent(piid, event, eventValue));
@@ -389,8 +371,7 @@ public final class PlaybackActivityMonitor
                     }
                 }
             }
-            if (apc.getPlayerType() == AudioPlaybackConfiguration.PLAYER_TYPE_JAM_SOUNDPOOL
-                    && event != AudioPlaybackConfiguration.PLAYER_STATE_RELEASED) {
+            if (apc.getPlayerType() == AudioPlaybackConfiguration.PLAYER_TYPE_JAM_SOUNDPOOL) {
                 // FIXME SoundPool not ready for state reporting
                 return;
             }
@@ -402,15 +383,9 @@ public final class PlaybackActivityMonitor
                 Log.e(TAG, "Error handling event " + event);
                 change = false;
             }
-            if (change) {
-                if (event == AudioPlaybackConfiguration.PLAYER_STATE_STARTED) {
-                    mDuckingManager.checkDuck(apc);
-                    mFadeOutManager.checkFade(apc);
-                }
-                if (doNotLog) {
-                    // do not dispatch events for "ignored" players
-                    change = false;
-                }
+            if (change && event == AudioPlaybackConfiguration.PLAYER_STATE_STARTED) {
+                mDuckingManager.checkDuck(apc);
+                mFadeOutManager.checkFade(apc);
             }
         }
         if (change) {
@@ -517,10 +492,6 @@ public final class PlaybackActivityMonitor
                     }
                 }
 
-                if (change && mDoNotLogPiidList.contains(piid)) {
-                    // do not dispatch a change for a "do not log" player
-                    change = false;
-                }
             }
         }
         if (change) {
@@ -672,9 +643,6 @@ public final class PlaybackActivityMonitor
             for (Integer piidInt : piidIntList) {
                 final AudioPlaybackConfiguration apc = mPlayers.get(piidInt);
                 if (apc != null) {
-                    if (mDoNotLogPiidList.contains(apc.getPlayerInterfaceId())) {
-                        pw.print("(not logged)");
-                    }
                     apc.dump(pw);
                 }
             }
