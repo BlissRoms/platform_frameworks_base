b25b0332397ce1d7b43e0c9acae5a7ab08bbed0b
Jun Wang <quic_junwan@quicinc.com>
2023-05-19 13:43:22 +0800

    Optimize AbsListView to reduce click operation latency
    Separate performClick from delayed message. Then we can do
    performClick in advance of PRESSED_STATE_DURATION(64ms) delay.
    This will benefit for click operation latency. The delayed
    message is still used to clear pressed status.
    
    Change-Id: I95eb72c5362a1838ab16ee8211120aee8cd548e5
    CRs-Fixed: 3503330
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- core/java/android/widget/AbsListView.java
+++ core/java/android/widget/AbsListView.java
@@ -4335,6 +4335,10 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
                                     }
                                     mSelector.setHotspot(x, ev.getY());
                                 }
+                                if (!mDataChanged && !mIsDetaching
+                                        && isAttachedToWindow()) {
+                                    performClick.run();
+                                }
                                 if (mTouchModeReset != null) {
                                     removeCallbacks(mTouchModeReset);
                                 }
@@ -4345,10 +4349,6 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
                                         mTouchMode = TOUCH_MODE_REST;
                                         child.setPressed(false);
                                         setPressed(false);
-                                        if (!mDataChanged && !mIsDetaching
-                                                && isAttachedToWindow()) {
-                                            performClick.run();
-                                        }
                                     }
                                 };
                                 postDelayed(mTouchModeReset,
