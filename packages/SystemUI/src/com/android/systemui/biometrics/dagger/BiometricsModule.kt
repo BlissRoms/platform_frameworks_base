313da926cbbc8492ff61feb7482ac32781fc1d03
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-06-22 23:11:13 +0530

    SystemUI: Add FingerprintInteractiveToAuthProvider implementation
    This is required for the "touch to unlock anytime" setting on devices
    with side mounted fingerprint sensor.
    
    Ref: 66a048d646fd816e7757e300c62fa5cebf04761d
    
    Change-Id: If3308860a428d9966f2b0a0024764df95f8f9a8b
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- packages/SystemUI/src/com/android/systemui/biometrics/dagger/BiometricsModule.kt
+++ packages/SystemUI/src/com/android/systemui/biometrics/dagger/BiometricsModule.kt
@@ -16,6 +16,8 @@
 
 package com.android.systemui.biometrics.dagger
 
+import android.content.Context
+
 import android.content.res.Resources
 import com.android.internal.R
 import com.android.systemui.CoreStartable
@@ -40,6 +42,8 @@ import com.android.systemui.biometrics.udfps.OverlapDetector
 import com.android.systemui.biometrics.ui.binder.SideFpsOverlayViewBinder
 import com.android.systemui.dagger.SysUISingleton
 import com.android.systemui.statusbar.policy.ConfigurationController.ConfigurationListener
+import com.android.systemui.biometrics.FingerprintInteractiveToAuthProvider
+import com.android.systemui.biometrics.FingerprintInteractiveToAuthProviderImpl
 import com.android.systemui.util.concurrency.ThreadFactory
 import dagger.Binds
 import dagger.Module
@@ -103,6 +107,10 @@ interface BiometricsModule {
             threadFactory.buildExecutorOnNewThread("biometrics")
 
         @Provides fun providesUdfpsUtils(): UdfpsUtils = UdfpsUtils()
+
+        @Provides
+        fun providesFingerprintInteractiveToAuth(ctx: Context): FingerprintInteractiveToAuthProvider =
+            FingerprintInteractiveToAuthProviderImpl(ctx);
 
         @Provides
         @SysUISingleton
