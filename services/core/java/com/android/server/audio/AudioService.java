3e8e16661903e3622c06c04017d4ce6c69ce0f31
minaripenguin <minaripenguin@users.noreply.github.com>
2023-09-07 22:33:55 +0800

    AudioService: Reduce redundant volume broadcast
    Issue 1:
    
    For devices with a high volume step count, the default step value of 10 is excessive.
    For instance, on devices with 25 volume steps, the default step of 10 is 2.5 times greater than what the system can smoothly handle.
    This causes delays in the UI slider due to large volume step updates. Also reduce the media volume count to 15 for faster volume ui updates
    
    Using the formula:
    
    Max Percent (100) / Stream Step Count
    
    We can make the ui slider updates more granular and precise.
    The only downside i can find is the step delays is not enough for single key presses which is
    currently mitigated by isLongPress() check since we dont have access to KeyEvents
    
    Issue 2:
    
    Volume updates causes UI delays when adjusting volume frequently or when using volume keys.
    
    Solution:
    
    Skip updating the whole volume group index when updating music stream
    Reference: https://android-review.googlesource.com/c/platform/frameworks/base/+/2647986
    
    * Fixes: lag/delays when adjusting volume using volume keys when using high media volume step count.
    * Fixes: 858102a54dbe05ee4b2274ced069233975a7d4ec
    
    test: manual, adjust volume keys on high cpu load (after boot and while watching videos)
    test 2: change volume step to 15/20, no delays on slider ui
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- services/core/java/com/android/server/audio/AudioService.java
+++ services/core/java/com/android/server/audio/AudioService.java
@@ -1329,10 +1329,7 @@ public class AudioService extends IAudioService.Stub
                     (MAX_STREAM_VOLUME[AudioSystem.STREAM_VOICE_CALL] * 3) / 4;
         }
 
-        int maxMusicVolume = SystemProperties.getInt("ro.config.media_vol_steps", -1);
-        if (maxMusicVolume != -1) {
-            MAX_STREAM_VOLUME[AudioSystem.STREAM_MUSIC] = maxMusicVolume;
-        }
+        MAX_STREAM_VOLUME[AudioSystem.STREAM_MUSIC] = 15;
 
         int defaultMusicVolume = SystemProperties.getInt("ro.config.media_vol_default", -1);
         if (defaultMusicVolume != -1 &&
@@ -3778,6 +3775,7 @@ public class AudioService extends IAudioService.Stub
         if (mUseFixedVolume) {
             return;
         }
+        mVolumeController.onVolumeKeyPressed();
         streamType = replaceBtScoStreamWithVoiceCall(streamType, "adjustStreamVolume");
 
         if (DEBUG_VOL) Log.d(TAG, "adjustStreamVolume() stream=" + streamType + ", dir=" + direction
@@ -3870,8 +3868,10 @@ public class AudioService extends IAudioService.Stub
             }
         } else {
             // convert one UI step (+/-1) into a number of internal units on the stream alias
-            step = rescaleStep((int) (10 * streamState.getIndexStepFactor()), streamType,
-                    streamTypeAlias);
+            int streamStep = mVolumeController.isLongPress() ? (100 / MAX_STREAM_VOLUME[streamTypeAlias]) : 10;
+            streamStep = streamStep > 10 ? 10 : streamStep;
+            if (DEBUG_VOL) Log.d("streamStep", "scale: [ streamStep=" + streamStep + " ]");
+            step = rescaleStep((int) (streamStep * streamState.getIndexStepFactor()), streamType, streamTypeAlias);
         }
 
         // If either the client forces allowing ringer modes for this adjustment,
@@ -5255,6 +5255,20 @@ public class AudioService extends IAudioService.Stub
             flags &= ~AudioManager.FLAG_SHOW_UI;
         }
         mVolumeController.postVolumeChanged(streamType, flags);
+        // skip updating the volume group index if stream type is music
+        if (mIsSingleVolume || streamType == AudioSystem.STREAM_MUSIC) {
+            if ((flags & AudioManager.FLAG_FIXED_VOLUME) == 0) {
+                oldIndex = (oldIndex + 5) / 10;
+                index = (index + 5) / 10;
+                if (index != oldIndex) {
+                    Intent intent = new Intent(AudioManager.VOLUME_CHANGED_ACTION);
+                    intent.putExtra(AudioManager.EXTRA_VOLUME_STREAM_TYPE, streamType);
+                    intent.putExtra(AudioManager.EXTRA_VOLUME_STREAM_VALUE, index);
+                    intent.putExtra(AudioManager.EXTRA_PREV_VOLUME_STREAM_VALUE, oldIndex);
+                    sendBroadcastToAll(intent, null);
+                }
+            }
+        }
     }
 
     // Don't show volume UI when:
@@ -9457,7 +9471,7 @@ public class AudioService extends IAudioService.Stub
                     }
                 }
             }
-            if (changed) {
+            if (changed && !mIsSingleVolume && mStreamType != AudioSystem.STREAM_MUSIC) {
                 // If associated to volume group, update group cache
                 updateVolumeGroupIndex(device, /* forceMuteState= */ false);
 
@@ -9478,7 +9492,7 @@ public class AudioService extends IAudioService.Stub
                     // on the alias stream
                     final int streamAlias = sStreamVolumeAlias.get(
                             mStreamType, /*valueIfKeyNotFound=*/-1);
-                    if (!mIsSingleVolume || streamAlias == mStreamType) {
+                    if (streamAlias == mStreamType) {
                         mVolumeChanged.putExtra(AudioManager.EXTRA_VOLUME_STREAM_VALUE, index);
                         mVolumeChanged.putExtra(AudioManager.EXTRA_PREV_VOLUME_STREAM_VALUE,
                                 oldIndex);
@@ -12744,11 +12758,14 @@ public class AudioService extends IAudioService.Stub
     /** Wrapper which encapsulates the {@link IVolumeController} functionality. */
     public class VolumeController implements ISafeHearingVolumeController {
         private static final String TAG = "VolumeController";
+        private static final long VOLUME_KEY_PRESS_INTERVAL = 80;
 
         private IVolumeController mController;
         private boolean mVisible;
         private long mNextLongPress;
+        private long mNextKeyLongPress;
         private int mLongPressTimeout;
+        private boolean mIsLongPress;
 
         public void setController(IVolumeController controller) {
             mController = controller;
@@ -12762,6 +12779,22 @@ public class AudioService extends IAudioService.Stub
         public void loadSettings(ContentResolver cr) {
             mLongPressTimeout = mSettings.getSecureIntForUser(cr,
                     Settings.Secure.LONG_PRESS_TIMEOUT, 500, UserHandle.USER_CURRENT);
+        }
+
+        public void onVolumeKeyPressed() {
+            long currentTime = SystemClock.uptimeMillis();
+            if (currentTime >= mNextKeyLongPress) {
+                this.mIsLongPress = false;
+                if (DEBUG_VOL) Log.d(TAG, "Short volume key press detected");
+            } else {
+                this.mIsLongPress = true;
+                if (DEBUG_VOL) Log.d(TAG, "Long volume key press detected");
+            }
+            mNextKeyLongPress = currentTime + VOLUME_KEY_PRESS_INTERVAL;
+        }
+
+        public boolean isLongPress() {
+            return mIsLongPress;
         }
 
         public boolean suppressAdjustment(int resolvedStream, int flags, boolean isMute) {
