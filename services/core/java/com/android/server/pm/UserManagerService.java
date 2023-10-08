b8ea0059962604cb809c9c54bf631ed0c31993b4
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-10-08 12:29:09 +0300

    restore Android 13 "pause work profile" behavior
    Stop the underlying user instead of merely suspending apps within it.
    
    Change-Id: I11dc3d30dc23ffff4b423d556d5c8c497b6e822c
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- services/core/java/com/android/server/pm/UserManagerService.java
+++ services/core/java/com/android/server/pm/UserManagerService.java
@@ -1826,6 +1826,14 @@ public class UserManagerService extends IUserManager.Stub {
                 LocalServices.getService(ActivityManagerInternal.class)
                         .killForegroundAppsForUser(userId);
             } else {
+            if (!enableQuietMode) {
+                try {
+                    getPackageManagerInternal().setPackagesSuspendedForQuietMode(userId, false);
+                    setAppOpsRestrictedForQuietMode(userId, false);
+                } catch (Exception e) {
+                    Slog.d("UserManager", "", e);
+                }
+            }
                 IProgressListener callback = target != null
                         ? new DisableQuietModeUserUnlockedCallback(target)
                         : null;
