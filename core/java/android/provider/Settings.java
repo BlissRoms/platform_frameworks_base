afda833ce53c0f5d4ae18e893f326fb75916c9ad
AnierinB <anierin@evolution-x.org>
2023-12-07 01:18:55 +0000

    PPU: Store the current array name in SettingsProvider
    So we can fetch the values currently set in PifResources Activity/PreferenceFragment.
    
    Change-Id: I890d655e543cdd7ed47a808602ccf764631fbcec
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6714,6 +6714,12 @@ public final class Settings {
         public static final String ENABLE_TASKBAR = "enable_taskbar";
 
         /**
+         * Stores the array name of the values currently used for SpoofBuildGms
+         * @hide
+         */
+        public static final String PPU_SPOOF_BUILD_GMS_ARRAY = "ppu_spoof_build_gms_array";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
