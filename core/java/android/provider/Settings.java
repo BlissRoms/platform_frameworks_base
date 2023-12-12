2bf5a30b259b0c4406fddcf70ba3f027871bf343
maxwen <max.weninger@gmail.com>
2023-12-11 21:06:40 -0600

    LockscreenCharging: squashed (1/3)
    needed system/core patch
    based on:
    [2/2] frameworks: show charging current & voltage on lockscreen by yank555-lu
    [2/2]Base: show more battery info on lockscreen when charging by yank555-lu
    Lockscreen charging: Formatting improvements by nathanchance
    
    @neobuddy89: Convert to variables to float where we expect decimal values.
    
    Change-Id: I665e36a147576292097544c73871b9fcf73469f2

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6583,6 +6583,12 @@ public final class Settings {
         public static final String ALERT_SLIDER_NOTIFICATIONS = "alert_slider_notifications";
 
         /**
+         * Whether to show the battery info on the lockscreen while charging
+         * @hide
+         */
+        public static final String LOCKSCREEN_BATTERY_INFO = "lockscreen_battery_info";
+
+        /**
          * These are all public system settings
          *
          * @hide
