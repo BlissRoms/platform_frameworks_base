758ae4dc753d76fc4e31057e9f543d9f169ded56
Pranav Vashi <neobuddy89@gmail.com>
2023-12-25 01:49:22 +0530

    PixelPropsUtils: Update Implementation (Again)
    Signed-off-by: sreelekshman <sreelekshmanchunakara@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- core/java/android/app/ApplicationPackageManager.java
+++ core/java/android/app/ApplicationPackageManager.java
@@ -820,6 +820,8 @@ public class ApplicationPackageManager extends PackageManager {
             };
 
     private static final String[] pTensorCodenames = {
+            "husky",
+            "shiba",
             "felix",
             "tangorpro",
             "lynx",
