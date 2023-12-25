2bf221e3f0f4db80de09eaea40ea869eb4aaad57
Dmitrii <bankersenator@gmail.com>
2023-12-25 13:51:12 +0000

    fwb: disable island feature by default
    - Match SpaceStation default state
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -4682,7 +4682,7 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
                     break;
                 case ISLAND_NOTIFICATION:
                     mUseIslandNotification =
-                            TunerService.parseIntegerSwitch(newValue, true);
+                            TunerService.parseIntegerSwitch(newValue, false);
                     mNotifIsland.setIslandEnabled(mUseIslandNotification);
                     break;
                 default:
