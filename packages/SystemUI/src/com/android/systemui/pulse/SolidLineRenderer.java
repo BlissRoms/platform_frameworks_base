cf22cab56b76203dba1258ceed4439d03bc8069f
Jenna-they-them <jenna@jenslody.de>
2024-02-29 03:20:43 +0100

    base: Pulse: fix changes not applied correctly in SolidLineRenderer
    Signed-off-by: Jenna-they-them <jenna@jenslody.de>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- packages/SystemUI/src/com/android/systemui/pulse/SolidLineRenderer.java
+++ packages/SystemUI/src/com/android/systemui/pulse/SolidLineRenderer.java
@@ -286,6 +286,8 @@ public class SolidLineRenderer extends Renderer {
                     UserHandle.USER_CURRENT);
 
             mPaint.setColor(ColorUtils.setAlphaComponent(mColor, mUnitsOpacity));
+
+            onSizeChanged(0, 0, 0, 0);
         }
     }
 
