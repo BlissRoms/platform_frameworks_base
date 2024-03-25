c0c45a76c0b855bdca07e725395ec55fae230391
Jis G Jacob <studiokeys@blissroms.org>
2024-03-24 21:27:54 -0400

    Revert "SystemUI: Hide notifications when quickly expanding QS"
    This reverts commit cabf425f58a7a9e966de7b71577fe5ca5f009229.

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -2736,9 +2736,6 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
             }
             mNotificationStackScrollLayoutController.setMaxAlphaForExpansion(alpha);
         }
-        if (mQsController.isExpandImmediate() && !mQsController.getFullyExpanded()) {
-            alpha = 0f;
-        }
     }
 
     private float getFadeoutAlpha() {
