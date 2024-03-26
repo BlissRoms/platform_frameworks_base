007fe468a320f90bb95961f2d00bc2c54fc6692d
LuK1337 <priv.luk@gmail.com>
2024-03-27 00:51:34 +0100

    SystemUI: Restore 14 QPR1 FingerprintInteractiveToAuth handling
    NOTE: This is modified to allow interactive to auth for all non-udfps
    sensors.
    
    Change-Id: I169435e755c68433955f5f6185c3fa74400fca64
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -2890,9 +2890,14 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
                 && !strongerAuthRequired
                 && userDoesNotHaveTrust);
 
+        final boolean shouldListenFpsState = !isUdfps
+                && (mFingerprintInteractiveToAuthProvider == null
+                        || !mFingerprintInteractiveToAuthProvider.isEnabled(user)
+                        || (isDeviceInteractive() && !mGoingToSleep));
+
         boolean shouldListen = shouldListenKeyguardState && shouldListenUserState
                 && shouldListenBouncerState && shouldListenUdfpsState && !mBiometricPromptShowing
-                && !mIsDeviceInPocket;
+                && shouldListenFpsState && !mIsDeviceInPocket;
         logListenerModelData(
                 new KeyguardFingerprintListenModel(
                     System.currentTimeMillis(),
