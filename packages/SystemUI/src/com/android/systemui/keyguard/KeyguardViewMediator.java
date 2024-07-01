81bd0208d8f50065a0bb3906db7de41e577db1b6
Josh Tsuji <tsuji@google.com>
2024-07-01 00:08:48 -0700

    Improve handling of launching translucent occluding activities.
    If the window is translucent we don't want to draw a solid color starting view. We also don't want to translate/scale it, since it's transparent.
    
    Bug: 303010980
    Test: launch assistant
    Flag: com.android.systemui.translucent_occluding_activity_fix
    (cherry picked from commit 22bbcfb1dabb95d2a1ec25bec59653c9946d27aa)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:9c7f00cec0fa7ed9326544cfba2377d9a1162268)
    Merged-In: I11e75fe0215ab812d32ccc8aa2758b6fe58d86c3
    Change-Id: I11e75fe0215ab812d32ccc8aa2758b6fe58d86c3
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -41,6 +41,7 @@ import static com.android.internal.widget.LockPatternUtils.StrongAuthTracker.STR
 import static com.android.systemui.DejankUtils.whitelistIpcs;
 import static com.android.systemui.Flags.notifyPowerManagerUserActivityBackground;
 import static com.android.systemui.Flags.refactorGetCurrentUser;
+import static com.android.systemui.Flags.translucentOccludingActivityFix;
 import static com.android.systemui.keyguard.ui.viewmodel.LockscreenToDreamingTransitionViewModel.DREAMING_ANIMATION_DURATION_MS;
 import static com.android.systemui.util.kotlin.JavaAdapterKt.collectFlow;
 
@@ -1030,6 +1031,17 @@ public class KeyguardViewMediator implements CoreStartable, Dumpable,
                                 (int) (fullWidth - initialWidth) /* left */,
                                 fullWidth /* right */,
                                 mWindowCornerRadius, mWindowCornerRadius);
+                    } else if (translucentOccludingActivityFix()
+                            && mOccludingRemoteAnimationTarget != null
+                            && mOccludingRemoteAnimationTarget.isTranslucent) {
+                        // Animating in a transparent window looks really weird. Just let it be
+                        // fullscreen and the app can do an internal animation if it wants to.
+                        return new TransitionAnimator.State(
+                                0,
+                                fullHeight,
+                                0,
+                                fullWidth,
+                                0f, 0f);
                     } else {
                         final float initialHeight = fullHeight / 2f;
                         final float initialWidth = fullWidth / 2f;
@@ -1363,6 +1375,11 @@ public class KeyguardViewMediator implements CoreStartable, Dumpable,
     private final Lazy<DreamingToLockscreenTransitionViewModel>
             mDreamingToLockscreenTransitionViewModel;
     private RemoteAnimationTarget mRemoteAnimationTarget;
+
+    /**
+     * The most recent RemoteAnimationTarget provided for an occluding activity animation.
+     */
+    private RemoteAnimationTarget mOccludingRemoteAnimationTarget;
 
     private final Lazy<WindowManagerLockscreenVisibilityManager> mWmLockscreenVisibilityManager;
 
@@ -3870,6 +3887,13 @@ public class KeyguardViewMediator implements CoreStartable, Dumpable,
         public void onAnimationStart(int transit, RemoteAnimationTarget[] apps,
                 RemoteAnimationTarget[] wallpapers, RemoteAnimationTarget[] nonApps,
                 IRemoteAnimationFinishedCallback finishedCallback) throws RemoteException {
+            // Save mRemoteAnimationTarget for reference in the animation controller. Needs to be
+            // called prior to super.onAnimationStart() since that's the call that eventually asks
+            // the animation controller to configure the animation state.
+            if (apps.length > 0) {
+                mOccludingRemoteAnimationTarget = apps[0];
+            }
+
             super.onAnimationStart(transit, apps, wallpapers, nonApps, finishedCallback);
 
             mInteractionJankMonitor.begin(
