4cdd60cec413d7617cad741a5a14ece4e45b9140
srgrusso <srgrusso240@gmail.com>
2024-06-30 19:54:13 -0500

    Revert "SystemUI: Update udfps animation position on sensor props change"
    Change-Id: I6ca9fe396d1f08a4e18cca8b7fdbb27cd8df0dce
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -426,10 +426,6 @@ public class UdfpsController implements DozeReceiver, Dumpable {
         if (mSensorProps.sensorId != sensorProps.sensorId) {
             mSensorProps = sensorProps;
             Log.w(TAG, "updateUdfpsParams | sensorId has changed");
-            // Update animation position on sensor props change.
-            if (mUdfpsAnimation != null) {
-                mUdfpsAnimation.updatePosition(sensorProps);
-            }
         }
 
         if (!mOverlayParams.equals(overlayParams)) {
@@ -757,13 +753,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
         mUnlockedScreenOffAnimationController = unlockedScreenOffAnimationController;
         mLatencyTracker = latencyTracker;
         mActivityTransitionAnimator = activityTransitionAnimator;
-        mSensorProps = new FingerprintSensorPropertiesInternal(
-                -1 /* sensorId */,
-                SensorProperties.STRENGTH_CONVENIENCE,
-                0 /* maxEnrollmentsPerUser */,
-                new ArrayList<>() /* componentInfo */,
-                FingerprintSensorProperties.TYPE_UNKNOWN,
-                false /* resetLockoutRequiresHardwareAuthToken */);
+        mSensorProps = findFirstUdfps();
 
         mBiometricExecutor = biometricsExecutor;
         mPrimaryBouncerInteractor = primaryBouncerInteractor;
@@ -859,6 +849,17 @@ public class UdfpsController implements DozeReceiver, Dumpable {
                         + "vibration. Either the controller overlay is null or has no view");
             }
         }
+    }
+
+    @Nullable
+    private FingerprintSensorPropertiesInternal findFirstUdfps() {
+        for (FingerprintSensorPropertiesInternal props :
+                mFingerprintManager.getSensorPropertiesInternal()) {
+            if (props.isAnyUdfpsType()) {
+                return props;
+            }
+        }
+        return null;
     }
 
     @Override
