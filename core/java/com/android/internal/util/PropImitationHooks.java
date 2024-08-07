58c57add115d4499a325e49c3f45dc0844c8b6c9
Jyotiraditya Panda <jyotiraditya@aospa.co>
2024-08-07 21:35:48 +0530

    PropImitationHooks: Allow user to disable prop imitation
     - Prevents spoofing props, bypassing task perms and blocking key attestation for GMS.
    
    [adithya2306: add a prop for disabling only key attestation too]
    
    Change-Id: I529e91e8d11a27d064c3ef85a8cf5e73e1857e3b
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- core/java/com/android/internal/util/PropImitationHooks.java
+++ core/java/com/android/internal/util/PropImitationHooks.java
@@ -27,6 +27,7 @@ import android.content.res.Resources;
 import android.os.Build;
 import android.os.Binder;
 import android.os.Process;
+import android.os.SystemProperties;
 import android.text.TextUtils;
 import android.util.Log;
 
@@ -38,7 +39,13 @@ import java.util.Arrays;
 public class PropImitationHooks {
 
     private static final String TAG = "PropImitationHooks";
-    private static final boolean DEBUG = false;
+    private static final boolean DEBUG = Log.isLoggable(TAG, Log.DEBUG);
+
+    private static final Boolean sDisableGmsProps = SystemProperties.getBoolean(
+            "persist.sys.pihooks.disable.gms_props", false);
+
+    private static final Boolean sDisableKeyAttestationBlock = SystemProperties.getBoolean(
+            "persist.sys.pihooks.disable.gms_key_attestation_block", false);
 
     private static final String PACKAGE_ARCORE = "com.google.ar.core";
     private static final String PACKAGE_FINSKY = "com.android.vending";
@@ -112,10 +119,16 @@ public class PropImitationHooks {
     }
 
     private static void setCertifiedPropsForGms() {
+        if (sDisableGmsProps) {
+            dlog("GMS prop imitation is disabled by user");
+            return;
+        }
+
         if (sCertifiedProps.length == 0) {
             dlog("Certified props are not set");
             return;
         }
+
         final boolean was = isGmsAddAccountActivityOnTop();
         final TaskStackListener taskStackListener = new TaskStackListener() {
             @Override
@@ -166,6 +179,10 @@ public class PropImitationHooks {
     }
 
     public static boolean shouldBypassTaskPermission(Context context) {
+        if (sDisableGmsProps) {
+            return false;
+        }
+
         // GMS doesn't have MANAGE_ACTIVITY_TASKS permission
         final int callingUid = Binder.getCallingUid();
         final int gmsUid;
@@ -185,6 +202,11 @@ public class PropImitationHooks {
     }
 
     public static void onEngineGetCertificateChain() {
+        if (sDisableKeyAttestationBlock) {
+            dlog("Key attestation blocking is disabled by user");
+            return;
+        }
+
         // Check stack for SafetyNet or Play Integrity
         if (isCallerSafetyNet() || sIsFinsky) {
             dlog("Blocked key attestation sIsGms=" + sIsGms + " sIsFinsky=" + sIsFinsky);
