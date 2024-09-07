569f7ac88d601df56bbc6c57d67779ee54332bd2
cjh1249131356 <cjh1249131356@gmail.com>
2024-09-07 12:38:31 +0800

    base: Inject WindowManagerServiceExt
    Change-Id: Ie22313c31b068069a9b19f8931974592f7d76012
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -898,6 +898,7 @@ public class WindowManagerService extends IWindowManager.Stub
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(mDevelopmentOverrideDesktopExperienceUri, false, this,
                     UserHandle.USER_ALL);
+            WindowManagerServiceExt.getInstance().registerContentObserver(this);
         }
 
         @Override
@@ -950,6 +951,10 @@ public class WindowManagerService extends IWindowManager.Stub
                 return;
             }
 
+            if (WindowManagerServiceExt.getInstance().onSettingsChanged(uri)) {
+                return;
+            }
+
             @UpdateAnimationScaleMode
             final int mode;
             if (mWindowAnimationScaleUri.equals(uri)) {
@@ -970,6 +975,7 @@ public class WindowManagerService extends IWindowManager.Stub
             updateMaximumObscuringOpacityForTouch();
             updateDisableSecureWindows();
             updateMagnifyIme();
+            WindowManagerServiceExt.getInstance().loadSettings();
         }
 
         void updateMaximumObscuringOpacityForTouch() {
@@ -1524,6 +1530,8 @@ public class WindowManagerService extends IWindowManager.Stub
         mContext.registerReceiverAsUser(mBroadcastReceiver, UserHandle.ALL, filter, null, null);
 
         mLatencyTracker = LatencyTracker.getInstance(context);
+
+        WindowManagerServiceExt.getInstance().init(this);
 
         mSettingsObserver = new SettingsObserver();
 
@@ -3678,6 +3686,7 @@ public class WindowManagerService extends IWindowManager.Stub
 
     @Override
     public void onUserSwitched() {
+        WindowManagerServiceExt.getInstance().onUserSwitched();
         synchronized (mGlobalLock) {
             // force a re-application of focused window sysui visibility on each display.
             mRoot.forAllDisplayPolicies(DisplayPolicy::resetSystemBarAttributes);
@@ -6117,6 +6126,8 @@ public class WindowManagerService extends IWindowManager.Stub
         if (mAppLockController != null) {
             mAppLockController.systemReady();
         }
+
+        WindowManagerServiceExt.getInstance().systemReady();
     }
 
 
