a84b6e207a2081037d35bdac006f14e41de754dc
Aston-Martinn <advaithbhat9@gmail.com>
2024-09-12 19:10:20 +0530

    base: [Utils] Use a divider overlay for CPU temperature
    Change-Id: If22c11e2f0a06ef05a17a064df72f7401646c8f0
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- core/java/com/android/internal/util/bliss/BlissUtils.java
+++ core/java/com/android/internal/util/bliss/BlissUtils.java
@@ -119,7 +119,9 @@ public class BlissUtils {
         } else {
             value = "Error";
         }
-        return value == "Error" ? "N/A" : String.format("%s", Integer.parseInt(value) / 1000) + "°C";
+        int cpuTempMultiplier = context.getResources().getInteger(
+                com.android.internal.R.integer.config_sysCPUTempMultiplier);
+        return value == "Error" ? "N/A" : String.format("%s", Integer.parseInt(value) / cpuTempMultiplier) + "°C";
     }
 
     public static boolean fileExists(String filename) {
