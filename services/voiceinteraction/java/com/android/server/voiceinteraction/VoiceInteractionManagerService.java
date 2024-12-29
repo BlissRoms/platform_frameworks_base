ac9387beef534664efe687f55586fe2d85ad163a
Pranav Vashi <neobuddy89@gmail.com>
2024-12-29 23:17:02 +0530

    VoiceInteraction: Check if default package is installed
    Change-Id: Id7891d373cd70e15663e80548e6b2750f09f6924
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- services/voiceinteraction/java/com/android/server/voiceinteraction/VoiceInteractionManagerService.java
+++ services/voiceinteraction/java/com/android/server/voiceinteraction/VoiceInteractionManagerService.java
@@ -125,6 +125,7 @@ import com.android.internal.compat.IPlatformCompat;
 import com.android.internal.content.PackageMonitor;
 import com.android.internal.os.BackgroundThread;
 import com.android.internal.util.DumpUtils;
+import com.android.internal.util.bliss.BlissUtils;
 import com.android.server.FgThread;
 import com.android.server.LocalServices;
 import com.android.server.SoundTriggerInternal;
@@ -784,7 +785,7 @@ public class VoiceInteractionManagerService extends SystemService {
             // If forceInteractorPackage exists, try to apply the interactor from this package if
             // possible and ignore the regular interactor setting.
             String forceInteractorPackage =
-                    getForceVoiceInteractionServicePackage(mContext.getResources());
+                    getForceVoiceInteractionServicePackage(mContext);
             if (forceInteractorPackage != null) {
                 curInteractorInfo = findAvailInteractor(userHandle, forceInteractorPackage);
                 if (curInteractorInfo != null) {
@@ -882,17 +883,18 @@ public class VoiceInteractionManagerService extends SystemService {
             // recognition feature (including low-ram devices where notLowRam="true" takes effect),
             // unless the device's configuration has explicitly set the config flag for a fixed
             // voice interaction service.
-            if (getForceVoiceInteractionServicePackage(context.getResources()) != null) {
+            if (getForceVoiceInteractionServicePackage(context) != null) {
                 return true;
             }
             return context.getPackageManager()
                     .hasSystemFeature(PackageManager.FEATURE_VOICE_RECOGNIZERS);
         }
 
-        private String getForceVoiceInteractionServicePackage(Resources res) {
-            String interactorPackage = res.getString(
+        private String getForceVoiceInteractionServicePackage(Context context) {
+            String interactorPackage = context.getResources().getString(
                     com.android.internal.R.string.config_forceVoiceInteractionServicePackage);
-            return TextUtils.isEmpty(interactorPackage) ? null : interactorPackage;
+            return TextUtils.isEmpty(interactorPackage)
+                || !BlissUtils.isPackageInstalled(context, interactorPackage) ? null : interactorPackage;
         }
 
         public void activityManagerReady() {
@@ -1483,7 +1485,7 @@ public class VoiceInteractionManagerService extends SystemService {
                                 .getString(R.string.config_defaultContextualSearchPackageName);
                         ComponentName currInteractor =
                                 getCurInteractor(Binder.getCallingUserHandle().getIdentifier());
-                        if (currInteractor == null
+                        if (currInteractor == null || !BlissUtils.isPackageInstalled(mContext, csPkgName)
                                 || !csPkgName.equals(currInteractor.getPackageName())) {
                             // Check if the interactor can handle Contextual Search.
                             // If not, return failure.
@@ -3415,7 +3417,7 @@ public class VoiceInteractionManagerService extends SystemService {
         private Intent getContextualSearchIntent(Bundle args) {
             String csPkgName = mContext.getResources()
                     .getString(R.string.config_defaultContextualSearchPackageName);
-            if (csPkgName.isEmpty()) {
+            if (csPkgName.isEmpty() || !BlissUtils.isPackageInstalled(mContext, csPkgName)) {
                 // Return null if csPackageName is not specified.
                 return null;
             }
