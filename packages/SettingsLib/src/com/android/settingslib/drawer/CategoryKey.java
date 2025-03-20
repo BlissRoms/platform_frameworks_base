0683d1a18f4f11550e6a9816fafe113948a929b8
Pranav Vashi <neobuddy89@gmail.com>
2025-03-20 05:36:34 -0400

    Bliss: Adapt Blissify for Android 15 scheming
    Change-Id: I9bf452630120671c6fcb0f01b8d88017810c295d
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- packages/SettingsLib/src/com/android/settingslib/drawer/CategoryKey.java
+++ packages/SettingsLib/src/com/android/settingslib/drawer/CategoryKey.java
@@ -78,6 +78,8 @@ public final class CategoryKey {
             "com.android.settings.category.ia.media_controls_settings";
     public static final String CATEGORY_PRIVACY_CONTROLS =
             "com.android.settings.category.ia.privacy_controls";
+    public static final String CATEGORY_BLISSIFY =
+            "com.android.settings.category.ia.blissify";
 
     public static final Map<String, String> KEY_COMPAT_MAP;
 
