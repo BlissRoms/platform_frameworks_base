7cf982e6dfe907009be48da8e6d2607e403b7beb
Abdelrahman Daim <adaim@meta.com>
2025-04-10 06:07:19 -0700

    Remove ServiceRecord from restart list if restart is cancelled as unnecessary
    Summary: This check cancels a Service restart without removing the ServiceRecords from the
    restarting list, so that even after the persistent auto-restart fails, later calls to
    startService for these components will always silently fail (since ActivityManager
    thinks they're in the middle of a restart).
    
    Remove them from the restarting list so that later attempts to manually start the
    services actually succeed.
    
    Test: Flash a build containing this commit with "-w" to trigger the race condition that
    causes this code path to be triggered
    
    Verify that even though this wtf prints, later attempts to restart the service
    succeed.
    
    Google: 3580777
    Change-Id: I12bb1e44f2a025230f2569827cacba6be3a5a60a
    Signed-off-by: Abdelrahman Daim <adaim@meta.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- services/core/java/com/android/server/am/ActiveServices.java
+++ services/core/java/com/android/server/am/ActiveServices.java
@@ -5656,6 +5656,7 @@ public final class ActiveServices {
             // ends up just being cached, so quickly killed, then restarted again and again.
             // Let's not let that happen.
             Slog.wtf(TAG, "Restarting service that is not needed: " + r);
+            mRestartingServices.remove(r);
             return;
         }
         try {
