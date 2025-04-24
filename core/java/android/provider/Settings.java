64c971695aec452650c57a35c8c2760829c64234
Pranav Vashi <neobuddy89@gmail.com>
2025-04-25 00:37:55 +0530

    services: Clean up unused settings for three finger swipe
    Change-Id: I94f1e06ed31c6aeafa6893e2a525de06552cf7dd
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7274,14 +7274,6 @@ public final class Settings {
         public static final String QS_SHOW_DATA_USAGE_WINDOW = "qs_show_data_usage_window";
 
         /**
-         * Whether three fingers swipe is active
-         * 0 = Inactive, 1 = Active
-         * @hide
-         */
-        @Readable
-        public static final String THREE_FINGER_GESTURE_ACTIVE = "three_fingers_swipe_active";
-
-        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
