d44eb8f7d4b2e7636290742381b76cfc19e88c5d
Julia Reynolds <juliacr@google.com>
2025-05-07 12:47:47 -0400

    Update isSameApp check
    Only check uid, without a sandbox sdk exception
    
    This small change involves a major test change since we previously
    too permissive in our test setup
    
    Test: NotificationManagerServiceTest
    Test: HSUM NotificationManagerServiceTest
    Test: SdkSandboxRestrictionsTest
    Bug: 396667508
    Fixes: 317957802
    Flag: EXEMPT bug fix
    
    (cherry picked from commit f5605ae25b15e1f8295e86c431d304e333ce56a4)
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:9931811bc94d5db0d6c17b3e2d18cca2959506a4
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:3d72900800a1bf9667336e40f3f6bdf24f09f295
    Merged-In: I2be1537803a83cf44cc605f2850a3bb705b9bf32
    Change-Id: I2be1537803a83cf44cc605f2850a3bb705b9bf32

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -4042,6 +4042,9 @@ public class NotificationManagerService extends SystemService {
          */
         @Override
         public boolean areNotificationsEnabledForPackage(String pkg, int uid) {
+            if (Process.isSdkSandboxUid(uid)) {
+                return false;
+            }
             enforceSystemOrSystemUIOrSamePackage(pkg,
                     "Caller not system or systemui or same package");
             if (UserHandle.getCallingUserId() != UserHandle.getUserId(uid)) {
@@ -4570,8 +4573,8 @@ public class NotificationManagerService extends SystemService {
         public NotificationChannel getConversationNotificationChannel(String callingPkg, int userId,
                 String targetPkg, String channelId, boolean returnParentIfNoConversationChannel,
                 String conversationId) {
-            if (canNotifyAsPackage(callingPkg, targetPkg, userId)
-                    || isCallerSystemOrSystemUiOrShell()) {
+            if (isCallerSystemOrSystemUiOrShell()
+                    || canNotifyAsPackage(callingPkg, targetPkg, userId)) {
                 int targetUid = INVALID_UID;
                 try {
                     targetUid = mPackageManagerClient.getPackageUidAsUser(targetPkg, userId);
@@ -5957,7 +5960,7 @@ public class NotificationManagerService extends SystemService {
                     String wellbeingPackage = getContext().getResources().getString(
                             com.android.internal.R.string.config_systemWellbeing);
                     boolean isCallerWellbeing = !TextUtils.isEmpty(wellbeingPackage)
-                            && mPackageManagerInternal.isSameApp(wellbeingPackage, uid, userId);
+                            && isCallerSameApp(wellbeingPackage, uid, userId);
                     if (!isCallerWellbeing) {
                         throw new IllegalArgumentException(
                                 "Only the 'Wellbeing' package can use AutomaticZenRules with "
@@ -6244,7 +6247,7 @@ public class NotificationManagerService extends SystemService {
                     ? mEffectsSuppressors.get(0)
                     : null;
             if (isCallerSystemOrSystemUiOrShell() || suppressor == null
-                    || mPackageManagerInternal.isSameApp(suppressor.getPackageName(),
+                    || isCallerSameApp(suppressor.getPackageName(),
                     Binder.getCallingUid(), UserHandle.getUserId(Binder.getCallingUid()))) {
                 return suppressor;
             }
@@ -8654,7 +8657,7 @@ public class NotificationManagerService extends SystemService {
     int resolveNotificationUid(String callingPkg, String targetPkg, int callingUid, int userId)
             throws NameNotFoundException {
         if (userId == USER_ALL) {
-            userId = USER_SYSTEM;
+            userId = UserHandle.getUserId(callingUid);
         }
         // posted from app A on behalf of app A
         if (isCallerSameApp(targetPkg, callingUid, userId)
@@ -11496,7 +11499,7 @@ public class NotificationManagerService extends SystemService {
         if (uid == Process.ROOT_UID && ROOT_PKG.equals(pkg)) {
             return;
         }
-        if (!mPackageManagerInternal.isSameApp(pkg, uid, userId)) {
+        if (!UserHandle.isSameApp(uid, mPackageManagerInternal.getPackageUid(pkg, 0L, userId))) {
             throw new SecurityException("Package " + pkg + " is not owned by uid " + uid);
         }
     }
