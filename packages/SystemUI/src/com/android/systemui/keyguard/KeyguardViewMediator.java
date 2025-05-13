1eea75465ee5b599ad52246c66e386ed9d600658
LuK1337 <priv.luk@gmail.com>
2025-05-13 16:47:43 +0200

    Revert "SystemUI: Dismiss keyguard on boot if disabled by current profile"
    This reverts commit 3c18b645cd0f97bc2f5d1bf42468ee743c4bf114.
    
    Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/8793
    Change-Id: Ib3206a33d43eeb768fda4cee73ad4934effa7dbe
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -2417,8 +2417,6 @@ public class KeyguardViewMediator implements CoreStartable, Dumpable,
         if (mLockPatternUtils.isLockScreenDisabled(mSelectedUserInteractor.getSelectedUserId())
                 && !lockedOrMissing && !forceShow) {
             if (DEBUG) Log.d(TAG, "doKeyguard: not showing because lockscreen is off");
-            setShowingLocked(false, mAodShowing, "doKeyguardLocked");
-            hideLocked();
             return;
         }
 
