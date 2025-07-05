6d667698f7d3b66d8d41e3218c4efb78f7f652d5
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-07-05 10:17:41 +0800

    SystemUI: InternetTileNewImpl: Fix secondary click
    Change-Id: Ib9420f5fb7dd0f249c6703a60b151b78fd6a5c24
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/InternetTileNewImpl.kt
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/InternetTileNewImpl.kt
@@ -37,6 +37,7 @@ import com.android.systemui.qs.logging.QSLogger
 import com.android.systemui.qs.tileimpl.QSTileImpl
 import com.android.systemui.qs.tiles.dialog.InternetDetailsViewModel
 import com.android.systemui.qs.tiles.dialog.InternetDialogManager
+import com.android.systemui.qs.tiles.dialog.WifiStateWorker
 import com.android.systemui.res.R
 import com.android.systemui.statusbar.connectivity.AccessPointController
 import com.android.systemui.statusbar.pipeline.shared.ui.binder.InternetTileBinder
@@ -60,6 +61,7 @@ constructor(
     private val internetDialogManager: InternetDialogManager,
     private val accessPointController: AccessPointController,
     private val internetDetailsViewModelFactory: InternetDetailsViewModel.Factory,
+    private val wifiStateWorker: WifiStateWorker,
 ) :
     QSTileImpl<QSTile.BooleanState>(
         host,
@@ -89,7 +91,10 @@ constructor(
         mContext.getString(R.string.quick_settings_internet_label)
 
     override fun newTileState(): QSTile.BooleanState {
-        return QSTile.BooleanState().also { it.forceExpandIcon = true }
+        return QSTile.BooleanState().also {
+             it.forceExpandIcon = true
+             it.handlesSecondaryClick = true
+        }
     }
 
     override fun handleClick(expandable: Expandable?) {
@@ -101,6 +106,10 @@ constructor(
                 expandable,
             )
         }
+    }
+
+    override fun handleSecondaryClick(expandable: Expandable?) {
+        wifiStateWorker.setWifiEnabled(!wifiStateWorker.isWifiEnabled())
     }
 
     override fun getDetailsViewModel(): TileDetailsViewModel {
