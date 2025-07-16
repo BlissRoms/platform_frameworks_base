9370559acc23c8e4b2fff8110312c4032435b84d
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-07-16 21:56:08 +0800

    [BUGFIX] fix settings material expressive issues
    Change-Id: Ib13d0be99487ba8acf6ec39fa1a9e22f3cc497f0
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- packages/SettingsLib/SettingsTheme/src/com/android/settingslib/widget/SettingsBasePreferenceFragment.kt
+++ packages/SettingsLib/SettingsTheme/src/com/android/settingslib/widget/SettingsBasePreferenceFragment.kt
@@ -28,6 +28,16 @@ import androidx.recyclerview.widget.RecyclerView
 /** Base class for Settings to use PreferenceFragmentCompat */
 abstract class SettingsBasePreferenceFragment : PreferenceFragmentCompat() {
 
+    companion object {
+        private val excludedFromTheming = setOf(
+            "com.android.settings.applications.RunningServices"
+        )
+
+        fun shouldSkipTheming(fragment: PreferenceFragmentCompat): Boolean {
+            return excludedFromTheming.contains(fragment::class.qualifiedName)
+        }
+    }
+
     @CallSuper
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -44,9 +54,15 @@ abstract class SettingsBasePreferenceFragment : PreferenceFragmentCompat() {
             // Don't allow any divider in between the preferences in expressive design.
             setDivider(null)
         }
+        
+        if (shouldSkipTheming(this)) return
+
     }
 
     override fun onCreateAdapter(preferenceScreen: PreferenceScreen): RecyclerView.Adapter<*> {
+        if (shouldSkipTheming(this)) {
+            return super.onCreateAdapter(preferenceScreen)
+        }
         if (SettingsThemeHelper.isExpressiveTheme(requireContext()))
             return SettingsPreferenceGroupAdapter(preferenceScreen)
         return super.onCreateAdapter(preferenceScreen)
