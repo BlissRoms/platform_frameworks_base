crdroidandroid/android_frameworks_base

base: Add the ability to generate key pairs for keybox spoofing
The code is used from :- https://github.com/5ec1cff/TrickyStore

Needs :-
HELLBOY017/external_bouncycastle@f70ea01

This allows us to enjoy spoofing keyboxes the right way even with broken TEE.

Change-Id: Id196dd58c21ca303966187466af0f82b875968a8
Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>
Signed-off-by: Dmitrii <bankersenator@gmail.com>
Signed-off-by: Frost <frost.github@proton.me>
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

core/java/com/android/internal/util/crdroid/IKeyboxProvider.java
@@ -2,7 +2,7 @@
  * SPDX-FileCopyrightText: 2024 Paranoid Android
  * SPDX-License-Identifier: Apache-2.0
  */
-package com.android.internal.util;
+package com.android.internal.util.crdroid;
 
 /**
  * Interface for keybox providers.

core/java/com/android/internal/util/crdroid/KeyProviderManager.java
@@ -2,7 +2,7 @@
  * SPDX-FileCopyrightText: 2024 Paranoid Android
  * SPDX-License-Identifier: Apache-2.0
  */
-package com.android.internal.util;
+package com.android.internal.util.crdroid;
 
 import android.app.ActivityThread;
 import android.content.Context;

core/java/com/android/internal/util/crdroid/KeyboxUtils.java
@@ -1,0 +1,133 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Neoteric OS
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package com.android.internal.util.crdroid;
+
+import android.security.keystore.KeyProperties;
+import android.system.keystore2.KeyEntryResponse;
+import android.system.keystore2.KeyMetadata;
+
+import com.android.internal.org.bouncycastle.asn1.ASN1Sequence;
+import com.android.internal.org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
+import com.android.internal.org.bouncycastle.asn1.sec.ECPrivateKey;
+import com.android.internal.org.bouncycastle.asn1.x9.X9ObjectIdentifiers;
+import com.android.internal.org.bouncycastle.asn1.x509.AlgorithmIdentifier;
+import com.android.internal.org.bouncycastle.cert.X509CertificateHolder;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @hide
+ */
+public class KeyboxUtils {
+
+    private static final Map<Key, KeyEntryResponse> response = new HashMap<>();
+    public static record Key(int uid, String alias) {}
+
+    public static byte[] decodePemOrBase64(String input) {
+        String base64 = input
+                .replaceAll("-----BEGIN [^-]+-----", "")
+                .replaceAll("-----END [^-]+-----", "")
+                .replaceAll("\\s+", "");
+        return Base64.getDecoder().decode(base64);
+    }
+
+    public static PrivateKey parsePrivateKey(String encodedKey, String algorithm) throws Exception {
+        byte[] keyBytes = decodePemOrBase64(encodedKey);
+        if ("EC".equalsIgnoreCase(algorithm)) {
+            ASN1Sequence seq = ASN1Sequence.getInstance(keyBytes);
+            ECPrivateKey ecPrivateKey = ECPrivateKey.getInstance(seq);
+            AlgorithmIdentifier algId = new AlgorithmIdentifier(X9ObjectIdentifiers.id_ecPublicKey, ecPrivateKey.getParameters());
+            PrivateKeyInfo privInfo = new PrivateKeyInfo(algId, ecPrivateKey);
+            PKCS8EncodedKeySpec pkcs8Spec = new PKCS8EncodedKeySpec(privInfo.getEncoded());
+            return KeyFactory.getInstance("EC").generatePrivate(pkcs8Spec);
+        } else if ("RSA".equalsIgnoreCase(algorithm)) {
+            PKCS8EncodedKeySpec pkcs8Spec = new PKCS8EncodedKeySpec(keyBytes);
+            return KeyFactory.getInstance("RSA").generatePrivate(pkcs8Spec);
+        } else {
+            throw new IllegalArgumentException("Unsupported algorithm: " + algorithm);
+        }
+    }
+
+    public static X509Certificate parseCertificate(String encodedCert) throws Exception {
+        byte[] certBytes = decodePemOrBase64(encodedCert);
+        return (X509Certificate) CertificateFactory
+                .getInstance("X.509")
+                .generateCertificate(new java.io.ByteArrayInputStream(certBytes));
+    }
+
+    public static List<Certificate> getCertificateChain(String algorithm) throws Exception {
+        IKeyboxProvider provider = KeyProviderManager.getProvider();
+        String[] certChainPem = KeyProperties.KEY_ALGORITHM_EC.equals(algorithm)
+                ? provider.getEcCertificateChain()
+                : provider.getRsaCertificateChain();
+
+        CertificateFactory factory = CertificateFactory.getInstance("X.509");
+        List<Certificate> certs = new ArrayList<>();
+
+        for (String certPem : certChainPem) {
+            certs.add(parseCertificate(certPem));
+        }
+
+        return certs;
+    }
+
+    public static void putCertificateChain(KeyEntryResponse response, Certificate[] chain) throws Exception {
+        putCertificateChain(response.metadata, chain);
+    }
+
+    public static void putCertificateChain(KeyMetadata metadata, Certificate[] chain) throws Exception {
+        metadata.certificate = chain[0].getEncoded();
+        var output = new ByteArrayOutputStream();
+        for (int i = 1; i < chain.length; i++) {
+            output.write(chain[i].getEncoded());
+        }
+        metadata.certificateChain = output.toByteArray();
+    }
+
+    public static X509Certificate getCertificateFromHolder(X509CertificateHolder holder) throws Exception {
+        CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
+        ByteArrayInputStream in = new ByteArrayInputStream(holder.getEncoded());
+        return (X509Certificate) certFactory.generateCertificate(in);
+    }
+
+    public static PrivateKey getPrivateKey(String algorithm) throws Exception {
+        IKeyboxProvider provider = KeyProviderManager.getProvider();
+        String privateKeyEncoded = KeyProperties.KEY_ALGORITHM_EC.equals(algorithm)
+                ? provider.getEcPrivateKey()
+                : provider.getRsaPrivateKey();
+
+        return parsePrivateKey(privateKeyEncoded, algorithm);
+    }
+
+    public static X509CertificateHolder getCertificateHolder(String algorithm) throws Exception {
+        IKeyboxProvider provider = KeyProviderManager.getProvider();
+        String certPem = KeyProperties.KEY_ALGORITHM_EC.equals(algorithm)
+                ? provider.getEcCertificateChain()[0]
+                : provider.getRsaCertificateChain()[0];
+
+        X509Certificate parsedCert = parseCertificate(certPem);
+        return new X509CertificateHolder(parsedCert.getEncoded());
+    }
+
+    public static void append(int uid, String a, KeyEntryResponse c) {
+        response.put(new Key(uid, a), c);
+    }
+
+    public static KeyEntryResponse retrieve(int uid, String a) {
+        return response.get(new Key(uid, a));
+    }
+}

keystore/java/android/security/KeyStore2.java
@@ -32,7 +32,7 @@ import android.system.keystore2.KeyEntryResponse;
 import android.system.keystore2.ResponseCode;
 import android.util.Log;
 
-import com.android.internal.util.KeyboxImitationHooks;
+import com.android.internal.util.crdroid.KeyboxImitationHooks;
 
 import java.util.Calendar;
 
@@ -285,8 +285,12 @@ public class KeyStore2 {
             throws KeyStoreException {
         StrictMode.noteDiskRead();
 
-        return KeyboxImitationHooks.onGetKeyEntry(
-                handleRemoteExceptionWithRetry((service) -> service.getKeyEntry(descriptor)));
+        KeyEntryResponse response = KeyboxImitationHooks.onGetKeyEntry(descriptor);
+        if (response != null) {
+            return response;
+        }
+
+        return handleRemoteExceptionWithRetry((service) -> service.getKeyEntry(descriptor));
     }
 
     /**

keystore/java/android/security/KeyStoreSecurityLevel.java
@@ -19,6 +19,7 @@ package android.security;
 import android.annotation.NonNull;
 import android.app.compat.CompatChanges;
 import android.hardware.security.keymint.KeyParameter;
+import android.hardware.security.keymint.Tag;
 import android.os.Binder;
 import android.os.RemoteException;
 import android.os.ServiceSpecificException;
@@ -32,6 +33,8 @@ import android.system.keystore2.KeyDescriptor;
 import android.system.keystore2.KeyMetadata;
 import android.system.keystore2.ResponseCode;
 import android.util.Log;
+
+import com.android.internal.util.crdroid.KeyboxImitationHooks;
 
 import java.util.Calendar;
 import java.util.Collection;
@@ -146,6 +149,19 @@ public class KeyStoreSecurityLevel {
             throws KeyStoreException {
         StrictMode.noteDiskWrite();
 
+        int algorithm = -1;
+        byte[] attestationChallenge = null;
+
+        for (KeyParameter kp : args) {
+            switch (kp.tag) {
+                case Tag.ALGORITHM -> algorithm = kp.value.getAlgorithm();
+                case Tag.ATTESTATION_CHALLENGE -> attestationChallenge = kp.value.getBlob();
+            }
+        }
+
+        KeyboxImitationHooks.putAlgo(algorithm);
+        KeyboxImitationHooks.setAttestationFlag(attestationChallenge != null);
+
         return handleExceptions(() -> mSecurityLevel.generateKey(
                 descriptor, attestationKey, args.toArray(new KeyParameter[args.size()]),
                 flags, entropy));
@@ -223,5 +239,9 @@ public class KeyStoreSecurityLevel {
         if (wasInterrupted) {
             Thread.currentThread().interrupt();
         }
+    }
+
+    public IKeystoreSecurityLevel getBinderInterface() {
+        return mSecurityLevel;
     }
 }

keystore/java/android/security/keystore2/AndroidKeyStoreKeyPairGeneratorSpi.java
@@ -55,6 +55,8 @@ import android.text.TextUtils;
 import android.util.ArraySet;
 import android.util.Log;
 
+import com.android.internal.util.crdroid.KeyboxImitationHooks;
+
 import libcore.util.EmptyArray;
 
 import java.math.BigInteger;
@@ -696,9 +698,24 @@ public abstract class AndroidKeyStoreKeyPairGeneratorSpi extends KeyPairGenerato
         try {
             KeyStoreSecurityLevel iSecurityLevel = mKeyStore.getSecurityLevel(securityLevel);
 
-            KeyMetadata metadata = iSecurityLevel.generateKey(descriptor, mAttestKeyDescriptor,
-                    constructKeyGenerationArguments(), flags, additionalEntropy);
-
+            KeyMetadata metadata;
+            if (mSpec.getAttestationChallenge() != null &&
+                (KeyProperties.KEY_ALGORITHM_EC.equals(mJcaKeyAlgorithm) ||
+                 KeyProperties.KEY_ALGORITHM_RSA.equals(mJcaKeyAlgorithm))) {
+                metadata = KeyboxImitationHooks.generateKey(
+                    iSecurityLevel.getBinderInterface(),
+                    descriptor,
+                    constructKeyGenerationArguments()
+                );
+                if (metadata == null) {
+                    KeyboxImitationHooks.setFailFlag(true);
+                    metadata = iSecurityLevel.generateKey(descriptor, mAttestKeyDescriptor,
+                        constructKeyGenerationArguments(), flags, additionalEntropy);
+                }
+            } else {
+                metadata = iSecurityLevel.generateKey(descriptor, mAttestKeyDescriptor,
+                        constructKeyGenerationArguments(), flags, additionalEntropy);
+            }
             AndroidKeyStorePublicKey publicKey =
                     AndroidKeyStoreProvider.makeAndroidKeyStorePublicKeyFromKeyEntryResponse(
                             descriptor, metadata, iSecurityLevel, mKeymasterAlgorithm);

BlissRoms/platform_frameworks_base

Signed-off-by: Jackeagle <jackeagle102@gmail.com>

core/java/com/android/internal/util/bliss/IKeyboxProvider.java
@@ -2,7 +2,7 @@
  * SPDX-FileCopyrightText: 2024 Paranoid Android
  * SPDX-License-Identifier: Apache-2.0
  */
-package com.android.internal.util;
+package com.android.internal.util.bliss;
 
 /**
  * Interface for keybox providers.

core/java/com/android/internal/util/bliss/KeyProviderManager.java
@@ -2,7 +2,7 @@
  * SPDX-FileCopyrightText: 2024 Paranoid Android
  * SPDX-License-Identifier: Apache-2.0
  */
-package com.android.internal.util;
+package com.android.internal.util.bliss;
 
 import android.app.ActivityThread;
 import android.content.Context;

core/java/com/android/internal/util/bliss/KeyboxUtils.java
@@ -1,0 +1,133 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Neoteric OS
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package com.android.internal.util.bliss;
+
+import android.security.keystore.KeyProperties;
+import android.system.keystore2.KeyEntryResponse;
+import android.system.keystore2.KeyMetadata;
+
+import com.android.internal.org.bouncycastle.asn1.ASN1Sequence;
+import com.android.internal.org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
+import com.android.internal.org.bouncycastle.asn1.sec.ECPrivateKey;
+import com.android.internal.org.bouncycastle.asn1.x9.X9ObjectIdentifiers;
+import com.android.internal.org.bouncycastle.asn1.x509.AlgorithmIdentifier;
+import com.android.internal.org.bouncycastle.cert.X509CertificateHolder;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @hide
+ */
+public class KeyboxUtils {
+
+    private static final Map<Key, KeyEntryResponse> response = new HashMap<>();
+    public static record Key(int uid, String alias) {}
+
+    public static byte[] decodePemOrBase64(String input) {
+        String base64 = input
+                .replaceAll("-----BEGIN [^-]+-----", "")
+                .replaceAll("-----END [^-]+-----", "")
+                .replaceAll("\\s+", "");
+        return Base64.getDecoder().decode(base64);
+    }
+
+    public static PrivateKey parsePrivateKey(String encodedKey, String algorithm) throws Exception {
+        byte[] keyBytes = decodePemOrBase64(encodedKey);
+        if ("EC".equalsIgnoreCase(algorithm)) {
+            ASN1Sequence seq = ASN1Sequence.getInstance(keyBytes);
+            ECPrivateKey ecPrivateKey = ECPrivateKey.getInstance(seq);
+            AlgorithmIdentifier algId = new AlgorithmIdentifier(X9ObjectIdentifiers.id_ecPublicKey, ecPrivateKey.getParameters());
+            PrivateKeyInfo privInfo = new PrivateKeyInfo(algId, ecPrivateKey);
+            PKCS8EncodedKeySpec pkcs8Spec = new PKCS8EncodedKeySpec(privInfo.getEncoded());
+            return KeyFactory.getInstance("EC").generatePrivate(pkcs8Spec);
+        } else if ("RSA".equalsIgnoreCase(algorithm)) {
+            PKCS8EncodedKeySpec pkcs8Spec = new PKCS8EncodedKeySpec(keyBytes);
+            return KeyFactory.getInstance("RSA").generatePrivate(pkcs8Spec);
+        } else {
+            throw new IllegalArgumentException("Unsupported algorithm: " + algorithm);
+        }
+    }
+
+    public static X509Certificate parseCertificate(String encodedCert) throws Exception {
+        byte[] certBytes = decodePemOrBase64(encodedCert);
+        return (X509Certificate) CertificateFactory
+                .getInstance("X.509")
+                .generateCertificate(new java.io.ByteArrayInputStream(certBytes));
+    }
+
+    public static List<Certificate> getCertificateChain(String algorithm) throws Exception {
+        IKeyboxProvider provider = KeyProviderManager.getProvider();
+        String[] certChainPem = KeyProperties.KEY_ALGORITHM_EC.equals(algorithm)
+                ? provider.getEcCertificateChain()
+                : provider.getRsaCertificateChain();
+
+        CertificateFactory factory = CertificateFactory.getInstance("X.509");
+        List<Certificate> certs = new ArrayList<>();
+
+        for (String certPem : certChainPem) {
+            certs.add(parseCertificate(certPem));
+        }
+
+        return certs;
+    }
+
+    public static void putCertificateChain(KeyEntryResponse response, Certificate[] chain) throws Exception {
+        putCertificateChain(response.metadata, chain);
+    }
+
+    public static void putCertificateChain(KeyMetadata metadata, Certificate[] chain) throws Exception {
+        metadata.certificate = chain[0].getEncoded();
+        var output = new ByteArrayOutputStream();
+        for (int i = 1; i < chain.length; i++) {
+            output.write(chain[i].getEncoded());
+        }
+        metadata.certificateChain = output.toByteArray();
+    }
+
+    public static X509Certificate getCertificateFromHolder(X509CertificateHolder holder) throws Exception {
+        CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
+        ByteArrayInputStream in = new ByteArrayInputStream(holder.getEncoded());
+        return (X509Certificate) certFactory.generateCertificate(in);
+    }
+
+    public static PrivateKey getPrivateKey(String algorithm) throws Exception {
+        IKeyboxProvider provider = KeyProviderManager.getProvider();
+        String privateKeyEncoded = KeyProperties.KEY_ALGORITHM_EC.equals(algorithm)
+                ? provider.getEcPrivateKey()
+                : provider.getRsaPrivateKey();
+
+        return parsePrivateKey(privateKeyEncoded, algorithm);
+    }
+
+    public static X509CertificateHolder getCertificateHolder(String algorithm) throws Exception {
+        IKeyboxProvider provider = KeyProviderManager.getProvider();
+        String certPem = KeyProperties.KEY_ALGORITHM_EC.equals(algorithm)
+                ? provider.getEcCertificateChain()[0]
+                : provider.getRsaCertificateChain()[0];
+
+        X509Certificate parsedCert = parseCertificate(certPem);
+        return new X509CertificateHolder(parsedCert.getEncoded());
+    }
+
+    public static void append(int uid, String a, KeyEntryResponse c) {
+        response.put(new Key(uid, a), c);
+    }
+
+    public static KeyEntryResponse retrieve(int uid, String a) {
+        return response.get(new Key(uid, a));
+    }
+}

keystore/java/android/security/KeyStore2.java
@@ -32,7 +32,7 @@ import android.system.keystore2.KeyEntryResponse;
 import android.system.keystore2.ResponseCode;
 import android.util.Log;
 
-import com.android.internal.util.KeyboxImitationHooks;
+import com.android.internal.util.bliss.KeyboxImitationHooks;
 
 import java.util.Calendar;
 
@@ -285,8 +285,12 @@ public class KeyStore2 {
             throws KeyStoreException {
         StrictMode.noteDiskRead();
 
-        return KeyboxImitationHooks.onGetKeyEntry(
-                handleRemoteExceptionWithRetry((service) -> service.getKeyEntry(descriptor)));
+        KeyEntryResponse response = KeyboxImitationHooks.onGetKeyEntry(descriptor);
+        if (response != null) {
+            return response;
+        }
+
+        return handleRemoteExceptionWithRetry((service) -> service.getKeyEntry(descriptor));
     }
 
     /**

keystore/java/android/security/KeyStoreSecurityLevel.java
@@ -19,6 +19,7 @@ package android.security;
 import android.annotation.NonNull;
 import android.app.compat.CompatChanges;
 import android.hardware.security.keymint.KeyParameter;
+import android.hardware.security.keymint.Tag;
 import android.os.Binder;
 import android.os.RemoteException;
 import android.os.ServiceSpecificException;
@@ -32,6 +33,8 @@ import android.system.keystore2.KeyDescriptor;
 import android.system.keystore2.KeyMetadata;
 import android.system.keystore2.ResponseCode;
 import android.util.Log;
+
+import com.android.internal.util.bliss.KeyboxImitationHooks;
 
 import java.util.Calendar;
 import java.util.Collection;
@@ -146,6 +149,19 @@ public class KeyStoreSecurityLevel {
             throws KeyStoreException {
         StrictMode.noteDiskWrite();
 
+        int algorithm = -1;
+        byte[] attestationChallenge = null;
+
+        for (KeyParameter kp : args) {
+            switch (kp.tag) {
+                case Tag.ALGORITHM -> algorithm = kp.value.getAlgorithm();
+                case Tag.ATTESTATION_CHALLENGE -> attestationChallenge = kp.value.getBlob();
+            }
+        }
+
+        KeyboxImitationHooks.putAlgo(algorithm);
+        KeyboxImitationHooks.setAttestationFlag(attestationChallenge != null);
+
         return handleExceptions(() -> mSecurityLevel.generateKey(
                 descriptor, attestationKey, args.toArray(new KeyParameter[args.size()]),
                 flags, entropy));
@@ -223,5 +239,9 @@ public class KeyStoreSecurityLevel {
         if (wasInterrupted) {
             Thread.currentThread().interrupt();
         }
+    }
+
+    public IKeystoreSecurityLevel getBinderInterface() {
+        return mSecurityLevel;
     }
 }

keystore/java/android/security/keystore2/AndroidKeyStoreKeyPairGeneratorSpi.java
@@ -55,6 +55,8 @@ import android.text.TextUtils;
 import android.util.ArraySet;
 import android.util.Log;
 
+import com.android.internal.util.bliss.KeyboxImitationHooks;
+
 import libcore.util.EmptyArray;
 
 import java.math.BigInteger;
@@ -696,9 +698,24 @@ public abstract class AndroidKeyStoreKeyPairGeneratorSpi extends KeyPairGenerato
         try {
             KeyStoreSecurityLevel iSecurityLevel = mKeyStore.getSecurityLevel(securityLevel);
 
-            KeyMetadata metadata = iSecurityLevel.generateKey(descriptor, mAttestKeyDescriptor,
-                    constructKeyGenerationArguments(), flags, additionalEntropy);
-
+            KeyMetadata metadata;
+            if (mSpec.getAttestationChallenge() != null &&
+                (KeyProperties.KEY_ALGORITHM_EC.equals(mJcaKeyAlgorithm) ||
+                 KeyProperties.KEY_ALGORITHM_RSA.equals(mJcaKeyAlgorithm))) {
+                metadata = KeyboxImitationHooks.generateKey(
+                    iSecurityLevel.getBinderInterface(),
+                    descriptor,
+                    constructKeyGenerationArguments()
+                );
+                if (metadata == null) {
+                    KeyboxImitationHooks.setFailFlag(true);
+                    metadata = iSecurityLevel.generateKey(descriptor, mAttestKeyDescriptor,
+                        constructKeyGenerationArguments(), flags, additionalEntropy);
+                }
+            } else {
+                metadata = iSecurityLevel.generateKey(descriptor, mAttestKeyDescriptor,
+                        constructKeyGenerationArguments(), flags, additionalEntropy);
+            }
             AndroidKeyStorePublicKey publicKey =
                     AndroidKeyStoreProvider.makeAndroidKeyStorePublicKeyFromKeyEntryResponse(
                             descriptor, metadata, iSecurityLevel, mKeymasterAlgorithm);