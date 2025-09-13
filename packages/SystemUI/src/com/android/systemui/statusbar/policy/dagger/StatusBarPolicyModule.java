4c55cabf2be94701c6aba63c69d93d49a3be12dd
Abhay Singh Gill <abhaygill017@gmail.com>
2025-09-13 21:32:55 +0530

    SystemUI: Add flashlight strength control [SQUASHED]
    The default behaviour is toggling the flashlight since we use secondaryClick as default onClick behaviour on small tiles.
    
    Squashed:
    
        From: NurKeinNeid <nurkeinneid@derpfest.org>
        Date: Fri, 19 Sep 2025 01:42:17 +0200
        Subject: SystemUI: Fix flashlight strength control detection
    
        Change detection logic from 'mMaxLevel > mDefaultLevel' to 'mMaxLevel > 1'
        to properly detect devices where max and default levels are equal.
    
        Fixes flashlight strength control not working on some devices.
    
        Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: Abhay Singh Gill <abhaygill017@gmail.com>
        Date: Fri, 19 Sep 2025 18:26:10 +0530
        Subject: SystemUI: Extend flashlight strength logic to flashlight controller
    
        Previously the flashlight strength was only applied when toggling it from the QS tile, now it works from other SystemUI components as well.
    
        Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: Abhay Singh Gill <abhaygill017@gmail.com>
        Date: Fri, 19 Sep 2025 22:22:13 +0530
        Subject: SystemUI: Allow long click on flashlight tile
    
        Would be useful for adjusting flashlight strength when the tile is small/ icon only.
    
        Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: ShevT <evgeny.shishkov@gmail.com>
        Date: Tue, 23 Sep 2025 14:10:54 +0300
        Subject: SystemUI: Changing the logic of the flashlight tile
    
         Single tap - on/off
         Long tap - flashlight strength dialog
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Change-Id: I8da1d9c80cd2209a60cde524aa58bd251610244b
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/dagger/StatusBarPolicyModule.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/dagger/StatusBarPolicyModule.java
@@ -29,6 +29,7 @@ import com.android.settingslib.devicestate.DeviceStateAutoRotateSettingManagerPr
 import com.android.settingslib.devicestate.PostureDeviceStateConverter;
 import com.android.settingslib.devicestate.SecureSettings;
 import com.android.settingslib.notification.modes.ZenIconLoader;
+import com.android.systemui.animation.DialogTransitionAnimator;
 import com.android.systemui.CoreStartable;
 import com.android.systemui.dagger.SysUISingleton;
 import com.android.systemui.dagger.qualifiers.Application;
@@ -37,6 +38,7 @@ import com.android.systemui.dagger.qualifiers.Main;
 import com.android.systemui.dagger.qualifiers.UiBackground;
 import com.android.systemui.log.LogBuffer;
 import com.android.systemui.log.LogBufferFactory;
+import com.android.systemui.plugins.ActivityStarter;
 import com.android.systemui.settings.UserTracker;
 import com.android.systemui.statusbar.connectivity.AccessPointController;
 import com.android.systemui.statusbar.connectivity.AccessPointControllerImpl;
@@ -104,6 +106,7 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 
 import javax.inject.Named;
+import javax.inject.Provider;
 
 /** Dagger Module for code in the statusbar.policy package. */
 @Module(includes = {DeviceProvisioningRepositoryModule.class, SupervisionRepositoryModule.class})
