VoltageOS-staging/frameworks_base_new

SystemUI: dual tone shade toggle 1/2
Signed-off-by: Dmitrii <bankersenator@gmail.com>

packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
@@ -60,6 +60,7 @@ import android.os.Handler;
 import android.os.UserHandle;
 import android.os.UserManager;
 import android.provider.Settings;
+import android.provider.Settings.System;
 import android.text.TextUtils;
 import android.util.ArrayMap;
 import android.util.ArraySet;
@@ -389,7 +390,7 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
                 jsonObject.put(OVERLAY_COLOR_SOURCE,
                         (flags == WallpaperManager.FLAG_LOCK) ? COLOR_SOURCE_LOCK
                                 : COLOR_SOURCE_HOME);
-                jsonObject.put(TIMESTAMP_FIELD, System.currentTimeMillis());
+                jsonObject.put(TIMESTAMP_FIELD, java.lang.System.currentTimeMillis());
                 if (DEBUG) {
                     Log.d(TAG, "Updating theme setting from "
                             + overlayPackageJson + " to " + jsonObject.toString());
@@ -552,6 +553,27 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
                         reevaluateSystemTheme(true /* forceReload */);
                     }
                 });
+
+        mSecureSettings.registerContentObserverForUserSync(
+                System.getUriFor("dual_tone_shade_enabled"),
+                false,
+                new ContentObserver(mBgHandler) {
+                    @Override
+                    public void onChange(boolean selfChange, Collection<Uri> collection, int flags,
+                            int userId) {
+                        if (DEBUG) Log.d(TAG, "dual_tone_shade_enabled changed for user: " + userId);
+                        if (mUserTracker.getUserId() != userId) {
+                            return;
+                        }
+                        if (!mDeviceProvisionedController.isUserSetup(userId)) {
+                            Log.i(TAG, "Theme application deferred when setting changed.");
+                            mDeferredThemeEvaluation = true;
+                            return;
+                        }
+                        reevaluateSystemTheme(true /* forceReload */);
+                    }
+                },
+                UserHandle.USER_ALL);
 
         mUserTracker.addCallback(mUserTrackerCallback, mMainExecutor);
         mConfigurationController.addCallback(mConfigurationListener);

BlissRoms/platform_frameworks_base

Change-Id: I060438db0bd98ad4ea8155a715b4face002fc0d3
Signed-off-by: Jackeagle <jackeagle102@gmail.com>

packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
@@ -60,6 +60,7 @@ import android.os.Handler;
 import android.os.UserHandle;
 import android.os.UserManager;
 import android.provider.Settings;
+import android.provider.Settings.System;
 import android.text.TextUtils;
 import android.util.ArrayMap;
 import android.util.ArraySet;
@@ -372,7 +373,7 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
                 jsonObject.put(OVERLAY_COLOR_SOURCE,
                         (flags == WallpaperManager.FLAG_LOCK) ? COLOR_SOURCE_LOCK
                                 : COLOR_SOURCE_HOME);
-                jsonObject.put(TIMESTAMP_FIELD, System.currentTimeMillis());
+                jsonObject.put(TIMESTAMP_FIELD, java.lang.System.currentTimeMillis());
                 if (DEBUG) {
                     Log.d(TAG, "Updating theme setting from "
                             + overlayPackageJson + " to " + jsonObject.toString());
@@ -583,6 +584,27 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
                         reevaluateSystemTheme(true /* forceReload */);
                     }
                 });
+
+        mSecureSettings.registerContentObserverForUserSync(
+                System.getUriFor(System.QS_DUAL_TONE),
+                false,
+                new ContentObserver(mBgHandler) {
+                    @Override
+                    public void onChange(boolean selfChange, Collection<Uri> collection, int flags,
+                            int userId) {
+                        if (DEBUG) Log.d(TAG, "qs_dual_tone changed for user: " + userId);
+                        if (mUserTracker.getUserId() != userId) {
+                            return;
+                        }
+                        if (!mDeviceProvisionedController.isUserSetup(userId)) {
+                            Log.i(TAG, "Theme application deferred when setting changed.");
+                            mDeferredThemeEvaluation = true;
+                            return;
+                        }
+                        reevaluateSystemTheme(true /* forceReload */);
+                    }
+                },
+                UserHandle.USER_ALL);
 
         mUserTracker.addCallback(mUserTrackerCallback, mMainExecutor);
         mDeviceProvisionedController.addCallback(mDeviceProvisionedListener);