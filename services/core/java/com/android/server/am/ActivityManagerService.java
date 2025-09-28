8e7d0748b4f28af5b7f874d87ba7425600dacba0
Pranav Vashi <neobuddy89@gmail.com>
2025-09-29 02:19:17 +0530

    ActivityManagerService: Perform system compaction with GC
    * Do not run this on screen-on path - as it is bit CPU intensive.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -19552,8 +19552,6 @@ public class ActivityManagerService extends IActivityManager.Stub
 
         synchronized (this) {
             synchronized (mProcLock) {
-                mOomAdjuster.mCachedAppOptimizer.compactAllSystem();
-
                 mProcessList.forEachLruProcessesLOSP(false, proc -> {
                     if (proc == null || proc.getThread() == null) return;
 
@@ -19587,6 +19585,15 @@ public class ActivityManagerService extends IActivityManager.Stub
             });
             killed++;
         }
+    }
+
+    @Override
+    public void compactAllSystem() {
+        mHandler.post(() -> {
+            synchronized (mProcLock) {
+                mOomAdjuster.mCachedAppOptimizer.compactAllSystem();
+            }
+        });
     }
 
     public class ProcessComparator implements Comparator<ProcessToKill> {
