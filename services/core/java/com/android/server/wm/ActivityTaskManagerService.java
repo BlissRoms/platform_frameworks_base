4b3cd94ef1aeb799418d18f19e61fc45ddff0c51
Achim Thesmann <achim@google.com>
2026-01-09 03:48:10 +0000

    RESTRICT AUTOMERGE Require to be bound with non-pinned visible window
    The original check only required any non-toast window, which means a PiP (pinned) window was sufficent to allow BAL.
    To be consistent with regular BAL behavior the BAL check now requires a non-pinned visible window, while FGS starts are still allowed with PiP windows.
    
    Flag: EXEMPT CVE_FIX
    Test: atest android.server.wm.BackgroundActivityLaunchTest
    Test: https://android-build.corp.google.com/builds/abtd/run/L20700030060843304
    Bug: 464526458
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:146d9a1f2dd90e9f259f92955a660f705c5d5f87
    Merged-In: I334c6774729745efbe04a4cb943cfb323bfd53ad
    Change-Id: I334c6774729745efbe04a4cb943cfb323bfd53ad
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- services/core/java/com/android/server/wm/ActivityTaskManagerService.java
+++ services/core/java/com/android/server/wm/ActivityTaskManagerService.java
@@ -6226,6 +6226,15 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
         return mActiveUids.hasNonAppVisibleWindow(uid);
     }
 
+    /** Similar to {@link #hasActiveVisibleWindow(int)}, but only considers non pinned app
+     * windows */
+    boolean hasActiveVisibleNotPinnedWindow(int uid) {
+        if (mVisibleActivityProcessTracker.hasVisibleNotPinnedActivity(uid)) {
+            return true;
+        }
+        return mActiveUids.hasNonAppVisibleWindow(uid);
+    }
+
     boolean isDeviceOwner(int uid) {
         return uid >= 0 && mDeviceOwnerUid == uid;
     }
