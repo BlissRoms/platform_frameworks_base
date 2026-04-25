9788446a883b24ab718ea4de92ce20582f29fe84
bijoyv9 <bijoyv9@gmail.com>
2026-04-25 11:38:29 +0000

    SystemUI: Prevent UDFPS screen flash during sleep transition
    On devices with optical UDFPS, starting fingerprint authentication while the screen
    is entering sleep can briefly trigger HBM/illumination and flash the panel.
    
    Gate UDFPS listening as soon as the sleep transition is dispatched, keep it blocked
    until the screen is fully off, and clear the gate on wakeup so fingerprint remains responsive.
    
    Change-Id: I9242668e2bfadf0e4bd7f2bc1ee68b749237293e
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -2131,18 +2131,16 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, CoreSt
                 cb.onStartedGoingToSleep(arg1);
             }
         }
-        mGoingToSleep = true;
         // Resetting assistant visibility state as the device is going to sleep now.
         // TaskStackChangeListener gets triggered a little late when we transition to AoD,
         // which results in face auth running once on AoD.
         mAssistantVisible = false;
-        mLogger.d("Started going to sleep, mGoingToSleep=true, mAssistantVisible=false");
+        mLogger.d("Started going to sleep, mAssistantVisible=false");
         updateFingerprintListeningState(BIOMETRIC_ACTION_UPDATE);
     }
 
     protected void handleFinishedGoingToSleep(int arg1) {
         Assert.isMainThread();
-        mGoingToSleep = false;
         for (int i = 0; i < mCallbacks.size(); i++) {
             KeyguardUpdateMonitorCallback cb = mCallbacks.get(i).get();
             if (cb != null) {
@@ -2155,6 +2153,8 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, CoreSt
     private void handleScreenTurnedOff() {
         Assert.isMainThread();
         mHardwareFingerprintUnavailableRetryCount = 0;
+        mGoingToSleep = false;
+        updateFingerprintListeningState(BIOMETRIC_ACTION_UPDATE);
     }
 
     private void handleDreamingStateChanged(int dreamStart) {
@@ -3153,7 +3153,8 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, CoreSt
 
         boolean shouldListen = shouldListenKeyguardState && shouldListenUserState
                 && shouldListenBouncerState && shouldListenUdfpsState && !mBiometricPromptShowing
-                && shouldListenSecureLockDeviceState && shouldListenFpsState;
+                && shouldListenSecureLockDeviceState && shouldListenFpsState
+                && (!mGoingToSleep || !isUdfps);
         logListenerModelData(
                 new KeyguardFingerprintListenModel(
                     System.currentTimeMillis(),
@@ -4143,11 +4144,15 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, CoreSt
     public void dispatchStartedWakingUp(@PowerManager.WakeReason int pmWakeReason) {
         synchronized (this) {
             mDeviceInteractive = true;
+            mGoingToSleep = false;
         }
         mHandler.sendMessage(mHandler.obtainMessage(MSG_STARTED_WAKING_UP, pmWakeReason, 0));
     }
 
     public void dispatchStartedGoingToSleep(int why) {
+        synchronized (this) {
+            mGoingToSleep = true;
+        }
         mHandler.sendMessage(mHandler.obtainMessage(MSG_STARTED_GOING_TO_SLEEP, why, 0));
     }
 
