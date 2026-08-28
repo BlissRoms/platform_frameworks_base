28cb66e58c6aac0950f7ff787d38cde9d8782d75
Ashwin Devi Srinivasan <ashwin@ashds.dev>
2026-08-28 13:14:25 +0000

    SystemUI: Implement reverse-engineered mobile data confirmation dialog
    Reverse-engineer the mobile data enable confirmation dialog from
    SystemUIGoogle (blazer beta, cp41.260731.005.b1)
    
    Add the dialog icon and "Don't ask again" option, persisting the user's
    choice per user so subsequent attempts to enable mobile data can bypass
    the confirmation dialog.
    
    Change-Id: Ia4b38a3bbbd848f57b07e02fc037d83e91890076
    Signed-off-by: Ashwin Devi Srinivasan <ashwin@ashds.dev>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/impl/cell/domain/interactor/MobileDataTileUserActionInteractor.kt
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/impl/cell/domain/interactor/MobileDataTileUserActionInteractor.kt
@@ -19,9 +19,13 @@ package com.android.systemui.qs.tiles.impl.cell.domain.interactor
 import android.content.Context
 import android.content.Intent
 import android.provider.Settings
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.CheckBox
 import com.android.systemui.animation.DialogTransitionAnimator
 import com.android.systemui.animation.Expandable
 import com.android.systemui.animation.TransitionAnimator
+import com.android.systemui.dagger.qualifiers.Background
 import com.android.systemui.dagger.qualifiers.Main
 import com.android.systemui.qs.tiles.base.domain.actions.QSTileIntentUserInputHandler
 import com.android.systemui.qs.tiles.base.domain.interactor.QSTileUserActionInteractor
@@ -30,6 +34,7 @@ import com.android.systemui.qs.tiles.base.shared.model.QSTileUserAction
 import com.android.systemui.qs.tiles.dialog.InternetDialogManager
 import com.android.systemui.qs.tiles.impl.cell.domain.model.MobileDataTileModel
 import com.android.systemui.res.R
+import com.android.systemui.settings.UserFileManager
 import com.android.systemui.shade.ShadeDisplayAware
 import com.android.systemui.statusbar.connectivity.AccessPointController
 import com.android.systemui.statusbar.phone.SystemUIDialog
@@ -38,6 +43,7 @@ import com.android.systemui.statusbar.pipeline.mobile.data.repository.MobileConn
 import javax.inject.Inject
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
+import kotlin.coroutines.CoroutineContext
 
 class MobileDataTileUserActionInteractor
 @Inject
@@ -47,11 +53,16 @@ constructor(
     private val qsTileIntentUserActionHandler: QSTileIntentUserInputHandler,
     private val systemUIDialogFactory: SystemUIDialog.Factory,
     @Main val mainDispatcher: CoroutineDispatcher,
+    @Background private val backgroundContext: CoroutineContext,
     private val dialogTransitionAnimator: DialogTransitionAnimator,
     private val internetDialogManager: InternetDialogManager,
     private val accessPointController: AccessPointController,
+    userFileManager: UserFileManager,
 ) : QSTileUserActionInteractor<MobileDataTileModel> {
     val longClickIntent = Intent(Settings.ACTION_MANAGE_ALL_SIM_PROFILES_SETTINGS)
+
+    private val sharedPreferences =
+        userFileManager.getSharedPreferences(PREFS_FILE, Context.MODE_PRIVATE, context.userId)
 
     override suspend fun handleInput(input: QSTileInput<MobileDataTileModel>) {
         when (input.action) {
@@ -82,23 +93,42 @@ constructor(
         val activeRepo = getDataRepo() ?: return
         // If mobile data is disabled, show a confirmation dialog to turn it on.
         if (!activeRepo.dataEnabled.value) {
-            withContext(mainDispatcher) { showEnableConfirmationDialog(expandable) }
+            if (withContext(backgroundContext) { isDontAskAgainEnabled() }) {
+                // The user asked not to be prompted again; just turn data on.
+                activeRepo.setDataEnabled(true)
+            } else {
+                withContext(mainDispatcher) { showEnableConfirmationDialog(expandable) }
+            }
         } else {
             // Otherwise, just turn it off without a dialog.
             activeRepo.setDataEnabled(false)
         }
     }
 
+    private fun isDontAskAgainEnabled(): Boolean {
+        return sharedPreferences.getBoolean(PREF_DONT_ASK_AGAIN, false)
+    }
+
     private fun showEnableConfirmationDialog(expandable: Expandable?) {
         val dialog: SystemUIDialog = systemUIDialogFactory.create()
         dialog.setTitle(context.getString(R.string.mobile_data_enable_title))
         dialog.setMessage(context.getString(R.string.mobile_data_enable_message))
+        dialog.setIcon(R.drawable.ic_swap_vert)
+
+        val view =
+            LayoutInflater.from(context)
+                .inflate(R.layout.mobile_data_tile_confirm_dialog, null as ViewGroup?)
+        val checkBox = view.requireViewById<CheckBox>(R.id.mobile_data_dialog_checkbox)
+        dialog.setView(view)
 
         dialog.setPositiveButton(R.string.mobile_data_enable_turn_on) { _, _ ->
+            if (checkBox.isChecked) {
+                sharedPreferences.edit().putBoolean(PREF_DONT_ASK_AGAIN, true).apply()
+            }
             getDataRepo()?.setDataEnabled(true)
         }
 
-        dialog.setNegativeButton(android.R.string.cancel) { _, _ -> /* Do nothing */ }
+        dialog.setNegativeButton(android.R.string.cancel, null, true)
 
         val controller = expandable?.dialogTransitionController()
         if (controller != null) {
@@ -122,5 +152,10 @@ constructor(
         return mobileConnectionsRepository.defaultDataSubId.value?.let {
             mobileConnectionsRepository.getRepoForSubId(it)
         }
+    }
+
+    companion object {
+        private const val PREFS_FILE = "mobile_data_tile"
+        private const val PREF_DONT_ASK_AGAIN = "mobile_data_dont_ask_again"
     }
 }
